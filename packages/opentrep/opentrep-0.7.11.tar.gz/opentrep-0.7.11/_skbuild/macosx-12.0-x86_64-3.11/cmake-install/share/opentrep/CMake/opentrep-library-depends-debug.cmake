844035a3e87b82530965b248c65ed53d96d43941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:26 +0000

    Add package opentrep/0.7.11/opentrep-0.7.11.tar.gz

---
+++ packages/opentrep/opentrep-0.7.11.tar.gz/opentrep-0.7.11/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/share/opentrep/CMake/opentrep-library-depends-debug.cmake
@@ -0,0 +1,46 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opentreplib" for configuration "Debug"
+set_property(TARGET opentreplib APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opentreplib PROPERTIES
+  IMPORTED_LOCATION_DEBUG "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/lib/libopentrep.0.07.12.dylib"
+  IMPORTED_SONAME_DEBUG "@rpath/libopentrep.0.07.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets opentreplib )
+list(APPEND _cmake_import_check_files_for_opentreplib "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/lib/libopentrep.0.07.12.dylib" )
+
+# Import target "opentrep-indexerbin" for configuration "Debug"
+set_property(TARGET opentrep-indexerbin APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opentrep-indexerbin PROPERTIES
+  IMPORTED_LOCATION_DEBUG "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/bin/opentrep-indexer"
+  )
+
+list(APPEND _cmake_import_check_targets opentrep-indexerbin )
+list(APPEND _cmake_import_check_files_for_opentrep-indexerbin "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/bin/opentrep-indexer" )
+
+# Import target "opentrep-searcherbin" for configuration "Debug"
+set_property(TARGET opentrep-searcherbin APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opentrep-searcherbin PROPERTIES
+  IMPORTED_LOCATION_DEBUG "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/bin/opentrep-searcher"
+  )
+
+list(APPEND _cmake_import_check_targets opentrep-searcherbin )
+list(APPEND _cmake_import_check_files_for_opentrep-searcherbin "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/bin/opentrep-searcher" )
+
+# Import target "opentrep-dbmgrbin" for configuration "Debug"
+set_property(TARGET opentrep-dbmgrbin APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opentrep-dbmgrbin PROPERTIES
+  IMPORTED_LOCATION_DEBUG "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/bin/opentrep-dbmgr"
+  )
+
+list(APPEND _cmake_import_check_targets opentrep-dbmgrbin )
+list(APPEND _cmake_import_check_files_for_opentrep-dbmgrbin "/Users/DENIS/dev/geo/opentrep/_skbuild/macosx-12.0-x86_64-3.11/cmake-install/bin/opentrep-dbmgr" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
