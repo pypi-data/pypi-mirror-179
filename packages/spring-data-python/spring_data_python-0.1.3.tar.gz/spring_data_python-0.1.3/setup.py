466cc3bb6fb8fe1b709413592c7d3a507ebdb351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:13 +0000

    Add package spring-data-python/0.1.3/spring_data_python-0.1.3.tar.gz

---
+++ packages/spring-data-python/spring_data_python-0.1.3.tar.gz/spring_data_python-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['springdata', 'springdata.async', 'springdata.domain']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'spring-data-python',
+    'version': '0.1.3',
+    'description': 'Spring Data Python is an offshoot of the Java-based Spring Data Framework, targeted for Python.',
+    'long_description': '# spring-data-python\nSpring Data Python is an offshoot of the Java-based Spring Data Framework, targeted for Python. \n',
+    'author': 'Vincent TERESE',
+    'author_email': 'vincent.terese@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kobibleu/spring-data-python',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
