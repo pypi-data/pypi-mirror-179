230b54c973688deecb01bc3c155673ff7f6bc548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:12 +0000

    Add package edc-sites/0.3.17/edc-sites-0.3.17.tar.gz

---
+++ packages/edc-sites/edc-sites-0.3.17.tar.gz/edc-sites-0.3.17/edc_sites/get_language_choices_for_site.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import json
+
+from django.apps import apps as django_apps
+from edc_constants.constants import OTHER
+
+
+def get_language_choices_for_site(site, other=None) -> tuple | None:
+    site_profile_model_cls = django_apps.get_model("edc_sites.siteprofile")
+    obj = site_profile_model_cls.objects.get(site=site)
+    if obj.languages:
+        languages = json.loads(obj.languages)
+        if other:
+            languages.update({OTHER: "Other"})
+        return tuple((k, v) for k, v in languages.items())
+    return None
---
+++ packages/edc-sites/edc-sites-0.3.17.tar.gz/edc-sites-0.3.17/edc_sites/tests/sites.py
@@ -0,0 +1,75 @@
+from edc_sites.single_site import SingleSite
+
+fqdn = "clinicedc.org"
+
+sites: list[SingleSite] = [
+    SingleSite(
+        10,
+        "mochudi",
+        title="Mochudi",
+        country="botswana",
+        country_code="bw",
+        languages={"tn": "Tswana"},
+        domain=f"mochudi.bw.{fqdn}",
+    ),
+    SingleSite(
+        20,
+        "molepolole",
+        title="Molepolole",
+        country="botswana",
+        country_code="bw",
+        languages={"tn": "Tswana"},
+        fqdn=fqdn,
+    ),
+    SingleSite(
+        30,
+        "lobatse",
+        title="Lobatse",
+        country="botswana",
+        country_code="bw",
+        languages={"tn": "Tswana"},
+        fqdn=fqdn,
+    ),
+    SingleSite(
+        40,
+        "gaborone",
+        title="Gaborone",
+        country="botswana",
+        country_code="bw",
+        languages={"tn": "Tswana"},
+        fqdn=fqdn,
+    ),
+    SingleSite(
+        50,
+        "karakobis",
+        title="Karakobis",
+        country="botswana",
+        country_code="bw",
+        languages={"tn": "Tswana"},
+        fqdn=fqdn,
+    ),
+    SingleSite(
+        60,
+        "windhoek",
+        title="Windhoek",
+        country="namibia",
+        country_code="na",
+        languages={"tn": "Tswana"},
+        fqdn=fqdn,
+    ),
+]
+
+
+more_sites = [
+    SingleSite(
+        60,
+        "windhoek",
+        title="Windhoek",
+        country="namibia",
+        country_code="na",
+        fqdn=fqdn,
+    ),
+]
+
+all_sites = {"botswana": sites, "namibia": more_sites}
+all_test_sites = {"botswana": sites, "namibia": more_sites}
