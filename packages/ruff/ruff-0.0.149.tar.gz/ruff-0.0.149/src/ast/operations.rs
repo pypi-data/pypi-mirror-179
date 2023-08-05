b287bd56b632f94f2415194b65cad737b2d80de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:42 +0000

    Add package ruff/0.0.149/ruff-0.0.149.tar.gz

---
+++ packages/ruff/ruff-0.0.149.tar.gz/ruff-0.0.149/src/ast/operations.rs
@@ -0,0 +1,112 @@
+use rustpython_parser::ast::{Constant, Expr, ExprKind, Stmt, StmtKind};
+
+use crate::ast::types::{BindingKind, Scope};
+
+/// Extract the names bound to a given __all__ assignment.
+pub fn extract_all_names(stmt: &Stmt, scope: &Scope) -> Vec<String> {
+    fn add_to_names(names: &mut Vec<String>, elts: &[Expr]) {
+        for elt in elts {
+            if let ExprKind::Constant {
+                value: Constant::Str(value),
+                ..
+            } = &elt.node
+            {
+                names.push(value.to_string());
+            }
+        }
+    }
+
+    let mut names: Vec<String> = vec![];
+
+    // Grab the existing bound __all__ values.
+    if let StmtKind::AugAssign { .. } = &stmt.node {
+        if let Some(binding) = scope.values.get("__all__") {
+            if let BindingKind::Export(existing) = &binding.kind {
+                names.extend_from_slice(existing);
+            }
+        }
+    }
+
+    if let Some(value) = match &stmt.node {
+        StmtKind::Assign { value, .. } => Some(value),
+        StmtKind::AnnAssign { value, .. } => value.as_ref(),
+        StmtKind::AugAssign { value, .. } => Some(value),
+        _ => None,
+    } {
+        match &value.node {
+            ExprKind::List { elts, .. } | ExprKind::Tuple { elts, .. } => {
+                add_to_names(&mut names, elts);
+            }
+            ExprKind::BinOp { left, right, .. } => {
+                let mut current_left = left;
+                let mut current_right = right;
+                while let Some(elts) = match &current_right.node {
+                    ExprKind::List { elts, .. } => Some(elts),
+                    ExprKind::Tuple { elts, .. } => Some(elts),
+                    _ => None,
+                } {
+                    add_to_names(&mut names, elts);
+                    match &current_left.node {
+                        ExprKind::BinOp { left, right, .. } => {
+                            current_left = left;
+                            current_right = right;
+                        }
+                        ExprKind::List { elts, .. } | ExprKind::Tuple { elts, .. } => {
+                            add_to_names(&mut names, elts);
+                            break;
+                        }
+                        _ => break,
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    names
+}
+
+/// Check if a node is parent of a conditional branch.
+pub fn on_conditional_branch<'a>(parents: &mut impl Iterator<Item = &'a Stmt>) -> bool {
+    parents.any(|parent| {
+        if matches!(parent.node, StmtKind::If { .. } | StmtKind::While { .. }) {
+            return true;
+        }
+        if let StmtKind::Expr { value } = &parent.node {
+            if matches!(value.node, ExprKind::IfExp { .. }) {
+                return true;
+            }
+        }
+        false
+    })
+}
+
+/// Check if a node is in a nested block.
+pub fn in_nested_block<'a>(parents: &mut impl Iterator<Item = &'a Stmt>) -> bool {
+    parents.any(|parent| {
+        matches!(
+            parent.node,
+            StmtKind::Try { .. } | StmtKind::If { .. } | StmtKind::With { .. }
+        )
+    })
+}
+
+/// Check if a node represents an unpacking assignment.
+pub fn is_unpacking_assignment(stmt: &Stmt) -> bool {
+    if let StmtKind::Assign { targets, value, .. } = &stmt.node {
+        if !targets.iter().any(|child| {
+            matches!(
+                child.node,
+                ExprKind::Set { .. } | ExprKind::List { .. } | ExprKind::Tuple { .. }
+            )
+        }) {
+            return false;
+        }
+        match &value.node {
+            ExprKind::Set { .. } | ExprKind::List { .. } | ExprKind::Tuple { .. } => return false,
+            _ => {}
+        }
+        return true;
+    }
+    false
+}
---
+++ packages/ruff/ruff-0.0.149.tar.gz/ruff-0.0.149/src/pyflakes/mod.rs
@@ -0,0 +1,5 @@
+pub mod cformat;
+pub mod checks;
+pub mod fixes;
+pub mod format;
+pub mod plugins;
