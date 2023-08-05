dd79b29d190d64815bcbd9074beaff77871c99f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:26 +0000

    Add package lazy-model/0.0.1/lazy_model-0.0.1.tar.gz

---
+++ packages/lazy-model/lazy_model-0.0.1.tar.gz/lazy_model-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lazy_model']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.9.0']
+
+setup_kwargs = {
+    'name': 'lazy-model',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': 'Lazy parsing for Pydantic models',
+    'author': 'Roman',
+    'author_email': 'roman-right@protonmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
