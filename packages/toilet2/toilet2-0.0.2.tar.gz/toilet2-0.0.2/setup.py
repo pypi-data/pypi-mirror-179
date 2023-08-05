cd0ff431bc6eedea048017a4e11bd65301d2a856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:21 +0000

    Add package toilet2/0.0.2/toilet2-0.0.2.tar.gz

---
+++ packages/toilet2/toilet2-0.0.2.tar.gz/toilet2-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="toilet2",  # 배포되는 package 이름
+    version="0.0.2",
+    author="hwangyoungjae",
+    author_email="dudwo56@gmail.com",
+    # description="description",
+    url="https://github.com/hwangyoungjae",
+    project_urls={
+        "Bug Tracker": "https://youngjae.com/profjt_url",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
