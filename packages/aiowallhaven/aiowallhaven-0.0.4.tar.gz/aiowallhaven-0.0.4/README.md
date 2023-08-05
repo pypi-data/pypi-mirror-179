87db63fa88798f0879bd946df856b267dce125c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:35 +0000

    Add package aiowallhaven/0.0.4/aiowallhaven-0.0.4.tar.gz

---
+++ packages/aiowallhaven/aiowallhaven-0.0.4.tar.gz/aiowallhaven-0.0.4/README.md
@@ -0,0 +1,42 @@
+# aiowallhaven
+
+aiowallhaven is an asynchronous API wrapper for popular
+wallpaper hosting site wallhaven.cc.
+
+## Basic Usage:
+
+```
+import asyncio
+from aiowallhaven import WallHavenAPI
+
+wallhaven = WallHavenAPI("Your-API-key")
+
+async def wallpaper_details():
+    request = await wallhaven.get_wallpaper("5758y8")
+    print(request)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(wallpaper_details())
+```
+
+## Prerequisites
+The following dependencies are required:
+
+- Python 3.10
+- aiohttp library
+- aiolimiter library
+
+## Installation
+
+```
+$ pip install aiowallhaven
+```
+
+## Documentation
+
+The documentation is available at [readthedocs.io](https://aiowallhaven.readthedocs.io/en/latest/).
+
+## License
+
+aiowallhaven is developed and distributed under the MIT
+license.
---
+++ packages/aiowallhaven/aiowallhaven-0.0.4.tar.gz/aiowallhaven-0.0.4/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import setuptools
+import pathlib
+import sys
+
+if sys.version_info < (3, 10):
+    raise RuntimeError('aiowallhaven requires Python 3.10 or greater')
+
+_ROOT = pathlib.Path(__file__).parent
+
+with open(str(_ROOT / 'README.md')) as f:
+    readme = f.read()
+
+setuptools.setup(
+        name="aiowallhaven",
+        version="0.0.4",
+        author="Dmitrii Efimov",
+        author_email="efimov.1992@outlook.com",
+        description="Async wrapper for Wallhaven's API",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        url="https://github.com/itsuchur/aiowallhaven",
+        download_url = 'https://github.com/itsuchur/aiowallhaven/archive/refs/tags/0.0.3.tar.gz',
+        packages=setuptools.find_packages(),
+        classifiers=[
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.10',
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        python_requires='>=3.10.0',
+        license='MIT',
+        )
