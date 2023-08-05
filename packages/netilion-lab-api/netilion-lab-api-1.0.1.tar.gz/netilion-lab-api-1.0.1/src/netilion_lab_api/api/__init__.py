43dd120a2fbf9096bd2e7426958eb1b44508eba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:45 +0000

    Add package netilion-lab-api/1.0.1/netilion-lab-api-1.0.1.tar.gz

---
+++ packages/netilion-lab-api/netilion-lab-api-1.0.1.tar.gz/netilion-lab-api-1.0.1/src/netilion_lab_api/api/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.add_on_api import AddOnApi
+from swagger_client.api.app_instance_api import AppInstanceApi
+from swagger_client.api.batch_api import BatchApi
+from swagger_client.api.batch_status_api import BatchStatusApi
+from swagger_client.api.batch_type_api import BatchTypeApi
+from swagger_client.api.connected_asset_api import ConnectedAssetApi
+from swagger_client.api.connected_asset_config_trigger_api import ConnectedAssetConfigTriggerApi
+from swagger_client.api.connected_asset_status_api import ConnectedAssetStatusApi
+from swagger_client.api.document_standard_api import DocumentStandardApi
+from swagger_client.api.mail_api import MailApi
+from swagger_client.api.recipe_api import RecipeApi
+from swagger_client.api.recipe_status_api import RecipeStatusApi
+from swagger_client.api.recipe_type_api import RecipeTypeApi
+from swagger_client.api.sim_card_api import SimCardApi
+from swagger_client.api.system_api import SystemApi
+from swagger_client.api.system_status_api import SystemStatusApi
+from swagger_client.api.system_type_api import SystemTypeApi
+from swagger_client.api.value_objects_api import ValueObjectsApi
