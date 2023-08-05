03f1fe5aa2bc35ebbdba4ec3d1eaa6ec83b93fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:42 +0000

    Add package pyCombiner/0.1.1/pyCombiner-0.1.1.tar.gz

---
+++ packages/pyCombiner/pyCombiner-0.1.1.tar.gz/pyCombiner-0.1.1/setup.py
@@ -0,0 +1,46 @@
+# !/usr/bin/env python
+# coding=utf-8
+# GitHub: GWillS163
+# User: 駿清清 
+# Date: 09/11/2022 
+# Time: 17:53
+
+from setuptools import setup, find_packages
+from pyCombiner import version, description
+setup(
+    name='pyCombiner',
+    version=version,
+    description=(
+        description
+    ),
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    author='JunQingQing',
+    author_email='gwills@163.com',
+    maintainer='JunQingQing',
+    maintainer_email='gwills@163.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://gwills163.github.io/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ],
+
+    # scripts=['bin/main'],
+    entry_points={
+        # 'console_scripts': ['pyCombiner=pyCombiner.command_line:main'],
+        'console_scripts': ['pyCombiner=pyCombiner.__main__:run'],
+    }
+)
