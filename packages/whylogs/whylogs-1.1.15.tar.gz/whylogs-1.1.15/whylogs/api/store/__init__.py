02d400a84f1ddde77cde50adb04d8e8faacfd79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:24 +0000

    Add package whylogs/1.1.15/whylogs-1.1.15.tar.gz

---
+++ packages/whylogs/whylogs-1.1.15.tar.gz/whylogs-1.1.15/whylogs/api/store/__init__.py
@@ -0,0 +1,5 @@
+from .local_store import LocalStore
+from .profile_store import ProfileStore
+from .query import DateQuery, ProfileNameQuery
+
+__ALL__ = [ProfileStore, LocalStore, DateQuery, ProfileNameQuery]
