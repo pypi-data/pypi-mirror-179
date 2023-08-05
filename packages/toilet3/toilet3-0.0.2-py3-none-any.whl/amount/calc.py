811c8f067f2c56115698cb88ccb19ac5e767303c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:26 +0000

    Add package toilet3/0.0.2/toilet3-0.0.2-py3-none-any.whl

---
+++ packages/toilet3/toilet3-0.0.2-py3-none-any.whl/amount/calc.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+
+class Calc:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return self.x + self.y
+
+    def sub(self):
+        return self.x - self.y
