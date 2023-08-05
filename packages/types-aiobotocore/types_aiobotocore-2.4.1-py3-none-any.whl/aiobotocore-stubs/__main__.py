375bbf2de70e425e8214765a7f91b486f5b22954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:58 +0000

    Add package types-aiobotocore/2.4.1/types_aiobotocore-2.4.1-py3-none-any.whl

---
+++ packages/types-aiobotocore/types_aiobotocore-2.4.1-py3-none-any.whl/aiobotocore-stubs/__main__.py
@@ -0,0 +1,37 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for aiobotocore 2.4.1\n"
+        "Version:         2.4.1\n"
+        "Builder version: 7.11.11\n"
+        "Docs:            https://pypi.org/project/types-aiobotocore/\n"
+        "Changelog:       https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("2.4.1")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
