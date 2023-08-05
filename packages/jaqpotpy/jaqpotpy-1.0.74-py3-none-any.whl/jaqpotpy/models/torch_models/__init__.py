4de35248485e62956862e16c856f908865e94105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:06 +0000

    Add package jaqpotpy/1.0.74/jaqpotpy-1.0.74-py3-none-any.whl

---
+++ packages/jaqpotpy/jaqpotpy-1.0.74-py3-none-any.whl/jaqpotpy/models/torch_models/__init__.py
@@ -0,0 +1,4 @@
+from .torch_geometric import GCN_V1, GhebConvV1\
+    , SAGEConv_V1, GraphConv_V1, GatedGraphConv_V1\
+    , ResGatedGraphConv_V1, GATConv_V1, AttentiveFP_V1, AttentiveFPModel_V1, AttentiveFP
+from .torch_vision import CNN
