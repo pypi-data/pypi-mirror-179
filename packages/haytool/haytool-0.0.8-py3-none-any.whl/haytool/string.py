2b140b4fb57fa48ce497f2b89a17aacd935be3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:30 +0000

    Add package haytool/0.0.8/haytool-0.0.8-py3-none-any.whl

---
+++ packages/haytool/haytool-0.0.8-py3-none-any.whl/haytool/string.py
@@ -0,0 +1,14 @@
+import secrets
+import string
+import random
+
+
+def generate_random_string(string_length=None, words=1, freestyle=False):
+    gen_words = []
+    words = random.randint(3, 20) if freestyle else 1
+    for word in range(words):
+        string_length = random.randint(2, 12) if string_length is None else string_length
+        rando_word = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(string_length))
+        gen_words.append(rando_word)
+        string_length = None
+    return ' '.join(gen_words)
