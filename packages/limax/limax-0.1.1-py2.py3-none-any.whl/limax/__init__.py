ec179e805c30210a7892d3b580099312193a4bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:07 +0000

    Add package limax/0.1.1/limax-0.1.1-py2.py3-none-any.whl

---
+++ packages/limax/limax-0.1.1-py2.py3-none-any.whl/limax/__init__.py
@@ -0,0 +1,12 @@
+"""limax - Python utilities for limax."""
+from pathlib import Path
+
+__author__ = "Matthias Koenig"
+__version__ = "0.1.1"
+__citation__ = "https://doi.org/10.5281/zenodo.3708271"
+
+
+program_name: str = "limax"
+RESOURCES_DIR: Path = Path(__file__).parent / "resources"
+EXAMPLE_LIMAX_PATH: Path = RESOURCES_DIR / "limax_example.csv"
+EXAMPLE_LIMAX_PROCESSED_PATH: Path = RESOURCES_DIR / "limax_example_processed.csv"
