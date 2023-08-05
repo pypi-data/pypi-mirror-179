cdd623d974eb69f976537cf0095ac3607af83690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:06 +0000

    Add package yascc/0.3.21/yascc-0.3.21-cp37-cp37m-manylinux_2_5_i686.manylinux1_i686.manylinux_2_24_i686.whl

---
+++ packages/yascc/yascc-0.3.21-cp37-cp37m-manylinux_2_5_i686.manylinux1_i686.manylinux_2_24_i686.whl/yascc/snakecase.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import Any
+
+from _case import _camelize
+from _case import _to_camel_case
+
+
+def to_camel_case(snake_case_string: str) -> str:
+    """Converts some_thing to someThing."""
+    return str(_to_camel_case(snake_case_string))
+
+
+def camelize(d: dict[str, Any] | list[dict[str, Any]]) -> dict[str, Any] | list[dict[str, Any]]:
+    """Converts {'some_thing': {'having_value': 'like_so'}} to {'someThing': {'havingValue': 'like_so'}}."""
+    return _camelize(d)
