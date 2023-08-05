0618350c23492c5142f7bb46de62b1d84567002f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:14 +0000

    Add package pb-testing/0.1.6.5/pb_testing-0.1.6.5-py3-none-any.whl

---
+++ packages/pb-testing/pb_testing-0.1.6.5-py3-none-any.whl/preparation/create_config_file.py
@@ -0,0 +1,24 @@
+from configparser import ConfigParser
+import pathlib
+
+
+def create_config_file():
+    config = ConfigParser()
+
+    config.read('resources_for_testing/config.ini')
+    if 'main' not in config:
+        config.add_section('main')
+    config.set('main', 'domain', 'http://localhost:3000')
+    config.set('main', 'username_field', 'username')
+    config.set('main', 'password_field', 'password')
+    config.set('main', 'username_value', 'jar')
+    config.set('main', 'password_value', 'jarjarjar')
+    config.set('main', 'login_path', '/login')
+    config.set('main', 'last_site', 'https://gram.pl')
+    config.set('main', 'project_path', str(pathlib.Path(__file__).parent.parent.parent.resolve()))
+    config.set('main', 'program_for_opening_html', 'code')
+
+    with open('resources_for_testing/config.ini', 'w') as f:
+        config.write(f)
+
+    print("Config file created")
