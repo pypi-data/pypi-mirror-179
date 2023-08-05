df196090ff45d153598f27fa762fa219139b620b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:28 +0000

    Add package dbt-coves/1.3.0a8/dbt_coves-1.3.0a8-py3-none-any.whl

---
+++ packages/dbt-coves/dbt_coves-1.3.0a8-py3-none-any.whl/dbt_coves/utils/jinja.py
@@ -0,0 +1,30 @@
+from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader
+
+
+def render_template_file(
+    name, context, output_path, templates_folder=".dbt_coves/templates"
+):
+    output = get_render_output(name, context, templates_folder=templates_folder)
+
+    with open(output_path, "w") as rendered:
+        rendered.write(output)
+
+    return output
+
+
+def render_template(template_content, context):
+    template = Environment().from_string(template_content)
+    return template.render(**context)
+
+
+def get_render_output(name, context, templates_folder=".dbt_coves/templates"):
+    env = Environment(
+        loader=ChoiceLoader(
+            [FileSystemLoader(templates_folder), PackageLoader("dbt_coves")]
+        ),
+        keep_trailing_newline=True,
+    )
+    template = env.get_template(name)
+    output = template.render(**context)
+
+    return output
