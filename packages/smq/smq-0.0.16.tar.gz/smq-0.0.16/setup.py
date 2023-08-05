bbe2975946bd6d37bcde94631e9543c5bd998bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:44 +0000

    Add package smq/0.0.16/smq-0.0.16.tar.gz

---
+++ packages/smq/smq-0.0.16.tar.gz/smq-0.0.16/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smq",
+    version="0.0.16",
+    author="ZQX",
+    author_email="262293446@qq.com",
+    description="A package for smq server and client.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
