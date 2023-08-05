80ac8e7467e0fceac9b10eb8302e51c46c8febd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:59 +0000

    Add package scipy-weave/0.18.0/scipy-weave-0.18.0.tar.gz

---
+++ packages/scipy-weave/scipy-weave-0.18.0.tar.gz/scipy-weave-0.18.0/weave/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM WEAVE SETUP.PY
+short_version = '0.18.0'
+version = '0.18.0'
+full_version = '0.18.0'
+git_revision = '5c1487d2e78c738e3f50a483eff7873b5d9b2073'
+release = True
+
+if not release:
+    version = full_version
