f1ca2fcfb49791e0a3351822f1a0d79bba2c446d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:57 +0000

    Add package vchpc-toolkit/10.7.8/vchpc_toolkit-10.7.8.tar.gz

---
+++ packages/vchpc-toolkit/vchpc_toolkit-10.7.8.tar.gz/vchpc_toolkit-10.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="vchpc_toolkit",
+    version="10.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="vchpc_toolkit of vchpc_toolkit of vchpc_toolkit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
