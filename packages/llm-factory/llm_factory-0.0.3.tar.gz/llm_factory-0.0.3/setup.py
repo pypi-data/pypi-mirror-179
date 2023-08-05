0d0d2e0bc2086082b7e3ad3394c13cc2b769422e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:15 +0000

    Add package llm-factory/0.0.3/llm_factory-0.0.3.tar.gz

---
+++ packages/llm-factory/llm_factory-0.0.3.tar.gz/llm_factory-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='llm_factory',
+    version='0.0.3',
+    description='',
+    long_description='',
+    url='https://github.com/abhimasand/LLM-Factory',
+    author='Abhishek Masand',
+    author_email='masand.abhishek@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
