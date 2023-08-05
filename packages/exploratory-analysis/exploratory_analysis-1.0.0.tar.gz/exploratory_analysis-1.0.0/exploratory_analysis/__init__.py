13765d637d9181e4e964ee283058a3b72cb07b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:11 +0000

    Add package exploratory-analysis/1.0.0/exploratory_analysis-1.0.0.tar.gz

---
+++ packages/exploratory-analysis/exploratory_analysis-1.0.0.tar.gz/exploratory_analysis-1.0.0/exploratory_analysis/__init__.py
@@ -0,0 +1,1 @@
+from .Data_Analysis import EDA 
---
+++ packages/exploratory-analysis/exploratory_analysis-1.0.0.tar.gz/exploratory_analysis-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0' 
+DESCRIPTION = 'Exploratory Data Analysis'
+LONG_DESCRIPTION = 'Python package to do exploratory data analysis and provide results in PDF'
+
+# Setting up
+setup(
+        name="exploratory_analysis", 
+        version=VERSION,
+        author="Abhilash Maspalli Srinivas",
+        author_email="<abhilash_maspallisrinivas@comcast.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'EDA', 'PDF'],
+        classifiers= [
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ]
+)
