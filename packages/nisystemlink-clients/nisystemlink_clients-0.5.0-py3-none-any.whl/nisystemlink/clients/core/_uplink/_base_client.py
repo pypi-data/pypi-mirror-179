45671229ef86158fc60e095ec5b5faab13b9bbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:34 +0000

    Add package nisystemlink-clients/0.5.0/nisystemlink_clients-0.5.0-py3-none-any.whl

---
+++ packages/nisystemlink-clients/nisystemlink_clients-0.5.0-py3-none-any.whl/nisystemlink/clients/core/_uplink/_base_client.py
@@ -0,0 +1,63 @@
+# mypy: disable-error-code = misc
+
+import json
+from typing import Dict, Optional, Type
+
+from nisystemlink.clients import core
+from requests import JSONDecodeError, Response
+from uplink import Consumer, dumps, response_handler
+
+from ._json_model import JsonModel
+
+
+@response_handler
+def _handle_http_status(response: Response) -> Optional[Response]:
+    """Checks an HTTP response's status code and raises an exception if necessary."""
+    if 200 <= response.status_code < 300:
+        # Return None for "204 No Content" responses.
+        if response.status_code == 204:
+            return None
+        return response
+
+    msg = "Server responded with <{} {}> ({}).".format(
+        response.status_code, response.reason, response.url
+    )
+
+    try:
+        content = response.json()
+        if content and "error" in content:
+            err_obj = core.ApiError.from_json_dict(content["error"])
+        else:
+            err_obj = None
+
+        raise core.ApiException(
+            msg, error=err_obj, http_status_code=response.status_code
+        )
+    except JSONDecodeError:
+        if response.text:
+            msg += ":\n\n" + response.text
+        raise core.ApiException(msg, http_status_code=response.status_code)
+
+
+@dumps.to_json(JsonModel)
+def _deserialize_model(model_cls: Type[JsonModel], model_instance: JsonModel) -> Dict:
+    """Turns a :class:`.JsonModel` instance into a dictionary for serialization."""
+    return json.loads(model_instance.json(by_alias=True, exclude_unset=True))
+
+
+class BaseClient(Consumer):
+    """Base class for SystemLink clients, built on top of `Uplink <https://github.com/prkumar/uplink>`_."""
+
+    def __init__(self, configuration: core.HttpConfiguration):
+        """Initialize an instance.
+
+        Args:
+            configuration: Defines the web server to connect to and information about how to connect.
+        """
+        super().__init__(
+            base_url=configuration.server_uri,
+            converter=_deserialize_model,
+            hooks=[_handle_http_status],
+        )
+        if configuration.api_keys:
+            self.session.headers.update(configuration.api_keys)
---
+++ packages/nisystemlink-clients/nisystemlink_clients-0.5.0-py3-none-any.whl/nisystemlink/clients/dataframe/models/__init__.py
@@ -0,0 +1,12 @@
+from ._api_info import ApiInfo, Operation, OperationsV1
+from ._create_table_request import CreateTableRequest
+from ._column import Column
+from ._column_type import ColumnType
+from ._data_type import DataType
+from ._modify_table_request import ColumnMetadataPatch, ModifyTableRequest
+from ._order_by import OrderBy
+from ._paged_tables import PagedTables
+from ._query_tables_request import QueryTablesRequest
+from ._table_metadata import TableMetadata
+
+# flake8: noqa
