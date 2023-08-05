02fa0a2fd6c1e059831de45851bfc71d1ef152ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:40 +0000

    Add package ibm-ray-config/1.0.0b8/ibm_ray_config-1.0.0b8-py3-none-any.whl

---
+++ packages/ibm-ray-config/ibm_ray_config-1.0.0b8-py3-none-any.whl/ibm_ray_config/modules/gen2/ray/profile.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict
+from ibm_ray_config.modules.gen2.profile import ProfileConfig
+
+class RayProfileConfig(ProfileConfig):
+    
+    def __init__(self, base_config: Dict[str, Any]) -> None:
+        super().__init__(base_config)
+        if self.base_config.get('available_node_types'):
+            for available_node_type in self.base_config['available_node_types']:
+                self.defaults['profile_name'] = self.base_config['available_node_types'][available_node_type][
+                    'node_config'].get('instance_profile_name')
+                break
+    
+    def update_config(self, profile_name):
+
+        # cpu number based on profile
+        cpu_num = int(profile_name.split('-')[1].split('x')[0])
+
+        if self.base_config.get('available_node_types'):
+            for available_node_type in self.base_config['available_node_types']:
+                self.base_config['available_node_types'][available_node_type][
+                    'node_config']['instance_profile_name'] = profile_name
+                self.base_config['available_node_types'][available_node_type]['resources']['CPU'] = cpu_num
+        else:
+            self.base_config['available_node_types']['ray_head_default']['node_config']['instance_profile_name'] = profile_name
+            self.base_config['available_node_types']['ray_head_default']['resources']['CPU'] = cpu_num
+
