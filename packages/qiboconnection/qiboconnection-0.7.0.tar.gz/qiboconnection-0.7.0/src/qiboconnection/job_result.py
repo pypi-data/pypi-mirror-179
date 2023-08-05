846b795f5e195338d0a0e5d92530b1c733296b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:48 +0000

    Add package qiboconnection/0.7.0/qiboconnection-0.7.0.tar.gz

---
+++ packages/qiboconnection/qiboconnection-0.7.0.tar.gz/qiboconnection-0.7.0/src/qiboconnection/job_result.py
@@ -0,0 +1,41 @@
+""" Job Result """
+
+from abc import ABC
+from dataclasses import dataclass, field
+from typing import List
+
+from numpy import typing as npt
+from qibo.abstractions.states import AbstractState
+
+from qiboconnection.typings.job import JobType
+from qiboconnection.util import (
+    decode_results_from_circuit,
+    decode_results_from_experiment,
+)
+
+
+@dataclass
+class JobResult(ABC):
+    """Job Result class"""
+
+    job_id: int
+    http_response: str
+    job_type: str | JobType
+    data: List[AbstractState] | AbstractState | npt.NDArray | List[int] | List[float] | dict | List[
+        dict
+    ] | None = field(init=False)
+
+    def __post_init__(self) -> None:
+        """
+        Decodes data from the http_response provided at instance creation, and uses that info to build the self.data
+        attribute.
+        """
+
+        if self.job_type == JobType.CIRCUIT:
+            self.data = decode_results_from_circuit(self.http_response)
+            return
+        if self.job_type == JobType.EXPERIMENT:
+            self.data = decode_results_from_experiment(self.http_response)
+            return
+        if self.job_type == JobType.PROGRAM:
+            raise ValueError(f"Job type {JobType.PROGRAM} not supported.")
