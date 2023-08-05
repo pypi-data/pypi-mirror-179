4d428519dfe82944df6dab9ff40eb5ad003413f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:58 +0000

    Add package pdbmender/0.6.1/pdbmender-0.6.1.tar.gz

---
+++ packages/pdbmender/pdbmender-0.6.1.tar.gz/pdbmender-0.6.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pdbmender',
+ 'pdbmender.pdb2pqr',
+ 'pdbmender.pdb2pqr.conf_avg',
+ 'pdbmender.pdb2pqr.extensions',
+ 'pdbmender.pdb2pqr.src']
+
+package_data = \
+{'': ['*'], 'pdbmender.pdb2pqr': ['dat/*', 'tools/*']}
+
+setup_kwargs = {
+    'name': 'pdbmender',
+    'version': '0.6.1',
+    'description': 'Mend PDB files',
+    'long_description': None,
+    'author': 'Pedro B.P.S. Reis',
+    'author_email': 'pdreis@fc.ul.pt',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
