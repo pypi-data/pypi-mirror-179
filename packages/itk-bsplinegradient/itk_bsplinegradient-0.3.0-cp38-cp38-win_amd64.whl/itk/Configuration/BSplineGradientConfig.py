737b0ac150825b8bab85b06ef7424d74ff6968e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:21 +0000

    Add package itk-bsplinegradient/0.3.0/itk_bsplinegradient-0.3.0-cp38-cp38-win_amd64.whl

---
+++ packages/itk-bsplinegradient/itk_bsplinegradient-0.3.0-cp38-cp38-win_amd64.whl/itk/Configuration/BSplineGradientConfig.py
@@ -0,0 +1,18 @@
+depends = ('ITKPyBase', 'MeshToPolyData', 'ITKMesh', 'ITKImageGrid', 'ITKCommon', )
+templates = (  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterISS2FF', True, 'itk::Image< signed short,2 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIUC2FF', True, 'itk::Image< unsigned char,2 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIUS2FF', True, 'itk::Image< unsigned short,2 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIF2FF', True, 'itk::Image< float,2 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterID2FF', True, 'itk::Image< double,2 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterISS3FF', True, 'itk::Image< signed short,3 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIUC3FF', True, 'itk::Image< unsigned char,3 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIUS3FF', True, 'itk::Image< unsigned short,3 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIF3FF', True, 'itk::Image< float,3 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterID3FF', True, 'itk::Image< double,3 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterISS4FF', True, 'itk::Image< signed short,4 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIUC4FF', True, 'itk::Image< unsigned char,4 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIUS4FF', True, 'itk::Image< unsigned short,4 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterIF4FF', True, 'itk::Image< float,4 >,float,float'),
+  ('BSplineGradientImageFilter', 'itk::BSplineGradientImageFilter', 'itkBSplineGradientImageFilterID4FF', True, 'itk::Image< double,4 >,float,float'),
+)
+factories = ()
