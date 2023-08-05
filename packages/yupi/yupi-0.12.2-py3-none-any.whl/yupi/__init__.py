9756bd40907ae307240725dea064b92535a34e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:54 +0000

    Add package yupi/0.12.2/yupi-0.12.2-py3-none-any.whl

---
+++ packages/yupi/yupi-0.12.2-py3-none-any.whl/yupi/__init__.py
@@ -0,0 +1,30 @@
+"""
+All the resources of the library should be imported directly
+from one of the aforementioned modules.
+"""
+
+import logging
+import warnings
+
+from yupi._differentiation import DiffMethod, WindowType
+from yupi.features import Features
+from yupi.trajectory import Trajectory, TrajectoryPoint
+from yupi.vector import Vector
+
+warnings.filterwarnings("default", category=DeprecationWarning, module="yupi")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+__all__ = [
+    "Trajectory",
+    "TrajectoryPoint",
+    "Features",
+    "Vector",
+    "DiffMethod",
+    "WindowType",
+]
+
+__version__ = "0.12.2"
