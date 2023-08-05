d8f8610e8c9f103f6092acaaf3faaea6833aaa29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:31 +0000

    Add package espaloma-charge/0.0.6/espaloma_charge-0.0.6.tar.gz

---
+++ packages/espaloma-charge/espaloma_charge-0.0.6.tar.gz/espaloma_charge-0.0.6/espaloma_charge/app.py
@@ -0,0 +1,51 @@
+import os
+import tempfile
+from urllib import request
+from rdkit import Chem
+import dgl
+try:
+    dgl.use_libxsmm(False)
+except:
+    pass
+import torch
+from torch.utils.model_zoo import load_url
+import numpy as np
+from .utils import from_rdkit_mol
+MODEL_URL = """
+https://github.com/choderalab/espaloma_charge/releases/download/v0.0.0/model.pt
+"""
+
+def charge(
+        molecule,
+        total_charge: float = None,
+        model_url: str = MODEL_URL,
+    ) -> np.ndarray:
+    """Assign machine-learned AM1-BCC partial charges to a molecule.
+
+    Parameters
+    ----------
+    molecule : rdkit.Chem.Mol
+        Input molecule.
+
+    total_charge : float = 0.0
+
+
+    model_url : str
+        Model url.
+
+    Returns
+    -------
+    np.ndarray : (n_atoms, ) array of partial charges.
+
+    """
+    with tempfile.TemporaryDirectory() as tempdir:
+        target_path = os.path.join(tempdir, "model.pt")
+        request.urlretrieve(model_url, target_path)
+        model = torch.load(target_path, map_location="cpu")
+    if total_charge is None:
+        total_charge = Chem.GetFormalCharge(molecule)
+    graph = from_rdkit_mol(molecule)
+    n_nodes = graph.number_of_nodes()
+    graph.ndata["q_ref"] = torch.ones(n_nodes, 1) * total_charge / n_nodes
+    graph = model(graph)
+    return graph.ndata["q"].cpu().detach().flatten().numpy()
