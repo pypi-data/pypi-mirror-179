7f41a548eda2c5c4bef1ed0cfa82ed683ad2e8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:04 +0000

    Add package gdsfactory/6.3.4/gdsfactory-6.3.4-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-6.3.4-py3-none-any.whl/gdsfactory/tests/test_rotate.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import gdsfactory as gf
+
+
+def test_rotate() -> None:
+    c1 = gf.components.straight()
+    c1r = c1.rotate()
+
+    c2 = gf.components.straight()
+    c2r = c2.rotate()
+
+    assert c1.uid == c2.uid
+    assert c1r.uid == c2r.uid
+
+
+if __name__ == "__main__":
+    c1 = gf.components.straight()
+    c1r = c1.rotate()
+
+    c2 = gf.components.straight()
+    c2r = c2.rotate()
+
+    assert c1.uid == c2.uid
+    assert c1r.uid == c2r.uid
+    c2r.show()
