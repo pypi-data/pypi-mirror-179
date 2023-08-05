afdb5096df55153e30caa3304465893eb2c6122e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:12 +0000

    Add package intecomm-edc/0.1.4/intecomm-edc-0.1.4.tar.gz

---
+++ packages/intecomm-edc/intecomm-edc-0.1.4.tar.gz/intecomm-edc-0.1.4/intecomm_prn/forms/__init__.py
@@ -0,0 +1,5 @@
+from .end_of_study_form import EndOfStudyForm
+from .loss_to_followup_form import LossToFollowupForm
+from .offschedule_followup_form import OffScheduleFollowupForm
+from .protocol_incident_form import ProtocolIncidentForm
+from .subject_transfer_form import SubjectTransferForm
