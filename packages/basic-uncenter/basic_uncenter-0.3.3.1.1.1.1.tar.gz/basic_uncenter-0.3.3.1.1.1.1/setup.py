02b22d8f2830b1957d136d4ab6575738afa9561f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:24 +0000

    Add package basic-uncenter/0.3.3.1.1.1.1/basic_uncenter-0.3.3.1.1.1.1.tar.gz

---
+++ packages/basic-uncenter/basic_uncenter-0.3.3.1.1.1.1.tar.gz/basic_uncenter-0.3.3.1.1.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['basic_uncenter']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'basic-uncenter',
+    'version': '0.3.3.1.1.1.1',
+    'description': 'Basic math, printing, and text formatting.',
+    'long_description': None,
+    'author': 'uncenter',
+    'author_email': 'uncenteristaken@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
