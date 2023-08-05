20fd28721082d7c7f7d9062b72d3bf287e52ca86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:41 +0000

    Add package PyRockPaperScissorsGame/0.0.2/PyRockPaperScissorsGame-0.0.2.tar.gz

---
+++ packages/PyRockPaperScissorsGame/PyRockPaperScissorsGame-0.0.2.tar.gz/PyRockPaperScissorsGame-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A quick rock paper scissors game'
+LONG_DESCRIPTION = 'You can play a rock paper scissors game in any time'
+
+# Setting up
+setup(
+    name="PyRockPaperScissorsGame",
+    version=VERSION,
+    author="NeShk",
+    author_email="<bodia.nazar4669@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/NeShkApp/RockPaperScissors",
+    packages=find_packages(),
+    install_requires=['python', 'python3'],
+    keywords=['python', 'game', 'function', 'rps', 'rock', 'paper', 'scissors'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
