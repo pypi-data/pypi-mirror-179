84a557131142c5e21fc92c688a902d6a69a0e932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:43 +0000

    Add package bitcoinshamir/0.0.12/bitcoinshamir-0.0.12-py3-none-any.whl

---
+++ packages/bitcoinshamir/bitcoinshamir-0.0.12-py3-none-any.whl/bitcoinshamir/bitcoinshamir.py
@@ -0,0 +1,100 @@
+import os
+import lagrange
+from typing import List
+from .point import Point
+from .polynomial import Polynomial
+
+# The field size is a prime number that should be near the max value of the
+# secret key. The BIP39 24-word seed phrase creates the largest seed phrase,
+# which is 256 bits, plus an 8-bit checksum.
+PRIME_MODULUS = 2 ** 256 - 2 ** 32 - 977
+
+
+def create_shares(threshold: int, sharecount: int, key: bytes) -> List[Point]:
+    """
+    Splits a secret key into a (k, n) threshold scheme according to the Shamir
+    Secret Sharing (SSS) system. The secret key can be recovered with any
+    combination of k number of shares, but no information is revealed about the
+    secret key, even with k - 1 shares.
+    """
+    # Create a polynomial of k - 1 degrees and set the x^0 coefficient to the
+    # secret key.
+    key_num = int.from_bytes(key, "big")
+    polynomial = Polynomial(key_num)
+
+    # Add random 240-bit coefficients.
+    for i in range(threshold - 1):
+        random_coefficient = int.from_bytes(os.urandom(32), "big")
+        polynomial.coefficients.append(random_coefficient)
+
+    # Create shares based on x = 1, x = 2, ..., x = (k - 1)
+    shares = []
+
+    for i in range(sharecount):
+        point = Point()
+        point.X = i + 1
+        point.Y = polynomial.solve(point.X, PRIME_MODULUS)
+        shares.append(point)
+
+    return shares
+
+
+def recover_key(shares: List[Point]) -> bytes:
+    """
+    Takes a list of Point objects, and uses Lagrange interpolation to find the
+    Y-intercept, which is the secret key. If incorrect or too few Point objects
+    are provided, an incorrect result will be returned. There is no error
+    checking.
+    """
+    point_list = [(share.X, share.Y) for share in shares]
+    key_int = lagrange.interpolate(point_list, PRIME_MODULUS)
+    key_bin = key_int.to_bytes(32, "big")
+
+    return key_bin
+
+
+def interpolate(points: List[Point], modulus: int, X: int) -> int:
+    """
+    Gets the X-value using Lagrange interpolation according to the given list of
+    points, over the finite field of the given modulus. Raises an error if the
+    modulus is less than 3 or if the given Point objects do not have int values.
+    """
+    # Validate the given arguments.
+    if not isinstance(modulus, int):
+        raise TypeError("The given modulus was not of the int type.")
+
+    if modulus < 3:
+        raise ValueError("The given modulus was not a positive prime number.")
+
+    if not isinstance(points, list):
+        raise TypeError("The given points argument was not of type list.")
+
+    if len(points) < 2:
+        raise ValueError(f"Only {len(points)} given. At least two required.")
+    else:
+        for point in points:
+            if not isinstance(point.X, int) or not isinstance(point.Y, int):
+                raise TypeError("The given point values are not of type int.")
+            elif point.X < 0 or point.Y < 0:
+                raise ValueError(f"({point.X}, {point.Y}) has negative value.")
+
+    # This will be the return value.
+    cumulative_sum = 0
+
+    # Sum loop.
+    for point_a in points:
+        cumulative_product = point_a.Y
+
+        # Product loop.
+        for point_b in [p for p in points if p is not point_a]:
+            numerator = (X - point_b.X) % modulus
+            denominator = (point_a.X - point_b.X) % modulus
+
+            # Multiplicitive inverse using Fermat's little theorem.
+            mul_inv = pow(denominator, modulus - 2, modulus)
+            product = numerator * mul_inv % modulus
+            cumulative_product = cumulative_product * product % modulus
+
+        cumulative_sum = (cumulative_sum + cumulative_product) % modulus
+
+    return cumulative_sum
---
+++ packages/bitcoinshamir/bitcoinshamir-0.0.12-py3-none-any.whl/bitcoinshamir/mnemonic.py
@@ -0,0 +1,96 @@
+from .BIP39_List import BIP39_List
+from hashlib import sha256
+from typing import List
+
+wordlist = BIP39_List()
+
+class Mnemonic:
+    """
+    Mnemonic class for holding a BIP44 seed phrase. The 24 words represent 264
+    bits. The first 256 bits are 24 words from a BIP39 word list. The final 8
+    bits represent a checksum, which is the first 8 bits of the sha256 hash of
+    the first 260 bits of the mnemonic.
+    """
+    def __init__(self, language: str) -> None:
+        """
+        Initializes a new instance of the Mnemonic class, and sets the language
+        attribute. Raises a ValueError if the language is not in the current
+        list of languages.
+        """
+        if language in BIP39_List.LANGUAGE_LIST:
+            self.language = language
+            self.seed = bytes(32)
+            self.checksum = bytes(1)
+        else:
+            raise ValueError(f"{language} not in the current language list.")
+
+
+    def set_word(self, index: int, word: str) -> None:
+        """
+        Sets a word in this Mnemonic class instance according to the zero-based
+        array and word given. Raises errors if the index is out of range or the
+        language is not in the current list of languages.
+        """
+        if not isinstance(index, int):
+            raise TypeError("The index argument given is not an int.")
+
+        if index < 0 or index > 23:
+            raise IndexError("The index argument given is out of bounds.")
+
+        if self.language not in wordlist.get_language(word):
+            raise ValueError(f"{word} not found in {self.language} list.")
+
+        # Convert the seed into binary text.
+        seed_num = int.from_bytes(self.seed, "big")
+        seed_bin_text = format(seed_num, "0264b")
+
+        # Switch out the bits for the given word argument.
+        word_index = wordlist.get_word_index(word, self.language)
+        word_bin_text = format(word_index, "011b")
+        char_count_left = index * 11 - 1
+        char_right_position = (index + 1) * 11
+
+        bin_text_left = seed_bin_text[:char_count_left]
+        bin_text_right = seed_bin_text[char_right_position:]
+        new_seed_bin_text = bin_text_left + word_bin_text + bin_text_right
+        new_seed_bin = int(new_seed_bin_text, base=2).to_bytes(33, "big")
+
+        # Recalculate the checksum if the last word, which contains the
+        # checksum, is not the word being set.
+        if index != 23:
+            new_seed_word_section = new_seed_bin[:32]
+            checksum = sha256(new_seed_word_section)[:1]
+            new_seed_bin = new_seed_word_section + checksum
+
+
+    def is_valid_phrase(phrase: List[str], language: str) -> bool:
+        """
+        Returns true if the given mnemonic phrase has words that exist in the
+        given language's word list, and the checksum is valid. Raises error if
+        the language given is not in the current language list.
+        """
+        if language not in BIP39_List.LANGUAGE_LIST:
+            raise ValueError(f"{language} not in the current language list.")
+
+        is_valid_wordcount = len(phrase) == 24
+        has_valid_words = all(
+            [language in wordlist.get_language(word) for word in phrase]
+            )
+
+        if not is_valid_wordcount or not has_valid_words:
+            return False
+
+        indices = [wordlist.get_word_index(word, language) for word in phrase]
+        bin_text_chunks = [format(index, "011b") for index in indices]
+        bin_text = "".join(bin_text_chunks)
+
+        # The seed is the first 256 bits. The checksum is the last 8 bits.
+        seed_val = int(bin_text[:256], base=2).to_bytes(32, "big")
+        checksum = int(bin_text[256:264], base=2).to_bytes(1, "big")
+
+        # The checksum is the first byte of the sha256 hash of the prior 32
+        # bytes.
+        recalculated_checksum = sha256(seed_val).digest()[:1]
+        is_valid_checksum = checksum == recalculated_checksum
+
+        return is_valid_checksum
