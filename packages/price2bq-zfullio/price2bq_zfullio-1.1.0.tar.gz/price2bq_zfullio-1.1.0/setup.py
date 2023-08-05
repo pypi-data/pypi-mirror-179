89bf2e56c6c661a1fe83414ab41ebdc64d5290e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:37 +0000

    Add package price2bq-zfullio/1.1.0/price2bq_zfullio-1.1.0.tar.gz

---
+++ packages/price2bq-zfullio/price2bq_zfullio-1.1.0.tar.gz/price2bq_zfullio-1.1.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['price2bq_zfullio']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bq-easy-zfullio>=1,<2',
+ 'loguru>=0.6.0,<0.7.0',
+ 'openpyxl>=3.0.10,<4.0.0',
+ 'pandas>=1.5,<2.0',
+ 'python-dotenv>=0.21,<0.22',
+ 'xlrd>=2.0.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'price2bq-zfullio',
+    'version': '1.1.0',
+    'description': "Экспорт файлов c площадок 'Яндекс Недвижимость', 'Авито' 'Циан' в BigQuery",
+    'long_description': "# Price to BQ\n\nЭкспорт файлов с площадок 'Яндекс Недвижимость', 'Циан', 'Avito', 'Новострой М' в BigQuery",
+    'author': 'viktor',
+    'author_email': 'vi.dave@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
