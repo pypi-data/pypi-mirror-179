fd523014e00068b73d8c214bb8603681c7c2058f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:13 +0000

    Add package starlite/1.43.1/starlite-1.43.1-py3-none-any.whl

---
+++ packages/starlite/starlite-1.43.1-py3-none-any.whl/starlite/utils/serialization.py
@@ -0,0 +1,23 @@
+from pathlib import PurePath, PurePosixPath
+from typing import Any
+
+from pydantic import BaseModel, SecretStr
+
+
+def default_serializer(value: Any) -> Any:
+    """Return the default serializer for a given object based on its type.
+
+    Args:
+        value: A value to serialize
+    Returns:
+        A serialized value
+    Raises:
+        TypeError: if value is not supported
+    """
+    if isinstance(value, BaseModel):
+        return value.dict()
+    if isinstance(value, SecretStr):
+        return value.get_secret_value()
+    if isinstance(value, (PurePath, PurePosixPath)):
+        return str(value)
+    raise TypeError("unsupported type")
