12d3c7e86395006cf4e27e309546daf31d3962c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:56 +0000

    Add package bobtail/0.0.9/bobtail-0.0.9-py3-none-any.whl

---
+++ packages/bobtail/bobtail-0.0.9-py3-none-any.whl/bobtail/__init__.py
@@ -0,0 +1,5 @@
+from .wsgi import BobTail
+from .route import AbstractRoute, Handler, Route
+from .request import Request
+from .response import Response
+from .middleware import Middleware, AbstractMiddleware, Tail
