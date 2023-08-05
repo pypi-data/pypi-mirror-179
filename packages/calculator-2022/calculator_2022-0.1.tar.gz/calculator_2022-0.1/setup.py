09ce825b089b43c55ec735e5011f9025ad876d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:55 +0000

    Add package calculator-2022/0.1/calculator_2022-0.1.tar.gz

---
+++ packages/calculator-2022/calculator_2022-0.1.tar.gz/calculator_2022-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='calculator_2022',
+    version='0.1',
+    author="Gabija Gedvilaite",
+    author_email="gabija_g@hotmail.com",
+    description="Calculator performs: Addition / Subtraction, Multiplication / Division, Take (n) root of a number",
+    packages=['calculator_2022'],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
