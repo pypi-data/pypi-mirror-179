a17c16795a74ab33e678320aea055128f9032493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:17:28 +0000

    Add package protarrow/0.0.3rc6/protarrow-0.0.3rc6.tar.gz

---
+++ packages/protarrow/protarrow-0.0.3rc6.tar.gz/protarrow-0.0.3rc6/README.md
@@ -0,0 +1,35 @@
+[![PyPI Version][pypi-image]][pypi-url]
+[![][versions-image]][versions-url]
+[![][stars-image]][stars-url]
+[![codecov](https://codecov.io/gh/tradewelltech/protarrow/branch/master/graph/badge.svg?token=XMFH27IL70)](https://codecov.io/gh/tradewelltech/protarrow)
+[![Build Status][build-image]][build-url]
+[![Documentation][doc-image]][doc-url]
+
+
+# Protarrow
+
+A python library for converting from protobuf to arrow and back 
+
+# Installation
+
+```shell
+pip install protarrow
+```
+
+# Usage
+
+See the [documentation](https://protarrow.readthedocs.io/en/latest/)
+
+
+<!-- Badges: -->
+
+[pypi-image]: https://img.shields.io/pypi/v/protarrow
+[pypi-url]: https://pypi.org/project/protarrow/
+[build-image]: https://github.com/tradewelltech/protarrow/actions/workflows/build.yaml/badge.svg
+[build-url]: https://github.com/tradewelltech/protarrow/actions/workflows/build.yaml
+[stars-image]: https://img.shields.io/github/stars/tradewelltech/protarrow
+[stars-url]: https://github.com/tradewelltech/protarrow
+[versions-image]: https://img.shields.io/pypi/pyversions/protarrow
+[versions-url]: https://pypi.org/project/protarrow/
+[doc-image]: https://readthedocs.org/projects/protarrow/badge/?version=latest
+[doc-url]: https://protarrow.readthedocs.io/en/latest/?badge=latest
---
+++ packages/protarrow/protarrow-0.0.3rc6.tar.gz/protarrow-0.0.3rc6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['protarrow']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['googleapis-common-protos>=1.53.0', 'protobuf>=3.20.1', 'pyarrow>=8.0.0']
+
+setup_kwargs = {
+    'name': 'protarrow',
+    'version': '0.0.3rc6',
+    'description': 'Convert from protobuf to arrow and back',
+    'long_description': '[![PyPI Version][pypi-image]][pypi-url]\n[![][versions-image]][versions-url]\n[![][stars-image]][stars-url]\n[![codecov](https://codecov.io/gh/tradewelltech/protarrow/branch/master/graph/badge.svg?token=XMFH27IL70)](https://codecov.io/gh/tradewelltech/protarrow)\n[![Build Status][build-image]][build-url]\n[![Documentation][doc-image]][doc-url]\n\n\n# Protarrow\n\nA python library for converting from protobuf to arrow and back \n\n# Installation\n\n```shell\npip install protarrow\n```\n\n# Usage\n\nSee the [documentation](https://protarrow.readthedocs.io/en/latest/)\n\n\n<!-- Badges: -->\n\n[pypi-image]: https://img.shields.io/pypi/v/protarrow\n[pypi-url]: https://pypi.org/project/protarrow/\n[build-image]: https://github.com/tradewelltech/protarrow/actions/workflows/build.yaml/badge.svg\n[build-url]: https://github.com/tradewelltech/protarrow/actions/workflows/build.yaml\n[stars-image]: https://img.shields.io/github/stars/tradewelltech/protarrow\n[stars-url]: https://github.com/tradewelltech/protarrow\n[versions-image]: https://img.shields.io/pypi/pyversions/protarrow\n[versions-url]: https://pypi.org/project/protarrow/\n[doc-image]: https://readthedocs.org/projects/protarrow/badge/?version=latest\n[doc-url]: https://protarrow.readthedocs.io/en/latest/?badge=latest\n',
+    'author': '0x26res',
+    'author_email': '0x26res@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/tradewelltech/protarrow',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
