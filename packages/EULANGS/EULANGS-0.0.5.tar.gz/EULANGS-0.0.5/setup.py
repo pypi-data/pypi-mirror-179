b33cdc462a5a2b3284d5e2039d6c4ea16ab6b200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:11 +0000

    Add package EULANGS/0.0.5/EULANGS-0.0.5.tar.gz

---
+++ packages/EULANGS/EULANGS-0.0.5.tar.gz/EULANGS-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="EULANGS",
+	version="0.0.5",
+	author="Simon Hengchen",
+	author_email="simon@iguanodon.ai",
+	description="This package allows you to import a list of official EU languages.",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    #package_dir = {"": ".EULANGS"},
+    packages = setuptools.find_packages(where="."),
+    python_requires = ">=3.6"
+)
