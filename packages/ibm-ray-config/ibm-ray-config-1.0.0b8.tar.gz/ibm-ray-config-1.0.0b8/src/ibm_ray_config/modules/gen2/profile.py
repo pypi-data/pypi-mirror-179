84f062887588411b56465c8358cdd01d0cd2a9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:40 +0000

    Add package ibm-ray-config/1.0.0b8/ibm-ray-config-1.0.0b8.tar.gz

---
+++ packages/ibm-ray-config/ibm-ray-config-1.0.0b8.tar.gz/ibm-ray-config-1.0.0b8/src/ibm_ray_config/modules/gen2/profile.py
@@ -0,0 +1,38 @@
+from ibm_ray_config.modules.config_builder import ConfigBuilder, update_decorator, spinner
+from typing import Any, Dict
+from ibm_ray_config.modules.utils import get_option_from_list, find_default, find_obj
+
+
+class ProfileConfig(ConfigBuilder):
+    
+    def __init__(self, base_config: Dict[str, Any]) -> None:
+        super().__init__(base_config)
+
+    @update_decorator
+    def run(self) -> Dict[str, Any]:
+
+        @spinner
+        def get_instance_profile_objects():
+            return self.ibm_vpc_client.list_instance_profiles().get_result()['profiles']
+
+        instance_profile_objects = get_instance_profile_objects()
+
+        default = find_default(
+            self.base_config, instance_profile_objects, name='instance_profile_name')
+        instance_profile = get_option_from_list(
+            'Carefully choose instance profile, please refer to https://cloud.ibm.com/docs/vpc?topic=vpc-profiles', instance_profile_objects, default=default)
+
+        return instance_profile['name']
+    
+    @update_decorator
+    def verify(self, base_config):
+        profile_name = self.defaults['profile_name']
+        instance_profile_objects = self.ibm_vpc_client.list_instance_profiles().get_result()['profiles']
+        profile = find_obj(instance_profile_objects, 'dummy', obj_name=profile_name)
+        if not profile:
+            raise Exception(f'Specified profile {profile_name} not found in the profile list {instance_profile_objects}')
+        return profile_name
+
+    @update_decorator    
+    def create_default(self):
+        return 'bx2-2x8'
---
+++ packages/ibm-ray-config/ibm-ray-config-1.0.0b8.tar.gz/ibm-ray-config-1.0.0b8/src/ibm_ray_config/modules/gen2/ray/workers.py
@@ -0,0 +1,50 @@
+import re
+from typing import Any, Dict
+
+import inquirer
+from ibm_ray_config.modules.config_builder import ConfigBuilder
+from ibm_ray_config.modules.utils import validate_cluster_name
+
+
+class WorkersConfig(ConfigBuilder):
+
+    def run(self) -> Dict[str, Any]:
+        default_cluster_name = self.base_config.get('cluster_name', 'default')
+        default_min_workers = self.base_config.get('min_workers', '0')
+        default_max_workers = default_min_workers
+
+        question = [
+            inquirer.Text(
+                'name', message="Enter cluster name following the pattern `[a-z]|[a-z][-a-z0-9]*[a-z0-9]`", validate = validate_cluster_name, default=default_cluster_name),
+            inquirer.Text('min_workers', message="Minimum number of worker nodes",
+                          default=default_min_workers, validate=lambda _, x: re.match('^[+]?[0-9]+$', x)),
+            inquirer.Text('max_workers', message="Maximum number of worker nodes", default=default_max_workers,
+                          validate=lambda answers, x: re.match('^[+]?[0-9]+$', x) and int(x) >= int(answers['min_workers']))
+        ]
+
+        answers = inquirer.prompt(question, raise_keyboard_interrupt=True)
+        self.base_config['cluster_name'] = answers['name']
+        self.base_config['max_workers'] = int(answers['max_workers'])
+
+        if self.base_config.get('available_node_types'):
+            for available_node_type in self.base_config['available_node_types']:
+                self.base_config['available_node_types'][available_node_type]['min_workers'] = int(
+                    answers['min_workers'])
+                self.base_config['available_node_types'][available_node_type]['max_workers'] = int(
+                    answers['max_workers'])
+        else:
+            self.base_config['available_node_types']['ray_head_default']['min_workers'] = int(
+                answers['min_workers'])
+            self.base_config['available_node_types']['ray_head_default']['max_workers'] = int(
+                answers['max_workers'])
+
+        return self.base_config
+    
+    def verify(self, base_config):
+        min_workers = base_config['available_node_types']['ray_head_default']['min_workers']
+        max_workers = base_config['available_node_types']['ray_head_default']['max_workers']
+        
+        if max_workers < min_workers:
+            raise Exception(f'specified min workers {min_workers} larger than max workers {max_workers}')
+
+        return base_config
