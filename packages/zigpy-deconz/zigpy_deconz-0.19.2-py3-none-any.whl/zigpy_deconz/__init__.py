0c92e0037bbb973deffd2ecdd1470a7bbcbf82cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:00 +0000

    Add package zigpy-deconz/0.19.2/zigpy_deconz-0.19.2-py3-none-any.whl

---
+++ packages/zigpy-deconz/zigpy_deconz-0.19.2-py3-none-any.whl/zigpy_deconz/__init__.py
@@ -0,0 +1,8 @@
+"""Init file for zigpy_deconz."""
+
+# coding: utf-8
+MAJOR_VERSION = 0
+MINOR_VERSION = 19
+PATCH_VERSION = "2"
+__short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
+__version__ = f"{__short_version__}.{PATCH_VERSION}"
