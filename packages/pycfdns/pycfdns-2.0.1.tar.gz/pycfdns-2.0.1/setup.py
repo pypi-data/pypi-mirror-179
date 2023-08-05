a78419df874f17fac46efa8466d5c88ce9791120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:36 +0000

    Add package pycfdns/2.0.1/pycfdns-2.0.1.tar.gz

---
+++ packages/pycfdns/pycfdns-2.0.1.tar.gz/pycfdns-2.0.1/setup.py
@@ -0,0 +1,22 @@
+"""Setup"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG = fh.read()
+setuptools.setup(
+    name="pycfdns",
+    version="2.0.1",
+    author="Joakim Sorensen",
+    author_email="hi@ludeeus.dev",
+    description="Update Cloudflare DNS records.",
+    install_requires=["aiohttp>=3.6.1,<4.0"],
+    long_description=LONG,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ludeeus/pycfdns",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
