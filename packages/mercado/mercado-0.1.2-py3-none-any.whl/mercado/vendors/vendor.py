f5cc44c81a175ae53a36c262fd2df4121f5bf4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:05 +0000

    Add package mercado/0.1.2/mercado-0.1.2-py3-none-any.whl

---
+++ packages/mercado/mercado-0.1.2-py3-none-any.whl/mercado/vendors/vendor.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from pathlib import Path
+
+
+class Label(Enum):
+    K8S = "k8s"
+    DOCS = "docs"
+    SECURITY = "security"
+    IAC = "iac"
+    VCS = "vcs"
+    VIRT = "virt"
+    CICD = "ci/cd"
+
+
+@dataclass(frozen=True)
+class Tool:
+    name: str
+    labels: tuple[Label] = field(default_factory=tuple)
+    target: Path = None
+
+
+@dataclass
+class Installer:
+    name: str
+    version: str
+
+    def install(self):
+        raise NotImplementedError()
+
+
+class ToolVendor:
+    def get_latest_version(self, tool: Tool) -> str:
+        raise NotImplementedError()
+
+    def get_installer(self, tool: Tool, version: str, os: str, arch: str) -> Installer:
+        raise NotImplementedError()
