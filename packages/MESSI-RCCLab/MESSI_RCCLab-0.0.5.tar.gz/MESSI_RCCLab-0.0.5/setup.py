b5a8398b4996552bf21419c52362983c541b0b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:16 +0000

    Add package MESSI-RCCLab/0.0.5/MESSI_RCCLab-0.0.5.tar.gz

---
+++ packages/MESSI-RCCLab/MESSI_RCCLab-0.0.5.tar.gz/MESSI_RCCLab-0.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+  
+with open('README.md') as file:
+    long_description = file.read()
+
+short_description = 'MESSI: Multi Ensamble Strategy for Structural Elucidation'
+requirements = ['tk', 
+                'pandas', 
+                'numpy', 
+                'sklearn', 
+                'scipy', 
+                'openpyxl',
+                'pathlib']
+  
+
+setup(
+        name ='MESSI_RCCLab',
+        version ='0.0.5',
+        author='María M. Zanardi & Ariel M. Sarotti',
+        author_email='zanardi@inv.rosario-conicet.gov.ar',
+        
+        #url='https://github.com/Sarotti-Lab/ML_J_DP4',
+        
+        description =short_description	,
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        
+        packages=find_packages(where="MESSI"),
+        package_dir={"": "MESSI"},
+        package_data={"UserManual": ["*.pdf"]},
+        
+        entry_points = {'console_scripts': ['MESSI = MESSI.MESSI:main']},
+        classifiers = [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"],
+        keywords ='NMR structural elucidation',
+        install_requires = requirements
+)
+
+
