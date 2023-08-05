f312cc6ee87deb72fb4f9f5a24411efbe1850926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:02 +0000

    Add package cubao-meshcat/0.3.2/cubao_meshcat-0.3.2.tar.gz

---
+++ packages/cubao-meshcat/cubao_meshcat-0.3.2.tar.gz/cubao_meshcat-0.3.2/setup.py
@@ -0,0 +1,30 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(name="cubao_meshcat",
+    version="0.3.2",
+    description="WebGL-based visualizer for 3D geometries and scenes",
+    url="https://github.com/cubao/meshcat-python",
+    author="tzx",
+    author_email="dvorak4tzx@gmail.com",
+    license="MIT",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    test_suite="meshcat",
+    entry_points={
+        "console_scripts": [
+            "cubao-meshcat-server=cubao_meshcat.servers.zmqserver:main"
+        ]
+    },
+    install_requires=[
+      "ipython >= 5",
+      "u-msgpack-python >= 2.4.1",
+      "numpy >= 1.14.0",
+      "tornado >= 4.0.0",
+      "pyzmq >= 17.0.0",
+      "pyngrok >= 4.1.6",
+      "pillow >= 7.0.0"
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
