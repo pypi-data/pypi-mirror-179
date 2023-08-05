c25830d6f067cc12c12baf74626c9b8ca1b63eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:29 +0000

    Add package yourmove-monitor/0.0.1/yourmove_monitor-0.0.1.tar.gz

---
+++ packages/yourmove-monitor/yourmove_monitor-0.0.1.tar.gz/yourmove_monitor-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yourmove_monitor",
+    version="0.0.1",
+    author="jup014",
+    author_email="jup014@ucsd.edu",
+    description="General Library to monitor YourMove Project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hekler-Designing-Health-Lab/YourMove-Monitor",
+    project_urls={
+        "Bug Tracker": "https://github.com/Hekler-Designing-Health-Lab/YourMove-Monitor/issues",
+    },
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['numpy', 'pandas', 'pymongo'],
+)
