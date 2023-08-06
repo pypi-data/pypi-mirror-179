d40b15e871cca80a98d6cfc6371fa6aacad6fd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:22 +0000

    Add package piknik/0.2.2/piknik-0.2.2.tar.gz

---
+++ packages/piknik/piknik-0.2.2.tar.gz/piknik-0.2.2/piknik/error.py
@@ -0,0 +1,14 @@
+class DeadIssue(Exception):
+    pass
+
+
+class AlreadyAssignedError(Exception):
+    pass
+
+
+class UnknownIdentityError(Exception):
+    pass
+
+
+class VerifyError(Exception):
+    pass
