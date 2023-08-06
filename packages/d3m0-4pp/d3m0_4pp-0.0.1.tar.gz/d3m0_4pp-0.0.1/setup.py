8550e1a3193007ce2c3a00a3b789af0e4c6285ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:57 +0000

    Add package d3m0-4pp/0.0.1/d3m0_4pp-0.0.1.tar.gz

---
+++ packages/d3m0-4pp/d3m0_4pp-0.0.1.tar.gz/d3m0_4pp-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+     name="d3m0_4pp",
+     version='0.0.1',
+     descripcion="prueba",
+     author="conocido",
+     author_email="conocido@demo.com",
+     url="https://www.python.org",
+     packages=setuptools.find_packages(),
+	)
