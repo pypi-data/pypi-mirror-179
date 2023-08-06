c6752057dce2a9c8d0bf9549452f0e200de83a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:17 +0000

    Add package lison-technology/0.0.4/lison_technology-0.0.4.tar.gz

---
+++ packages/lison-technology/lison_technology-0.0.4.tar.gz/lison_technology-0.0.4/lison_technology/lison-output.py
@@ -0,0 +1,22 @@
+import os
+
+def version_declaration():
+    """
+    The module is about library`s version
+    And the author is HuiBuBu(LanBuBu)
+    """
+    print("""
+lison_technology[v0.0.1]-->The program is stable
+    """)
+
+# TEXTING #
+# class out:
+#     def motion(self, frames, second):
+#         """
+#         Achieve dynamic effects
+#         """
+#         second / frames
+#         print(self)
+#         time.sleep(round(second, 2))
+#         os.system("cls")
+#
