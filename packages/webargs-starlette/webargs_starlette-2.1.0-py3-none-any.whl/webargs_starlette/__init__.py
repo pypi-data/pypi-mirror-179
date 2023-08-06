eb9d532f707f3cc6933216aa3498c8f6ee8e0cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:35 +0000

    Add package webargs-starlette/2.1.0/webargs_starlette-2.1.0-py3-none-any.whl

---
+++ packages/webargs-starlette/webargs_starlette-2.1.0-py3-none-any.whl/webargs_starlette/__init__.py
@@ -0,0 +1,18 @@
+from .starletteparser import (
+    StarletteParser,
+    parser,
+    use_args,
+    use_kwargs,
+    use_annotations,
+    WebargsHTTPException,
+)
+
+__version__ = "2.1.0"
+__all__ = [
+    "StarletteParser",
+    "parser",
+    "use_args",
+    "use_kwargs",
+    "use_annotations",
+    "WebargsHTTPException",
+]
