882cdab8c6b240bbddb65c9cd9589b0573b7e3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:26 +0000

    Add package pp-project-hmv/0.4.2/pp_project_hmv-0.4.2-py3-none-any.whl

---
+++ packages/pp-project-hmv/pp_project_hmv-0.4.2-py3-none-any.whl/pp_project_hmv/maths.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+
+def add_numbers(
+    a: Union[int, float], b: Union[int, float]
+) -> Union[int, float]:
+    return a + b
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+
+def multiply_numbers(a, b):
+    return a * b
