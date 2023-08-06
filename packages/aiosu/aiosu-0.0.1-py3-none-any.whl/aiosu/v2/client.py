dccde4108b428974e538b1fe41d01f3363aaac03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:14 +0000

    Add package aiosu/0.0.1/aiosu-0.0.1-py3-none-any.whl

---
+++ packages/aiosu/aiosu-0.0.1-py3-none-any.whl/aiosu/v2/client.py
@@ -0,0 +1,408 @@
+"""
+This module handles API requests for API v2 (OAuth).
+
+You can read more about it here: https://osu.ppy.sh/docs/index.html
+"""
+from __future__ import annotations
+
+import datetime
+import functools
+from types import TracebackType
+from typing import Any
+from typing import Callable
+from typing import Optional
+from typing import Type
+from typing import Union
+
+import aiohttp
+from aiolimiter import AsyncLimiter
+
+from .. import helpers
+from ..classes import APIException
+from ..classes import Beatmap
+from ..classes import BeatmapDifficultyAttributes
+from ..classes import Gamemode
+from ..classes import Mods
+from ..classes import OAuthToken
+from ..classes import Score
+from ..classes import User
+from ..classes import UserQueryType
+
+
+def check_token(func: Callable) -> Callable:
+    """
+    A decorator that checks the current token, to be used as:
+    @check_token
+    """
+
+    @functools.wraps(func)
+    async def _check_token(*args: Any, **kwargs: Any) -> Any:
+        self = args[0]
+        if datetime.datetime.now() > self.token.expires_on:
+            self.__session.headers[
+                "Authorization"
+            ] = f"Bearer {self.token.access_token}"
+        return await func(*args, **kwargs)
+
+    return _check_token
+
+
+def rate_limited(func: Callable) -> Callable:
+    """
+    A decorator that enforces rate limiting, to be used as:
+    @rate_limited
+    """
+
+    @functools.wraps(func)
+    async def _rate_limited(*args: Any, **kwargs: Any) -> Any:
+        self = args[0]
+        async with self._limiter:
+            return await func(*args, **kwargs)
+
+    return _rate_limited
+
+
+class Client:
+    r"""osu! API v2 Client
+
+    :param \**kwargs:
+        See below
+
+    :Keyword Arguments:
+        * *client_secret* (``str``)
+        * *client_id* (``int``)
+        * *base_url* (``str``) --
+            Optional, base API URL, defaults to \"https://osu.ppy.sh/api/v2/\"
+        * *token* (``aiosu.classes.token.OAuthToken``)
+        * *limiter* (``aiolimiter.AsyncLimiter``) --
+            Optional, custom AsyncLimiter, defaults to AsyncLimiter(1200, 60)
+    """
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.client_secret: str = kwargs.pop("client_secret", None)
+        self.client_id: int = kwargs.pop("client_id", None)
+        self.token: OAuthToken = kwargs.pop("token", OAuthToken())
+        self.base_url: str = kwargs.pop("base_url", "https://osu.ppy.sh/api/v2")
+        self._limiter: AsyncLimiter = kwargs.pop("limiter", AsyncLimiter(1200, 60))
+        self.__session: aiohttp.ClientSession = aiohttp.ClientSession(
+            headers={
+                "Authorization": f"Bearer {self.token.access_token}",
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+        )
+
+    async def __aenter__(self) -> Client:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        await self.close()
+
+    async def close(self) -> None:
+        """Closes the client session."""
+        await self.__session.close()
+
+    @rate_limited
+    @check_token
+    async def get_me(self) -> User:
+        r"""Gets the user who owns the current token
+
+        :raises APIException: Contains status code and error message
+        :return: Requested user
+        :rtype: aiosu.classes.user.User
+        """
+        url = f"{self.base_url}/me"
+        async with self.__session.get(url) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return User.parse_obj(json)
+
+    @rate_limited
+    @check_token
+    async def get_user(self, user_query: Union[str, int], **kwargs: Any) -> User:
+        r"""Gets a user by a query.
+
+        :param user_query: Username or ID to search by
+        :type user_query: Union[str, int]
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *qtype* (``str``) --
+                Optional, \"string\" or \"id\". Type of the user_query
+
+        :raises APIException: Contains status code and error message
+        :return: Requested user
+        :rtype: aiosu.classes.user.User
+        """
+        url = f"{self.base_url}/{user_query}"
+        params = {}
+        if "mode" in kwargs:
+            mode = Gamemode(kwargs.pop("mode"))  # type: ignore
+            url += f"/{mode}"
+        if "qtype" in kwargs:
+            qtype = UserQueryType(kwargs.pop("qtype"))  # type: ignore
+            params["type"] = qtype.new_api_name
+        async with self.__session.get(url, params=params) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return User.parse_obj(json)
+
+    @rate_limited
+    @check_token
+    async def __get_type_scores(
+        self, user_id: int, request_type: str, **kwargs: Any
+    ) -> list[Score]:
+        r"""INTERNAL: Get a user's scores by type
+
+        :param user_id: User ID to search by
+        :type user_id: int
+        :param request_type: "recent", "best" or "firsts"
+        :type request_type: str
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *limit* (``int``) --
+                Optional, number of scores to get. Min: 1, Max: 100, defaults to 100
+            * *include_fails* (``bool``) --
+                Optional, whether to include failed scores, defaults to ``False``
+            * *offset* (``int``) --
+                Optional, page offset to start from, defaults to 0
+
+        :raises ValueError: If limit is not between 1 and 100
+        :raises ValueError: If type is invalid
+        :raises APIException: Contains status code and error message
+        :return: List of requested scores
+        :rtype: list[aiosu.classes.score.Score]
+        """
+        if not 1 <= kwargs.get("limit", 100) <= 100:
+            raise ValueError("Invalid limit specified. Limit must be between 1 and 100")
+        if request_type not in ("recent", "best", "firsts"):
+            raise ValueError(
+                f'"{request_type}" is not a valid request_type. Valid options are: "recent", "best", "firsts"',
+            )
+        url = f"{self.base_url}/users/{user_id}/scores/{request_type}"
+        params = {
+            "include_fails": kwargs.pop("include_fails", False),
+            "offset": kwargs.pop("offset", 0),
+            "limit": kwargs.pop("limit", 100),
+        }
+        if "mode" in kwargs:
+            mode = Gamemode(kwargs.pop("mode"))  # type: ignore
+            params["mode"] = str(mode)
+        if "limit" in kwargs:
+            params["limit"] = kwargs.pop("limit")
+        async with self.__session.get(url, params=params) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return helpers.from_list(Score.parse_obj, json)
+
+    async def get_user_recents(self, user_id: int, **kwargs: Any) -> list[Score]:
+        r"""Get a user's recent scores.
+
+        :param user_id: User ID to search by
+        :type user_id: int
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *limit* (``int``) --
+                Optional, number of scores to get. Min: 1, Max: 100, defaults to 100
+            * *include_fails* (``bool``) --
+                Optional, whether to include failed scores, defaults to ``False``
+            * *offset* (``int``) --
+                Optional, page offset to start from, defaults to 0
+
+        :raises APIException: Contains status code and error message
+        :return: List of requested scores
+        :rtype: list[aiosu.classes.score.Score]
+        """
+        return await self.__get_type_scores(user_id, "recent", **kwargs)
+
+    async def get_user_bests(self, user_id: int, **kwargs: Any) -> list[Score]:
+        r"""Get a user's top scores.
+
+        :param user_id: User ID to search by
+        :type user_id: int
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *limit* (``int``) --
+                Optional, number of scores to get. Min: 1, Max: 100, defaults to 100
+            * *include_fails* (``bool``) --
+                Optional, whether to include failed scores, defaults to ``False``
+            * *offset* (``int``) --
+                Optional, page offset to start from, defaults to 0
+
+        :raises APIException: Contains status code and error message
+        :return: List of requested scores
+        :rtype: list[aiosu.classes.score.Score]
+        """
+        return await self.__get_type_scores(user_id, "best", **kwargs)
+
+    async def get_user_firsts(self, user_id: int, **kwargs: Any) -> list[Score]:
+        r"""Get a user's first place scores.
+
+        :param user_id: User ID to search by
+        :type user_id: int
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *limit* (``int``) --
+                Optional, number of scores to get. Min: 1, Max: 100, defaults to 100
+            * *include_fails* (``bool``) --
+                Optional, whether to include failed scores, defaults to ``False``
+            * *offset* (``int``) --
+                Optional, page offset to start from, defaults to 0
+
+        :raises APIException: Contains status code and error message
+        :return: List of requested scores
+        :rtype: list[aiosu.classes.score.Score]
+        """
+        return await self.__get_type_scores(user_id, "firsts", **kwargs)
+
+    @rate_limited
+    @check_token
+    async def get_user_beatmap_scores(
+        self, user_id: int, beatmap_id: int, **kwargs: Any
+    ) -> list[Score]:
+        r"""Get a user's scores on a specific beatmap.
+
+        :param user_id: User ID to search by
+        :type user_id: int
+        :param beatmap_id: Beatmap ID to search by
+        :type beatmap_id: int
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+
+        :raises APIException: Contains status code and error message
+        :return: List of requested scores
+        :rtype: list[aiosu.classes.score.Score]
+        """
+        url = f"{self.base_url}/beatmaps/{beatmap_id}/scores/users/{user_id}/all"
+        params = {}
+        if "mode" in kwargs:
+            mode = Gamemode(kwargs.pop("mode"))  # type: ignore
+            params["mode"] = str(mode)
+        async with self.__session.get(url) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return helpers.from_list(Score.parse_obj, json.get("scores", []))
+
+    @rate_limited
+    @check_token
+    async def get_beatmap_scores(self, beatmap_id: int, **kwargs: Any) -> list[Score]:
+        r"""Get scores submitted on a specific beatmap.
+
+        :param beatmap_id: Beatmap ID to search by
+        :type beatmap_id: int
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *mods* (``aiosu.classes.mods.Mods``) --
+                Optional, mods to search for
+            * *type* (``str``) --
+                Optional, beatmap score ranking type
+
+        :raises APIException: Contains status code and error message
+        :return: List of requested scores
+        :rtype: list[aiosu.classes.score.Score]
+        """
+        url = f"{self.base_url}/beatmaps/{beatmap_id}/scores"
+        params = {}
+        if "mode" in kwargs:
+            mode = Gamemode(kwargs.pop("mode"))  # type: ignore
+            params["mode"] = str(mode)
+        if "mods" in kwargs:
+            mods = Mods(kwargs.pop("mods"))
+            params["mode"] = str(mods)
+        if "type" in kwargs:
+            params["type"] = kwargs.pop("type")
+        async with self.__session.get(url) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return helpers.from_list(Score.parse_obj, json.get("scores", []))
+
+    @rate_limited
+    @check_token
+    async def get_beatmap(self, beatmap_id: int) -> Beatmap:
+        r"""Get beatmap data.
+
+        :param beatmap_id: The ID of the beatmap
+        :type beatmap_id: int
+        :raises APIException: Contains status code and error message
+        :return: Beatmap data object
+        :rtype: aiosu.classes.beatmap.Beatmap
+        """
+        url = f"{self.base_url}/beatmaps/{beatmap_id}"
+        async with self.__session.get(url) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return Beatmap.parse_obj(json)
+
+    @rate_limited
+    @check_token
+    async def get_beatmap_attributes(
+        self, beatmap_id: int, **kwargs: Any
+    ) -> BeatmapDifficultyAttributes:
+        r"""Gets difficulty attributes for a beatmap.
+
+        :param beatmap_id: The ID of the beatmap
+        :type beatmap_id: int
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *mode* (``aiosu.classes.gamemode.Gamemode``) --
+                Optional, gamemode to search for
+            * *mods* (``aiosu.classes.mods.Mods``) --
+                Optional, mods to apply to the result
+
+        :raises APIException: Contains status code and error message
+        :return: Difficulty attributes for a beatmap
+        :rtype: aiosu.classes.beatmap.BeatmapDifficultyAttributes
+        """
+        url = f"{self.base_url}/beatmaps/{beatmap_id}/attributes"
+        params: dict[str, Any] = {}
+        if "mode" in kwargs:
+            mode = Gamemode(kwargs.pop("mode"))  # type: ignore
+            params["ruleset_id"] = int(mode)
+        if "mods" in kwargs:
+            mods = Mods(kwargs.pop("mods"))
+            params["mods"] = str(mods)
+        async with self.__session.post(url, data=params) as resp:
+            json = await resp.json()
+            if resp.status != 200:
+                raise APIException(resp.status, json.get("error", ""))
+            return BeatmapDifficultyAttributes.parse_obj(json.get("attributes"))
---
+++ packages/aiosu/aiosu-0.0.1-py3-none-any.whl/aiosu/v2/clientstorage.py
@@ -0,0 +1,122 @@
+"""
+This module handles multiple API v2 Client sessions.
+"""
+from __future__ import annotations
+
+from types import TracebackType
+from typing import Any
+from typing import Optional
+from typing import Type
+from typing import Union
+
+from . import Client
+from ..classes import OAuthToken
+
+
+class ClientStorage:
+    r"""OAuth sessions manager.
+
+    :param \**kwargs:
+        See below
+
+    :Keyword Arguments:
+        * *client_secret* (``str``)
+        * *client_id* (``int``)
+        * *base_url* (``str``) --
+            Optional, base API URL, defaults to \"https://osu.ppy.sh/api/v2/\"
+        * *create_app_client* (``bool``) --
+            Optional, whether to automatically create guest clients, defaults to False
+        * *app_client* (``str``) --
+            Optional, guest client, defaults to None
+    """
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.client_secret: str = kwargs.pop("client_secret", None)
+        self.client_id: int = kwargs.pop("client_id", None)
+        self.base_url: str = kwargs.pop("base_url", "https://osu.ppy.sh/api/v2")
+        self.__create_app_client: bool = kwargs.pop("create_app_client", False)
+        self.__app_client: Client = kwargs.pop("app_client", None)
+        self.clients: dict[int, Client] = {}
+
+    async def __aenter__(self) -> ClientStorage:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        await self.close()
+
+    @property
+    async def app_client(self) -> Client:
+        r"""Client credentials guest client.
+
+        :raises NotImplementedError: If no app client is provided and creation is disabled
+        :return: Client credentials guest client session
+        :rtype: aiosu.v1.client.Client
+        """
+        if self.__app_client is None:
+            raise NotImplementedError("Client credential grant creation is still WIP")
+
+        return self.__app_client
+
+    def _get_client_args(self) -> dict[str, Union[str, int]]:
+        return {
+            "client_secret": self.client_secret,
+            "client_id": self.client_id,
+            "base_url": self.base_url,
+        }
+
+    def client_exists(self, client_uid: int) -> bool:
+        r"""Checks if a client exists.
+
+        :param client_uid: The owner user ID of the client
+        :type client_uid: int
+        :return: Whether the client with the given ID exists
+        :rtype: bool
+        """
+        return client_uid in self.clients
+
+    async def add_client(self, token: OAuthToken) -> Client:
+        r"""Adds a client to storage.
+
+        :param token: Token object for the client
+        :type token: aiosu.classes.token.OAuthToken
+        :return: The added client
+        :rtype: aiosu.v1.client.Client
+        """
+        client = Client(token=token, **self._get_client_args())
+        client_user = await client.get_me()
+        self.clients[client_user.id] = client
+        return client
+
+    async def get_client(self, **kwargs: Any) -> Client:
+        r"""Gets a client from storage.
+
+        :param \**kwargs:
+            See below
+
+        :Keyword Arguments:
+            * *id* (``int``) --
+                Optional, whether to automatically create guest clients, defaults False
+            * *token* (``aiosu.classes.token.OAuthToken``) --
+                Optional, token of client to add, defaults to None
+
+        :raises ValueError: If neither id nor token are specified
+        :return: The requested client
+        :rtype: aiosu.v1.client.Client
+        """
+        client_uid: int = kwargs.pop("id", None)
+        token: OAuthToken = kwargs.pop("token", None)
+        if self.client_exists(client_uid):
+            return self.clients[client_uid]
+        if token is not None:
+            return await self.add_client(token)
+        raise ValueError("Either id or token must be specified.")
+
+    async def close(self) -> None:
+        r"""Closes all client sessions."""
+        for client in self.clients.values():
+            await client.close()
