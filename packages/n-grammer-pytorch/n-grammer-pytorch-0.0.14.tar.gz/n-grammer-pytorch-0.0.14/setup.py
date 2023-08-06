580a52eade95d45b58a81308d654cc0880899f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:46 +0000

    Add package n-grammer-pytorch/0.0.14/n-grammer-pytorch-0.0.14.tar.gz

---
+++ packages/n-grammer-pytorch/n-grammer-pytorch-0.0.14.tar.gz/n-grammer-pytorch-0.0.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'n-grammer-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.14',
+  license='MIT',
+  description = 'N-Grammer - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/n-grammer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers',
+    'n-grams',
+    'memory'
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'sympy',
+    'torch>=1.6'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
