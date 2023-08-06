da9f87dbaf85d22d3070b8d8518c9e5a1c225acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:59 +0000

    Add package milkman-py/0.0.6/milkman_py-0.0.6.tar.gz

---
+++ packages/milkman-py/milkman_py-0.0.6.tar.gz/milkman_py-0.0.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['milkman_py']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['eth_abi==2.2.0']
+
+setup_kwargs = {
+    'name': 'milkman-py',
+    'version': '0.0.6',
+    'description': '',
+    'long_description': '',
+    'author': 'charlesndalton',
+    'author_email': 'charles.n.dalton@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
