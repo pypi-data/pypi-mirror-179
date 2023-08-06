dce92f66a346c869fa55281ad18e7772daf0a24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:16 +0000

    Add package maxmods/0.1.2/maxmods-0.1.2.tar.gz

---
+++ packages/maxmods/maxmods-0.1.2.tar.gz/maxmods-0.1.2/tests/auth_speed_test.py
@@ -0,0 +1,35 @@
+from maxmods.primitives.randomfuncs import Timer
+from maxmods.auth import AuthSesh as ash
+from maxmods.imports.authimports import *
+
+t = Timer()
+def main():
+    with ash('https://127.0.0.1:5678/') as server:
+        server.set_vals('max', 'max')
+        server.login()
+        server.save('sdfsfg/sdfg/dfgdfg/dfgdsdgjdguomfbxgh/cgyicxvsZEF', {'URMOM':'test'})
+        print(server.load())
+        server.delete('sdfsfg/sdfg/dfgdfg/dfgdsdgjdguomfbxgh')
+        print(server.load())
+        server.remove()
+        server.set_vals('max1', 'max1')
+        server.login()
+        server.save('sdfsfg/sdfg/dfgdfg/dfgdsdgjdguomfbxgh/cgyicxvsZEF', {'URMOM':'test'})
+        print(server.load())
+        server.delete('sdfsfg/sdfg/dfgdfg/dfgdsdgjdguomfbxgh')
+        print(server.load())
+        server.set_vals('max', 'max')
+        server.signup()
+        server.login()
+        print(server.load())
+        server.set_vals('max1', 'max1')
+        server.login()
+        print(server.load())
+        
+t.run_time(main)
+print(t.message)        
+
+# with ascm('https://127.0.0.1:5678/') as f:
+#     f.set_vals('max1', 'max1')
+#     f.signup()
+#     f.login()
