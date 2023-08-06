b953d05ce29e55e6944c6a2c07f45e9b6f940345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:01 +0000

    Add package great-expectations/0.15.35/great_expectations-0.15.35-py3-none-any.whl

---
+++ packages/great-expectations/great_expectations-0.15.35-py3-none-any.whl/great_expectations/expectations/core/expect_column_values_to_match_like_pattern_list.py
@@ -0,0 +1,88 @@
+from typing import Optional
+
+from great_expectations.core import (
+    ExpectationConfiguration,
+    ExpectationValidationResult,
+)
+from great_expectations.expectations.expectation import (
+    ColumnMapExpectation,
+    InvalidExpectationConfigurationError,
+    render_evaluation_parameter_string,
+)
+from great_expectations.render import LegacyRendererType
+from great_expectations.render.renderer.renderer import renderer
+from great_expectations.render.util import substitute_none_for_missing
+
+
+class ExpectColumnValuesToMatchLikePatternList(ColumnMapExpectation):
+    """Expect the column entries to be strings that match any of a provided list of like pattern expressions."""
+
+    library_metadata = {
+        "maturity": "production",
+        "tags": ["core expectation", "column map expectation"],
+        "contributors": [
+            "@great_expectations",
+        ],
+        "requirements": [],
+        "has_full_test_suite": True,
+        "manually_reviewed_code": True,
+    }
+
+    map_metric = "column_values.match_like_pattern_list"
+    success_keys = ("mostly", "like_pattern_list", "match_on")
+    default_kwarg_values = {
+        "like_pattern_list": None,
+        "match_on": "any",
+        "row_condition": None,
+        "condition_parser": None,  # we expect this to be explicitly set whenever a row_condition is passed
+        "mostly": 1,
+        "result_format": "BASIC",
+        "include_config": True,
+        "catch_exceptions": True,
+    }
+    args_keys = (
+        "column",
+        "like_pattern_list",
+    )
+
+    def validate_configuration(
+        self, configuration: Optional[ExpectationConfiguration]
+    ) -> None:
+        super().validate_configuration(configuration)
+        try:
+            assert (
+                "like_pattern_list" in configuration.kwargs
+            ), "Must provide like_pattern_list"
+            assert isinstance(
+                configuration.kwargs.get("like_pattern_list"), (list, dict)
+            ), "like_pattern_list must be a list"
+            assert isinstance(configuration.kwargs.get("like_pattern_list"), dict) or (
+                len(configuration.kwargs.get("like_pattern_list")) > 0
+            ), "At least one like_pattern must be supplied in the like_pattern_list."
+            if isinstance(configuration.kwargs.get("like_pattern_list"), dict):
+                assert "$PARAMETER" in configuration.kwargs.get(
+                    "like_pattern_list"
+                ), 'Evaluation Parameter dict for like_pattern_list kwarg must have "$PARAMETER" key.'
+
+        except AssertionError as e:
+            raise InvalidExpectationConfigurationError(str(e))
+
+    @classmethod
+    @renderer(renderer_type=LegacyRendererType.PRESCRIPTIVE)
+    @render_evaluation_parameter_string
+    def _prescriptive_renderer(
+        cls,
+        configuration: Optional[ExpectationConfiguration] = None,
+        result: Optional[ExpectationValidationResult] = None,
+        runtime_configuration: Optional[dict] = None,
+        **kwargs
+    ) -> None:
+        runtime_configuration = runtime_configuration or {}
+        include_column_name = (
+            False if runtime_configuration.get("include_column_name") is False else True
+        )
+        styling = runtime_configuration.get("styling")
+        params = substitute_none_for_missing(
+            configuration.kwargs,
+            ["column", "mostly", "row_condition", "condition_parser"],
+        )
