96d2a53da62915943b3d55f1b061dabbd68abf83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:40 +0000

    Add package bbcinds/0.0.3.2/bbcinds-0.0.3.2.tar.gz

---
+++ packages/bbcinds/bbcinds-0.0.3.2.tar.gz/bbcinds-0.0.3.2/bbcinds/src/bbcinds.py
@@ -0,0 +1,19 @@
+'''
+commands 
+python setup.py sdist bdist_wheel
+python -m twine upload --repository pypi dist/*
+'''
+
+def update():
+    """
+    Parameters
+    ----------
+    name : str
+        The name of the animal
+    sound : str
+        The sound the animal makes
+    num_legs : int, optional
+        The number of legs the animal (default is 4)
+    """
+
+	print('Hi There!')
