f19d0844f39057c480f6ab3bd4c775dc517fe699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:51 +0000

    Add package xplt/0.2.0/xplt-0.2.0-py3-none-any.whl

---
+++ packages/xplt/xplt-0.2.0-py3-none-any.whl/xplt/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Philipp Niedermayer"
+__contact__ = "eltos@outlook.de"
+
+
+__version__ = "0.2.0"
+
+
+from .colors import *
+from .line import *
+from .twiss import *
+from .phasespace import *
+from .timestructure import *
+
+
+from . import hooks
+
+try:
+    hooks.register_matplotlib_options()
+except:
+    pass
+
+try:
+    hooks.register_pint_options()
+except:
+    pass
