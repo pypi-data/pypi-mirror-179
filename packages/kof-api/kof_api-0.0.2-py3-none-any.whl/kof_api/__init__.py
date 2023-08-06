7c16e6b6d55c81015d3b5f7962e3a87fd6d419f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:23 +0000

    Add package kof-api/0.0.2/kof_api-0.0.2-py3-none-any.whl

---
+++ packages/kof-api/kof_api-0.0.2-py3-none-any.whl/kof_api/__init__.py
@@ -0,0 +1,48 @@
+## Default Classes ##
+from kof_api.game_objects.Animal import Animal
+from kof_api.game_objects.Building import Building
+from kof_api.game_objects.City import City
+from kof_api.game_objects.Item import Item
+from kof_api.game_objects.NPC import NPC
+from kof_api.game_objects.Player import Player
+from kof_api.game_objects.Region import Region
+from kof_api.game_objects.Ship import Ship
+
+## Utils ##
+from kof_api.game_objects.Utils.Math import Math
+from kof_api.game_objects.Utils.Country_City_Names import CCN
+
+
+## Default Armour Classes ##
+from kof_api.game_objects.Items.Helmet import Helmet
+from kof_api.game_objects.Items.Chestplate import Chestplate
+from kof_api.game_objects.Items.Shield import Shield
+
+## NPC's ##
+from kof_api.game_objects.NPCS.ArmourMerchant import ArmourMerchant
+
+## Ship's ##
+from kof_api.game_objects.Ships.Sloop import Sloop
+
+## Animals ##
+from kof_api.game_objects.Animals.Donkey import Donkey
+
+## Helmets ##
+from kof_api.game_objects.Items.Helmets.Kettle_Hat import KettleHat
+from kof_api.game_objects.Items.Helmets.Spangenhelm import Spangenhelm
+
+## Chestplates ##
+from kof_api.game_objects.Items.Chestplates.BronzeLamellar import BronzeLamellar
+from kof_api.game_objects.Items.Chestplates.Hauberk import Hauberk
+from kof_api.game_objects.Items.Chestplates.IronLamellar import IronLamellar
+from kof_api.game_objects.Items.Chestplates.Jazerant import Jazerant
+from kof_api.game_objects.Items.Chestplates.Kiritsuke import Kiritsuke
+from kof_api.game_objects.Items.Chestplates.LeatherLamellar import LeatherLamellar
+from kof_api.game_objects.Items.Chestplates.LoricaHamata import LoricaHamata
+from kof_api.game_objects.Items.Chestplates.WoolGambeson import WoolGambeson
+
+
+## Shields ##
+
+from kof_api.game_objects.Items.Shields.LegionaryScutum import LegionaryScutum
+from kof_api.game_objects.Items.Shields.AuxiliaryScutum import AuxiliaryScutum
