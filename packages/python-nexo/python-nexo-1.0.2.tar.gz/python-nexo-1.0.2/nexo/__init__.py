646b906ea338942b827ad120ff84a267d5d3c0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:30 +0000

    Add package python-nexo/1.0.2/python-nexo-1.0.2.tar.gz

---
+++ packages/python-nexo/python-nexo-1.0.2.tar.gz/python-nexo-1.0.2/nexo/__init__.py
@@ -0,0 +1,25 @@
+"""An unofficial Python wrapper for the Nexo Pro exchange API v1
+.. moduleauthor:: Erwin Lejeune
+"""
+
+__version__ = '1.0.2'
+
+from nexo.async_client import AsyncClient
+from nexo.client import Client
+from nexo.response_serializers import (
+    AdvancedOrderResponse,
+    Balances,
+    Orders,
+    Pairs,
+    Quote,
+    TradeHistory,
+    Transaction,
+    OrderResponse,
+    OrderDetails,
+)
+from nexo.exceptions import (
+    NexoAPIException,
+    NotImplementedException,
+    NexoRequestException,
+    NEXO_API_ERROR_CODES,
+)
