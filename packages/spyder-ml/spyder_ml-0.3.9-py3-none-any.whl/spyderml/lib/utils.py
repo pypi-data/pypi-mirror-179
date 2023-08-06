888cbd0c4df90b719aa324d4bfe604b01de0d8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:50 +0000

    Add package spyder-ml/0.3.9/spyder_ml-0.3.9-py3-none-any.whl

---
+++ packages/spyder-ml/spyder_ml-0.3.9-py3-none-any.whl/spyderml/lib/utils.py
@@ -0,0 +1,67 @@
+import requests
+import urllib3
+import logging
+
+from bs4 import BeautifulSoup as Bs
+from bs4 import Comment
+
+from spyderml.lib.file import save_output
+
+
+def treat_objects(objects: str):
+    if "," in objects:
+        objects = objects.split(',')
+        return objects
+    return objects
+
+
+def spyder_request(target):
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    try:
+        r = requests.get(target)
+        return r
+    except requests.exceptions.SSLError:
+        print(f"{target}:\nErro de ssl")
+        exit()
+    except requests.exceptions.MissingSchema:
+        print(f"{target}:\nInvalid url\n http://?")
+        exit()
+    except requests.exceptions.InvalidSchema:
+        print(f"{target} ERROR")
+        exit()
+    except Exception as e:
+        logging.exception(e)
+        exit()
+
+
+def soup_tags(document, object, file=None):
+    html = Bs(document.content, 'html.parser')
+    results = html.find_all(object)
+    for result in results:
+        if file is not None:
+            save_output(filename=file, text=result)
+        print(result)
+
+
+def soup_comments(document, file=None):
+    html = Bs(document.content, 'html.parser')
+    comments = html.find_all(string=lambda text: isinstance(text, Comment))
+    for comment in comments:
+        if file is not None:
+            save_output(filename=file, text=comment)
+        print(comment)
+
+
+def soup_attrs(document, object, file=None):
+    html = Bs(document.content, 'html.parser')
+    if type(object) == list:
+        for o in object:
+            for attribute in html.select(f"[{o}]"):
+                if file is not None:
+                    save_output(filename=file, text=attribute)
+                print(attribute)
+    else:
+        for attribute in html.select(f"[{object}]"):
+            if file is not None:
+                save_output(filename=file, text=attribute)
+            print(attribute)
---
+++ packages/spyder-ml/spyder_ml-0.3.9-py3-none-any.whl/spyderml/main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from spyderml.lib.asciiarts import banner
+from spyderml.lib.utils import spyder_request, treat_objects, soup_tags, soup_comments, soup_attrs
+from spyderml.lib.file import open_file
+
+
+class SpyderHtml:
+    def __init__(self, args):
+        self.args = args
+
+    def run(self):
+        print(banner())
+
+        if self.args['file']:
+            targets = open_file(filepath=self.args['file'])
+            htmls = []
+            for target in targets:
+                htmls.append(spyder_request(target))
+                try:
+                    for html in htmls:
+                        if self.args['tags']:
+                            tags = treat_objects(objects=self.args['tags'])
+                            soup_tags(document=html, object=tags, file=self.args['output'])
+                        elif self.args['comments']:
+                            soup_comments(document=html, file=self.args['output'])
+                        else:
+                            attr = treat_objects(objects=self.args['attribs'])
+                            soup_attrs(document=html, object=attr, file=self.args['output'])
+
+                except AttributeError:
+                    pass
+
+        else:
+            text = spyder_request(target=self.args['target'])
+            if self.args['tags']:
+                tags = treat_objects(objects=self.args['tags'])
+                soup_tags(document=text, object=tags, file=self.args['output'])
+            elif self.args['comments']:
+                soup_comments(document=text, file=self.args['output'])
+            else:
+                attr = treat_objects(objects=self.args['attribs'])
+                soup_attrs(document=text, object=attr, file=self.args['output'])
+
+
+def main():
+    parser = argparse.ArgumentParser(description="A tool made to facilitate the analysis of html code.")
+
+    group_target = parser.add_mutually_exclusive_group()
+
+    group_action = parser.add_mutually_exclusive_group()
+
+    group_target.add_argument('-t', '--target', type=str, help="Parameter that defines the target URL "
+                                                               "http://example.com/index.html")
+    group_target.add_argument('-f', '--file', type=str, help="Parameter that defines target URLs")
+
+    group_action.add_argument('--tags', type=str, help="Flag that defines which tags the program will bring")
+
+    group_action.add_argument('--comments', help="Flag that brings the comments", default=False, action='store_true', )
+
+    group_action.add_argument('--attribs', type=str,
+                              help="Flag that defines which attributes the application will look for.")
+
+    parser.add_argument('-o', '--output', type=str, help="Flag that defines in which file the command output will be "
+                                                         "saved.")
+
+    args = vars(parser.parse_args())
+
+    if not any(args.values()):
+        parser.error('No arguments provided.')
+
+    sp = SpyderHtml(args)
+    sp.run()
+
+
+if __name__ == '__main__':
+    main()
