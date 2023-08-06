2b0aab5a1babc88d83e2e2a4b2842141864a426d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package widgetastic.patternfly4/1.2.1/widgetastic.patternfly4-1.2.1-py3-none-any.whl

---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-1.2.1-py3-none-any.whl/widgetastic_patternfly4/piechart.py
@@ -0,0 +1,10 @@
+from widgetastic_patternfly4 import BulletChart
+
+
+class PieChart(BulletChart):
+    """
+    Represents the Pie Chart
+    from Patternfly 4 (https://www.patternfly.org/v4/documentation/react/charts/chartpie)
+    """
+
+    APPLY_OFFSET = False
