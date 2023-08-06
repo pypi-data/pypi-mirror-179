6eebaf2d49a1a6ce286a2cb26127c3b5fd2d7410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:22 +0000

    Add package pylertatron/1.0.2/pylertatron-1.0.2.tar.gz

---
+++ packages/pylertatron/pylertatron-1.0.2.tar.gz/pylertatron-1.0.2/pylertatron/sync/pylertatron.py
@@ -0,0 +1,19 @@
+from httpx import Client
+
+from ..Pylertatron import Pylertatron
+from ..commands import Command
+
+
+class SyncPylertatron(Pylertatron):
+    def __init__(self, webhook_url, balance_ratio, api_key_name, client):
+        super().__init__(webhook_url, balance_ratio, api_key_name)
+        self.client: Client = client
+
+    def send_alert(self, symbol: str, commands: list[Command], tags: list[str] = None):
+        alert = self.generate_alert(symbol, commands, tags)
+        self.client.post(self.webhook_url, data=alert)
+
+
+def create_pylertatron(webhook_url, balance_ratio, api_key_name) -> Pylertatron:
+    client = Client()
+    return SyncPylertatron(webhook_url, balance_ratio, api_key_name, client=client)
