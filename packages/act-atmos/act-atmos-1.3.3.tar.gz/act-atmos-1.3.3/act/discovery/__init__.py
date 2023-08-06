bc1a7cd6a7293e6685b9b021ef3c32e1f25501ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:04 +0000

    Add package act-atmos/1.3.3/act-atmos-1.3.3.tar.gz

---
+++ packages/act-atmos/act-atmos-1.3.3.tar.gz/act-atmos-1.3.3/act/discovery/__init__.py
@@ -0,0 +1,19 @@
+"""
+This module contains procedures for exploring and downloading data
+from a variety of web services
+
+"""
+
+import lazy_loader as lazy
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submodules=['get_armfiles', 'get_cropscape', 'get_airnow', 'get_noaa_psl'],
+    submod_attrs={
+        'get_armfiles': ['download_data'],
+        'get_asos': ['get_asos'],
+        'get_airnow': ['get_airnow_bounded_obs', 'get_airnow_obs', 'get_airnow_forecast'],
+        'get_cropscape': ['croptype'],
+        'get_noaapsl': ['download_noaa_psl_data'],
+    },
+)
