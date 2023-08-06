95e4aed2b47f989963c0c59381998235f9133f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:22 +0000

    Add package aws-solutions-constructs.aws-alb-fargate/1.177.0/aws-solutions-constructs.aws-alb-fargate-1.177.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-alb-fargate/aws-solutions-constructs.aws-alb-fargate-1.177.0.tar.gz/aws-solutions-constructs.aws-alb-fargate-1.177.0/setup.py
@@ -0,0 +1,72 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-solutions-constructs.aws-alb-fargate",
+    "version": "1.177.0",
+    "description": "CDK Constructs for Application Load Balancer to AWS Fargate integration",
+    "license": "Apache-2.0",
+    "url": "https://github.com/awslabs/aws-solutions-constructs.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-solutions-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_solutions_constructs.aws_alb_fargate",
+        "aws_solutions_constructs.aws_alb_fargate._jsii"
+    ],
+    "package_data": {
+        "aws_solutions_constructs.aws_alb_fargate._jsii": [
+            "aws-alb-fargate@1.177.0.jsii.tgz"
+        ],
+        "aws_solutions_constructs.aws_alb_fargate": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "aws-cdk.aws-certificatemanager==1.177.0",
+        "aws-cdk.aws-ec2==1.177.0",
+        "aws-cdk.aws-ecr==1.177.0",
+        "aws-cdk.aws-ecs==1.177.0",
+        "aws-cdk.aws-elasticloadbalancingv2==1.177.0",
+        "aws-cdk.aws-iam==1.177.0",
+        "aws-cdk.aws-s3==1.177.0",
+        "aws-cdk.core==1.177.0",
+        "aws-solutions-constructs.core==1.177.0",
+        "constructs>=3.2.0, <4.0.0",
+        "jsii>=1.71.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
