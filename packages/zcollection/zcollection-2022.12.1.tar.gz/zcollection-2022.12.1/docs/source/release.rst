ace2a4e72f57a6e90d8080eadd925b686f0e30e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:40 +0000

    Add package zcollection/2022.12.1/zcollection-2022.12.1.tar.gz

---
+++ packages/zcollection/zcollection-2022.12.1.tar.gz/zcollection-2022.12.1/docs/source/release.rst
@@ -0,0 +1,44 @@
+Release notes
+=============
+
+2022.10.2/2022.10.1
+-------------------
+
+Release on October 13, 20212
+
+* Add compatibility with Python 3.8.
+
+2022.10.0
+---------
+
+Release on October 7, 20212
+
+* Added an option to the method ``drop_partitions`` to drop partitions
+  older than a specified time delta relative to the current time.
+
+2022.8.0
+--------
+
+Release on August 14, 2022
+
+* Support Python starting 3.9.
+* Refactor convenience functions.
+* Refactor dataset & variables modules.
+* The indexer can return only the partition keys.
+* Optimization of dataset handling.
+* Bug fixes.
+
+0.2 / 2020-04-04
+----------------
+
+Release on April 4, 2020
+
+* Installation from PyPi.
+* Unsigned integers are not handled.
+
+0.1 / 2022-08-30
+-----------------
+
+Release on March 30, 2020
+
+* First public version.
---
+++ packages/zcollection/zcollection-2022.12.1.tar.gz/zcollection-2022.12.1/examples/ex_view.py
@@ -0,0 +1,158 @@
+"""
+Overview of a View.
+===================
+
+This section outlines the steps required to get started with the main features
+of a ``View``.
+"""
+import pprint
+
+import dask.distributed
+import fsspec
+
+import zcollection
+import zcollection.tests.data
+
+
+# %%
+# Initialization of the environment
+# ---------------------------------
+#
+# As in the example of handling
+# :ref:`collections <sphx_glr_auto_examples_ex_collection.py>`, we will create
+# the test environment and a collection.
+def create_dataset():
+    """Create a dataset to record."""
+    generator = zcollection.tests.data.create_test_dataset_with_fillvalue()
+    return next(generator)
+
+
+cluster = dask.distributed.LocalCluster(processes=False)
+client = dask.distributed.Client(cluster)
+
+ds = create_dataset()
+fs = fsspec.filesystem('memory')
+collection = zcollection.create_collection('time',
+                                           ds,
+                                           zcollection.partitioning.Date(
+                                               ('time', ), resolution='M'),
+                                           '/view_reference',
+                                           filesystem=fs)
+collection.insert(ds, merge_callable=zcollection.merging.merge_time_series)
+
+# %%
+# Creation of views
+# -----------------
+#
+# A :py:class:`view<zcollection.view.View>` allows you to extend a collection
+# (:py:class:`a view reference<zcollection.view.ViewReference>`) that you are
+# not allowed to modify.
+view = zcollection.create_view('/my_view',
+                               zcollection.view.ViewReference(
+                                   '/view_reference', fs),
+                               filesystem=fs)
+
+# %%
+# .. note::
+#
+#     The created view can be accessed using the following command ::
+#
+#         >>> view = zcollection.open_view("/my_view", filesystem=fs)
+#
+# Editing variables
+# -----------------
+# When the view is created, it has no data of its own, it uses all the data
+# defined in the reference view.
+pprint.pprint(fs.listdir('/my_view'))
+
+# %%
+# It's not yet possible to read data from the view, as it does not yet have any
+# data. To minimize the risk of mismatches with the reference view, the data
+# present in the view drives the range of data that can be read.
+try:
+    view.load()
+except ValueError as err:
+    print(err)
+
+# %%
+# Such a state of the view is not very interesting. But it is possible to
+# :py:meth:`add<zcollection.view.View.add_variable>` and modify variables in
+# order to enhance the view.
+var3 = ds.metadata().variables['var2']
+var3.name = 'var3'
+view.add_variable(var3)
+
+# %%
+# This step creates all necessary partitions for the new variable.
+pprint.pprint(fs.listdir('/my_view/year=2000'))
+
+# %%
+# The new variable is not initialized.
+ds = view.load()
+assert ds is not None
+ds.variables['var3'].values
+
+# %%
+# The same principle used by the collection allows to
+# :py:meth:`update<zcollection.view.View.update>` the variables.
+view.update(
+    lambda ds: dict(var3=ds['var1'].values * 0 + 1))  # type: ignore[arg-type]
+
+# %%
+# Like the :py:meth:`update<zcollection.collection.Collection.update>` method
+# of the collection, the update method of view allows to selecting the
+# neighboring partitions with the keyword argument ``depth``.
+
+# %%
+ds = view.load()
+assert ds is not None
+var3 = ds['var3'].values
+var3
+
+# %%
+# **Warning**: The variables of the reference collection cannot be edited.
+try:
+    view.update(
+        lambda ds: dict(var2=ds['var2'].values * 0))  # type: ignore[arg-type]
+except ValueError as exc:
+    print(str(exc))
+
+# %%
+# Map a function over the view
+# ----------------------------
+# It's possible to map a function over the partitions of the view.
+for partition, array in view.map(lambda ds: (  # type: ignore[arg-type]
+        ds['var1'].values + ds['var2'].values)).compute():
+    print(f' * partition = {partition}: mean = {array.mean()}')
+
+# %%
+# .. seealso::
+#
+#     See the :py:meth:`map_overlap<zcollection.view.View.map_overlap>` method
+#     apply a function over the partitions of the view of selecting the
+#     neighboring partitions.
+#
+# Drop a variable
+# ----------------
+# A method allows you to
+# :py:meth:`drop_variable<zcollection.view.View.drop_variable>` variables from
+# the view.
+view.drop_variable('var3')
+try:
+    view.load()
+except ValueError as err:
+    # The view no longer has a variable.
+    print(err)
+
+# %%
+# **Warning**: The variables of the reference collection cannot be dropped.
+try:
+    view.drop_variable('var2')
+except ValueError as exc:
+    print(str(exc))
+
+# %%
+# Close the local cluster to avoid printing warning messages in the other
+# examples.
+client.close()
+cluster.close()
