4c538861f9aaff3623358ed024b3d8134480a65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:26 +0000

    Add package dm-cli/0.1.16/dm-cli-0.1.16.tar.gz

---
+++ packages/dm-cli/dm-cli-0.1.16.tar.gz/dm-cli-0.1.16/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read()
+
+setup(
+    name="dm-cli",
+    version="0.1.16",
+    author="",
+    author_email="",
+    license="MIT",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/equinor/dm-cli",
+    packages=["dm_cli"],
+    install_requires=[requirements],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        "dm_cli/bin/dm",
+    ],
+)
