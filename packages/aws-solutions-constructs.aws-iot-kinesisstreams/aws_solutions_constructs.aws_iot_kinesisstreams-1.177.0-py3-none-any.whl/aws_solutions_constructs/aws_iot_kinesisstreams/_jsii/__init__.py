b29ca7a10d75c16ca70749fb3fa104fe6721162c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:17 +0000

    Add package aws-solutions-constructs.aws-iot-kinesisstreams/1.177.0/aws_solutions_constructs.aws_iot_kinesisstreams-1.177.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-iot-kinesisstreams/aws_solutions_constructs.aws_iot_kinesisstreams-1.177.0-py3-none-any.whl/aws_solutions_constructs/aws_iot_kinesisstreams/_jsii/__init__.py
@@ -0,0 +1,32 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_iot._jsii
+import aws_cdk.aws_kinesis._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-iot-kinesisstreams",
+    "1.177.0",
+    __name__[0:-6],
+    "aws-iot-kinesisstreams@1.177.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
