e1ba9c02661aadc3ec0db59c9466cc7702ab72df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:50 +0000

    Add package BoxKit/2.7/BoxKit-2.7.tar.gz

---
+++ packages/BoxKit/BoxKit-2.7.tar.gz/BoxKit-2.7/boxkit/api/measure.py
@@ -0,0 +1,37 @@
+""" Module with implemenation of region methods"""
+
+from ..resources import stencils
+
+from ..library.utilities import Process
+
+from . import create
+
+
+@Process(stencils=[stencils.skimeasure])
+def bubbles(self, dataframes, lsetkey, **attributes):
+    """
+    Create a list of bubbles in a region
+
+    Parameters
+    ----------
+    dataframes : list of Dataset objects
+    lsetkey    : key containing level-set/binary data
+
+    Returns
+    -------
+    listbubbles : list of bubble properties
+    """
+
+    regionlist = []
+    bubblekey = "bubble"
+
+    for dataset in dataframes:
+        dataset.addvar(bubblekey)
+        regionlist.append(create.region(dataset, **attributes))
+
+    listbubbles = self.tasks["skimeasure"]["region"](regionlist, lsetkey, bubblekey)
+
+    for dataset in dataframes:
+        dataset.delvar(bubblekey)
+
+    return listbubbles
---
+++ packages/BoxKit/BoxKit-2.7.tar.gz/BoxKit-2.7/boxkit/cbox/include/cbox/utilities.h
@@ -0,0 +1,69 @@
+#ifndef CBOX_UTILITIES_H
+#define CBOX_UTILITIES_H
+
+#include <cbox/pytypes.hpp>
+
+#include <indicators/progress_bar.hpp>
+#include <indicators/progress_spinner.hpp>
+
+namespace pytypes = cbox::pytypes;
+//namespace indicators = indicators;
+/*
+ *
+ *
+ */
+namespace cbox::utilities {
+class Action : public pytypes::CPyObject {
+public:
+  // constructors
+  Action();
+  Action(PyObject *ptrAction);
+
+  // destructors
+  virtual ~Action();
+
+  // attributes
+  unsigned int nthreads = 1;
+  bool monitor = false;
+
+  PyObject *pyTarget = NULL;
+
+  // methods
+  PyObject *pyAction() { return this->getPyObject(); }
+};
+/*
+ */
+class Monitor {
+public:
+  // constructors
+  Monitor() {}
+  Monitor(const char *type);
+
+  // destructors
+  virtual ~Monitor() {}
+
+  // methods
+  void setlimit(int iterlimit);
+  void update(std::string msg = "", int progress = 0);
+  const char *gettype();
+
+private:
+  // private attributes
+  int max_progress = 0;
+  int progress = 0;
+  const char *type = "none";
+  indicators::ProgressBar *bar = new indicators::ProgressBar;
+  indicators::ProgressSpinner *spinner = new indicators::ProgressSpinner;
+};
+/*
+ */
+pytypes::CPyList execute_pyTask(Action &action, pytypes::CPyList &unitList,
+                                pytypes::CPyTuple &argsTuple);
+/*
+ */
+} // namespace cbox::utilities
+#endif
+/*
+ *
+ *
+ */
