8682a6d016031d9a1d14406bb910531a57877daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:28 +0000

    Add package mynameisWS/0.0.1/mynameisWS-0.0.1.tar.gz

---
+++ packages/mynameisWS/mynameisWS-0.0.1.tar.gz/mynameisWS-0.0.1/setup.py
@@ -0,0 +1,38 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'mynameisWS',      
+  packages = ['mynameisWS'], 
+  version = '0.0.1', 
+  license='MIT', 
+  description = 'What is WS',
+  long_description=DESCRIPTION,
+  author = 'Wise System',                 
+  author_email = 'test@gmail.com',     
+  url = 'https://github.com/PalmWS/',  
+  download_url = 'https://github.com/PalmWS/',  
+  keywords = ['WS', 'Wise System', 'ASCII ART'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
