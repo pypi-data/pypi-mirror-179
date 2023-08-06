9771b868df14a6701df7bbba4bfdb4697f57f8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:57 +0000

    Add package felipesilverioudacity/0.1/felipesilverioudacity-0.1.tar.gz

---
+++ packages/felipesilverioudacity/felipesilverioudacity-0.1.tar.gz/felipesilverioudacity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='felipesilverioudacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['felipesilverioudacity'],
+      author= 'Felipe Silverio',
+      author_email = 'felipe.dsrv@gmail.com',
+      zip_safe=False)
