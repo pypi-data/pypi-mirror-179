4d8c3f399f6ecfd93a3607de524fa468afecac59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:24 +0000

    Add package aoc-library/0.1.0/aoc-library-0.1.0.tar.gz

---
+++ packages/aoc-library/aoc-library-0.1.0.tar.gz/aoc-library-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name='aoc-library',
+    version='0.1.0',
+    description='Advent of Code Library',
+    keyword='aoc',
+    author='Liborsaf, Thr0nSK',
+    author_email='liborsaf@wetian.eu',
+    license='GPL-v3',
+    url='https://github.com/Liborsaf/AdventOfCodeLibrary/',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['requests'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    python_requires='>=3.11'
+)
