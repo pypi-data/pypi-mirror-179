25b346d5c61c03546d981a3062d3ba4e5dd9dc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:20 +0000

    Add package fiddler-client/1.5.1.dev2/fiddler_client-1.5.1.dev2-py3-none-any.whl

---
+++ packages/fiddler-client/fiddler_client-1.5.1.dev2-py3-none-any.whl/fiddler/v2/schema/baseline.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from fiddler.v2.schema.base import BaseDataSchema
+
+
+class CreateBaseline(BaseDataSchema):
+    id: Optional[int] = None
+    name: str
+    project_name: Optional[str]
+    organization_name: Optional[str]
+    type: Optional[str]
+    model_name: Optional[str]
+
+    dataset_name: Optional[str] = None
+    start_time: Optional[int] = None
+    end_time: Optional[int] = None
+    offset: Optional[int] = None
+    window_size: Optional[int] = None
+
+class ReadBaseline(BaseDataSchema):
+    id: Optional[int] = None
+    name: str
+    project_name: Optional[str]
+    organization_name: Optional[str]
+    type: Optional[str]
+
+    dataset_name: Optional[str] = None
+    start_time: Optional[int] = None
+    end_time: Optional[int] = None
+    offset: Optional[int] = None
+    window_size: Optional[int] = None
