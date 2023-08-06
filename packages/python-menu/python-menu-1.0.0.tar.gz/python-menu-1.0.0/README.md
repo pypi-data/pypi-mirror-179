7196e02fd48ff86ed702fc04447bf3fef9345591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:41 +0000

    Add package python-menu/1.0.0/python-menu-1.0.0.tar.gz

---
+++ packages/python-menu/python-menu-1.0.0.tar.gz/python-menu-1.0.0/README.md
@@ -0,0 +1,21 @@
+# Menus
+Easy CLI menu creation tool
+
+# Example
+```py
+>>> from menus import *
+>>>  menu1 = Menu(title="Menu1",options=["option1","option2","option3"], result="index"
+>>> menu1.show()
+[+] Menu1        [1] option1     [2] option2     [3] option3
+
+(Menu) > 1
+>>> print(menu1.value)
+1
+>>> menu1.update(title="Updated Menu1",options=["option1","option2","option3"], result="value")
+>>> menu1.show()
+[+] Updated Menu1        [1] option1     [2] option2     [3] option3
+
+(Menu) > 1
+>>> print(menu1.value)
+option1
+```
---
+++ packages/python-menu/python-menu-1.0.0.tar.gz/python-menu-1.0.0/menu/main.py
@@ -0,0 +1,81 @@
+# Adding in colourful text
+class Colour:
+    Black = "\u001b[30m"
+    Red = "\u001b[31m"
+    Green = "\u001b[32m"
+    Yellow = "\u001b[33m"
+    Blue = "\u001b[34m"
+    Magenta = "\u001b[35m"
+    White = "\u001b[37m"
+    Cyan = "\u001b[36m"
+    Reset = "\u001b[0m"
+    bracketsymbol = Blue
+    normaltext = Cyan
+    plussymbol = Red
+    lines = White
+    text = Yellow
+
+# Adding in colourful text
+class Colour:
+    Black = "\u001b[30m"
+    Red = "\u001b[31m"
+    Green = "\u001b[32m"
+    Yellow = "\u001b[33m"
+    Blue = "\u001b[34m"
+    Magenta = "\u001b[35m"
+    White = "\u001b[37m"
+    Cyan = "\u001b[36m"
+    Reset = "\u001b[0m"
+    bracketsymbol = Blue
+    normaltext = Cyan
+    plussymbol = Red
+    lines = White
+    text = Yellow
+
+class Menu():
+    def __init__(self, title="Menu",options=[],result='index'):
+        self.title = title
+        self.options = options
+        self.result = result
+
+    # Displays text in a nice colourful format
+    def display(self, text):
+        print(f"{Colour.bracketsymbol}[{Colour.plussymbol}+{Colour.bracketsymbol}] {Colour.Reset}{text}\n")
+
+    # Function to get input with formated and coloured chacters
+    def getinput(self, title, options, result):
+        while True:
+            try:
+                if not options:
+                    return "Error: No options found"
+
+                choice = int(input(f"({Colour.text}{title}{Colour.Reset}) > "))
+
+                if 0 < choice <= len(options):
+                    if result == 'value':
+                        return options[choice-1]
+                    else:
+                        return choice
+                else:
+                    self.display("Invalid Option")
+            except Exception:
+                self.display("Invalid Option")
+
+    def show(self):
+        print(f"{Colour.bracketsymbol}[{Colour.plussymbol}+{Colour.bracketsymbol}]{Colour.Yellow} {self.title}\t",end="")
+        if type(self.options) == type([]):
+            for index, option in enumerate(self.options):
+                print(f"{Colour.Yellow}[{index+1}] {option}\t",end="")
+            print(f"{Colour.Reset}\n")
+        elif type(self.options) == type(""):
+            print(f"{Colour.Yellow}[1] {self.options}{Colour.Reset}\n",end="")
+
+        self.value = self.getinput(self.title, self.options, self.result)
+
+    def update(self, title=None, options=None, result=None):
+        if title != None:
+            self.title = title
+        if options != None:
+            self.options = options
+        if result != None:
+            self.result = result
