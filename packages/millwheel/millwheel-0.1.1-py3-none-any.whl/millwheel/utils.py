986ded517603dff7fa0988c8d7f3bd6c08eadd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:19 +0000

    Add package millwheel/0.1.1/millwheel-0.1.1-py3-none-any.whl

---
+++ packages/millwheel/millwheel-0.1.1-py3-none-any.whl/millwheel/utils.py
@@ -0,0 +1,43 @@
+import logging
+from functools import wraps
+from subprocess import PIPE, STDOUT, Popen
+
+logger = logging.getLogger(__name__)
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def locked(lock):
+    def wrapper(function):
+        @wraps(function)
+        def inner(*a, **kw):
+            name = function.__qualname__
+            logger.info(f"{name:15s} - acquire lock")
+            lock.acquire()
+            logger.info(f"{name:15s} - acquired lock")
+            try:
+                return function(*a, **kw)
+            finally:
+                lock.release()
+                logger.info(f"{name:15s} - released lock")
+
+        return inner
+
+    return wrapper
+
+
+def syscaller(print):
+    def system(w):
+        print(w)
+        p = Popen(w, shell=True, stdout=PIPE, stderr=STDOUT, text=True)
+        for line in iter(p.stdout.readline, ""):
+            line = line.rstrip()
+            print(line)
+        p.wait()
+        p.stdout.close()
+        return p.returncode
+
+    return system
