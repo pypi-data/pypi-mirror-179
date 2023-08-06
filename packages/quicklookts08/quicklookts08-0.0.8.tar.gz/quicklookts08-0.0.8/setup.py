f6aa4bc23362895e5d0839a25390d01e46bc4602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:36 +0000

    Add package quicklookts08/0.0.8/quicklookts08-0.0.8.tar.gz

---
+++ packages/quicklookts08/quicklookts08-0.0.8.tar.gz/quicklookts08-0.0.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+# from distutils.core import setup
+import codecs
+import os
+
+
+VERSION = '0.0.8'
+DESCRIPTION = 'quicklookts08'
+
+# Setting up
+setup(
+    name="quicklookts08",
+    version=VERSION,
+    author="Hong Tang",
+    author_email="<stanghong@gmail.com>",
+    description=DESCRIPTION,
+    url = 'https://github.com/stanghong/pypi_publication', 
+   packages=find_packages('quicklookts08'),
+    install_requires=['numpy','pandas', 'matplotlib' ],
+    download_url = 'https://github.com/stanghong/pypi_publication/archive/refs/tags/v0.0.8.tar.gz',
+    
+    keywords=['python', 'timeseries','plot'],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
