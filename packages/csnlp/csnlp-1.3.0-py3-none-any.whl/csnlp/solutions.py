18d29f85c28bef48ae10f3f91a37c8216b9a0c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:44 +0000

    Add package csnlp/1.3.0/csnlp-1.3.0-py3-none-any.whl

---
+++ packages/csnlp/csnlp-1.3.0-py3-none-any.whl/csnlp/solutions.py
@@ -0,0 +1,133 @@
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Iterable, Union
+
+import casadi as cs
+import numpy as np
+from casadi.tools.structure3 import CasadiStructured, DMStruct
+
+
+@dataclass(frozen=True)
+class Solution:
+    """Class containing information on the solution of an NLP solver's run."""
+
+    f: float
+    vars: Union[CasadiStructured, Dict[str, cs.SX], Dict[str, cs.MX]]
+    vals: Union[DMStruct, Dict[str, cs.DM]]
+    stats: Dict[str, Any]
+    _get_value: Callable[[Union[cs.SX, cs.MX], bool], cs.DM]
+
+    @property
+    def status(self) -> str:
+        """Gets the status of the solver at this solution."""
+        return self.stats["return_status"]
+
+    @property
+    def success(self) -> bool:
+        """Gets whether the run was run successfully."""
+        return self.stats["success"]
+
+    @property
+    def barrier_parameter(self) -> float:
+        """Gets the IPOPT barrier parameter at the optimal solution"""
+        return self.stats["iterations"]["mu"][-1]
+
+    def value(
+        self, x: Union[cs.SX, cs.MX], eval: bool = True
+    ) -> Union[cs.SX, cs.MX, cs.DM]:
+        """Computes the value of the expression substituting the values of this
+        solution in the expression.
+
+        Parameters
+        ----------
+        x : Union[cs.SX, cs.MX]
+            The symbolic expression to be evaluated at the solution's values.
+        eval : bool, optional
+            Evaluates numerically the new expression. By default, `True`. See
+            `csnlp.solutions.subsevalf` for more details.
+
+        Returns
+        -------
+        cs.SX or MX or DM
+            The expression evaluated with the solution's values.
+
+        Raises
+        ------
+        RuntimeError
+            Raises if `eval=True` but there are symbolic variables that are
+            still free since they are outside the solution's variables.
+        """
+        return self._get_value(x, eval=eval)
+
+
+def subsevalf(
+    expr: Union[cs.SX, cs.MX, np.ndarray],
+    old: Union[
+        cs.SX,
+        cs.MX,
+        Dict[str, Union[cs.SX, cs.MX]],
+        Iterable[Union[cs.SX, cs.MX]],
+        CasadiStructured,
+    ],
+    new: Union[
+        cs.SX,
+        cs.MX,
+        Dict[str, Union[cs.SX, cs.MX]],
+        Iterable[Union[cs.SX, cs.MX]],
+        CasadiStructured,
+    ],
+    eval: bool = True,
+) -> Union[cs.SX, cs.DM, np.ndarray]:
+    """
+    Substitutes the old variables with the new ones in the symbolic expression,
+    and evaluates it, if required.
+
+    Parameters
+    ----------
+    expr : casadi.SX, MX or an array of these
+        Expression for substitution and, possibly, evaluation.
+    old : casadi.SX, MX (or struct, dict, iterable of)
+        Old variable to be substituted.
+    new : numpy.array or casadi.SX, MX, DM (or struct, dict, iterable of)
+        New variable that substitutes the old one. If a collection, it is
+        assumed the type is the same of `old` (so, old and new should share
+        collection type).
+    eval : bool, optional
+        Evaluates numerically the new expression. By default, `True`.
+
+    Returns
+    -------
+    new_expr : casadi.SX, MX, DM or an array of these
+        New expression after substitution (SX, MX) and, possibly, evaluation
+        (DM).
+
+    Raises
+    ------
+    TypeError
+        Raises if the `old` and `new` are neither SX, MX, dict, Iterable.
+    RuntimeError
+        Raises if `eval=True` but there are symbolic variables that are still
+        free, i.e., the expression cannot be evaluated numerically since it is
+        still (partially) symbolic.
+    """
+    if isinstance(expr, np.ndarray):
+        out = np.empty(expr.shape, dtype=object)
+        for i in np.ndindex(expr.shape):
+            out[i] = subsevalf(expr[i], old, new, eval=eval)
+        if eval:
+            out = out.astype(float)
+        return out
+
+    if isinstance(old, (cs.SX, cs.MX, CasadiStructured)):
+        expr = cs.substitute(expr, old, new)
+    elif isinstance(old, dict):
+        for name, o in old.items():
+            expr = cs.substitute(expr, o, new[name])
+    elif isinstance(old, Iterable):
+        for o, n in zip(old, new):
+            expr = cs.substitute(expr, o, n)
+    else:
+        raise TypeError(f"Invalid type {old.__class__.__name__} for old.")
+
+    if eval:
+        expr = cs.evalf(expr)
+    return expr
---
+++ packages/csnlp/csnlp-1.3.0-py3-none-any.whl/csnlp/util/funcs.py
@@ -0,0 +1,102 @@
+from functools import _lru_cache_wrapper, cached_property, wraps
+from typing import Callable, List, Optional, Tuple
+
+import numpy as np
+
+
+def invalidate_cache(*callables: Callable) -> Callable:
+    """Decorator that allows to enhance a function or method with the ability, when
+    called, to invalidate and clear the cached of some other target methods/properties.
+    This is especially useful to reset the cache of a given cached method/property when
+    another method makes changes to the underlying data, thus compromising the cached
+    results.
+
+    Note: the wrapper can invalidate other cached properties, but for doing so it
+    assumes the instance of the object (which the property to invalidate belongs to) is
+    the first argument of the wrapped method. For lru caches the issue does not subsist.
+
+    Parameters
+    ----------
+    callables : cached_property or lru cache wrapper
+        The cached properties or methods to be reset in this decorator.
+
+    Returns
+    -------
+    decorated_func : Callable
+        Returns the function wrapped with this decorator.
+
+    Raises
+    ------
+    TypeError
+        Raises if the given inputs are not instances of
+        `functools.cached_property` or `functools._lru_cache_wrapper`.
+    """
+    cached_properties: List[cached_property] = []
+    lru_caches: List[_lru_cache_wrapper] = []
+    for p in callables:
+        if isinstance(p, cached_property):
+            cached_properties.append(p)
+        elif hasattr(p, "cache_clear"):
+            lru_caches.append(p)
+        else:
+            raise TypeError(
+                "Expected cached properties or lru wrappers; got "
+                f"{p.__class__.__name__} instead."
+            )
+
+    def decorating_function(func):
+        def invalidate_cached_properties(self):
+            for prop in cached_properties:
+                n = prop.attrname
+                if n is not None and n in self.__dict__:
+                    del self.__dict__[n]
+
+        def invalidate_lru_caches():
+            for lru in lru_caches:
+                lru.cache_clear()
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if args:
+                invalidate_cached_properties(args[0])  # assume self is args[0]
+            invalidate_lru_caches()
+            return func(*args, **kwargs)
+
+        wrapper.cached_properties = cached_properties
+        wrapper.lru_caches = lru_caches
+        wrapper.invalidate_cached_properties = invalidate_cached_properties
+        wrapper.invalidate_lru_caches = invalidate_lru_caches
+        return wrapper
+
+    return decorating_function
+
+
+def np_random(seed: Optional[int] = None) -> Tuple[np.random.Generator, int]:
+    """Generates a random number generator from the seed and returns the Generator and
+    seed.
+
+    Full credit to OpenAI implementation at
+    https://github.com/openai/gym/blob/master/gym/utils/seeding.py.
+
+
+    Parameters
+    ----------
+    seed : int, optional
+        The seed used to create the generator.
+
+    Returns
+    -------
+    Tuple[Generator, int]
+        The generator and resulting seed.
+
+    Raises
+    ------
+    ValueError
+        Seed must be a non-negative integer or omitted.
+    """
+    if seed is not None and not (isinstance(seed, int) and seed >= 0):
+        raise ValueError(f"Seed must be a non-negative integer or omitted, not {seed}")
+    seed_seq = np.random.SeedSequence(seed)
+    np_seed = seed_seq.entropy
+    rng = np.random.Generator(np.random.PCG64(seed_seq))
+    return rng, np_seed
