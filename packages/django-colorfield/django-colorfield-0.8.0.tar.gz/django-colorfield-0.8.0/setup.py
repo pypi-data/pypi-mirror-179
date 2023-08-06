dd82b146910916faf8db39cfbc494b69c9a2c159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:36 +0000

    Add package django-colorfield/0.8.0/django-colorfield-0.8.0.tar.gz

---
+++ packages/django-colorfield/django-colorfield-0.8.0.tar.gz/django-colorfield-0.8.0/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+exec(open("colorfield/version.py").read())
+
+github_url = "https://github.com/fabiocaccamo"
+sponsor_url = "https://github.com/sponsors/fabiocaccamo/"
+twitter_url = "https://twitter.com/fabiocaccamo"
+package_name = "django-colorfield"
+package_url = "{}/{}".format(github_url, package_name)
+package_path = os.path.abspath(os.path.dirname(__file__))
+long_description_file_path = os.path.join(package_path, "README.md")
+long_description_content_type = "text/markdown"
+long_description = ""
+try:
+    with open(long_description_file_path, "r", encoding="utf-8") as f:
+        long_description = f.read()
+except IOError:
+    pass
+
+setup(
+    name=package_name,
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    include_package_data=True,
+    version=__version__,
+    description="simple color field for your models with a nice color-picker in the admin-interface.",
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    author="Jared Forsyth, Fabio Caccamo",
+    author_email="jared@jaredforsyth.com, fabio.caccamo@gmail.com",
+    url=package_url,
+    download_url="{}/archive/{}.tar.gz".format(package_url, __version__),
+    project_urls={
+        "Documentation": "{}#readme".format(package_url),
+        "Issues": "{}/issues".format(package_url),
+        "Funding": sponsor_url,
+        "Twitter": twitter_url,
+    },
+    keywords=[
+        "django",
+        "colorfield",
+        "colorpicker",
+        "color",
+        "field",
+        "picker",
+        "chooser",
+        "admin",
+        "python",
+    ],
+    requires=["django (>=2.2)"],
+    install_requires=["Pillow (>=9.0.0)"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    license="MIT",
+    test_suite="runtests.runtests",
+)
