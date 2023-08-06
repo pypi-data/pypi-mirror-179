a28fdc3bbf4fda11199944600a6eca3e5abaa649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:41 +0000

    Add package kmeans-to-pyLDAvis/0.1.1/kmeans_to_pyLDAvis-0.1.1.tar.gz

---
+++ packages/kmeans-to-pyLDAvis/kmeans_to_pyLDAvis-0.1.1.tar.gz/kmeans_to_pyLDAvis-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kmeans_to_pyLDAvis',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['kmeans_to_pyLDAvis'],
+      author_email='behi.rabiei@gmail.com',
+      zip_safe=False)
