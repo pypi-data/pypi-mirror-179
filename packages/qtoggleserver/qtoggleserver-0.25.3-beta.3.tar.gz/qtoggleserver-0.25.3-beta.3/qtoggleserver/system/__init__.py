dd838896f4c48cc9de353368aeb41fc6fe3028c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:18 +0000

    Add package qtoggleserver/0.25.3b3/qtoggleserver-0.25.3-beta.3.tar.gz

---
+++ packages/qtoggleserver/qtoggleserver-0.25.3-beta.3.tar.gz/qtoggleserver-0.25.3-beta.3/qtoggleserver/system/__init__.py
@@ -0,0 +1,42 @@
+import logging
+import os
+
+import psutil
+
+from qtoggleserver.conf import settings
+
+from . import battery, conf, date, dns, fwupdate, net, storage, temperature
+
+
+logger = logging.getLogger(__name__)
+
+
+def reboot() -> None:
+    logger.debug('rebooting')
+    if not settings.debug:
+        os.system('reboot')
+
+
+def uptime() -> int:
+    with open('/proc/uptime', 'r') as f:
+        line = f.readlines()[0]
+        return int(float(line.split()[0]))
+
+
+def get_cpu_usage() -> int:
+    return int(psutil.cpu_percent())
+
+
+def get_mem_usage() -> int:
+    vm = psutil.virtual_memory()
+    return int(100 * (1 - vm.available / vm.total))
+
+
+async def init() -> None:
+    logger.debug('initializing DNS')
+    await dns.init()
+
+
+async def cleanup() -> None:
+    logger.debug('cleaning up DNS')
+    await dns.cleanup()
