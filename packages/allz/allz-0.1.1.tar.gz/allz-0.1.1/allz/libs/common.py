31fd3c3bce4d61fabdf2f087bc2a0420ba56dcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package allz/0.1.1/allz-0.1.1.tar.gz

---
+++ packages/allz/allz-0.1.1.tar.gz/allz-0.1.1/allz/libs/common.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import sys
+
+from allz.defs import LOG_LEVEL, LOG_STDOUT_FORMAT, LOG_MODE_NORMAL
+from loguru import logger
+
+
+def get_logger(name: str, log_mode=LOG_MODE_NORMAL):
+    logger.remove()
+
+    mylogger = logger.bind(name=name)
+    if log_mode != LOG_MODE_NORMAL:
+        logger.disable(name)
+    else:
+        logger.add(sys.stdout, format=LOG_STDOUT_FORMAT, level=LOG_LEVEL, filter=lambda record: record["extra"].get('name') == name)
+        logger.enable(name)
+
+    return mylogger
+
+
+def on_success(src_path: str, des_path: str, log_mode=LOG_MODE_NORMAL) -> None:
+    mylogger = get_logger("common", log_mode)
+    mylogger.info(f"The compressed file {src_path} was successfully extracted to {des_path}")
+
+
+def on_failure(src_path: str, des_path: str, log_mode=LOG_MODE_NORMAL) -> None:
+    mylogger = get_logger("common", log_mode)
+    mylogger.info(f"The compressed file {src_path} failed to be extracted to {des_path}")
+
+
+def set_log_mode(class_name, log_mode=LOG_MODE_NORMAL):
+    return get_logger(class_name, log_mode)
