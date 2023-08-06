a68f312ac630b04e5a627fae52fbef7ed4efb75b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:45 +0000

    Add package xia-engine/0.1.38/xia_engine-0.1.38-cp310-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-engine/xia_engine-0.1.38-cp310-none-macosx_11_0_x86_64.whl/xia_engine/__init__.py
@@ -0,0 +1,20 @@
+from xia_engine.base import Base, BaseEngine, BaseDocument, BaseEmbeddedDocument, EmbeddedDocument
+from xia_engine.fields import EmbeddedDocumentField, ReferenceField, ListField, ListRuntime, ExternalField
+from xia_engine.document import Document
+from xia_engine.engine import Engine, BaseEngine, RamEngine
+from xia_engine.acl import Acl, AclItem
+from xia_engine.exception import AuthorizationError, AuthenticationError, NotFoundError, ConflictError, BadRequestError
+from xia_engine.exception import ServerError
+
+
+__all__ = [
+    "Base", "BaseEngine", "BaseDocument", "BaseEmbeddedDocument", "EmbeddedDocument",
+    "EmbeddedDocumentField", "ReferenceField", "ListField", "ListRuntime", "ExternalField",
+    "Document",
+    "Engine", "BaseEngine", "RamEngine",
+    "Acl", "AclItem",
+    'AuthorizationError', 'AuthenticationError', 'NotFoundError', 'ConflictError', 'BadRequestError',
+    "ServerError"
+]
+
+__version__ = "0.1.38"
