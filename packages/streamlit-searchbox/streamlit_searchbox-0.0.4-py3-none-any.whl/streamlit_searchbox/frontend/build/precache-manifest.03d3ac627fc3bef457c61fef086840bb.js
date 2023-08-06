18dec4266b772ef0b063eb2a84b8c07ca07419e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:34 +0000

    Add package streamlit-searchbox/0.0.4/streamlit_searchbox-0.0.4-py3-none-any.whl

---
+++ packages/streamlit-searchbox/streamlit_searchbox-0.0.4-py3-none-any.whl/streamlit_searchbox/frontend/build/precache-manifest.03d3ac627fc3bef457c61fef086840bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceea41d2d51ad4848b196449d4940140",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28343d51bf6cb7a1c5d8",
+    "url": "./static/js/2.53733b56.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.53733b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1fc302145fa16a0a7c",
+    "url": "./static/js/main.058e43e7.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "5de43616554e7f30803771e5c9a87ad0",
+    "url": "./static/media/magnifying-glass-solid.5de43616.svg"
+  }
+]);
---
+++ packages/streamlit-searchbox/streamlit_searchbox-0.0.4-py3-none-any.whl/streamlit_searchbox/frontend/build/precache-manifest.7fb1bb1c512f386bdcd99f8a363e1006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53e278a83d16cbc6d0c71e11da43bc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28343d51bf6cb7a1c5d8",
+    "url": "./static/js/2.53733b56.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.53733b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c962d2091e60f33eee",
+    "url": "./static/js/main.7d1166ac.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "5de43616554e7f30803771e5c9a87ad0",
+    "url": "./static/media/magnifying-glass-solid.5de43616.svg"
+  }
+]);
