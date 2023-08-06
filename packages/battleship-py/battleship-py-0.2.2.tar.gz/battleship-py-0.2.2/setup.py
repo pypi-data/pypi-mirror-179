818b70bda5cd14bb843cb15729cf2eee04923362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:20 +0000

    Add package battleship-py/0.2.2/battleship-py-0.2.2.tar.gz

---
+++ packages/battleship-py/battleship-py-0.2.2.tar.gz/battleship-py-0.2.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+setuptools.setup(
+    name="battleship-py",
+    version="0.2.2",
+    author="João Magalhães",
+    author_email="joamag@gmail.com",
+    description="Simple Battleship game",
+    license="Apache License, Version 2.0",
+    keywords="game battleship",
+    url="https://github.com/joamag/battleship",
+    zip_safe=False,
+    packages=["battleship", "battleship.test"],
+    test_suite="battleship.test",
+    package_dir={"": os.path.normpath("src")},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md"), "rb")
+    .read()
+    .decode("utf-8"),
+    long_description_content_type="text/markdown",
+)
---
+++ packages/battleship-py/battleship-py-0.2.2.tar.gz/battleship-py-0.2.2/src/battleship/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from . import console
+from . import game
+
+from .console import BattleshipConsole
+from .game import Battleship
