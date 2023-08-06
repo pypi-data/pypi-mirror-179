e035a0dc11463e04d682f7d620480e20c2ec6d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:10 +0000

    Add package smudge/1.0.1/smudge-1.0.1.tar.gz

---
+++ packages/smudge/smudge-1.0.1.tar.gz/smudge-1.0.1/tests/test_signature_matching.py
@@ -0,0 +1,12 @@
+"""Smudge Test module for Signature class"""
+import unittest
+from scapy.all import sniff
+from smudge import Signature
+
+class SignatureMatchingTestcase(unittest.TestCase):
+    "Test class for Signature class"
+    def test_signature_1(self):
+        '''Signature.'''
+        packet_1 = sniff(offline="smudge/tests/test.pcap")[0]
+        signature_1 = Signature(packet_1)
+        self.assertTrue(signature_1.version == str(4))
