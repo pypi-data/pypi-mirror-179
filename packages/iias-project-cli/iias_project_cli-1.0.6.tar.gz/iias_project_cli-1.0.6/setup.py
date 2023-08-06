b61f04abe9f248f3b63b9e025bbee1b4d0cff70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package iias-project-cli/1.0.6/iias_project_cli-1.0.6.tar.gz

---
+++ packages/iias-project-cli/iias_project_cli-1.0.6.tar.gz/iias_project_cli-1.0.6/setup.py
@@ -0,0 +1,17 @@
+"""This is the installation toolset for this project."""
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='iias_project_cli',
+      version='1.0.6',
+      author='Jules Debeaumont',
+      description='A commandline interface for creating project structures',
+      long_description=long_description,
+      packages=find_packages(exclude=('tests',)),
+      entry_points={
+          'console_scripts': [
+              'iias_project_cli = iias_project_cli.__main__:main'
+          ]
+      })
