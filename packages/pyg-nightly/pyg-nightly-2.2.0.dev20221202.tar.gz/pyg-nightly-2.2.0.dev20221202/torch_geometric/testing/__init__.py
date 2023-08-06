2ae491c437e836418880423950f544c780af0908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:11 +0000

    Add package pyg-nightly/2.2.0.dev20221202/pyg-nightly-2.2.0.dev20221202.tar.gz

---
+++ packages/pyg-nightly/pyg-nightly-2.2.0.dev20221202.tar.gz/pyg-nightly-2.2.0.dev20221202/torch_geometric/testing/__init__.py
@@ -0,0 +1,12 @@
+from .decorators import (is_full_test, onlyFullTest, onlyUnix, onlyPython,
+                         withPackage, onlyCUDA, withCUDA)
+
+__all__ = [
+    'is_full_test',
+    'onlyFullTest',
+    'onlyUnix',
+    'onlyPython',
+    'withPackage',
+    'onlyCUDA',
+    'withCUDA',
+]
