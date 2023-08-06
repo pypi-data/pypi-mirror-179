36b632310a926af0e8aebb2fa5431986abdd99a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:56 +0000

    Add package Finance-Hermes/0.2.1/Finance-Hermes-0.2.1.tar.gz

---
+++ packages/Finance-Hermes/Finance-Hermes-0.2.1.tar.gz/Finance-Hermes-0.2.1/hermes/factors/technical/factor_statistics.py
@@ -0,0 +1,119 @@
+# -*- encoding:utf-8 -*-
+from hermes.factors.base import FactorBase, LongCallMixin, ShortMixin
+from hermes.factors.technical.core.statistics import *
+
+
+class FactorStatistics(FactorBase, LongCallMixin, ShortMixin):
+
+    def __init__(self, **kwargs):
+        __str__ = 'statistics'
+        self.category = 'statistics'
+
+    def _init_self(self, **kwargs):
+        pass
+
+    def KURTOSIS(self,
+                 data,
+                 length=None,
+                 offset=None,
+                 dependencies=['close'],
+                 **kwargs):
+        length = int(length) if length and length > 0 else 30
+        result = kurtosis(data['close'],
+                          length=length,
+                          offset=offset,
+                          **kwargs)
+        return self._format(result, f"KURT_{length}")
+
+    def MAD(self,
+            data,
+            length=None,
+            offset=None,
+            dependencies=['close'],
+            **kwargs):
+        length = int(length) if length and length > 0 else 30
+        result = mad(data['close'], length=length, offset=offset, **kwargs)
+        return self._format(result, f"MAD_{length}")
+
+    def MEDIAN(self,
+               data,
+               length=None,
+               offset=None,
+               dependencies=['close'],
+               **kwargs):
+        length = int(length) if length and length > 0 else 30
+        result = median(data['close'], length=length, offset=offset, **kwargs)
+        return self._format(result, f"MEDIAN_{length}")
+
+    def QUANTILE(self,
+                 data,
+                 length=None,
+                 offset=None,
+                 q=None,
+                 dependencies=['close'],
+                 **kwargs):
+        length = int(length) if length and length > 0 else 20
+        q = float(q) if q and q > 0 and q < 1 else 0.5
+        result = median(data['close'],
+                        length=length,
+                        offset=offset,
+                        q=q,
+                        **kwargs)
+        return self._format(result, f"QTL_{length}_{int(q)}")
+
+    def SKEW(self,
+             data,
+             length=None,
+             offset=None,
+             dependencies=['close'],
+             **kwargs):
+
+        length = int(length) if length and length > 0 else 30
+        result = skew(data['close'], length=length, offset=offset, **kwargs)
+        return self._format(result, f"SKEW_{length}")
+
+    def STDEV(self,
+              data,
+              length=None,
+              ddof=None,
+              offset=None,
+              dependencies=['close'],
+              **kwargs):
+        length = int(length) if length and length > 0 else 30
+        result = stdev(data['close'],
+                       length=length,
+                       ddof=ddof,
+                       offset=offset,
+                       **kwargs)
+        return self._format(result, f"STDEV_{length}")
+
+    def VARIANCE(self,
+                 data,
+                 length=None,
+                 ddof=None,
+                 offset=None,
+                 dependencies=['close'],
+                 **kwargs):
+
+        length = int(length) if length and length > 0 else 30
+        result = variance(data['close'],
+                          length=length,
+                          ddof=ddof,
+                          offset=offset,
+                          **kwargs)
+        return self._format(result, f"VAR_{length}")
+
+    def ZSCORE(self,
+               data,
+               length=None,
+               std=None,
+               offset=None,
+               dependencies=['close'],
+               **kwargs):
+        length = int(length) if length and length > 0 else 30
+        result = zscore(data['close'],
+                        length=length,
+                        std=std,
+                        offset=offset,
+                        **kwargs)
+        return self._format(result, f"ZS_{length}")
