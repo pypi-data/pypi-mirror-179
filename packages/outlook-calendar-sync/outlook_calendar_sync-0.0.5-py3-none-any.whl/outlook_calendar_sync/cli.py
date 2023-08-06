48f00c3d17c36c602d1c1dc6f242146c7c285db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:42 +0000

    Add package outlook-calendar-sync/0.0.5/outlook_calendar_sync-0.0.5-py3-none-any.whl

---
+++ packages/outlook-calendar-sync/outlook_calendar_sync-0.0.5-py3-none-any.whl/outlook_calendar_sync/cli.py
@@ -0,0 +1,100 @@
+"""
+Module that contains the command line app.
+
+Why does this file exist, and why not put this in __main__?
+
+  You might be tempted to import things from __main__ later, but that will cause
+  problems: the code will get executed twice:
+
+  - When you run `python -moutlook_calendar_sync` python will execute
+    ``__main__.py`` as a script. That means there won't be any
+    ``outlook_calendar_sync.__main__`` in ``sys.modules``.
+  - When you import __main__ it will get executed again (as a module) because
+    there's no ``outlook_calendar_sync.__main__`` in ``sys.modules``.
+
+  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
+"""
+import logging
+import os
+import sys
+from argparse import ArgumentParser, Namespace
+
+from outlook_calendar_sync.application import main as app
+from outlook_calendar_sync.utils import _init_config, default_config_path, load_config
+
+
+def app_handler(args: Namespace):
+
+    config = load_config(args.config)
+    config.read(args.config)
+
+    username = args.email or config["OutlookCredentials"]["OutlookEmail"]
+    password = args.password or config["OutlookCredentials"]["OutlookPassword"]
+    calendar_id = args.calendar_id or config["GoogleCredentials"]["GoogleCalendarID"]
+    calendar_uri = config.get("config", "outlookurl")
+    requires_auth = not args.no_auth
+    days_to_fetch = int(args.n or config.get("config", "daystofetch"))
+    page_load_delay = int(config.get("config", "pageloaddelay"))
+    show_browser_window = args.show_browser
+
+    auth_code = args.auth_code
+
+    assert username
+    assert password
+    assert calendar_id
+
+    # if hasattr(logging, log_level):
+    #     log.setLevel(getattr(logging, log_level))
+    # else:
+    #     log.warning("Tried to set invalid log level %s, defaulting to INFO", log_level)
+    #     log.setLevel(logging.INFO)
+
+    app(
+        username,
+        password,
+        calendar_id,
+        calendar_uri,
+        days_to_fetch=days_to_fetch,
+        requires_auth=requires_auth,
+        auth_code=auth_code,
+        page_load_delay=page_load_delay,
+        show_browser_window=show_browser_window,
+    )
+
+
+def main():
+    parser = ArgumentParser("outlook-calendar-sync")
+    subp = parser.add_subparsers()
+    init_parser = subp.add_parser("init", description="Setup the initial config file")
+    init_parser.add_argument(
+        "--path", help=f"Path to save the config file to (defaults to {default_config_path})", default=None, type=os.PathLike
+    )
+    init_parser.set_defaults(func=lambda arg: _init_config(arg.path))
+
+    parser.add_argument("-n", type=int, help="Number of days in the future to sync", default=5)
+    parser.add_argument(
+        "--config", type=os.PathLike, help=f"Path to configuration file (defaults to {default_config_path})", default=default_config_path
+    )
+    parser.add_argument("--email", type=str, help="Outlook username to sync to")
+    parser.add_argument("--password", type=str, help="Outlook password")
+    parser.add_argument("--calendar-id", type=str, help="Google calendar ID to sync to")
+    parser.add_argument(
+        "--auth-code", type=str, help="Microsoft authenticator 2fa code. If not specified, will ask for input during execution"
+    )
+    parser.add_argument("--show-browser", action="store_true", default=False, help="Show the browser window")
+    parser.add_argument("--log", type=str, help="Log level to use (defaults to INFO)", default="INFO")
+    parser.add_argument("--no-auth", action="store_true", default=False, help="Pass this flag if 2fa auth is not required")
+
+    parser.set_defaults(func=app_handler)
+    args = parser.parse_args(sys.argv[1:])
+
+    # Setup the logging
+    logging.basicConfig(level=getattr(logging, args.log))
+    # logging.getLogger("selenium").setLevel(logging.INFO)
+    # logging.getLogger("urllib3").setLevel(logging.INFO)
+
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/outlook-calendar-sync/outlook_calendar_sync-0.0.5-py3-none-any.whl/outlook_calendar_sync/utils.py
@@ -0,0 +1,135 @@
+import configparser
+import logging
+import re
+from datetime import datetime
+from os import PathLike
+from pathlib import Path
+from typing import TypeVar
+
+default_config_path = Path.home() / ".outlook-parser-config.ini"
+
+log = logging.getLogger(__name__)
+
+
+def find_event(tag):
+    return tag.has_attr("aria-label") and "event from" in tag["aria-label"]
+
+
+def get_event(detail: str):
+    log.debug("Processing event with detail <<<%s>>>", detail)
+    match = re.match(
+        r"^(all day )?(?:event from )(\w+), (\w+) (\d+), (\d+) (?:([\d:]+))?\s?to (?:(\w+), (\w+) (\d+), (\d+))?(?:([\d:]+))? (.*?)(?: \w{0,3} )(?=location|organiser|recurring|event)(?#After 3 chars)(?:(?:location )(.*?)(?=organiser))?(?#after match location)(?:(?:organiser )(.*?)(?: recurring)?(?= event shown as))?(?#Ater match organiser)(?:(?:recurring)?(?: event shown as) (\w+))$(?#After match event transparency)",  # noqa
+        detail,
+    )
+    if not match:
+        log.error("Failed to parse event detail: '%s'", detail)
+        return None
+
+    start_day = match.group(2)
+    start_month = match.group(3)
+    start_date = match.group(4)
+    start_year = match.group(5)
+
+    # If an all day event
+    if match.group(1):
+        end_day = match.group(7)
+        end_month = match.group(8)
+        end_date = match.group(9)
+        end_year = match.group(10)
+
+        start_timestamp = datetime.strptime(f"{start_day} {start_month} {start_date} {start_year}", "%A %B %d %Y")
+        end_timestamp = datetime.strptime(f"{end_day} {end_month} {end_date} {end_year}", "%A %B %d %Y")
+
+        start = {"date": start_timestamp.strftime("%Y-%m-%d"), "timeZone": "Europe/London"}
+        end = {"date": end_timestamp.strftime("%Y-%m-%d"), "timeZone": "Europe/London"}
+    else:
+        start_time = match.group(6)
+        end_time = match.group(11)
+        start_timestamp = datetime.strptime(f"{start_day} {start_month} {start_date} {start_year} {start_time}", "%A %B %d %Y %H:%M")
+        end_timestamp = datetime.strptime(f"{start_day} {start_month} {start_date} {start_year} {end_time}", "%A %B %d %Y %H:%M")
+
+        start = {"dateTime": start_timestamp.isoformat(), "timeZone": "Europe/London"}
+        end = {"dateTime": end_timestamp.isoformat(), "timeZone": "Europe/London"}
+
+    summary = sanitize(match.group(12))
+    location = sanitize(match.group(13))
+    organiser = sanitize(match.group(14))
+    show_as = sanitize(match.group(15))
+
+    transparancy = "opaque" if show_as and show_as.lower() == "busy" else "transparent"
+
+    event = {
+        "summary": summary,
+        "location": location,
+        "organizer": {"displayName": organiser or "Ellis Lunnon", "self": "true" if not organiser else "false"},
+        "transparency": transparancy,
+        "start": start,
+        "end": end,
+    }
+    log.debug("New event %s", summary)
+    return event
+
+
+def compare_events(g_event, o_event) -> bool:
+    return g_event == o_event
+
+
+def is_all_day(event) -> bool:
+    """
+    Check if an event is an all-day event.
+    """
+    return "dateTime" in event.get("start", {})
+
+
+T = TypeVar("T")
+
+
+def sanitize(field: str | None) -> str | None:
+    if field:
+        return field.strip()
+    return None
+
+
+def dedupe_events(events: list[T]) -> list[T]:
+    unique_events = []
+    for event in events:
+        if event not in unique_events:
+            unique_events.append(event)
+
+    return unique_events
+
+
+def _init_config(user_path: PathLike = None):
+    if user_path:
+        user_path = Path(user_path)
+    else:
+        user_path = default_config_path
+
+    log.info("Setting up default configuration file at %s", str(user_path))
+
+    config = configparser.ConfigParser()
+    config["DEFAULT"] = {
+        "daystofetch": 1,
+        "loglevel": "INFO",
+        "outlookurl": "https://outlook.office.com/calendar/view/day",
+        "requiresauthcode": True,
+        "pageloaddelay": 10,
+        "self": "firstname lastname",
+    }
+
+    config["OutlookCredentials"] = {"OutlookEmail": "user.name@example.com", "OutlookPassword": "password123"}
+    config["GoogleCredentials"] = {"GoogleCalendarID": "1234567890abcdef@group.calendar.google.com"}
+
+    config["config"] = {"daystofetch": 1}
+
+    with user_path.open("w") as f:
+        config.write(f)
+
+
+def load_config(config_file: PathLike = None):
+    if not config_file:
+        config_file = default_config_path
+    log.debug("Reading config file from %s", str(config_file))
+    config = configparser.ConfigParser()
+    config.read(str(config_file))
+    return config
