991f092abdf133fa539b7154cd199250e91fb26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:52 +0000

    Add package tileset-analyzer/0.0.16/tileset_analyzer-0.0.16-py2.py3-none-any.whl

---
+++ packages/tileset-analyzer/tileset_analyzer-0.0.16-py2.py3-none-any.whl/tileset_analyzer/data_source/tile_source.py
@@ -0,0 +1,23 @@
+import abc
+from sqlite3 import Connection
+from typing import List
+from tileset_analyzer.entities.level_count import LevelCount
+from tileset_analyzer.entities.tileset_analysis_result import TilesetAnalysisResult
+
+
+class TileSource(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self, src_path: str):
+        pass
+
+    @abc.abstractmethod
+    def count_tiles(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def count_tiles_by_z(self) -> List[LevelCount]:
+        pass
+
+    @abc.abstractmethod
+    def analyze(self) -> TilesetAnalysisResult:
+        pass
