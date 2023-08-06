fd0506216df441092416c9f30be1714e270e4a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:10 +0000

    Add package xiaogang/1.0.3/xiaogang-1.0.3.tar.gz

---
+++ packages/xiaogang/xiaogang-1.0.3.tar.gz/xiaogang-1.0.3/README.md
@@ -0,0 +1,8 @@
+# 小刚
+This software is some simple search
+
+# 安装
+pip install xiaogang
+
+# 介绍
+搜索开始
---
+++ packages/xiaogang/xiaogang-1.0.3.tar.gz/xiaogang-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='xiaogang',
+    version='1.0.3',
+    author='yangXX',
+    author_email='75281291@qq.com',
+
+    description='This software is some simple search',
+
+    url = 'https://gitee.com/yang-xiao-gang/p9-xiaogang',
+
+    packages=find_packages()
+)
