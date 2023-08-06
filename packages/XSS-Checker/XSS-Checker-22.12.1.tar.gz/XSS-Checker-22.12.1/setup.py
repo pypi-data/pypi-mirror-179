24b08d4d1083a6205030d62e87fd78cfdcb4f816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:11 +0000

    Add package XSS-Checker/22.12.1/XSS-Checker-22.12.1.tar.gz

---
+++ packages/XSS-Checker/XSS-Checker-22.12.1.tar.gz/XSS-Checker-22.12.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+with open('requirements.txt') as f:
+	requirements = f.readlines()
+
+setuptools.setup(
+	name = "XSS-Checker",  
+	version = "22.12.1",
+	author = "Truoc Phan",
+	license = "MIT",
+	author_email = "truocphan112017@gmail.com",
+	description = "XSS-Checker is a tool used to Check / Validate for XSS vulnerabilities",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	install_requires = requirements,
+	url = "https://github.com/truocphan/XSS-Checker",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3"
+	],
+	entry_points = {
+		"console_scripts": [
+			"XSS-Checker = XSS_Checker:vFaT3_jrF1FN30CA_o46KQ432mUyxGsUjjQqkXNFQzO0iHoX"
+		]
+	},
+)
