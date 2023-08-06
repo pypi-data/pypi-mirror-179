2d4f23656caa7366d002293ac75a91b8b94a3029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package Metric-Learning-Layers/0.1.6/Metric%20Learning%20Layers-0.1.6.tar.gz

---
+++ packages/Metric-Learning-Layers/Metric%20Learning%20Layers-0.1.6.tar.gz/Metric Learning Layers-0.1.6/metric_learning_layers/__init__.py
@@ -0,0 +1,10 @@
+from metric_learning_layers.mml import (
+    NormalizedLinear,
+    ScaledNormalizedLinear,
+    ScaledNormalizedLinearSqrt,
+    AdaCos,
+    ArcFace,
+    FixedAdaCos,
+    CosFace,
+    DeepNCM
+)
