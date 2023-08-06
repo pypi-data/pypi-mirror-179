d8c3b7d70a2123cbbac8adb8989042ed4c95cce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:54 +0000

    Add package lona/1.10.5/lona-1.10.5.tar.gz

---
+++ packages/lona/lona-1.10.5.tar.gz/lona-1.10.5/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+from lona import VERSION_STRING
+
+setup(
+    include_package_data=True,
+    name='lona',
+    version=VERSION_STRING,
+    author='Florian Scherf',
+    url='https://github.com/lona-web-org/lona',
+    author_email='mail@florianscherf.de',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp>=3,<4',
+        'jinja2',
+        'rlpython',
+        'typing-extensions',
+    ],
+    scripts=[
+        'bin/lona',
+    ],
+    entry_points={
+        'pytest11': [
+            'lona = lona.pytest',
+        ],
+    },
+)
