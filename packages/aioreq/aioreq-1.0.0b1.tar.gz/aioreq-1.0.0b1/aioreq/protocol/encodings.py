93a65c9a8f817c28f289cb3fed80b84e78dcf745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package aioreq/1.0.0b1/aioreq-1.0.0b1.tar.gz

---
+++ packages/aioreq/aioreq-1.0.0b1.tar.gz/aioreq-1.0.0b1/aioreq/protocol/encodings.py
@@ -0,0 +1,59 @@
+import zlib
+import base64
+import gzip as _gzip
+
+from enum import Enum
+from abc import ABC
+from abc import abstractmethod
+
+
+
+class Encoding(ABC):
+    all_encodings = []
+
+    @classmethod
+    @abstractmethod
+    def decompress(cls, text: bytes) -> bytes:
+        ...
+
+    def __init_subclass__(cls):
+        Encoding.all_encodings.append(cls)
+
+    @classmethod
+    def stringify(cls) -> str:
+        return cls.__name__
+
+
+# Encodings
+# -------------
+
+class gzip(Encoding):
+
+    @classmethod
+    def decompress(cls, text: bytes) -> bytes:
+        return _gzip.decompress(text)
+
+
+class deflate(Encoding):
+
+    @classmethod
+    def decompress(cls, text: bytes) -> bytes:
+        decoded_data = base64.b64decode(text)
+        res = zlib.decompress(decoded_data, -15)
+        return res
+# class compress(Encoding):
+#
+#     @classmethod
+#     def decompress(cls, text: bytes) -> bytes:
+#         raise NotImplementedError
+
+
+# Enum for encodings
+# -------------
+
+class Encodings(Enum):
+    gzip = gzip
+    deflate = deflate
+
+    def decompress(self, text: bytes) -> bytes:
+        return self.value.decompress(text)
