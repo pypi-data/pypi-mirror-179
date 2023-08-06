e714b8d9c6f4ec7e0e2c0c75a83aa01c543c4095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:15 +0000

    Add package computer-vision-python/2.0.3/computer-vision-python-2.0.3.tar.gz

---
+++ packages/computer-vision-python/computer-vision-python-2.0.3.tar.gz/computer-vision-python-2.0.3/computer_vision_python/__init__.py
@@ -0,0 +1,6 @@
+from computer_vision_python.FaceDetection import Face_det
+from computer_vision_python.PoseDet import Pose_Det
+from computer_vision_python.Serialconn import Serial_object
+from computer_vision_python.Handtracker import Hand_detector
+from computer_vision_python.Facemeshmodule import Face_mesh_det
+import computer_vision_python
