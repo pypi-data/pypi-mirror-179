131e4cda7e4956c1208b055c85d32fa78efa180e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:08 +0000

    Add package exokheia/0.1.0/exokheia-0.1.0-py3-none-any.whl

---
+++ packages/exokheia/exokheia-0.1.0-py3-none-any.whl/exokheia/__init__.py
@@ -0,0 +1,12 @@
+"""
+Abstraction layer for modeling sources of opinion
+
+- Placeholder
+
+"""
+
+__author__ = """Brendan Strejcek"""
+__email__ = 'brendan@datagazing.com'
+__version__ = '0.1.0'
+
+# from .exokheia import SomeClass # noqa F401
