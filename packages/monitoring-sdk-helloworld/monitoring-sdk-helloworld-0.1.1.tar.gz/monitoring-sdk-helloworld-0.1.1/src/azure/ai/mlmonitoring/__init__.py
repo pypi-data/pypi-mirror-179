d49dbcbb7fcec336b0e8ed830cb7855fc30291b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:31 +0000

    Add package monitoring-sdk-helloworld/0.1.1/monitoring-sdk-helloworld-0.1.1.tar.gz

---
+++ packages/monitoring-sdk-helloworld/monitoring-sdk-helloworld-0.1.1.tar.gz/monitoring-sdk-helloworld-0.1.1/src/azure/ai/mlmonitoring/__init__.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+# hide global init/teardown
+#from .init import init, teardown
+
+from .collector import Collector
+
+__version__ = "0.1.1"
