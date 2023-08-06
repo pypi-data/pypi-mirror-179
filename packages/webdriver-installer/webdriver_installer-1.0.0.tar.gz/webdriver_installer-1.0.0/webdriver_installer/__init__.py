a506d453a94c056602ab101f33a28b4fdbd137e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:10 +0000

    Add package webdriver-installer/1.0.0/webdriver_installer-1.0.0.tar.gz

---
+++ packages/webdriver-installer/webdriver_installer-1.0.0.tar.gz/webdriver_installer-1.0.0/webdriver_installer/__init__.py
@@ -0,0 +1,7 @@
+from webdriver_installer.core import *
+from webdriver_installer.utils import *
+from webdriver_installer.core.driver_installer import uninstall
+from webdriver_installer.installers.firefox import FireFox
+from webdriver_installer.installers.chrome import Chrome
+from webdriver_installer.installers.opera import Opera
+from webdriver_installer.installers.edge import Edge
