0251b4cb901fc7feb64aa77f7831f48a9a4421c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package social-stats/0.0.2/social-stats-0.0.2.tar.gz

---
+++ packages/social-stats/social-stats-0.0.2.tar.gz/social-stats-0.0.2/socialstats/medium/medium_user.py
@@ -0,0 +1,16 @@
+from socialstats.base.user import BaseUser
+
+
+class MediumUser(BaseUser):
+    """Medium user class."""
+
+    id: str = ''
+    image_id: str = ''
+    bio: str = ''
+    max_rank: str = ''
+    medium_member_at: int = 0
+    registration_unix_time: int = 0
+    follower: int = 0
+    following: int = 0
+    number_of_posts_published: int = 0
+    language_code: str = ''
