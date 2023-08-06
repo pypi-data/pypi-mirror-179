f8194bf32960958282e9c35a92cb712668a4b032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:41 +0000

    Add package robotransforms/0.3.0/robotransforms-0.3.0.tar.gz

---
+++ packages/robotransforms/robotransforms-0.3.0.tar.gz/robotransforms-0.3.0/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup, Extension
+
+with open("transforms/version.py") as f:
+    exec(f.readline())
+
+reqs = []
+with open("requirements.txt") as f:
+    for l in f:
+        reqs.append(l.strip())
+
+def main():
+    setup(
+        name="robotransforms",
+        version=__version__,
+        description="", # TODO
+        author="Jonathan D. B. Van Schenck",
+        author_email="jvschenck@novadynamics.com",
+        license="MIT",
+        ext_modules=[
+            Extension("utils_wrapper", [
+                "transforms/utils/wrapper.cpp",
+                "transforms/utils/base.cpp"
+            ]),
+            Extension("euclidean_wrapper", [
+                "transforms/euclidean/wrapper.cpp",
+                "transforms/euclidean/base.cpp"
+            ]),
+            Extension("dead_reckon_wrapper", [
+                "transforms/dead_reckon/wrapper.cpp",
+                "transforms/dead_reckon/base.cpp",
+                "transforms/utils/base.cpp",
+                "transforms/euclidean/base.cpp"
+            ]),
+        ]
+    )
+
+if __name__ == "__main__":
+    main()
