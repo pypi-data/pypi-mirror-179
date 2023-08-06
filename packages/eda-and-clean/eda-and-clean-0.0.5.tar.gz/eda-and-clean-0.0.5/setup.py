91153775379cae76e54c0cecce5540f1bfe7e17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:00 +0000

    Add package eda-and-clean/0.0.5/eda-and-clean-0.0.5.tar.gz

---
+++ packages/eda-and-clean/eda-and-clean-0.0.5.tar.gz/eda-and-clean-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eda-and-clean",
+    version="0.0.5",
+    author="Aravind Ganesan",
+    author_email="1988.aravind@gmail.com",
+    description="A package of automation tools for EDA and cleaning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aravindganesan88/eda_and_clean",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["Nbformat"],
+    python_requires=">=3.7",
+)
