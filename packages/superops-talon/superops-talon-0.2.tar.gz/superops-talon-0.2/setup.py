3f2e5e627890048d3d861475f7614d14717036b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:40 +0000

    Add package superops-talon/0.2/superops-talon-0.2.tar.gz

---
+++ packages/superops-talon/superops-talon-0.2.tar.gz/superops-talon-0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'superops-talon',
+  packages = ['superops-talon'],
+  version = '0.2',
+  license='APACHE2',
+  description = 'Mailgun library to extract message quotations and signatures (talon-1.6.0)',
+  author = 'SuperOps',
+  author_email = 'app.integrations@superops.com'
+)
