3d72584cc4c81e02116aecf28d0c234254065cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:48 +0000

    Add package flexible-list-of-values/0.1.1/flexible_list_of_values-0.1.1.tar.gz

---
+++ packages/flexible-list-of-values/flexible_list_of_values-0.1.1.tar.gz/flexible_list_of_values-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flexible_list_of_values', 'flexible_list_of_values.migrations']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'flexible-list-of-values',
+    'version': '0.1.1',
+    'description': 'Flexible Lists of Values (LOV) for Django',
+    'long_description': '# flexible-list-of-values\nFlexible and Extensible Lists of Values (LOV) for Django\n',
+    'author': 'Jack Linke',
+    'author_email': 'jack@watervize.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/jacklinke/flexible-list-of-values',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
