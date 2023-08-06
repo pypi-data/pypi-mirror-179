949e8cd93ed71dbe063fca5667b98d352ae3c346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:20 +0000

    Add package kxflv-distributions/0.1/kxflv-distributions-0.1.tar.gz

---
+++ packages/kxflv-distributions/kxflv-distributions-0.1.tar.gz/kxflv-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='kxflv-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kxflv-distributions'],
+      zip_safe=False)
