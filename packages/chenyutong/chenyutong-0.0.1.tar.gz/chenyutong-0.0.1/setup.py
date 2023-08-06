a8ab6a51e4bccc81e654b0bc87b0f17c7d30a8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:43 +0000

    Add package chenyutong/0.0.1/chenyutong-0.0.1.tar.gz

---
+++ packages/chenyutong/chenyutong-0.0.1.tar.gz/chenyutong-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="chenyutong",
+    version="0.0.1",
+    author="chenyutong",
+    author_email="2463400852@qq.com",
+    description="This is me xiajibaxiede",
+
+    url="https://github.com/sunjian-github/ezface",
+
+    packages=find_packages()
+)
