3a96d09dafc61f9acdd978baae9cfbba2f396e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:03 +0000

    Add package studentresume/0.5.0/studentresume-0.5.0.tar.gz

---
+++ packages/studentresume/studentresume-0.5.0.tar.gz/studentresume-0.5.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='studentresume',
+    version='0.5.0',    
+    description='Student Resume Generator Python package',
+    url='https://github.com/open-uofa/studentresume',
+    author='Diana, Ivan, Curtis, Yijing, Jason, Mohammed',
+    author_email='dble@ualberta.ca, yzhang24@ualberta.ca, jrobock@ualberta.ca, ckennedy@ualberta.ca, qu8@ualberta.ca, alzafara@ualberta.ca',
+    license='MIT License',
+    packages=['studentresume'],
+    install_requires=[
+                    'jsonschema==4.16.0',
+                    'reportlab==3.6.11',         
+                    ],
+    package_data={'studentresume': ['requiredFields.json', 
+                                    'sample.resume.json',
+                                    'schema.json',
+                                    'testonepage.resume.json',
+                                    'themes/*',
+                                    'fonts/*',]},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
---
+++ packages/studentresume/studentresume-0.5.0.tar.gz/studentresume-0.5.0/studentresume/__init__.py
@@ -0,0 +1,2 @@
+_version_="0.5.0"
+_author_="Diana, Ivan, Curtis, Yijing, Jason, Mohammed"
