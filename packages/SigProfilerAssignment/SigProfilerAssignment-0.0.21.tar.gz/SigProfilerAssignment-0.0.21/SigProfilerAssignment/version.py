f538d78e8c23fa46d45fe9d5510dce1684da7d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:12 +0000

    Add package SigProfilerAssignment/0.0.21/SigProfilerAssignment-0.0.21.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.21.tar.gz/SigProfilerAssignment-0.0.21/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.21'
+version = '0.0.21'
+Update = 'Resolve reference signature default value issue for decomposition plots.'
+
+    
+    
