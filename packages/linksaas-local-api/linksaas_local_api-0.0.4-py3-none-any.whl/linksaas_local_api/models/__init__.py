669677099166b7ff4a2ed6991997e7c2ae8d5542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package linksaas-local-api/0.0.4/linksaas_local_api-0.0.4-py3-none-any.whl

---
+++ packages/linksaas-local-api/linksaas_local_api-0.0.4-py3-none-any.whl/linksaas_local_api/models/__init__.py
@@ -0,0 +1,32 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from linksaas_local_api.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from linksaas_local_api.model.block_coll_info import BlockCollInfo
+from linksaas_local_api.model.block_content_info import BlockContentInfo
+from linksaas_local_api.model.block_info import BlockInfo
+from linksaas_local_api.model.bug_info import BugInfo
+from linksaas_local_api.model.channel_info import ChannelInfo
+from linksaas_local_api.model.doc_info import DocInfo
+from linksaas_local_api.model.doc_space_info import DocSpaceInfo
+from linksaas_local_api.model.err_info import ErrInfo
+from linksaas_local_api.model.event_info import EventInfo
+from linksaas_local_api.model.issue_info import IssueInfo
+from linksaas_local_api.model.msg_info import MsgInfo
+from linksaas_local_api.model.project_project_id_block_coll_block_coll_id_get200_response import ProjectProjectIdBlockCollBlockCollIdGet200Response
+from linksaas_local_api.model.project_project_id_block_coll_get200_response import ProjectProjectIdBlockCollGet200Response
+from linksaas_local_api.model.project_project_id_bug_all_get200_response import ProjectProjectIdBugAllGet200Response
+from linksaas_local_api.model.project_project_id_doc_space_doc_space_id_get200_response import ProjectProjectIdDocSpaceDocSpaceIdGet200Response
+from linksaas_local_api.model.project_project_id_event_get200_response import ProjectProjectIdEventGet200Response
+from linksaas_local_api.model.project_project_id_task_all_get200_response import ProjectProjectIdTaskAllGet200Response
+from linksaas_local_api.model.project_project_id_task_record_task_id_depend_get200_response import ProjectProjectIdTaskRecordTaskIdDependGet200Response
+from linksaas_local_api.model.simple_member_info import SimpleMemberInfo
+from linksaas_local_api.model.sub_task_info import SubTaskInfo
+from linksaas_local_api.model.task_info import TaskInfo
