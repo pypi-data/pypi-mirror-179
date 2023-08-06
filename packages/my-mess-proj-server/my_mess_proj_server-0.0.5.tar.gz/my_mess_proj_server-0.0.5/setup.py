520c16fe1572ee8bdb0f91aa0fb6e4e1e6572368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:11 +0000

    Add package my-mess-proj-server/0.0.5/my_mess_proj_server-0.0.5.tar.gz

---
+++ packages/my-mess-proj-server/my_mess_proj_server-0.0.5.tar.gz/my_mess_proj_server-0.0.5/setup.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="my_mess_proj_server",
+      version="0.0.5",
+      description="my_mess_proj_server",
+      author="Sergey Petin",
+      author_email="pillager@bk.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodomex'],
+      scripts=['server/server_run']
+      )
