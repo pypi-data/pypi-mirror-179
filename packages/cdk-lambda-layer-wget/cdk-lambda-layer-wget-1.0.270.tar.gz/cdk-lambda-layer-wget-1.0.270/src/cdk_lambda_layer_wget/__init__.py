1dbac737ecb3bf4a098be93443e72e5b9e00c5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:28 +0000

    Add package cdk-lambda-layer-wget/1.0.270/cdk-lambda-layer-wget-1.0.270.tar.gz

---
+++ packages/cdk-lambda-layer-wget/cdk-lambda-layer-wget-1.0.270.tar.gz/cdk-lambda-layer-wget-1.0.270/src/cdk_lambda_layer_wget/__init__.py
@@ -0,0 +1,74 @@
+'''
+# AWS Lambda Layer with wget
+
+[![NPM version](https://badge.fury.io/js/cdk-lambda-layer-wget.svg)](https://badge.fury.io/js/cdk-lambda-layer-wget)
+[![PyPI version](https://badge.fury.io/py/cdk-lambda-layer-wget.svg)](https://badge.fury.io/py/cdk-lambda-layer-wget)
+![Release](https://github.com/clarencetw/cdk-lambda-layer-wget/workflows/release/badge.svg)
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/clarencetw/cdk-lambda-layer-wget)
+
+Usage:
+
+```python
+// WgetLayer bundles the wget in a lambda layer
+import { WgetLayer } from 'cdk-lambda-layer-wget';
+
+declare const fn: lambda.Function;
+fn.addLayers(new WgetLayer(this, 'WgetLayer'));
+```
+
+```python
+import { WgetLayer } from 'cdk-lambda-layer-wget'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+
+new lambda.Function(this, 'MyLambda', {
+  code: lambda.Code.fromAsset(path.join(__dirname, 'my-lambda-handler')),
+  handler: 'index.main',
+  runtime: lambda.Runtime.PYTHON_3_9,
+  layers: [new WgetLayer(this, 'WgetLayer')]
+});
+```
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+from ._jsii import *
+
+import aws_cdk.aws_lambda
+import aws_cdk.core
+
+
+class WgetLayer(
+    aws_cdk.aws_lambda.LayerVersion,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="cdk-lambda-layer-wget.WgetLayer",
+):
+    '''An AWS Lambda layer that includes the wget.'''
+
+    def __init__(self, scope: aws_cdk.core.Construct, id: builtins.str) -> None:
+        '''
+        :param scope: -
+        :param id: -
+        '''
+        if __debug__:
+            def stub(scope: aws_cdk.core.Construct, id: builtins.str) -> None:
+                ...
+            type_hints = typing.get_type_hints(stub)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        jsii.create(self.__class__, self, [scope, id])
+
+
+__all__ = [
+    "WgetLayer",
+]
+
+publication.publish()
