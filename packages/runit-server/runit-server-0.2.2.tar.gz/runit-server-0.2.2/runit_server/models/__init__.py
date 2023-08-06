1ba1d75c97267c4ef0eaaa4f1008555eb8b6415c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:26 +0000

    Add package runit-server/0.2.2/runit-server-0.2.2.tar.gz

---
+++ packages/runit-server/runit-server-0.2.2.tar.gz/runit-server-0.2.2/runit_server/models/__init__.py
@@ -0,0 +1,5 @@
+from .function import Function
+from .project import Project
+from .user import User
+from .role import Role
+from .admin import Admin
---
+++ packages/runit-server/runit-server-0.2.2.tar.gz/runit-server-0.2.2/runit_server/models/role.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+
+from bson.objectid import ObjectId
+
+from odbms import DBMS, Model
+from ..common.utils import Utils
+
+
+class Role(Model):
+    '''A model class for role'''
+    TABLE_NAME = 'roles'
+
+    def __init__(self, name, permission_ids, created_at=None, updated_at=None, id=None):
+        super().__init__(created_at, updated_at, id)
+        self.name = name
+        #self.permission_ids =  permission_ids.split('::') if type(permission_ids) == str \
+        #                        else permission_ids
+        self.permission_ids = permission_ids
+        
+
+    def save(self):
+        '''
+        Instance Method for saving Role instance to database
+
+        @params None
+        @return None
+        '''
+
+        data = {
+            "name": self.name,
+            "permission_ids": self.permission_ids,
+        }
+
+        if DBMS.Database.dbms == 'mongodb':
+            data["created_at"] = self.created_at
+            data["updated_at"] = self.updated_at
+
+        return DBMS.Database.insert(Role.TABLE_NAME, data)
+    
+    def json(self)-> dict:
+        '''
+        Instance Method for converting Role Instance to Dict
+
+        @paramas None
+        @return dict() format of Function instance
+        '''
+
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "permissions": self.permissions(),
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+
+    def permissions(self):
+        '''
+        Class Method for retrieving role by username 
+
+        @param username username of the role 
+        @return Role instance
+        '''
+        permissions = []
+        for perm_id in self.permission_ids:
+            permissions.append(DBMS.Database.db.find_one('permissions', {'id': perm_id}))
+        
+        return permissions
