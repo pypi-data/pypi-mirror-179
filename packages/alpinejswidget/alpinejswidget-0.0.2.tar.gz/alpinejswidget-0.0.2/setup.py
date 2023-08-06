11b6364d1e527f99a248becff4ba013fe040fee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:38 +0000

    Add package alpinejswidget/0.0.2/alpinejswidget-0.0.2.tar.gz

---
+++ packages/alpinejswidget/alpinejswidget-0.0.2.tar.gz/alpinejswidget-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alpinejswidget']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['soupwidget>=0.0.2,<0.0.3']
+
+setup_kwargs = {
+    'name': 'alpinejswidget',
+    'version': '0.0.2',
+    'description': '',
+    'long_description': '',
+    'author': 'Thiago Cesar',
+    'author_email': 'k1mbl3@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
