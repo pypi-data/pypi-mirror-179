046384407b2f6bb58e20e84b80b57ace9a422206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:03 +0000

    Add package getdaft/0.0.19/getdaft-0.0.19-cp37-cp37m-manylinux_2_17_x86_64.whl

---
+++ packages/getdaft/getdaft-0.0.19-cp37-cp37m-manylinux_2_17_x86_64.whl/daft/filesystem.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from fsspec import AbstractFileSystem, get_filesystem_class
+from fsspec.implementations.cached import SimpleCacheFileSystem
+
+from daft.context import get_context
+
+
+def get_filesystem(protocol: str, **kwargs) -> AbstractFileSystem:
+    klass = get_filesystem_class(protocol)
+    fs = klass(**kwargs)
+    return fs
+
+
+def get_protocol_from_path(path: str, **kwargs) -> str:
+    split = path.split(":")
+    assert len(split) <= 2, f"too many colons found in {path}"
+    protocol = split[0] if len(split) == 2 else "file"
+    return protocol
+
+
+def get_filesystem_from_path(path: str, **kwargs) -> AbstractFileSystem:
+    protocol = get_protocol_from_path(path)
+    fs = get_filesystem(protocol, **kwargs)
+
+    # If a cache location is set and the protocol is not a local file, use as a simple disk-based cache
+    cache_location = get_context().cache_location
+    if protocol != "file" and cache_location is not None:
+        fs = SimpleCacheFileSystem(fs=fs, cache_storage=str(cache_location / "simple-cache-file-system"))
+
+    return fs
