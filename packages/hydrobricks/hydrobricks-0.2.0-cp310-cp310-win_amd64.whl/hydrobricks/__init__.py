2da440057eb1751d934e97a97ce48e25fbc78134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:59 +0000

    Add package hydrobricks/0.2.0/hydrobricks-0.2.0-cp310-cp310-win_amd64.whl

---
+++ packages/hydrobricks/hydrobricks-0.2.0-cp310-cp310-win_amd64.whl/hydrobricks/__init__.py
@@ -0,0 +1,48 @@
+from _hydrobricks import (
+    close_log,
+    init,
+    init_log,
+    set_debug_log_level,
+    set_max_log_level,
+    set_message_log_level,
+)
+
+from .forcing import Forcing
+from .hydro_units import HydroUnits
+from .observations import Observations
+from .parameters import ParameterSet
+from .time_series import TimeSeries
+
+try:
+    from netCDF4 import Dataset
+except ImportError:
+    has_netcdf = False
+else:
+    has_netcdf = True
+
+try:
+    import rasterio
+except ImportError:
+    has_rasterio = False
+else:
+    from rasterio.mask import mask
+    has_rasterio = True
+
+try:
+    import geopandas as gpd
+except ImportError:
+    has_geopandas = False
+else:
+    has_geopandas = True
+
+try:
+    from shapely.geometry import mapping
+except ImportError:
+    has_shapely = False
+else:
+    has_shapely = True
+
+init()
+__all__ = ('ParameterSet', 'HydroUnits', 'Forcing', 'Observations', 'TimeSeries',
+           'init', 'init_log', 'close_log', 'set_debug_log_level', 'set_max_log_level',
+           'set_message_log_level', 'Dataset', 'rasterio', 'gpd', 'mapping', 'mask')
