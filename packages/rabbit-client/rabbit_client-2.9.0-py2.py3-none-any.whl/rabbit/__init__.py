a33a5a8eff01f869b86b98c7051d5892d06d3c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:33 +0000

    Add package rabbit-client/2.9.0/rabbit_client-2.9.0-py2.py3-none-any.whl

---
+++ packages/rabbit-client/rabbit_client-2.9.0-py2.py3-none-any.whl/rabbit/__init__.py
@@ -0,0 +1,21 @@
+from ._wait import constant, expo, fibo
+from .client import AioRabbitClient
+from .dlx import DLX
+from .exchange import Exchange
+from .publish import Publish
+from .queue import Queue
+from .subscribe import Subscribe
+
+__version__ = "2.9.0"
+__all__ = [
+    "__version__",
+    "AioRabbitClient",
+    "DLX",
+    "Exchange",
+    "Queue",
+    "Publish",
+    "Subscribe",
+    "expo",
+    "constant",
+    "fibo",
+]
