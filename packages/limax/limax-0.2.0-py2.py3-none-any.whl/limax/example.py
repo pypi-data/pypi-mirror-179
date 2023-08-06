c86c555e47666245d4ee22263639b75ee9ec751b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:10 +0000

    Add package limax/0.2.0/limax-0.2.0-py2.py3-none-any.whl

---
+++ packages/limax/limax-0.2.0-py2.py3-none-any.whl/limax/example.py
@@ -0,0 +1,18 @@
+"""Examples for limax processing."""
+from limax import EXAMPLE_LIMAX_PATH, PROCESSED_DIR, RAW_DIR
+from limax.io import read_limax_dir, read_limax_file
+
+
+def example_limax_file() -> None:
+    """Run processing of single file."""
+    read_limax_file(EXAMPLE_LIMAX_PATH, PROCESSED_DIR)
+
+
+def example_limax_dir() -> None:
+    """Run processing of folder."""
+    read_limax_dir(RAW_DIR, PROCESSED_DIR)
+
+
+if __name__ == "__main__":
+    example_limax_file()
+    example_limax_dir()
