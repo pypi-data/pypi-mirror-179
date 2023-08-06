645f2463c354c6f368eb4e33e5d97e3408f08319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:00 +0000

    Add package zpp_menu/1.2/zpp_menu-1.2.tar.gz

---
+++ packages/zpp_menu/zpp_menu-1.2.tar.gz/zpp_menu-1.2/zpp_menu/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.2'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = 'Générateur de menu à touches fléchées'
+__lastupdate__ = '02/12/2022'
+__releasenote__ = 'Add Selected option'
+
+from .menu import *
