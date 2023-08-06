2399d4a1f807ec59b25a29080a150a985b270b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:10 +0000

    Add package circuitsvis/1.15.0/circuitsvis-1.15.0.tar.gz

---
+++ packages/circuitsvis/circuitsvis-1.15.0.tar.gz/circuitsvis-1.15.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['circuitsvis',
+ 'circuitsvis.tests',
+ 'circuitsvis.tests.snapshots',
+ 'circuitsvis.utils',
+ 'circuitsvis.utils.tests',
+ 'circuitsvis.utils.tests.snapshots']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['importlib-metadata>=5.1.0,<6.0.0', 'numpy>=1.20,<2.0']
+
+setup_kwargs = {
+    'name': 'circuitsvis',
+    'version': '1.15.0',
+    'description': 'Mechanistic Interpretability Visualizations',
+    'long_description': '# Circuits Vis\n\nMechanistic Interpretability visualizations.\n',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
