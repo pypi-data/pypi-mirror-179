ca8b2ff1d22016c6fe8542c205db3dab92b5af43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:36 +0000

    Add package Number-of-students/1.0/Number_of_students-1.0.tar.gz

---
+++ packages/Number-of-students/Number_of_students-1.0.tar.gz/Number_of_students-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Number_of_students',
+    version= '1.0',
+    description='Takes student information like name, regno, marks and prints at end',
+
+    #url='https://github.com/15Praveenreddy/sample',
+    author='Praveen_Reddy_Pullagurala',
+    #author_email='15praveenreddy@gmail.com',
+
+    py_modules=['Number_of_students'],
+    zip_safe=False
+)
