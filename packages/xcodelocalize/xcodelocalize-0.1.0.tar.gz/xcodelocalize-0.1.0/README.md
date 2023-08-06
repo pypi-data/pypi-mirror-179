26a9a4161166c22e5a4558228171370a42489622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:57 +0000

    Add package xcodelocalize/0.1.0/xcodelocalize-0.1.0.tar.gz

---
+++ packages/xcodelocalize/xcodelocalize-0.1.0.tar.gz/xcodelocalize-0.1.0/README.md
@@ -0,0 +1,10 @@
+# XCodeLocalize
+
+# Requirments
+Python3.9+
+
+## Installation
+> pip install xcodelocalize 
+
+## Usage
+> xcodelocalize --help
---
+++ packages/xcodelocalize/xcodelocalize-0.1.0.tar.gz/xcodelocalize-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['xcodelocalize']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mtranslate>=1.8,<2.0', 'typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['xcodelocalize = xcodelocalize.main:run']}
+
+setup_kwargs = {
+    'name': 'xcodelocalize',
+    'version': '0.1.0',
+    'description': 'Tool for automatic search and localization of .strings files',
+    'long_description': '# XCodeLocalize\n\n# Requirments\nPython3.9+\n\n## Installation\n> pip install xcodelocalize \n\n## Usage\n> xcodelocalize --help',
+    'author': 'MarkParker5',
+    'author_email': 'markparker.it@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
