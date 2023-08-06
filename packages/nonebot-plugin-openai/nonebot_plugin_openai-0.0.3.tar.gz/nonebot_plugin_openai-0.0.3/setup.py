ab136fd4c63124951e59d93027759506c947d364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:33 +0000

    Add package nonebot-plugin-openai/0.0.3/nonebot_plugin_openai-0.0.3.tar.gz

---
+++ packages/nonebot-plugin-openai/nonebot_plugin_openai-0.0.3.tar.gz/nonebot_plugin_openai-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nonebot_plugin_openai",
+    version="0.0.3",
+    author="InariInDream",
+    author_email="inariindream@163.com",
+    description="A plugin for nonebot to chat",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/InariInDream/nonebot_plugin_openai",
+    packages=setuptools.find_packages(),
+    install_requires=["openai >= 0.25.0",
+                      "nonebot-plugin-apscheduler>=0.1.3",
+                      'nonebot-adapter-onebot>=2.0.0-beta.1',
+                      'nonebot2>=2.0.0-beta.4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent", ]
+
+)
