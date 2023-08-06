76cd968ec8791d7ed5cad32e37797aaaae681f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:00 +0000

    Add package addup/0.0.3/addup-0.0.3.tar.gz

---
+++ packages/addup/addup-0.0.3.tar.gz/addup-0.0.3/libname/addup.py
@@ -0,0 +1,14 @@
+def adds(a, b, *args, **kwargs):
+    '''
+    sumation
+        
+    '''
+    #1. create copy of dataframe
+    import numpy as np
+    import pandas as pd
+
+    
+    try:
+        return a+b
+    except:
+        print("Something else went wrong")
