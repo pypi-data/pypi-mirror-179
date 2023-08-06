476198d5b58078331dc62c5f2e7a1f82b9daedcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:00 +0000

    Add package cornflakes/3.0.4/cornflakes-3.0.4-cp38-cp38-win_amd64.whl

---
+++ packages/cornflakes/cornflakes-3.0.4-cp38-cp38-win_amd64.whl/cornflakes/decorator/__init__.py
@@ -0,0 +1,23 @@
+"""cornflakes decorator module.
+__________________________________
+"""  # noqa: RST303 D205
+from cornflakes import patch_module
+from cornflakes.decorator.config import config, config_group, config_field
+from cornflakes.decorator._add_dataclass_slots import add_slots
+from cornflakes.decorator._click_cli import click_cli
+from cornflakes.decorator.dataclass import field, dataclass
+from cornflakes.decorator._types import ConfigArguments, Loader
+
+__all__ = [
+    "config",
+    "config_group",
+    "config_field",
+    "add_slots",
+    "click_cli",
+    "dataclass",
+    "field",
+    "Loader",
+    "ConfigArguments",
+]
+
+patch_module(globals())
