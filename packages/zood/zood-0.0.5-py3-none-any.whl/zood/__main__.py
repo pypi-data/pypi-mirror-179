1b89af626a1c62cb7a3f96be1e1e6d2f35e55b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:45 +0000

    Add package zood/0.0.5/zood-0.0.5-py3-none-any.whl

---
+++ packages/zood/zood-0.0.5-py3-none-any.whl/zood/__main__.py
@@ -0,0 +1,17 @@
+
+import sys
+from Parser import test
+
+def main():
+    
+    print("hello zood!")
+    a = test.A()
+    a.fun()
+
+
+def run():
+    main()
+
+
+if __name__ == "__main__":
+    main()
