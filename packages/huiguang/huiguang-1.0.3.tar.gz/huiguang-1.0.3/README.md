09b25dcfb836ce274a60c376f664eee9f7d2fafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:48 +0000

    Add package huiguang/1.0.3/huiguang-1.0.3.tar.gz

---
+++ packages/huiguang/huiguang-1.0.3.tar.gz/huiguang-1.0.3/README.md
@@ -0,0 +1,8 @@
+# huiguang
+This software is some simple search
+
+# 安装
+pip install huiguang
+
+# 介绍
+搜索开始
---
+++ packages/huiguang/huiguang-1.0.3.tar.gz/huiguang-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='huiguang',
+    version='1.0.3',
+    author='tianmou',
+    author_email='1039211901@qq.com',
+
+    description='This software is some simple search',
+
+    url = 'https://gitee.com/tian-huiguang/tian-huiguang-homework',
+
+    packages=find_packages()
+)
