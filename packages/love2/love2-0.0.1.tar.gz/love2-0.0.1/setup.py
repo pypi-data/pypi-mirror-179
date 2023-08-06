1c0a1de30ae6b68ea5cf5224c0d4121701b797ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:11 +0000

    Add package love2/0.0.1/love2-0.0.1.tar.gz

---
+++ packages/love2/love2-0.0.1.tar.gz/love2-0.0.1/setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup,find_packages
+
+
+setup(
+    name = "love2",
+    version="0.0.1",
+    author ="tiancheng",
+    author_email="3277909872@qq.com",
+    description = 'Code of Love',
+    packages=find_packages()
+)
+
+
+
+
+
+
