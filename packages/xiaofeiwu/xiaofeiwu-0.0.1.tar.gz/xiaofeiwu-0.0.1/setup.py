33544f3bab50af06dae2f78fa1a2cac85a46cb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:10 +0000

    Add package xiaofeiwu/0.0.1/xiaofeiwu-0.0.1.tar.gz

---
+++ packages/xiaofeiwu/xiaofeiwu-0.0.1.tar.gz/xiaofeiwu-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="xiaofeiwu",
+    version="0.0.1",
+    author="Jnin",
+    author_email="2463400852@qq.com",
+    description="This is me xiajibaxiede",
+
+    url="https://github.com/sunjian-github/ezface",
+
+    packages=find_packages()
+)
