4606a2a429c0f07f9ed0aca7462578a25300f687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:57 +0000

    Add package vector-ai-ml/0.0.2/vector-ai-ml-0.0.2.tar.gz

---
+++ packages/vector-ai-ml/vector-ai-ml-0.0.2.tar.gz/vector-ai-ml-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A simple python vector package.'
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="vector-ai-ml",
+    version=VERSION,
+    author="AtlasAerospace (Alexander Armitage)",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    keywords=['python', 'math', 'ai', 'ml', 'artificialintelligence'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
