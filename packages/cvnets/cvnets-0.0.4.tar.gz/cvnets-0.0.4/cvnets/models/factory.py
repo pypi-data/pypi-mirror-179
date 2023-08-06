c90868a94498b764a325893210423b7222b5b5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:07 +0000

    Add package cvnets/0.0.4/cvnets-0.0.4.tar.gz

---
+++ packages/cvnets/cvnets-0.0.4.tar.gz/cvnets-0.0.4/cvnets/models/factory.py
@@ -0,0 +1,38 @@
+import timm
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+    def model():
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        return NeuralNetwork().to(device)
+
+
+def create_model(name, classes=None, pretrained=False):
+    if name == "NeuralNetwork":
+        model = NeuralNetwork.model()
+    elif len(name):
+        model = timm.create_model(
+            name, num_classes=len(classes), pretrained=pretrained)
+    return model
