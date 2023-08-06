20bce408bd601a6b6008d39f9f98469e92901f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:56 +0000

    Add package s3utilz/0.1.0/s3utilz-0.1.0.tar.gz

---
+++ packages/s3utilz/s3utilz-0.1.0.tar.gz/s3utilz-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="s3utilz",
+    description="A s3 connection and read/write module for Python",
+    author = "Zebracat AI",
+    version="0.1.0",
+    install_requires=["boto3"],
+)
