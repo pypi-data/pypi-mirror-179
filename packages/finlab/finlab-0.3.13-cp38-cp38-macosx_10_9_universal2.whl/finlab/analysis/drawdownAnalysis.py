dbdb6919db4a3d5c6f1b7d147f969a002d9323c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:34 +0000

    Add package finlab/0.3.13/finlab-0.3.13-cp38-cp38-macosx_10_9_universal2.whl

---
+++ packages/finlab/finlab-0.3.13-cp38-cp38-macosx_10_9_universal2.whl/finlab/analysis/drawdownAnalysis.py
@@ -0,0 +1,38 @@
+from finlab.analysis import Analysis
+from finlab.ffn_core import drawdown_details
+
+
+class DrawdownAnalysis(Analysis):
+
+    @staticmethod
+    def drawdown(series):
+        drawdown = series.to_drawdown_series()
+        drawdown_obj = {
+                'values': drawdown.values.tolist(),
+                'index': drawdown.index.values.tolist()
+                }
+
+        details = (drawdown_details(drawdown)
+            .assign(Start=lambda df: df.Start.astype(str).str.split(' ').str[0])
+            .assign(End=lambda df: df.Start.astype(str).str.split(' ').str[0])
+            .set_index('Start')
+            )
+
+        longest_drawdown_details = (details
+            .sort_values('Length')
+            .tail(5)
+            .to_dict('index'))
+
+        max_drawdown_details = (details
+            .sort_values('drawdown')
+            .head(5)
+            .to_dict('index'))
+
+        return {'drawdown': drawdown_obj, 'longest_drawdown': longest_drawdown_details, 
+                'largest_drawdown':max_drawdown_details}
+
+    def analyze(self, report):
+        return {
+            'strategy': self.drawdown(report.daily_creturn),
+            'benchmark': self.drawdown(report.daily_benchmark)
+        }
