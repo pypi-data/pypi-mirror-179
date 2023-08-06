5ec06dcad9ba3bdd0d60e52fd1f792ff028e45f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:52 +0000

    Add package md410-2023-conv-common/2.23.0/md410_2023_conv_common-2.23.0.tar.gz

---
+++ packages/md410-2023-conv-common/md410_2023_conv_common-2.23.0.tar.gz/md410_2023_conv_common-2.23.0/md410_2023_conv_common/test.py
@@ -0,0 +1,19 @@
+import json
+
+import models
+from rich import print
+
+with open("data.json", "r") as fh:
+    reg_form_data_list = [fh.read()]
+
+reg_num = 1
+attendee_num = 1
+for reg_form_data in reg_form_data_list:
+    reg_form_dict = json.loads(reg_form_data)
+    reg_form_dict["reg_num"] = reg_num
+    for attendee_dict in reg_form_dict["attendees"]:
+        attendee_dict["attendee_num"] = attendee_num
+        attendee_num += 1
+    registration = models.Registration(**reg_form_dict)
+    print(registration)
+    reg_num += 1
