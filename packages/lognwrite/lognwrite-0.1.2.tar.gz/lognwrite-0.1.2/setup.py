178c236f16ea23ef6648d8c5186319dc3a9cadcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:08 +0000

    Add package lognwrite/0.1.2/lognwrite-0.1.2.tar.gz

---
+++ packages/lognwrite/lognwrite-0.1.2.tar.gz/lognwrite-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='lognwrite',
+    version='0.1.2',
+    license='MIT',
+    author="Muhammad Alif Putra Yasa",
+    author_email='malifputrayasa@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/malifpy/log-n-write',
+    keywords='log',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'typing_extensions',
+    ],
+)
