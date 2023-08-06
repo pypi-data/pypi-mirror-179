18e75f1757b3065c773608e8851495e6661deb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:46 +0000

    Add package seismicio/0.0.11/seismicio-0.0.11.tar.gz

---
+++ packages/seismicio/seismicio-0.0.11.tar.gz/seismicio-0.0.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.11'
+DESCRIPTION = 'Read and write .su files for seismic data'
+
+# Setting up
+setup(
+    name="seismicio",
+    version=VERSION,
+    author="cadu-sg",
+    author_email="",
+    description=DESCRIPTION,
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['numpy', 'fs'],
+    keywords=['python', 'io'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
