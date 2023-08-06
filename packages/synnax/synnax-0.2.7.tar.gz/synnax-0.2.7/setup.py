42844c764a2634f0f409f202aab33201392bbddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:42 +0000

    Add package synnax/0.2.7/synnax-0.2.7.tar.gz

---
+++ packages/synnax/synnax-0.2.7.tar.gz/synnax-0.2.7/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['synnax',
+ 'synnax.channel',
+ 'synnax.cli',
+ 'synnax.cli.console',
+ 'synnax.cli.flow',
+ 'synnax.config',
+ 'synnax.framer',
+ 'synnax.ingest',
+ 'synnax.io',
+ 'synnax.user']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyJWT>=2.5.0,<3.0.0',
+ 'keyring>=23.9.3,<24.0.0',
+ 'mypy>=0.971,<0.972',
+ 'pandas>=1.4.3,<2.0.0',
+ 'pick>=2.0.2,<3.0.0',
+ 'pydantic>=1.10.2,<2.0.0',
+ 'rich>=12.6.0,<13.0.0',
+ 'synnax-freighter==0.2.4',
+ 'websockets>=10.3,<11.0']
+
+entry_points = \
+{'console_scripts': ['credentials = synnax.cli.login:credentials',
+                     'ingest = synnax.cli.ingest:ingestion',
+                     'login = synnax.cli.login:login',
+                     'tsconvert = synnax.cli.util:convert_timestamp_precision']}
+
+setup_kwargs = {
+    'name': 'synnax',
+    'version': '0.2.7',
+    'description': 'Synnax Client Library',
+    'long_description': 'None',
+    'author': 'emiliano bonilla',
+    'author_email': 'emilbon99@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://synnaxlabs.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
