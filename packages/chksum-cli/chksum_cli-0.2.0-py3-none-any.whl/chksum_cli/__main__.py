c6d6f918a9fd7a9b0be9781fe8e3313c7ba2ec5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:57 +0000

    Add package chksum-cli/0.2.0/chksum_cli-0.2.0-py3-none-any.whl

---
+++ packages/chksum-cli/chksum_cli-0.2.0-py3-none-any.whl/chksum_cli/__main__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022, espehon
+# All rights reserved.
+
+#  This file is used as the entry point for chksum
+import sys
+
+from chksum import cli
+
+if __name__ == "__main__":
+    sys.exit(cli())
