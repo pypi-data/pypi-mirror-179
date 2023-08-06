63fbbb726749c1a413224f02e7a3c94baa8ff0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:38 +0000

    Add package kof-api/0.0.1/kof_api-0.0.1-py3-none-any.whl

---
+++ packages/kof-api/kof_api-0.0.1-py3-none-any.whl/kof_api/__init__.py
@@ -0,0 +1,48 @@
+## Default Classes ##
+from game_objects.Animal import Animal
+from game_objects.Building import Building
+from game_objects.City import City
+from game_objects.Item import Item
+from game_objects.NPC import NPC
+from game_objects.Player import Player
+from game_objects.Region import Region
+from game_objects.Ship import Ship
+
+## Utils ##
+from game_objects.Utils.Math import Math
+from game_objects.Utils.Country_City_Names import CCN
+
+
+## Default Armour Classes ##
+from game_objects.Items.Helmet import Helmet
+from game_objects.Items.Chestplate import Chestplate
+from game_objects.Items.Shield import Shield
+
+## NPC's ##
+from game_objects.NPCS.ArmourMerchant import ArmourMerchant
+
+## Ship's ##
+from game_objects.Ships.Sloop import Sloop
+
+## Animals ##
+from game_objects.Animals.Donkey import Donkey
+
+## Helmets ##
+from game_objects.Items.Helmets.Kettle_Hat import KettleHat
+from game_objects.Items.Chestplates.Spangenhelm import Spangenhelm
+
+## Chestplates ##
+from game_objects.Items.Chestplates.BronzeLamellar import BronzeLamellar
+from game_objects.Items.Chestplates.Hauberk import Hauberk
+from game_objects.Items.Chestplates.IronLamellar import IronLamellar
+from game_objects.Items.Chestplates.Jazerant import Jazerant
+from game_objects.Items.Chestplates.Kiritsuke import Kiritsuke
+from game_objects.Items.Chestplates.LeatherLamellar import LeatherLamellar
+from game_objects.Items.Chestplates.LoricaHamata import LoricaHamata
+from game_objects.Items.Chestplates.WoolGambeson import WoolGambeson
+
+
+## Shields ##
+
+from game_objects.Items.Shields.LegionaryScutum import LegionaryScutum
+from game_objects.Items.Shields.AuxiliaryScutum import AuxiliaryScutum
