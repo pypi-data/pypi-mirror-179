f19e70728d2344ef24ae650ecd668571549b6957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:40 +0000

    Add package fanxiaoxi/1.0.3/fanxiaoxi-1.0.3.tar.gz

---
+++ packages/fanxiaoxi/fanxiaoxi-1.0.3.tar.gz/fanxiaoxi-1.0.3/README.md
@@ -0,0 +1,8 @@
+# huiguang
+This software is some simple search
+
+# 安装
+pip install xiaoxi
+
+# 介绍
+搜索开始
---
+++ packages/fanxiaoxi/fanxiaoxi-1.0.3.tar.gz/fanxiaoxi-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fanxiaoxi',
+    version='1.0.3',
+    author='fan',
+    author_email='1014054577@qq.com',
+    description='This software is some simple search',
+    url='https://gitee.com/fan-xiaoxixi/fan-xiaoxis-operation.git',
+    packages=find_packages()
+)
