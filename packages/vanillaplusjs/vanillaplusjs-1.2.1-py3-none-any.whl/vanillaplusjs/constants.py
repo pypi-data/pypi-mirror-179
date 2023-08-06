9fdde68e1a7c0dd2a4aad64131bfad42e424be1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:28 +0000

    Add package vanillaplusjs/1.2.1/vanillaplusjs-1.2.1-py3-none-any.whl

---
+++ packages/vanillaplusjs/vanillaplusjs-1.2.1-py3-none-any.whl/vanillaplusjs/constants.py
@@ -0,0 +1,7 @@
+CONFIGURATION_VERSION = "1.0.0"
+"""The configuration version for vanillaplusjs.json, so that we can
+automatically migrate old configuration files.
+"""
+
+PROCESSOR_VERSION = "1.2.1"
+"""The version of vanillaplusjs we're using"""
