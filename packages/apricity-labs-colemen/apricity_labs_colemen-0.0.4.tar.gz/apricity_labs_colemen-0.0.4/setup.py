b77455ab3e9e312c96ba26bad8bfa156942ed10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:30 +0000

    Add package apricity-labs-colemen/0.0.4/apricity_labs_colemen-0.0.4.tar.gz

---
+++ packages/apricity-labs-colemen/apricity_labs_colemen-0.0.4.tar.gz/apricity_labs_colemen-0.0.4/setup.py
@@ -0,0 +1,58 @@
+
+# import colemen_utils as _c
+import build_utils as _bu
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'apricity_labs_colemen'
+LONG_DESCRIPTION = 'apricity_labs_colemen'
+
+PY_MODULES = _bu.list_py_modules()
+
+# Setting up
+setup(
+    name="apricity_labs_colemen",
+    version=VERSION,
+    author="Colemen Atwood",
+    author_email="<atwoodcolemen@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    py_modules=[
+        'apricity_labs',
+        "modules.Log",
+        "modules.Result",
+        "modules.Route",
+        "modules.Token",
+        "modules.Token.Token",
+        "modules.Validator",
+        "modules.Validator.cerberus",
+        "services.auth",
+        "services.auth.authorize",
+        "services.auth.routes",
+        "services.home",
+        "services.home.routes",
+        "settings",
+        "settings.auth",
+        "settings.master_control",
+        "settings.types"        
+        ],
+    # add any additional packages that
+    # need to be installed along with your package. Eg: 'caer'
+    install_requires=[
+        'colemen_utils',
+        'flask',
+        'pyjwt',
+        'cerberus',
+    ],
+
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
