f63a008ed031703f9746b2a14b95fd700ea0ebc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:08 +0000

    Add package pysumreg/1.0.3/pysumreg-1.0.3-py3-none-any.whl

---
+++ packages/pysumreg/pysumreg-1.0.3-py3-none-any.whl/pysumreg/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: iso-8859-1 -*-
+
+"""Configuration of pysumreg package."""
+
+from .sum_registers import SummationRegisters
+
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version()
+    del get_version
+except (LookupError, ModuleNotFoundError, Exception):
+    from importlib.metadata import version
+
+    __version__ = version("pysumreg")
+
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2022, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
