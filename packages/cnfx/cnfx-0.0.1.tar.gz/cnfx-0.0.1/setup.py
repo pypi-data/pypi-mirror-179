c6e690da16ab6de46ca9c1d400fbd79a2baa97dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:58 +0000

    Add package cnfx/0.0.1/cnfx-0.0.1.tar.gz

---
+++ packages/cnfx/cnfx-0.0.1.tar.gz/cnfx-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import sys
+import setuptools
+from distutils.core import setup
+
+
+setup(
+    name='cnfx',
+    version='0.0.1',
+    packages=['cnfx'],
+    url='https://twitter.com/maxtuno',
+    license='Proprietary',
+    author='Oscar Riveros',
+    description='CNFX - A base -i - 1 CNF encoder for complex field operations.',
+)
