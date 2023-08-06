748442e0d3562b545c6ca25ffd7ae49b995d9ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:36 +0000

    Add package cloudvision/1.7.2/cloudvision-1.7.2-py3-none-any.whl

---
+++ packages/cloudvision/cloudvision-1.7.2-py3-none-any.whl/fmp/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
+
+import fmp.wrappers_pb2 as wrappers
+import fmp.inet_pb2 as inet
+import fmp.yang_pb2 as yang
+import fmp.extensions_pb2 as extensions
