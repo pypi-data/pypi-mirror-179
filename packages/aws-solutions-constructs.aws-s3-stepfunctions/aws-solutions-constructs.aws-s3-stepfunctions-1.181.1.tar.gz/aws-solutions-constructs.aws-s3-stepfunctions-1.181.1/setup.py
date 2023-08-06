493fc0497c5ec13897d61632f92fb7295d814a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:33 +0000

    Add package aws-solutions-constructs.aws-s3-stepfunctions/1.181.1/aws-solutions-constructs.aws-s3-stepfunctions-1.181.1.tar.gz

---
+++ packages/aws-solutions-constructs.aws-s3-stepfunctions/aws-solutions-constructs.aws-s3-stepfunctions-1.181.1.tar.gz/aws-solutions-constructs.aws-s3-stepfunctions-1.181.1/setup.py
@@ -0,0 +1,75 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-solutions-constructs.aws-s3-stepfunctions",
+    "version": "1.181.1",
+    "description": "CDK Constructs for AWS S3 to AWS Step Functions integration",
+    "license": "Apache-2.0",
+    "url": "https://github.com/awslabs/aws-solutions-constructs.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-solutions-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_solutions_constructs.aws_s3_stepfunctions",
+        "aws_solutions_constructs.aws_s3_stepfunctions._jsii"
+    ],
+    "package_data": {
+        "aws_solutions_constructs.aws_s3_stepfunctions._jsii": [
+            "aws-s3-stepfunctions@1.181.1.jsii.tgz"
+        ],
+        "aws_solutions_constructs.aws_s3_stepfunctions": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "aws-cdk.aws-cloudtrail==1.181.1",
+        "aws-cdk.aws-cloudwatch==1.181.1",
+        "aws-cdk.aws-events==1.181.1",
+        "aws-cdk.aws-iam==1.181.1",
+        "aws-cdk.aws-lambda==1.181.1",
+        "aws-cdk.aws-logs==1.181.1",
+        "aws-cdk.aws-s3==1.181.1",
+        "aws-cdk.aws-stepfunctions-tasks==1.181.1",
+        "aws-cdk.aws-stepfunctions==1.181.1",
+        "aws-cdk.core==1.181.1",
+        "aws-solutions-constructs.aws-eventbridge-stepfunctions==1.181.1",
+        "aws-solutions-constructs.core==1.181.1",
+        "constructs>=3.2.0, <4.0.0",
+        "jsii>=1.71.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
