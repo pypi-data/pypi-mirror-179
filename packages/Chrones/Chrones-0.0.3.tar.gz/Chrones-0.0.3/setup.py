2b123ef766b2a0b440d5666dc6c556e8e872beac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package Chrones/0.0.3/Chrones-0.0.3.tar.gz

---
+++ packages/Chrones/Chrones-0.0.3.tar.gz/Chrones-0.0.3/setup.py
@@ -0,0 +1,27 @@
+# Copyright 2020-2022 Laurent Cabaret
+# Copyright 2020-2022 Vincent Jacques
+
+# https://packaging.python.org/en/latest/
+
+import setuptools
+
+
+version = "0.0.3"
+
+setuptools.setup(
+    name="Chrones",
+    version=version,
+    description="Software development tool to visualize runtime statistics about your program and correlate them with its phases",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacquev6/Chrones",
+    author="Vincent Jacques",
+    author_email="vincent@vincent-jacuqes.net",
+    license="MIT",
+    install_requires=open("requirements.txt").readlines(),
+    entry_points={
+        "console_scripts": [
+            "chrones = Chrones.command_line_interface:main",
+        ],
+    },
+)
