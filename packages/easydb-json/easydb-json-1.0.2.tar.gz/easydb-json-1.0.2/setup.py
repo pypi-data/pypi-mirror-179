092733339bd6ed3a2b1e4ff712a7ac37073c0b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:37 +0000

    Add package easydb-json/1.0.2/easydb-json-1.0.2.tar.gz

---
+++ packages/easydb-json/easydb-json-1.0.2.tar.gz/easydb-json-1.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="easydb-json",
+    version="1.0.2",
+    author="Lactua",
+    author_email="lactua@lactua.com",
+    description="A simple way to handle database in json.",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lactua/easydb-json",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
