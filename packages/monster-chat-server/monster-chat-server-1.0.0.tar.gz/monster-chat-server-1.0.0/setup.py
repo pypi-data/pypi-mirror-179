524dc48ed977aecbe03688e5ed6fa00a4fee2548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:19 +0000

    Add package monster-chat-server/1.0.0/monster-chat-server-1.0.0.tar.gz

---
+++ packages/monster-chat-server/monster-chat-server-1.0.0.tar.gz/monster-chat-server-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="monster-chat-server",
+      version="1.0.0",
+      description="MMMMonsterChat server application",
+      author="KTo ETo",
+      author_email="kto@lovetou.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
