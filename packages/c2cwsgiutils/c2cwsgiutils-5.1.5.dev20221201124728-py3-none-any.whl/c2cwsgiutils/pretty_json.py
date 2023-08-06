83d3405c4eaa44aa416dc40e6ce04e7b337fec47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:06 +0000

    Add package c2cwsgiutils/5.1.5.dev20221201124728/c2cwsgiutils-5.1.5.dev20221201124728-py3-none-any.whl

---
+++ packages/c2cwsgiutils/c2cwsgiutils-5.1.5.dev20221201124728-py3-none-any.whl/c2cwsgiutils/pretty_json.py
@@ -0,0 +1,26 @@
+import warnings
+from typing import Any
+
+import pyramid.config
+import ujson
+from cornice.renderer import CorniceRenderer
+from pyramid.renderers import JSON
+
+
+def fast_dumps(v: Any, **_kargv: Any) -> str:
+    """Dump the json fast using ujson."""
+    return ujson.dumps(v, ensure_ascii=False, indent=2, sort_keys=True, escape_forward_slashes=False)
+
+
+def init(config: pyramid.config.Configurator) -> None:
+    """Initialize json and fast_json renderer, for backward compatibility."""
+    warnings.warn("init function is deprecated; use includeme instead")
+    includeme(config)
+
+
+def includeme(config: pyramid.config.Configurator) -> None:
+    """Initialize json and fast_json renderer."""
+    config.add_renderer("json", JSON(indent=2, sort_keys=True))
+    config.add_renderer("fast_json", JSON(serializer=fast_dumps))
+    config.add_renderer("cornice_json", CorniceRenderer(indent=2, sort_keys=True))
+    config.add_renderer("cornice_fast_json", CorniceRenderer(serializer=fast_dumps))
