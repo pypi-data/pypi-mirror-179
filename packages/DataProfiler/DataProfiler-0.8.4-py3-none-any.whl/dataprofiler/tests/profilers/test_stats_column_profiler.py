0cf218fac1e923189cad7dd252579a26abfcdb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:40 +0000

    Add package DataProfiler/0.8.4/DataProfiler-0.8.4-py3-none-any.whl

---
+++ packages/DataProfiler/DataProfiler-0.8.4-py3-none-any.whl/dataprofiler/tests/profilers/test_stats_column_profiler.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import os
+import unittest
+
+from dataprofiler.profilers.column_profile_compilers import ColumnStatsProfileCompiler
+from dataprofiler.tests.profilers.test_base_column_profilers import (
+    AbstractTestColumnProfiler,
+)
+
+test_root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+class TestColumnStatsProfiler(AbstractTestColumnProfiler, unittest.TestCase):
+
+    column_profiler = ColumnStatsProfileCompiler
+    profile_types = ["data type", "statistics"]
+
+    def setUp(self):
+        AbstractTestColumnProfiler.setUp(self)
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestColumnStatsProfiler, cls).setUpClass()
+
+
+if __name__ == "__main__":
+    unittest.main()
