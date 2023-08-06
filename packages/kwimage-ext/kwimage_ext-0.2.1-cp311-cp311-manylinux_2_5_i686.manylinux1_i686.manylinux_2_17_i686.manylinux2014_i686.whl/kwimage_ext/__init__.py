92b9ab075f2ab166c7115b3886b237c51507e9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:09 +0000

    Add package kwimage-ext/0.2.1/kwimage_ext-0.2.1-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/kwimage-ext/kwimage_ext-0.2.1-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/kwimage_ext/__init__.py
@@ -0,0 +1,12 @@
+"""
+Binary extensions for kwimage
+"""
+__version__ = '0.2.1'
+
+__author__ = 'Kitware Inc., Jon Crall'
+__author_email__ = 'kitware@kitware.com, jon.crall@kitware.com'
+__url__ = 'https://gitlab.kitware.com/computer-vision/kwimage_ext'
+
+__mkinit__ = """
+mkinit -m kwimage_ext
+"""
