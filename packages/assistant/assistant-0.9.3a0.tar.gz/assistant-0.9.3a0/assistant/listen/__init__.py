b9d85f437f39ec56b2e9fb0641928f5d2ba99190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:55 +0000

    Add package assistant/0.9.3a0/assistant-0.9.3a0.tar.gz

---
+++ packages/assistant/assistant-0.9.3a0.tar.gz/assistant-0.9.3a0/assistant/listen/__init__.py
@@ -0,0 +1,36 @@
+import os, sys
+
+from assistant import HOME
+
+def checkIfServiceIsOK(host="localhost", port="5068"):
+	try:
+		r = requests.get(f'http://{host}:{port}')
+		return r.ok
+	except (ConnectionError, Exception) as e:
+		return False
+
+def enable_service_now():
+    if not os.path.isfile("/usr/lib/systemd/user/assistant.listen.service"):
+        print("No listen service present.")
+        print("To download it type:")
+        print("    wget https://gitlab.com/waser-technologies/technologies/assistant/-/raw/main/assistant.listen.service.example")
+        print("    mv assistant.listen.service.example /usr/lib/systemd/user/assistant.listen.service")
+        sys.exit(1)
+    if not any([
+        os.path.exists(f"{HOME}/.config/systemd/user/default.target.wants/assistant.listen.service"),
+        os.path.exists("/usr/lib/systemd/user/default.target.wants/assistant.listen.service"),
+        checkIfServiceIsOK()
+    ]):
+        print("Listen STT service for Assistant is disabled.")
+        print("To enable it type:")
+        print(f"systemctl --user enable --now dmt listen assistant")
+        print(f"systemctl --user enable --now assistant.listen")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    from assistant.listen.main import main
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(1)
