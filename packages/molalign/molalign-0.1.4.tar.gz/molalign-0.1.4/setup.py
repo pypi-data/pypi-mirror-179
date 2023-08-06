c85316eab3434e6d430b6dde9bbbd1d97f22c7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:48 +0000

    Add package molalign/0.1.4/molalign-0.1.4.tar.gz

---
+++ packages/molalign/molalign-0.1.4.tar.gz/molalign-0.1.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
+from os import path
+
+f90_files = []
+with open(path.join('molalignlib', 'f90_files'), 'r') as file:
+    for line in file.readlines():
+        if not line.startswith('#'):
+            f90_files.append(path.join('molalignlib', line.rstrip()))
+
+f2py_files = []
+with open(path.join('molalignlib', 'f2py_files'), 'r') as file:
+    for line in file.readlines():
+        if not line.startswith('#'):
+            f2py_files.append(path.join('molalignlib', line.rstrip()))
+
+f90_compile_args = ['-std=f2008', '-O3', '-ffast-math']
+molalignlib = ('molalignlib', dict(sources=f90_files, extra_f90_compile_args=f90_compile_args))
+molalignlibext = Extension('molalignlibext', sources=f2py_files, libraries=['molalignlib', 'lapack'])
+
+setup(
+    libraries = [molalignlib],
+    ext_modules = [molalignlibext]
+)
