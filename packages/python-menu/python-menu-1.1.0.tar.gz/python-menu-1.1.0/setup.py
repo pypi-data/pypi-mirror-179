a9f46b061310a2870d04fde480b91e865049b6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:39 +0000

    Add package python-menu/1.1.0/python-menu-1.1.0.tar.gz

---
+++ packages/python-menu/python-menu-1.1.0.tar.gz/python-menu-1.1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='python-menu',
+    version='1.1.0',
+    description='Easy menu tool for gathering inputs',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bensmithyx/menus.git',
+    author='Ben Smith',
+    author_email='ben.work.smith@gmail.com',
+    license='GNU GPLv3',
+    packages=["menu"],
+    install_requires=[],
+
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
