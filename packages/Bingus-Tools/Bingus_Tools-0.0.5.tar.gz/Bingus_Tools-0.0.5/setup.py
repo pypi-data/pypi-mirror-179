6de9d81de2f6aea6fe4a430d4d74db728d6ee82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:51:15 +0000

    Add package Bingus-Tools/0.0.5/Bingus_Tools-0.0.5.tar.gz

---
+++ packages/Bingus-Tools/Bingus_Tools-0.0.5.tar.gz/Bingus_Tools-0.0.5/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name = "Bingus_Tools",
+    version = "0.0.5",
+    author = "MrHDumpty",
+    description = "Just some tools to make your coding easier!",
+    packages = ["Bingus_Tools"]
+)
