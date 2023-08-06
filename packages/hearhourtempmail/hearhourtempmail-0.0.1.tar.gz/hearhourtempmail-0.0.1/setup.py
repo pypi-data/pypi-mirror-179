19bd29c7cb596a45fb7d0a1f27146ef53412eac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:24 +0000

    Add package hearhourtempmail/0.0.1/hearhourtempmail-0.0.1.tar.gz

---
+++ packages/hearhourtempmail/hearhourtempmail-0.0.1.tar.gz/hearhourtempmail-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='hearhourtempmail',
+  version='0.0.1',
+  description='This for testing',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='HearHour',
+  author_email='hourfoto@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='hearhourtempmail',
+  packages=find_packages(),
+  install_requires=['']
+)
