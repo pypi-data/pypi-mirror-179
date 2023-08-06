570a166d0bc8fb0f45bb6ef37b44547bee65d3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:51 +0000

    Add package poetry-dotenv/0.2.7/poetry_dotenv-0.2.7.tar.gz

---
+++ packages/poetry-dotenv/poetry_dotenv-0.2.7.tar.gz/poetry_dotenv-0.2.7/src/poetry_dotenv/__init__.py
@@ -0,0 +1,16 @@
+"""poetry-dotenv - is the plugin that automatically loads environment variables from a dotenv file into the environment before poetry commands are run."""
+
+__title__ = "poetry-dotenv"
+__summary__ = "poetry-dotenv - is the plugin that automatically loads environment variables from a dotenv file into the environment before poetry commands are run."
+__uri__ = "https://github.com/volopivoshenko/poetry-dotenv"
+
+__version__ = "0.2.7"
+
+__author__ = "Volodymyr Pivoshenko"
+__email__ = "volodymyr.pivoshenko@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2022, Volodymyr Pivoshenko"
+
+from poetry_dotenv import dotenv
+from poetry_dotenv import plugin
