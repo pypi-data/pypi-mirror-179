199050c31b8eb5f269f9f1455d30d46c63637c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:00 +0000

    Add package pylertatron/1.0.4/pylertatron-1.0.4.tar.gz

---
+++ packages/pylertatron/pylertatron-1.0.4.tar.gz/pylertatron-1.0.4/pylertatron/async_/pylertatron.py
@@ -0,0 +1,23 @@
+from httpx import AsyncClient
+
+from ..Pylertatron import Pylertatron
+from ..commands import Command
+
+
+class AsyncPylertatron(Pylertatron):
+    def __init__(self, webhook_url, balance_ratio, api_key_name, client):
+        if type(client) is not AsyncClient:
+            raise TypeError("client must be of type httpx.AsyncClient, "
+                            "use create_pylertatron() to create a Pylertatron instance")
+        self.client: AsyncClient = client
+        super().__init__(webhook_url, balance_ratio, api_key_name)
+
+    async def send_alert(self, symbol: str, commands: list[Command], tags: list[str] = None):
+        alert = self.generate_alert(symbol, commands, tags)
+        data = {"message": alert}
+        await self.client.post(self.webhook_url, json=data)
+
+
+async def create_pylertatron(webhook_url, balance_ratio, api_key_name) -> AsyncPylertatron:
+    client = AsyncClient()
+    return AsyncPylertatron(webhook_url, balance_ratio, api_key_name, client=client)
