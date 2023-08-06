91309d05c06a0d647da619d6f40fb48c9970b98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:08 +0000

    Add package computer-vision-python/2.0.0/computer-vision-python-2.0.0.tar.gz

---
+++ packages/computer-vision-python/computer-vision-python-2.0.0.tar.gz/computer-vision-python-2.0.0/ComputerVision/__init__.py
@@ -0,0 +1,5 @@
+from ComputerVision.FaceDetection import Face_det
+from ComputerVision.PoseDet import Pose_Det
+from ComputerVision.Serialconn import Serial_object
+from ComputerVision.Handtracker import Hand_detector
+import ComputerVision
