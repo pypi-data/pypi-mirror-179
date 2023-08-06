d9786385d232646b5bbdefed3c2316a7e0628a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:20 +0000

    Add package addup/0.0.4/addup-0.0.4.tar.gz

---
+++ packages/addup/addup-0.0.4.tar.gz/addup-0.0.4/libname/addup.py
@@ -0,0 +1,16 @@
+def adds(a, b):
+    '''
+    sumation
+    res=a+b
+        
+    '''
+    #1. create copy of dataframe
+    import numpy as np
+    import pandas as pd
+
+    
+    try:
+        res=a+b
+        return res
+    except:
+        print("Something else went wrong")
---
+++ packages/addup/addup-0.0.4.tar.gz/addup-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+# from distutils.core import setup
+import codecs
+import os
+
+
+VERSION = '0.0.4'
+DESCRIPTION = 'addup'
+
+# Setting up
+setup(
+    name="addup",
+    version=VERSION,
+    author="Hong Tang",
+    author_email="<stanghong@gmail.com>",
+    description=DESCRIPTION,
+    # url = 'https://github.com/stanghong/pypi_publication', 
+    # packages=find_packages('addup'),
+    packages=['libname'],
+    install_requires=['numpy','pandas',],
+    # download_url = 'https://github.com/stanghong/pypi_publication/archive/refs/tags/v0.0.9.tar.gz',
+    
+    keywords=['python', 'test','plot'],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
