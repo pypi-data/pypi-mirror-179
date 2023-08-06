c56e14060a1d45d033d962c177d3ef5ae4de1664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:50 +0000

    Add package pastastore/0.9.1/pastastore-0.9.1-py3-none-any.whl

---
+++ packages/pastastore/pastastore-0.9.1-py3-none-any.whl/pastastore/__init__.py
@@ -0,0 +1,9 @@
+from . import connectors, util
+from .connectors import (
+    ArcticConnector,
+    DictConnector,
+    PasConnector,
+    PystoreConnector,
+)
+from .store import PastaStore
+from .version import __version__
