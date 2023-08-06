bd9c1936893398dd4dd290820b9e4ebe16c38440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:45 +0000

    Add package karaml/0.1.1/karaml-0.1.1.tar.gz

---
+++ packages/karaml/karaml-0.1.1.tar.gz/karaml-0.1.1/karaml/__init__.py
@@ -0,0 +1,17 @@
+
+#print("Running __init__.py in karaml package")
+print("Welcome to the KARaML Tools package (version 0.1.1)")
+import os
+HOST_DIR = os.getcwd() 
+if not os.path.exists('KARaML_Tools'):
+  os.mkdir('KARaML_Tools')
+import gdown
+gdown.download(url="https://colab.research.google.com/drive/14iuvYqoCWdUS_Adf3t1fBNS8tpOj-09Z", 
+               output='KARaML_Tools/karaml_setup.py',
+               quiet=True)
+os.chdir('KARaML_Tools')
+import karaml_setup
+os.chdir(HOST_DIR)
+
+#print("Import everything from karaml_tools.py")
+#from .karaml_tools import *
