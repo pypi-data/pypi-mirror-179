25ce05f6bf89877186c3fcb7f3249551e8982aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:06 +0000

    Add package dp-greet/0.3.0/dp_greet-0.3.0-py3-none-any.whl

---
+++ packages/dp-greet/dp_greet-0.3.0-py3-none-any.whl/dp_greet/main.py
@@ -0,0 +1,34 @@
+import typer
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Awesome Portal Gun
+    """
+
+
+@app.command()
+def common(upper: bool = typer.Option(False, help="Say hi uppercase.")):
+    if upper:
+        text = "HI MATE!" 
+    else:
+        text = "Hi mate!" 
+    typer.echo(text)
+
+
+@app.command()
+def official():
+    typer.echo("Hello, sir!")
+
+@app.command()
+def test():
+    print("Test OK!")
+
+
+@app.command()
+def name(name: str = typer.Option(...)):
+    print(f"Hello, {name}!")
