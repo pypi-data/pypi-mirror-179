c66ef7a800a18eece72c835215ea28c89a38d666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:55 +0000

    Add package tsdownsample/0.1.0a5/tsdownsample-0.1.0a5.tar.gz

---
+++ packages/tsdownsample/tsdownsample-0.1.0a5.tar.gz/tsdownsample-0.1.0a5/local_dependencies/downsample_rs/src/minmaxlttb/simd.rs
@@ -0,0 +1,124 @@
+use super::super::minmax;
+use super::generic::{minmaxlttb_generic, minmaxlttb_generic_without_x};
+
+// use num_traits::{Num, ToPrimitive};
+use super::super::lttb::utils::Num;
+use ndarray::{Array1, ArrayView1};
+
+extern crate argminmax;
+use argminmax::ArgMinMax;
+
+// ----------------------------------- NON-PARALLEL ------------------------------------
+
+// ----------- WITH X
+
+pub fn minmaxlttb_simd<Tx: Num, Ty: Num + PartialOrd>(
+    x: ArrayView1<Tx>,
+    y: ArrayView1<Ty>,
+    n_out: usize,
+    minmax_ratio: usize,
+) -> Array1<usize>
+where
+    for<'a> ArrayView1<'a, Ty>: ArgMinMax,
+{
+    minmaxlttb_generic(x, y, n_out, minmax_ratio, minmax::min_max_simd)
+}
+
+// ----------- WITHOUT X
+
+pub fn minmaxlttb_simd_without_x<Ty: Num + PartialOrd>(
+    y: ArrayView1<Ty>,
+    n_out: usize,
+    minmax_ratio: usize,
+) -> Array1<usize>
+where
+    for<'a> ArrayView1<'a, Ty>: ArgMinMax,
+{
+    minmaxlttb_generic_without_x(y, n_out, minmax_ratio, minmax::min_max_simd)
+}
+
+// ------------------------------------- PARALLEL --------------------------------------
+
+// ----------- WITH X
+
+pub fn minmaxlttb_simd_parallel<Tx: Num, Ty: Num + PartialOrd + Send + Sync>(
+    x: ArrayView1<Tx>,
+    y: ArrayView1<Ty>,
+    n_out: usize,
+    minmax_ratio: usize,
+) -> Array1<usize>
+where
+    for<'a> ArrayView1<'a, Ty>: ArgMinMax,
+{
+    minmaxlttb_generic(x, y, n_out, minmax_ratio, minmax::min_max_simd_parallel)
+}
+
+// ----------- WITHOUT X
+
+pub fn minmaxlttb_simd_without_x_parallel<Ty: Num + PartialOrd + Send + Sync>(
+    y: ArrayView1<Ty>,
+    n_out: usize,
+    minmax_ratio: usize,
+) -> Array1<usize>
+where
+    for<'a> ArrayView1<'a, Ty>: ArgMinMax,
+{
+    minmaxlttb_generic_without_x(y, n_out, minmax_ratio, minmax::min_max_simd_parallel)
+}
+
+// ---- TEST
+
+#[cfg(test)]
+mod tests {
+    use super::{minmaxlttb_simd, minmaxlttb_simd_without_x};
+    use super::{minmaxlttb_simd_parallel, minmaxlttb_simd_without_x_parallel};
+    use ndarray::{array, s, Array1};
+
+    extern crate dev_utils;
+    use dev_utils::utils;
+
+    fn get_array_f32(n: usize) -> Array1<f32> {
+        utils::get_random_array(n, f32::MIN, f32::MAX)
+    }
+
+    #[test]
+    fn test_minmaxlttb() {
+        let x = array![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let y = array![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+        let sampled_indices = minmaxlttb_simd(x.view(), y.view(), 4, 2);
+        assert_eq!(sampled_indices, array![0, 1, 5, 9]);
+    }
+
+    #[test]
+    fn test_minmaxlttb_without_x() {
+        let y = array![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+        let sampled_indices = minmaxlttb_simd_without_x(y.view(), 4, 2);
+        assert_eq!(sampled_indices, array![0, 1, 5, 9]);
+    }
+
+    #[test]
+    fn test_minmaxlttb_parallel() {
+        let x = array![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let y = array![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+        let sampled_indices = minmaxlttb_simd_parallel(x.view(), y.view(), 4, 2);
+        assert_eq!(sampled_indices, array![0, 1, 5, 9]);
+    }
+
+    #[test]
+    fn test_minmaxlttb_without_x_parallel() {
+        let y = array![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+        let sampled_indices = minmaxlttb_simd_without_x_parallel(y.view(), 4, 2);
+        assert_eq!(sampled_indices, array![0, 1, 5, 9]);
+    }
+
+    #[test]
+    fn test_many_random_runs_same_output() {
+        let n = 20_000;
+        for _ in 0..100 {
+            let arr = get_array_f32(n);
+            let idxs1 = minmaxlttb_simd_without_x(arr.view(), 100, 5);
+            let idxs2 = minmaxlttb_simd_without_x_parallel(arr.view(), 100, 5);
+            assert_eq!(idxs1, idxs2);
+        }
+    }
+}
---
+++ packages/tsdownsample/tsdownsample-0.1.0a5.tar.gz/tsdownsample-0.1.0a5/tests/test_tsdownsample.py
@@ -0,0 +1,188 @@
+import numpy as np
+import pytest
+
+from tsdownsample import (  # MeanDownsampler,; MedianDownsampler,
+    EveryNthDownsampler,
+    LTTBDownsampler,
+    M4Downsampler,
+    MinMaxDownsampler,
+    MinMaxLTTBDownsampler,
+)
+
+# Very basic & poor tests
+# TODO: Improve tests
+#   - test with and without x
+#   - test with and without parallel
+#   - test with different data types
+#   - test with different downsamplers
+#   - compare implementations with existing plotly_resampler implementations
+
+
+def test_m4_downsampler():
+    """Test M4 downsampler."""
+    arr = np.array(np.arange(10_000))
+    s_downsampled = M4Downsampler().downsample(arr, n_out=100)
+    assert s_downsampled[0] == 0
+    assert s_downsampled[-1] == len(arr) - 1
+
+
+def test_minmax_downsampler():
+    """Test MinMax downsampler."""
+    arr = np.array(np.arange(10_000))
+    s_downsampled = MinMaxDownsampler().downsample(arr, n_out=100)
+    assert s_downsampled[0] == 0
+    assert s_downsampled[-1] == len(arr) - 1
+
+
+def test_lttb_downsampler():
+    """Test LTTB downsampler."""
+    arr = np.array(np.arange(10_000))
+    s_downsampled = LTTBDownsampler().downsample(arr, n_out=100)
+    assert s_downsampled[0] == 0
+    assert s_downsampled[-1] == len(arr) - 1
+
+
+def test_minmaxlttb_downsampler():
+    """Test MinMaxLTTB downsampler."""
+    arr = np.array(np.arange(10_000))
+    s_downsampled = MinMaxLTTBDownsampler().downsample(arr, n_out=100)
+    assert s_downsampled[0] == 0
+    assert s_downsampled[-1] == len(arr) - 1
+
+
+# def test_mean_downsampler():
+#     """Test Mean downsampler."""
+#     arr = np.array(np.arange(10_000))
+#     s = pd.Series(arr)
+#     s_downsampled = MeanDownsampler().downsample(s, 100)
+#     assert s_downsampled.values[0] == 49.5
+#     assert s_downsampled.values[-1] == 9_949.5
+
+
+# def test_median_downsampler():
+#     """Test Median downsampler."""
+#     arr = np.array(np.arange(10_000))
+#     s = pd.Series(arr)
+#     s_downsampled = MedianDownsampler().downsample(s, 100)
+#     assert s_downsampled.values[0] == 49.5
+#     assert s_downsampled.values[-1] == 9_949.5
+
+
+def test_everynth_downsampler():
+    """Test EveryNth downsampler."""
+    arr = np.array(np.arange(10_000))
+    s_downsampled = EveryNthDownsampler().downsample(arr, n_out=100)
+    assert s_downsampled[0] == 0
+    assert s_downsampled[-1] == 9_900
+
+
+## Parallel downsampling
+
+rust_downsamplers = [
+    MinMaxDownsampler(),
+    M4Downsampler(),
+    LTTBDownsampler(),
+    MinMaxLTTBDownsampler(),
+]
+
+
+def test_parallel_downsampling():
+    """Test parallel downsampling."""
+    arr = np.random.randn(10_000).astype(np.float32)
+    for downsampler in rust_downsamplers:
+        s_downsampled = downsampler.downsample(arr, n_out=100, parallel=False)
+        s_downsampled_p = downsampler.downsample(arr, n_out=100, parallel=True)
+        assert np.all(s_downsampled == s_downsampled_p)
+
+
+## Using x
+
+
+def test_downsampling_with_x():
+    """Test downsampling with x."""
+    arr = np.random.randn(10_000).astype(np.float32)
+    idx = np.arange(len(arr))
+    for downsampler in rust_downsamplers:
+        s_downsampled = downsampler.downsample(arr, n_out=100)
+        s_downsampled_x = downsampler.downsample(idx, arr, n_out=100)
+        assert np.all(s_downsampled == s_downsampled_x)
+
+
+## Data types
+
+supported_dtypes = [
+    np.float16,
+    np.float32,
+    np.float64,
+    np.int16,
+    np.int32,
+    np.int64,
+    np.uint16,
+    np.uint32,
+    np.uint64,
+]
+
+
+def test_downsampling_different_dtypes():
+    """Test downsampling with different data types."""
+    arr_orig = np.random.randint(0, 100, size=10_000)
+    res = []
+    for dtype in supported_dtypes:
+        arr = arr_orig.astype(dtype)
+        s_downsampled = MinMaxDownsampler().downsample(arr, n_out=100)
+        res += [s_downsampled]
+    for i in range(1, len(res)):
+        assert np.all(res[0] == res[i])
+
+
+def test_downsampling_different_dtypes_with_x():
+    """Test downsampling with different data types."""
+    arr_orig = np.random.randint(0, 100, size=10_000)
+    idx_orig = np.arange(len(arr_orig))
+    for dtype_x in supported_dtypes:
+        res = []
+        idx = idx_orig.astype(dtype_x)
+        for dtype_y in supported_dtypes:
+            arr = arr_orig.astype(dtype_y)
+            s_downsampled = MinMaxLTTBDownsampler().downsample(idx, arr, n_out=100)
+            res += [s_downsampled]
+        for i in range(1, len(res)):
+            assert np.all(res[0] == res[i])
+
+
+### Unsupported dtype
+
+
+def test_error_unsupported_dtype():
+    """Test unsupported dtype."""
+    arr = np.random.randint(0, 100, size=10_000)
+    arr = arr.astype(np.bool)
+    with pytest.raises(ValueError):
+        MinMaxDownsampler().downsample(arr, n_out=100)
+
+
+def test_error_invalid_args():
+    """Test invalid arguments."""
+    arr = np.random.randint(0, 100, size=10_000)
+    # No args
+    with pytest.raises(ValueError) as e_msg:
+        MinMaxDownsampler().downsample(n_out=100, parallel=True)
+    assert "takes 1 or 2 positional arguments" in str(e_msg.value)
+    # Too many args
+    with pytest.raises(ValueError) as e_msg:
+        MinMaxDownsampler().downsample(arr, arr, arr, n_out=100, parallel=True)
+    assert "takes 1 or 2 positional arguments" in str(e_msg.value)
+    # Invalid y
+    with pytest.raises(ValueError) as e_msg:
+        MinMaxDownsampler().downsample(arr.reshape(5, 2_000), n_out=100, parallel=True)
+    assert "y must be 1D" in str(e_msg.value)
+    # Invalid x
+    with pytest.raises(ValueError) as e_msg:
+        MinMaxDownsampler().downsample(
+            arr.reshape(5, 2_000), arr, n_out=100, parallel=True
+        )
+    assert "x must be 1D" in str(e_msg.value)
+    # Invalid x and y (different length)
+    with pytest.raises(ValueError) as e_msg:
+        MinMaxDownsampler().downsample(arr, arr[:-1], n_out=100, parallel=True)
+    assert "x and y must have the same length" in str(e_msg.value)
