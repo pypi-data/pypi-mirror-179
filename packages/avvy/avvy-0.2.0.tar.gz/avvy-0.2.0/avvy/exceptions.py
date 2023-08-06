43d7e07d0df593acf53260b10cffdb25529fbe16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:33 +0000

    Add package avvy/0.2.0/avvy-0.2.0.tar.gz

---
+++ packages/avvy/avvy-0.2.0.tar.gz/avvy-0.2.0/avvy/exceptions.py
@@ -0,0 +1,16 @@
+class InvalidKeyException(Exception):
+	pass
+
+
+class DomainExpiredException(Exception):
+	pass
+
+
+class ResolverNotSetException(Exception):
+	pass
+
+
+class ReverseResolutionNotSupportedException(Exception):
+	pass
+
+
