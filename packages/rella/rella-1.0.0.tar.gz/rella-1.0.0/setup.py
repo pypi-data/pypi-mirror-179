e1e05ab37d91f2a50a71b68413f1200fe1f7c1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:33 +0000

    Add package rella/1.0.0/rella-1.0.0.tar.gz

---
+++ packages/rella/rella-1.0.0.tar.gz/rella-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name="rella",
+  version="1.0.0",
+  description="A simple Python package for tracking events with Rella",
+  url="https://github.com/rella/python-sdk",
+  author="Your Name",
+  author_email="harry@rella.so",
+  license="MIT",
+  packages=["."],
+  install_requires=["requests"],
+  zip_safe=False,
+)
