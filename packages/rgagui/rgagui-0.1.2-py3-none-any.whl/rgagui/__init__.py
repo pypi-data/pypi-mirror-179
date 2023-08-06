bb4745bf543d42be97ffa57ffe6cb99d185c3fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:57 +0000

    Add package rgagui/0.1.2/rgagui-0.1.2-py3-none-any.whl

---
+++ packages/rgagui/rgagui-0.1.2-py3-none-any.whl/rgagui/__init__.py
@@ -0,0 +1,9 @@
+
+from rgagui.task.task import Task
+from rgagui.task.inputs import IntegerInput, FloatInput, StringInput, ListInput, InstrumentInput
+from rgagui.plots.timeplot import TimePlot
+
+from rgagui.inst.instrument import Instrument
+from rgagui.inst.component import Component
+
+__version__ = "0.1.2"  # Global version number
