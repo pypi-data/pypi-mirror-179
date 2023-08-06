1945600a48346a61709913a25e43be64c900941a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:38 +0000

    Add package xia-api/0.0.20/xia_api-0.0.20-cp310-none-win_amd64.whl

---
+++ packages/xia-api/xia_api-0.0.20-cp310-none-win_amd64.whl/xia_api/__init__.py
@@ -0,0 +1,12 @@
+from xia_api.rest import RestApi
+from xia_api.open_api import OpenApi
+from xia_api.message import XiaCollectionDeleteMsg, XiaDocumentDeleteMsg, XiaFileMsg
+
+
+__all__ = [
+    "RestApi",
+    "OpenApi",
+    "XiaCollectionDeleteMsg", "XiaDocumentDeleteMsg", "XiaFileMsg",
+]
+
+__version__ = "0.0.20"
