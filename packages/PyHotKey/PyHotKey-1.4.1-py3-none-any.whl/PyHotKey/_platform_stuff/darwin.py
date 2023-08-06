48ab3316cdcd4c7e0490260795de1b82a2321571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:02 +0000

    Add package PyHotKey/1.4.1/PyHotKey-1.4.1-py3-none-any.whl

---
+++ packages/PyHotKey/PyHotKey-1.4.1-py3-none-any.whl/PyHotKey/_platform_stuff/darwin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019-2023 Xpp521
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+from pynput._util.darwin import keycode_context, keycode_to_string
+from pynput.keyboard._darwin import Key, KeyCode, Controller, Listener
+
+with keycode_context() as context:
+    _context = context
+
+
+def pre_process_key(key):
+    if isinstance(key, KeyCode):
+        return KeyCode(char=keycode_to_string(_context, key.vk))
+    return key
