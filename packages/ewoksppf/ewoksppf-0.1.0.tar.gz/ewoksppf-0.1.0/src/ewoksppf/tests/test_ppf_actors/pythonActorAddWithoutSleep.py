4aa1da5e8801a9694aa1c5180f1e0c78e3d1988d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:46 +0000

    Add package ewoksppf/0.1.0/ewoksppf-0.1.0.tar.gz

---
+++ packages/ewoksppf/ewoksppf-0.1.0.tar.gz/ewoksppf-0.1.0/src/ewoksppf/tests/test_ppf_actors/pythonActorAddWithoutSleep.py
@@ -0,0 +1,8 @@
+def run(value=None, **kwargs):
+
+    if value is None:
+        raise RuntimeError("Missing argument 'value'!")
+    value += 1
+    return {
+        "value": value,
+    }
---
+++ packages/ewoksppf/ewoksppf-0.1.0.tar.gz/ewoksppf-0.1.0/src/ewoksppf/tests/test_ppf_actors/pythonErrorHandlerTest.py
@@ -0,0 +1,9 @@
+def run(name, **kwargs):
+
+    reply = None
+    # This actor throws an exception
+    raise RuntimeError("Runtime error in pythonErrorHandlerTest.py!")
+    if name is not None:
+        reply = "Hello " + name + "!"
+
+    return {"reply": reply}
