fcdaed68d66714aa9d89143fe99d5a8ee30020a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:41 +0000

    Add package PyTangoArchiving/8.27.1/PyTangoArchiving-8.27.1.tar.gz

---
+++ packages/PyTangoArchiving/PyTangoArchiving-8.27.1.tar.gz/PyTangoArchiving-8.27.1/PyTangoArchiving/hdbpp/grant_privileges.sql
@@ -0,0 +1,4 @@
+grant all privileges on hdbpp.* to 'manager'@'localhost' identified by 'manager';
+grant all privileges on hdbpp.* to 'manager'@'%' identified by 'manager';
+grant all privileges on hdbpp.* to 'archiver'@'%' identified by 'archiver';
+grant all privileges on hdbpp.* to 'browser'@'%' identified by 'browser';
