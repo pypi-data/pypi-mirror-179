aafcc8ed46ef73f40a59a684b9e6fa0e5bfa3698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package allz/0.1.1/allz-0.1.1-py3-none-any.whl

---
+++ packages/allz/allz-0.1.1-py3-none-any.whl/allz/decompress/bz2_split_process.py
@@ -0,0 +1,23 @@
+from allz.libs.abstract_decompress import AbstractDecompress
+
+
+class Bz2SplitProcess(AbstractDecompress):
+    def __init__(self):
+        super().__init__()
+
+    def split_decompress(self, split_files, dest_path, is_force_mode=False):
+        cmd = ""
+        if len(split_files) > 0:
+            src_path = ".".join(str(split_files[0]).split(".")[:-1]) + ".*"
+            if is_force_mode:
+                cmd = f"cat {src_path} | tar jx -C {dest_path} --overwrite"
+            else:
+                cmd = f"cat {src_path} | tar jx -C {dest_path} --skip-old-files"
+
+        return cmd if cmd else None
+    
+    def handle(self, src_path, dest_path, is_force_mode=False):
+        pass
+
+    def decompress_test(self):
+        pass
