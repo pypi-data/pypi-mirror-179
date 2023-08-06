280ab37109fcd1b46da5b91b09d5d574d3f5265c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:29 +0000

    Add package trashpandas/0.0.6/trashpandas-0.0.6.tar.gz

---
+++ packages/trashpandas/trashpandas-0.0.6.tar.gz/trashpandas-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="trashpandas",
+    version="0.0.6",
+    description="Persistent Pandas DataFrame storage and retrieval using a SQL database, CSV files, HDF5, or pickle files.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eddiethedean/trashpandas",
+    author="Odos Matthews",
+    author_email="odosmatthews@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.9',
+    install_requires=['SQLAlchemy', 'pandas', 'h5py', 'tables']
+)
