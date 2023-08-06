6a6689cff0c6cfa87264680a06221b49380b0662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:18 +0000

    Add package testsing/0.0.1/testsing-0.0.1.tar.gz

---
+++ packages/testsing/testsing-0.0.1.tar.gz/testsing-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testsing',
+    version='0.0.1',
+    author='ZhangPeng',
+    author_email='apyizhidouzai@163.com',
+    description='A useless test tool',
+    packages=find_packages()
+)
