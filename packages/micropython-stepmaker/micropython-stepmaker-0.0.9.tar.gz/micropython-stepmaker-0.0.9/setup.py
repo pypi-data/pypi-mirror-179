8d803f4ef5d9bf5bee1773184a28a2cb000bc3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:38 +0000

    Add package micropython-stepmaker/0.0.9/micropython-stepmaker-0.0.9.tar.gz

---
+++ packages/micropython-stepmaker/micropython-stepmaker-0.0.9.tar.gz/micropython-stepmaker-0.0.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='micropython-stepmaker',
+version='0.0.9',
+description='Stepmaker Package For Stepmaker Kit Users and Rpi Pico Users.',
+long_description=long_description,
+long_description_content_type='text/markdown',
+author='HwangJungeon',
+author_email='hje@takeup.cc',
+license='CC BY-NC-SA 4.0',
+keywords=['stepmaker', 'step', 'maker', 'micropython'],
+packages=['stepmaker']
+)
