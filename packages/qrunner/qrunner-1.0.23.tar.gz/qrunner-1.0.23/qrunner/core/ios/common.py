7fc1d8b361a1fd06404153e2037f3d4bef168c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:09 +0000

    Add package qrunner/1.0.23/qrunner-1.0.23.tar.gz

---
+++ packages/qrunner/qrunner-1.0.23.tar.gz/qrunner-1.0.23/qrunner/core/ios/common.py
@@ -0,0 +1,36 @@
+import os
+import socket
+
+from qrunner.utils.exceptions import DeviceNotFoundException
+
+
+def get_device_list():
+    """获取当前连接的设备列表"""
+    cmd = 'tidevice list'
+    output = os.popen(cmd).read()
+    device_list = [item.split(' ')[0] for item in output.split('\n') if item]
+    if len(device_list) > 0:
+        return device_list
+    else:
+        raise DeviceNotFoundException(msg=f"无已连接设备")
+
+
+def get_current_device():
+    """连接一个手机时，返回设备id"""
+    device_list = get_device_list()
+    return device_list[0]
+
+
+def get_tcp_port(udid: str):
+    """获取可用端口号"""
+    # tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # tcp.bind(("", 0))
+    # _, port = tcp.getsockname()
+    # tcp.close()
+    port = int(udid.split('-')[0])
+    return port
+
+
+if __name__ == '__main__':
+    print(get_device_list())
+
