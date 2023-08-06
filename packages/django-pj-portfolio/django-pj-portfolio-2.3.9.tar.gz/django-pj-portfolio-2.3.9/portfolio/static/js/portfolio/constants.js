4875999276eb6a1d4852272338b57f0114c8e9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:15 +0000

    Add package django-pj-portfolio/2.3.9/django-pj-portfolio-2.3.9.tar.gz

---
+++ packages/django-pj-portfolio/django-pj-portfolio-2.3.9.tar.gz/django-pj-portfolio-2.3.9/portfolio/static/js/portfolio/constants.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('portfolio')
+        .constant('portfolioConfig', {
+            APIMinWaitTime: 5000, // 5 secs
+            APIMaxWaitTime: 10000, // 10 secs
+            APIStandardWaitTime: 1000 // 1 sec
+        });
+}());
