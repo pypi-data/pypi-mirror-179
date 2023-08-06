168ad4e34bd24ef9225bff66d4a6d2b2928f33d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package oauth-dropins/6.0/oauth-dropins-6.0.tar.gz

---
+++ packages/oauth-dropins/oauth-dropins-6.0.tar.gz/oauth-dropins-6.0/oauth_dropins/webutil/tests/__init__.py
@@ -0,0 +1,18 @@
+import logging, os, sys
+
+from ..appengine_info import DEBUG
+
+assert DEBUG
+creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+assert not creds or creds.endswith('fake_user_account.json')
+
+logging.getLogger('chardet').setLevel(logging.INFO)
+logging.getLogger('google.cloud').setLevel(logging.INFO)
+logging.getLogger('urllib3').setLevel(logging.INFO)
+
+# Piggyback on unittest's -v and -q flags to show/hide logging.
+logging.basicConfig()
+if 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
+  logging.disable(logging.CRITICAL + 1)
+elif '-v' in sys.argv:
+  logging.getLogger().setLevel(logging.DEBUG)
