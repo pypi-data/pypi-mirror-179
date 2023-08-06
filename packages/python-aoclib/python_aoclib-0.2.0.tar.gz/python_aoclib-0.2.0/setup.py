73c95be193b91f2c3d108a6e0825d6dae5bb1306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:04 +0000

    Add package python-aoclib/0.2.0/python_aoclib-0.2.0.tar.gz

---
+++ packages/python-aoclib/python_aoclib-0.2.0.tar.gz/python_aoclib-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aoclib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.21.0,<0.22.0']
+
+setup_kwargs = {
+    'name': 'python-aoclib',
+    'version': '0.2.0',
+    'description': 'A library for solving AdventOfCode problems using Python.',
+    'long_description': '# aoclib\nA library for solving AdventOfCode problems using Python.\n',
+    'author': 'Unknown15082',
+    'author_email': 'trangiahuy15082006@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
