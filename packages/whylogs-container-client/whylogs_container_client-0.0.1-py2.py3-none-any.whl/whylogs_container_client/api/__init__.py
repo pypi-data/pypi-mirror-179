07ba5bbb1aa432e56d232245a86d559f06c4cfe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:36 +0000

    Add package whylogs-container-client/0.0.1/whylogs_container_client-0.0.1-py2.py3-none-any.whl

---
+++ packages/whylogs-container-client/whylogs_container_client-0.0.1-py2.py3-none-any.whl/whylogs_container_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from whylogs_container_client.api.debug_api import DebugApi
+from whylogs_container_client.api.whylogs_api import WhylogsApi
