65d2ed80a8fd529e2cd592bc7766dfde6153d623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:54 +0000

    Add package faifah/0.0.1/faifah-0.0.1.tar.gz

---
+++ packages/faifah/faifah-0.0.1.tar.gz/faifah-0.0.1/faifah/__init__.py
@@ -0,0 +1,16 @@
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+import cmath
+import math
+import datetime
+
+# Require :
+# - numpy
+# - pandas
+
+
+class Grid:
+
+  def __init__(self, num ):
+    print(num)
