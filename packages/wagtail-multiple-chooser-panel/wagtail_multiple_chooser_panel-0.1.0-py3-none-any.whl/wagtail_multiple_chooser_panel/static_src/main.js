39eb54763427166e9070b38df53b528b5f9d94dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:59 +0000

    Add package wagtail-multiple-chooser-panel/0.1.0/wagtail_multiple_chooser_panel-0.1.0-py3-none-any.whl

---
+++ packages/wagtail-multiple-chooser-panel/wagtail_multiple_chooser_panel-0.1.0-py3-none-any.whl/wagtail_multiple_chooser_panel/static_src/main.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+
+import { InlinePanel } from './InlinePanel';
+
+class MultipleChooserPanel extends InlinePanel {
+    constructor(opts) {
+        super(opts);
+
+        this.chooserWidgetFactory = window.telepath.unpack(
+            JSON.parse(document.getElementById(`${ opts.formsetPrefix }-CHOOSER_WIDGET`).textContent)
+        );
+
+        $(`#${ opts.formsetPrefix }-OPEN_MODAL`).on('click', () => {
+            this.chooserWidgetFactory.openModal(
+                (result) => {
+                    result.forEach(item => {
+                        this.addForm();
+                        const formIndex = this.formCount - 1;
+                        const formPrefix = `${ opts.formsetPrefix }-${ formIndex }`;
+                        const chooserFieldId = `${ formPrefix }-${ opts.chooserFieldName }`
+                        const chooserWidget = this.chooserWidgetFactory.getById(chooserFieldId);
+                        chooserWidget.setStateFromModalData(item);
+                    });
+                },
+                {multiple: true}
+            );
+        });
+    }
+}
+
+window.MultipleChooserPanel = MultipleChooserPanel;
