7f19f1c5923c3db8a53b085355ae5fb2ef777481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:45 +0000

    Add package pystemd/0.11.0/pystemd-0.11.0.tar.gz

---
+++ packages/pystemd/pystemd-0.11.0.tar.gz/pystemd-0.11.0/pystemd/__version__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree.
+#
+
+import sys
+
+__version__ = "0.11.0"
+
+sys.modules[__name__] = __version__
