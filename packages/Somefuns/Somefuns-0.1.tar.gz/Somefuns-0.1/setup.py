a52550ad14aa3c69fb14b13aa49a02739efb1db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:26 +0000

    Add package Somefuns/0.1/Somefuns-0.1.tar.gz

---
+++ packages/Somefuns/Somefuns-0.1.tar.gz/Somefuns-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+setup(
+  name='Somefuns',
+  version='0.1',
+  description='Some funs of Environment and Climate Science for Group Liu',
+  url='https://www.python.org/',
+  author='Zhenyuan Cui',
+  author_email='chuan_yes@126.com',
+  python_requires=">=3.5.0",
+  license = "MIT Licence",
+  packages=find_packages(),
+  zip_safe=True
+     )
