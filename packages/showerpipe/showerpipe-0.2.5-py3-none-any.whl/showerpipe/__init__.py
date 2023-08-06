ca3c743ec0c284089aff68edd1c9b412aed13571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:19 +0000

    Add package showerpipe/0.2.5/showerpipe-0.2.5-py3-none-any.whl

---
+++ packages/showerpipe/showerpipe-0.2.5-py3-none-any.whl/showerpipe/__init__.py
@@ -0,0 +1,12 @@
+"""
+``showerpipe``
+==============
+
+Provides adapter and facade interfaces for high energy physics collision
+event generation programs for Pythonic usage, and exposing the data via
+NumPy arrays.
+"""
+from ._version import __version__
+
+
+__all__ = ["__version__"]
