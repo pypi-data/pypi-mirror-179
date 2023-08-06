1fd6af050347db459d0f069c6bbfaba30fe2f79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:29 +0000

    Add package page-object-elements/0.1.3/page-object-elements-0.1.3.tar.gz

---
+++ packages/page-object-elements/page-object-elements-0.1.3.tar.gz/page-object-elements-0.1.3/poe/__init__.py
@@ -0,0 +1,3 @@
+from .aspect import logger, log, exception_logger, timer
+from .elements import ScreenElement, InputElement, SectionElement, LabelElement, SelectElement, CheckBoxElement
+from .utility import get_methods_and_properties, get_all_methods_and_properties
