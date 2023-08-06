12302147dabb9b642cf68eeae2ffaa96ffe35df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:03 +0000

    Add package solidity-merkletools/0.0.2/solidity-merkletools-0.0.2.tar.gz

---
+++ packages/solidity-merkletools/solidity-merkletools-0.0.2.tar.gz/solidity-merkletools-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="solidity-merkletools",
+    version="0.0.2",
+    description="Merkle Tools",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    url="https://github.com/f8n/solidity-pymerkletools",
+    author="Hsu Han Ooi",
+    keywords="merkle tree, blockchain",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=False,
+    zip_safe=False,
+)
