69713184c7753f335fcd913ab4c55edbb0125de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:38 +0000

    Add package shubham/0.0.2/shubham-0.0.2.tar.gz

---
+++ packages/shubham/shubham-0.0.2.tar.gz/shubham-0.0.2/shubham/__init__.py
@@ -0,0 +1,9 @@
+
+
+def knowme():
+    print('''Hi, I'm Shubham Mohanty
+A passionate student, learner, science enthusiast from India.
+    A school student interested in programming, robotics, arduino, computers, gamedev and rc building.
+''')
+   
+    
