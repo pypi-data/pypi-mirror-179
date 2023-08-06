df1f5866b750a396f56dc193af72f61ffa5dcab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:32 +0000

    Add package cfr/0.1.6/cfr-0.1.6.tar.gz

---
+++ packages/cfr/cfr-0.1.6.tar.gz/cfr-0.1.6/cfr/__init__.py
@@ -0,0 +1,26 @@
+from .climate import ClimateField
+from .proxy import ProxyRecord, ProxyDatabase
+from .reconjob import ReconJob
+from .reconres import ReconRes
+from . import graphem, utils
+try:
+    from . import ml
+except:
+    pass
+
+from .visual import (
+    set_style,
+    showfig,
+    closefig,
+    savefig,
+)
+set_style(style='journal', font_scale=1.2)
+
+# get the version
+from importlib.metadata import version
+__version__ = version('cfr')
+
+
+# mute future warnings from pkgs like pandas
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
