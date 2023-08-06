bfea28970aa2258ebc9d76bada6461b13667d6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:02 +0000

    Add package cnfx/0.0.3/cnfx-0.0.3.tar.gz

---
+++ packages/cnfx/cnfx-0.0.3.tar.gz/cnfx-0.0.3/cnfx/test.py
@@ -0,0 +1,24 @@
+import math
+import cnfx
+
+enc = cnfx.Encoder(bit_depth=1, cnf_path='test.cnf')
+
+_0 = cnfx.Unit(enc, value=0)
+_e = cnfx.Unit(enc, value=math.e)
+_pi = cnfx.Unit(enc, value=math.pi)
+x = cnfx.Unit(enc)
+y = cnfx.Unit(enc)
+z = cnfx.Unit(enc)
+w = cnfx.Unit(enc)
+u = cnfx.Unit(enc)
+v = cnfx.Unit(enc)
+
+assert x + y - z == w - u - v + _e
+assert x - v != _0
+assert z + u == _pi
+
+while cnfx.satisfy(encoder=enc, solver='kissat'):
+    print(x.value, y.value, z.value, w.value, w.value, v.value,
+          x.value + y.value - z.value == w.value - u.value - v.value + _e.value,
+          x.value - v.value != _0.value,
+          z.value + u.value == _pi.value)
