14efd04e51a4e811cdf9e3fef0eebbff0b207c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:49 +0000

    Add package audplot/1.4.4/audplot-1.4.4.tar.gz

---
+++ packages/audplot/audplot-1.4.4.tar.gz/audplot-1.4.4/tests/test_api.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+import audplot
+
+
+np.random.seed(1)
+
+
+@pytest.mark.parametrize(
+    'number, expected_string',
+    [
+        (0, '0'),
+        (1, '1'),
+        (10, '10'),
+        (100, '100'),
+        (1000, '1k'),
+        (10000, '10k'),
+        (100000, '100k'),
+        (1000000, '1M'),
+        (0.1, '100m'),
+        (0.01, '10m'),
+        (0.001, '1m'),
+        (0.0015, '1.5m'),
+        (0.0001, '100u'),
+        (-1, '-1'),
+        (-0.001, '-1m'),
+        (-0.0015, '-1.5m'),
+        pytest.param(
+            1000 ** 9,
+            '',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+        pytest.param(
+            1000 ** -4,
+            '',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+    ]
+)
+def test_human_format(number, expected_string):
+    string = audplot.human_format(number)
+    assert string == expected_string
+
+
+def test_waveform():
+    # Fail for non mono signals
+    with pytest.raises(RuntimeError):
+        x = np.ones((2, 100))
+        audplot.waveform(x)
+    signal = np.random.randn(2000)
+    for n in [400, 800, 1200]:
+        audplot.waveform(signal[:n])
+        plt.close()
