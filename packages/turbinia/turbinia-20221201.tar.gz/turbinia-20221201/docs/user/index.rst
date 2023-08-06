7416a148a701929ebbc817a1c2e003b4ec9d5fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:28 +0000

    Add package turbinia/20221201/turbinia-20221201.tar.gz

---
+++ packages/turbinia/turbinia-20221201.tar.gz/turbinia-20221201/docs/user/index.rst
@@ -0,0 +1,20 @@
+#######################
+User documentation
+#######################
+
+.. toctree::
+   :maxdepth: 3
+   install
+   install-gke-celery
+   install-gke-pubsub
+   install-gke-monitoring
+   install-gcp-pubsub
+   gke-sre
+   how-it-works
+   turbinia-local-stack
+   using-docker
+   operational-details
+   recipes
+   debugging
+   faq
+   turbinia-web-ui
---
+++ packages/turbinia/turbinia-20221201.tar.gz/turbinia-20221201/turbinia/yapf_test.py
@@ -0,0 +1,29 @@
+"""Enforce code style with YAPF."""
+
+import os
+import subprocess
+import unittest
+
+
+class StyleTest(unittest.TestCase):
+  """Enforce code style requirements."""
+
+  def testCodeStyle(self):
+    """Check YAPF style enforcement runs cleanly."""
+    turbinia_path = os.path.abspath(os.path.dirname(__file__))
+    config_path = os.path.join(turbinia_path, '..', '.style.yapf')
+    try:
+      subprocess.check_output(
+          ['yapf', '--style', config_path, '--diff', '-r', turbinia_path])
+    except subprocess.CalledProcessError as exception:
+      if hasattr(exception, 'output'):
+        raise Exception(
+            'From the root directory of the repository, run '
+            '"yapf --style {0:s} -i -r {1:s}" to correct '
+            'these problems: {2:s}'.format(
+                config_path, turbinia_path, exception.output.decode('utf-8')))
+      raise
+
+
+if __name__ == '__main__':
+  unittest.main()
