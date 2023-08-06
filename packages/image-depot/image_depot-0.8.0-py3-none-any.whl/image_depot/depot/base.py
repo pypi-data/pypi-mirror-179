ad7eb1b90e0c50cef428b996a1522143abb0e24a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:17 +0000

    Add package image-depot/0.8.0/image_depot-0.8.0-py3-none-any.whl

---
+++ packages/image-depot/image_depot-0.8.0-py3-none-any.whl/image_depot/depot/base.py
@@ -0,0 +1,74 @@
+import os
+import random
+import uuid
+from abc import abstractmethod, ABC
+from typing import Optional
+import filetype
+from image_depot import DepotType
+from .error import DepotError
+
+
+class Depot(ABC):
+    def __init__(self):
+        self._err = None
+
+    @classmethod
+    @abstractmethod
+    def depot_type(cls) -> DepotType:
+        """
+        当前类型
+        :return:
+        """
+        pass
+
+    # 上传图片, 二进制内容
+    @abstractmethod
+    def upload(self, content) -> Optional[str]:
+        pass
+
+    # 上传图片, 本地路径
+    def upload_file(self, file_path) -> Optional[str]:
+        if not os.path.exists(file_path):
+            return self._set_error(f'file {file_path} not exist')
+        with open(file_path, 'rb') as f:
+            return self.upload(f.read())
+
+    def error(self) -> Optional[DepotError]:
+        """
+        获取错误信息
+        :return:
+        """
+        if self._err:
+            return self._err
+        return None
+
+    def _set_error(self, err) -> None:
+        self._err = DepotError(err, back_num=1)
+        return None
+
+    def _random_str(self, length: int, rand_str: str = None):
+        """
+        获取指定长度的随机字符串
+        :param length:
+        :param rand_str:
+        :return:
+        """
+        if not rand_str:
+            rand_str = '0123456789abcdefghijklmnopqrstuvwxyz'
+        return ''.join(random.sample(list(rand_str) * length, length))
+
+    def _random_file_name(self, content):
+        """
+        获取随机的文件名, 会自动识别文件后缀
+        :param content:
+        :return:
+        """
+        # 创建临时文件, 获取文件后缀
+        tmp_file = '/tmp/' + str(uuid.uuid4())
+        with open(tmp_file, 'wb') as f:
+            f.write(content)
+        suffix = filetype.guess_extension(tmp_file)
+        if not suffix:
+            return self._set_error(f'parse mimetype error.')
+        os.remove(tmp_file)
+        return str(uuid.uuid4()) + '.' + suffix
---
+++ packages/image-depot/image_depot-0.8.0-py3-none-any.whl/image_depot/depot_type.py
@@ -0,0 +1,96 @@
+"""
+@author hujing
+"""
+from enum import Enum, auto
+
+
+class DepotType(Enum):
+    CatBox = auto()
+    """
+    图床
+    
+    url: https://catbox.moe/
+    """
+    PngCm = auto()
+    """
+    图床
+    
+    url: https://png.cm/
+    """
+    Riyugo = auto()
+    """
+    限制: 
+        1. 图片3天内有效
+    url: https://riyugo.com/
+    """
+    NiuPic = auto()
+    """
+    牛客网
+    
+    url: https://www.niupic.com/
+    """
+    ImgUrl = auto()
+    """
+    免费账户有上传限制:
+        1. 每日限制上传15张图片
+        2. 每月限制上传400张
+    图片链接限制: 
+        1. 浏览器直接打开图片时, 会重定向到图床网址
+    url: https://www.imgurl.org/
+    """
+    SmMs = auto()
+    """
+    限制: 
+        1. 免费账号限制存储空间(可注册多个账号)
+        2. 图片链接空 referer 访问时, 会重定向到图床网址
+    url: https://sm.ms/
+    """
+    Imgbb = auto()
+    """
+    限制: 
+        1. 图片链接空 referer 访问时, 会重定向到图床网址
+    url: https://imgbb.com/
+    """
+    Xywm = auto()
+    """
+    图床
+        1. 注册用户存在限制, 可能登录后查看
+            * 限制并发数量及上传数量
+    
+    url: https://pic.xywm.ltd/
+    """
+    BoLuo = auto()
+    """
+    图床, 限制: 
+        1. 每天限制上传10张
+    url: https://www.boluo.link/
+    """
+    Hakaimg = auto()
+    """
+    url: https://hakaimg.com/
+    """
+    ImgAx = auto()
+    """
+    url: https://img.ax/
+    """
+    HuaBan = auto()
+    """
+    花瓣网站的图片上传. 图片上传接口在图集上传处发现
+    
+    限制: 
+        1. 需要先登录
+        2. 图片做了 referer 判断, 空 referer 可访问
+    url: https://huaban.com/
+    """
+    Ai58 = auto()
+    """
+    58同城客服, 在线上传图片
+    
+    url: https://ai.58.com
+    """
+    Github = auto()
+    """
+    借用 github 仓库
+    
+    url: https://github.com
+    """
