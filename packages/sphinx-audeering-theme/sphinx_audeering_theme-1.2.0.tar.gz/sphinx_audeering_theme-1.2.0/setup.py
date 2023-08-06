e3ad05eb1f3de1d84aaa953f2588ee9b548e5334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:47 +0000

    Add package sphinx-audeering-theme/1.2.0/sphinx_audeering_theme-1.2.0.tar.gz

---
+++ packages/sphinx-audeering-theme/sphinx_audeering_theme-1.2.0.tar.gz/sphinx_audeering_theme-1.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    use_scm_version=True,
+    package_data={
+        'sphinx_audeering_theme': [
+            'theme.conf',
+            '*.html',
+            'static/css/*.css',
+            'static/js/*.js',
+            'static/font/*.*',
+            'static/font/Lato/*.*',
+            'static/font/RobotoSlab/*.*',
+            'static/images/*.*',
+            'static/*.png',
+        ],
+    },
+    entry_points={
+        'sphinx.html_themes': [
+            'sphinx_audeering_theme = sphinx_audeering_theme',
+        ],
+    },
+)
