f3e3e517dddd6be55c22fc203671d5b01eb6f129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:42 +0000

    Add package fiser-tools/1.92/fiser_tools-1.92.tar.gz

---
+++ packages/fiser-tools/fiser_tools-1.92.tar.gz/fiser_tools-1.92/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name='fiser_tools',
+        version='1.92',
+        author="fiser",
+        author_email="sergi.garcia.munoz@gmail.com",
+        description="Personal DS tools",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/f1se4/fiser_tools",
+        packages=['fiser_tools'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        license='MIT',
+        install_requires=['pandas', 'matplotlib','seaborn',
+                          'numpy','plotly','kaleido',
+                          'yellowbrick','sklearn','statsmodels'],
+    )
