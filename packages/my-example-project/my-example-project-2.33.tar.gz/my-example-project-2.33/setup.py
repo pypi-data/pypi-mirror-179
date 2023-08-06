0ac137c43c2cc5b76a73e51403bb3fc9483ee925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:14 +0000

    Add package my-example-project/2.33/my-example-project-2.33.tar.gz

---
+++ packages/my-example-project/my-example-project-2.33.tar.gz/my-example-project-2.33/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['my-example-project', 'my-example-project.my-example-project']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry-version-plugin>=0.1.3,<0.2.0', 'toml>=0.10.2,<0.11.0']
+
+setup_kwargs = {
+    'name': 'my-example-project',
+    'version': '2.33',
+    'description': '',
+    'long_description': None,
+    'author': 'Lingling',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
