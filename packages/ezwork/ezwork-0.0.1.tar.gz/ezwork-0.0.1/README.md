d60d5c5484d4538389e004887e0138d8bbd73cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:33 +0000

    Add package ezwork/0.0.1/ezwork-0.0.1.tar.gz

---
+++ packages/ezwork/ezwork-0.0.1.tar.gz/ezwork-0.0.1/README.md
@@ -0,0 +1,7 @@
+#ezwork
+A simple exercise
+#安装
+pip install ezwork
+
+#准备工作
+无
---
+++ packages/ezwork/ezwork-0.0.1.tar.gz/ezwork-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+
+
+setup(
+    # 名称
+    name="ezwork",
+    # 版本号
+    version="0.0.1",
+    # 作者
+    author="GuoZhuang",
+    # 邮箱
+    author_email="1531016681@qq.com",
+    # 描述
+    description="A simple exercise",
+    #项目主页
+    url="",
+    # 要安装的包通过find_packages找到当前目录下有哪些包
+    packages = find_packages()
+)
+
+
