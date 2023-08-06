951cb056971a1ba2cb0fa0fe5c2cd5b6ae6efd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:42 +0000

    Add package hestia-earth-distribution/0.0.1/hestia-earth-distribution-0.0.1.tar.gz

---
+++ packages/hestia-earth-distribution/hestia-earth-distribution-0.0.1.tar.gz/hestia-earth-distribution-0.0.1/hestia_earth/distribution/utils/storage/_local_client.py
@@ -0,0 +1,21 @@
+import os
+
+
+def _get_folder() -> str:
+    folder = os.getenv('DISTRIBUTION_DATA_FOLDER', './data')
+    os.makedirs(folder, exist_ok=True)
+    return folder
+
+
+def _load_from_folder(folder: str, key: str):
+    try:
+        with open(os.path.join(folder, key), 'rb') as f:
+            return f.read()
+    except Exception:
+        # in case the file does not exist, should simply return None
+        return None
+
+
+def _write_to_folder(folder, filepath, content):
+    with open(os.path.join(folder, filepath), 'wb') as f:
+        f.write(content)
