c99067f4328d1642f9f27a7900c5b7851393bafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:56 +0000

    Add package hspylib-datasource/0.9.3/hspylib_datasource-0.9.3-py3-none-any.whl

---
+++ packages/hspylib-datasource/hspylib_datasource-0.9.3-py3-none-any.whl/datasource/__init__.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-firebase v0.9.3
+#
+# Package: main.datasource
+"""Package initialization."""
+
+__all__ = [
+  'cassandra', 
+  'crud_entity', 
+  'crud_repository', 
+  'crud_service', 
+  'db_configuration', 
+  'db_repository', 
+  'firebase', 
+  'identity', 
+  'mysql', 
+  'postgres', 
+  'redis', 
+  'sqlite'
+]
+__version__ = '0.9.3'
---
+++ packages/hspylib-datasource/hspylib_datasource-0.9.3-py3-none-any.whl/datasource/cassandra/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-firebase v0.9.3
+#
+# Package: main.datasource.cassandra
+"""Package initialization."""
+
+__all__ = [
+  'cassandra_configuration', 
+  'cassandra_repository'
+]
+__version__ = '0.9.3'
