80e8261ae989d42068f4c4acd3c4894da64b9c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:57 +0000

    Add package nonebot-plugin-pixivbot/1.5.4.post1/nonebot_plugin_pixivbot-1.5.4.post1.tar.gz

---
+++ packages/nonebot-plugin-pixivbot/nonebot_plugin_pixivbot-1.5.4.post1.tar.gz/nonebot_plugin_pixivbot-1.5.4.post1/src/nonebot_plugin_pixivbot/data/source/__init__.py
@@ -0,0 +1,46 @@
+from contextlib import asynccontextmanager
+from typing import Protocol, Callable, Union, Awaitable, Any
+
+from nonebot_plugin_pixivbot import context
+from nonebot_plugin_pixivbot.config import Config
+from nonebot_plugin_pixivbot.enums import DataSourceType
+
+
+class DataSource(Protocol):
+    async def initialize(self):
+        ...
+
+    async def close(self):
+        ...
+
+    def on_initializing(self, func: Callable[[], Union[None, Awaitable[None]]]):
+        ...
+
+    def on_initialized(self, func: Callable[[], Union[None, Awaitable[None]]]):
+        ...
+
+    def on_closing(self, func: Callable[[], Union[None, Awaitable[None]]]):
+        ...
+
+    def on_closed(self, func: Callable[[], Union[None, Awaitable[None]]]):
+        ...
+
+    def session(self) -> Any:
+        ...
+
+    @asynccontextmanager
+    async def session_scope(self):
+        ...
+
+
+conf = context.require(Config)
+if conf.pixiv_data_source == DataSourceType.mongo:
+    from .mongo import MongoDataSource
+
+    context.bind(DataSource, MongoDataSource)
+else:
+    from .sql import SqlDataSource
+
+    context.bind(DataSource, SqlDataSource)
+
+__all__ = ("DataSource",)
