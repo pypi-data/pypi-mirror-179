2137e883e513f11b542c244d837811443b4e66f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:10 +0000

    Add package Bingus-Tools/0.0.6/Bingus_Tools-0.0.6.tar.gz

---
+++ packages/Bingus-Tools/Bingus_Tools-0.0.6.tar.gz/Bingus_Tools-0.0.6/Bingus_Tools/tools.py
@@ -0,0 +1,30 @@
+def random_number(start:int, end:int):
+    import random
+    print(random.randint(start, end))
+def random_letter(case:str):
+    if case == "lower-case" or case == "lowercase" or case == "lower case":
+        import string
+        import random
+        print(random.choice(string.ascii_lowercase))
+    elif case == "upper-case" or case == "uppercase" or case == "upper case":
+        import string
+        import random
+        print(random.choice(string.ascii_uppercase))
+def random_string(length:int):
+    import random
+    import string
+    print(''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation, k=length)))
+def random_words(numberofwords:int):
+    for i in range(numberofwords):
+        import random
+        words = open('words.txt').read().splitlines()
+        randomword = random.choice(words)
+        print(randomword)
+def random_country():
+    import pycountry
+    import random
+    country = random.choice(list(pycountry.countries))
+    print(f"Name = {country.name}")
+    if hasattr(country, 'official_name'):
+        print(f"Official Name = {country.official_name}")
+
