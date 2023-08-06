b4972f92186b42911d27d7775bc34ef34e54b97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:41 +0000

    Add package sozlukpy/0.0.3/sozlukpy-0.0.3.tar.gz

---
+++ packages/sozlukpy/sozlukpy-0.0.3.tar.gz/sozlukpy-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sozlukpy",
+    version="0.0.3",
+    description="A basic Turkish dictionary",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Meinos10",
+    author_email="E.tmen2023@gmail.com",
+    license="MIT",
+    #classifiers = [
+    #"Programming Language :: Python :: 3",
+    #"License :: OSI Approved :: MIT License",
+    #"Operating System :: OS Independent",
+    #],
+    keywords=["sozlukpy", "sozluk", "sozluk-python"],
+    packages=find_packages(),
+    requires=["requests"]
+)
