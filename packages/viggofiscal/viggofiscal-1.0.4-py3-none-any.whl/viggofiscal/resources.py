1239ced0c37707f4dccc1d8f01813c62463a4b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:09 +0000

    Add package viggofiscal/1.0.4/viggofiscal-1.0.4-py3-none-any.whl

---
+++ packages/viggofiscal/viggofiscal-1.0.4-py3-none-any.whl/viggofiscal/resources.py
@@ -0,0 +1,14 @@
+SYSADMIN_EXCLUSIVE_POLICIES = [
+    # ('/arquivos_ibpt', ['POST']),
+    # ('/arquivos_ibpt/<id>', ['PUT', 'DELETE']),
+
+    # ('/ibpt_syncs', ['POST', 'GET']),
+    # ('/ibpt_syncs/<id>', ['PUT', 'GET', 'DELETE']),
+]
+SYSADMIN_RESOURCES = [
+
+]
+USER_RESOURCES = []
+
+# *ibpt* sysadmin
+# ibpt, ibpt_ncm, GET ALL
