38a434e1a4bbeaa1bb8ea2143f7e2409009c5782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:13 +0000

    Add package zato-ext-bunch/1.2/zato-ext-bunch-1.2.tar.gz

---
+++ packages/zato-ext-bunch/zato-ext-bunch-1.2.tar.gz/zato-ext-bunch-1.2/setup.py
@@ -0,0 +1,42 @@
+#!python
+# -*- coding: utf-8 -*-
+import sys, os, re
+from os.path import dirname, abspath, join
+from setuptools import setup
+
+
+HERE = abspath(dirname(__file__))
+readme = open(join(HERE, 'README.rst')).read()
+
+package_file = open(join(HERE, 'bunch/__init__.py'), 'r')
+__version__ = '1.2'
+
+setup(
+    name             = "zato-ext-bunch",
+    version          = __version__,
+    description      = "A dot-accessible dictionary (a la JavaScript objects)",
+    long_description = readme,
+    url              = "http://github.com/dsc/bunch",
+
+    author           = "David Schoonover",
+    author_email     = "dsc@less.ly",
+
+    packages         = ['bunch',],
+
+    keywords         = ['bunch', 'dict', 'mapping', 'container', 'collection'],
+    classifiers      = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+    # download_url     = "http://pypi.python.org/packages/source/b/bunch/bunch-%s.tar.gz" % __version__,
+    license          = 'MIT',
+)
