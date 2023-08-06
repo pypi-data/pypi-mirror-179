4fc9acc9365dd2d81212c35a05e8209f34544dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:41 +0000

    Add package robotpy-toolkit-7407/0.5.9/robotpy_toolkit_7407-0.5.9.tar.gz

---
+++ packages/robotpy-toolkit-7407/robotpy_toolkit_7407-0.5.9.tar.gz/robotpy_toolkit_7407-0.5.9/robotpy_toolkit_7407/subsystem_templates/drivetrain/differential_drivetrain.py
@@ -0,0 +1,29 @@
+from robotpy_toolkit_7407.unum import Unum
+
+from robotpy_toolkit_7407.motor import PIDMotor
+from robotpy_toolkit_7407.oi.joysticks import JoystickAxis
+from robotpy_toolkit_7407.subsystem import Subsystem
+from robotpy_toolkit_7407.utils import logger
+from robotpy_toolkit_7407.utils.units import rad, m, radians_per_meter, meters_per_second
+
+
+class DifferentialDrivetrain(Subsystem):
+    m_left: PIDMotor = None
+    m_right: PIDMotor = None
+    axis_x: JoystickAxis = None
+    axis_y: JoystickAxis = None
+    gear_ratio: radians_per_meter
+
+    def init(self):
+        logger.info("initializing differential drivetrain", "[differential_drivetrain]")
+        self.m_left.init()
+        self.m_right.init()
+        logger.info("initialization complete", "[differential_drivetrain]")
+
+    def set_motor_percent_output(self, left: float, right: float):
+        self.m_left.set_raw_output(left)
+        self.m_right.set_raw_output(right)
+
+    def set_motor_velocity(self, left_vel: meters_per_second, right_vel: meters_per_second):
+        self.m_left.set_target_velocity(left_vel * self.gear_ratio)
+        self.m_right.set_target_velocity(right_vel * self.gear_ratio)
