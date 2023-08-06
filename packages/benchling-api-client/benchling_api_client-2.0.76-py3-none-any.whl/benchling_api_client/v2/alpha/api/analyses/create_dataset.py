19fd3c002d4a0710b58024a8cfbf964d3d03ada0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:38 +0000

    Add package benchling-api-client/2.0.76/benchling_api_client-2.0.76-py3-none-any.whl

---
+++ packages/benchling-api-client/benchling_api_client-2.0.76-py3-none-any.whl/benchling_api_client/v2/alpha/api/analyses/create_dataset.py
@@ -0,0 +1,102 @@
+from typing import Any, Dict, Optional, Union
+
+import httpx
+
+from ...client import Client
+from ...models.bad_request_error import BadRequestError
+from ...models.dataset import Dataset
+from ...types import Response
+
+
+def _get_kwargs(
+    *,
+    client: Client,
+) -> Dict[str, Any]:
+    url = "{}/datasets".format(client.base_url)
+
+    headers: Dict[str, Any] = client.httpx_client.headers
+    headers.update(client.get_headers())
+
+    cookies: Dict[str, Any] = client.httpx_client.cookies
+    cookies.update(client.get_cookies())
+
+    return {
+        "url": url,
+        "headers": headers,
+        "cookies": cookies,
+        "timeout": client.get_timeout(),
+    }
+
+
+def _parse_response(*, response: httpx.Response) -> Optional[Union[Dataset, BadRequestError]]:
+    if response.status_code == 200:
+        response_200 = Dataset.from_dict(response.json(), strict=False)
+
+        return response_200
+    if response.status_code == 400:
+        response_400 = BadRequestError.from_dict(response.json(), strict=False)
+
+        return response_400
+    return None
+
+
+def _build_response(*, response: httpx.Response) -> Response[Union[Dataset, BadRequestError]]:
+    return Response(
+        status_code=response.status_code,
+        content=response.content,
+        headers=response.headers,
+        parsed=_parse_response(response=response),
+    )
+
+
+def sync_detailed(
+    *,
+    client: Client,
+) -> Response[Union[Dataset, BadRequestError]]:
+    kwargs = _get_kwargs(
+        client=client,
+    )
+
+    response = client.httpx_client.post(
+        **kwargs,
+    )
+
+    return _build_response(response=response)
+
+
+def sync(
+    *,
+    client: Client,
+) -> Optional[Union[Dataset, BadRequestError]]:
+    """Creates a dataset from a file inlined with the request as a multi-part form upload."""
+
+    return sync_detailed(
+        client=client,
+    ).parsed
+
+
+async def asyncio_detailed(
+    *,
+    client: Client,
+) -> Response[Union[Dataset, BadRequestError]]:
+    kwargs = _get_kwargs(
+        client=client,
+    )
+
+    async with httpx.AsyncClient() as _client:
+        response = await _client.post(**kwargs)
+
+    return _build_response(response=response)
+
+
+async def asyncio(
+    *,
+    client: Client,
+) -> Optional[Union[Dataset, BadRequestError]]:
+    """Creates a dataset from a file inlined with the request as a multi-part form upload."""
+
+    return (
+        await asyncio_detailed(
+            client=client,
+        )
+    ).parsed
