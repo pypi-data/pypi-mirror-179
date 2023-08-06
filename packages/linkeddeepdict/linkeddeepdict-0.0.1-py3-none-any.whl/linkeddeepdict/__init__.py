a3bca2365c7ca7e6594e2e83cfff092b8b31bf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:31 +0000

    Add package linkeddeepdict/0.0.1/linkeddeepdict-0.0.1-py3-none-any.whl

---
+++ packages/linkeddeepdict/linkeddeepdict-0.0.1-py3-none-any.whl/linkeddeepdict/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .linkeddeepdict import LinkedDeepDict
+#from .deepdict import DeepDict as Library
+from .linkeddeepdict import LinkedDeepDict as DeepDict
+
+__version__ = "0.0.1"
+
+__description__ = "Linked Deep Dictionaries in Python."
