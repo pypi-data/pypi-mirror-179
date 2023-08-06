0bb235ba477f167ebcf0b845a4d51ab0466a74a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:18 +0000

    Add package aws-solutions-constructs.aws-wafwebacl-apigateway/1.181.0/aws_solutions_constructs.aws_wafwebacl_apigateway-1.181.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-wafwebacl-apigateway/aws_solutions_constructs.aws_wafwebacl_apigateway-1.181.0-py3-none-any.whl/aws_solutions_constructs/aws_wafwebacl_apigateway/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_apigateway._jsii
+import aws_cdk.aws_autoscaling._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_elasticloadbalancingv2._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_wafv2._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-wafwebacl-apigateway",
+    "1.181.0",
+    __name__[0:-6],
+    "aws-wafwebacl-apigateway@1.181.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
