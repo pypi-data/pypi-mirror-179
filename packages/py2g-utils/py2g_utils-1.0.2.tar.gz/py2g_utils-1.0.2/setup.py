0b9ad864c408d214920ee70063c2fd7fbd723a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:35 +0000

    Add package py2g-utils/1.0.2/py2g_utils-1.0.2.tar.gz

---
+++ packages/py2g-utils/py2g_utils-1.0.2.tar.gz/py2g_utils-1.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py2g_utils',
+      version='1.0.2',
+      description='Some python utilities',
+      author='Yannick Habecker',
+      author_email='dev@y2g.de',
+      url='https://github.com/habecker/py2g-utils',
+      packages=['py2g_utils'],
+     )
