d7eca59d3bfd6a042dcb903c80d2763a5716a49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:06 +0000

    Add package posym/0.2.7/posym-0.2.7-cp310-cp310-win32.whl

---
+++ packages/posym/posym-0.2.7-cp310-cp310-win32.whl/posym/operations/identity.py
@@ -0,0 +1,40 @@
+from posym.operations import Operation
+import numpy as np
+
+
+class Identity(Operation):
+    def __init__(self, label):
+        super().__init__(label)
+
+    def __hash__(self):
+        return hash((self._label))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def get_measure(self, coordinates, modes, symbols, orientation=None):
+        self._measure_mode = [1.0] * len(modes)
+
+        return np.array(self._measure_mode)
+
+    def get_overlap_func(self, op_function1, op_function2, orientation=None):
+        return (op_function1*op_function2).integrate
+
+    def get_measure_pos(self, coordinates, symbols, orientation=None):
+        self._measure_coor = 0.0
+
+        return self._measure_coor
+
+    def apply_rotation(self, orientation):
+        pass
+
+    @property
+    def operation_matrix_list(self):
+        return [np.identity(3)]
+
+    def __mul__(self, other):
+        if not other.__class__.__bases__[0] is Operation:
+            raise Exception('Product only defined between Operation subclasses')
+        else:
+            return [Identity(self._label)]
+
