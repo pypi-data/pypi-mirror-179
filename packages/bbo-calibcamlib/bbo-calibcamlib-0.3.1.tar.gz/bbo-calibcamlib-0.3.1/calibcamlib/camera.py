7546c5de147be1ad853e465dd5778aabc26d880a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:35 +0000

    Add package bbo-calibcamlib/0.3.1/bbo-calibcamlib-0.3.1.tar.gz

---
+++ packages/bbo-calibcamlib/bbo-calibcamlib-0.3.1.tar.gz/bbo-calibcamlib-0.3.1/calibcamlib/camera.py
@@ -0,0 +1,55 @@
+import numpy as np
+from calibcamlib import distortion as dist  # TODO: make model variable
+
+
+class Camera:
+    def __init__(self, A, k, xi=0, offset=None, distortion=None):  # TODO: Implement variable distortion
+        if offset is None:
+            offset = [0, 0]
+
+        self.offset = offset
+        self.A = A.reshape(3, 3)
+        self.k = k.reshape(5)
+        self.xi = xi
+
+    def space_to_sensor(self, X, offset=None):
+        if offset is None:
+            offset = self.offset
+
+        assert self.k[2] == 0 and self.k[3] == 0 and self.k[4] == 0
+
+        if not self.xi == 0:
+            norm = np.linalg.norm(X, axis=-1, keepdims=True)
+            X = np.where(norm == 0, X, X / norm)
+            X[..., (2,)] = X[..., (2,)] + self.xi
+
+        # code from calibcam.multical_plot.project_board
+        x = X / X[:, 2, np.newaxis]
+        print(x[:, 0:2])
+        x[:, 0:2] = dist.distort(x[:, 0:2], self.k)
+
+        x = x @ self.A.T
+
+        return x[:, 0:2] - offset
+
+    def sensor_to_space(self, x, offset=None):
+        if offset is None:
+            offset = self.offset
+
+        # assert self.k[2] == 0 and self.k[3] == 0 and self.k[4] == 0
+        x = x + offset
+
+        X = np.zeros(shape=(x.shape[0], 3))
+        X[:, 0:2] = x
+        X[:, 2] = 1
+
+        X = X @ np.linalg.inv(self.A.T)
+
+        X[:, 0:2] = dist.distort_inverse(X[:, 0:2], self.k)
+
+        if not self.xi == 0:
+            X[..., (2,)] = X[..., (2,)] - self.xi
+
+        X /= np.sqrt(np.sum(X ** 2, axis=1))[:, np.newaxis]
+
+        return X
