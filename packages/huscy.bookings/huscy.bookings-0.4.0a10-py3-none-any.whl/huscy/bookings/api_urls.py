742c27c50e2cbd54dfec1412c5aab032b1f687aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:48 +0000

    Add package huscy.bookings/0.4.0a10/huscy.bookings-0.4.0a10-py3-none-any.whl

---
+++ packages/huscy.bookings/huscy.bookings-0.4.0a10-py3-none-any.whl/huscy/bookings/api_urls.py
@@ -0,0 +1,20 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
+
+from huscy.bookings import views
+from huscy.project_design.urls import project_router
+
+router = DefaultRouter()
+router.register('timeslots', views.TimeslotViewSet)
+
+project_router.register('timeslots', views.ListTimeslotsViewSet, basename='project-timeslots')
+
+experiment_router = NestedDefaultRouter(project_router, 'experiments', lookup='experiment')
+experiment_router.register('timeslots', views.ListTimeslotsViewSet, basename='experiment-timeslots')
+
+urlpatterns = [
+]
+
+urlpatterns += router.urls
+urlpatterns += project_router.urls
+urlpatterns += experiment_router.urls
