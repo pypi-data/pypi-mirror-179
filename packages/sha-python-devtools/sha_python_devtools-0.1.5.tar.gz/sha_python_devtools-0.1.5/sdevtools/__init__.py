79cc4567bd5bb8a6278d5edc130601d279beeca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:18 +0000

    Add package sha-python-devtools/0.1.5/sha_python_devtools-0.1.5.tar.gz

---
+++ packages/sha-python-devtools/sha_python_devtools-0.1.5.tar.gz/sha_python_devtools-0.1.5/sdevtools/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Rontomai"
+__email__ = "rontomai@gmail.com"
+
+
+from .style import *
+from .debug import *
+from .prettier import *
+from .timer import *
+from .version import VERSION
+
+__version__ = VERSION
