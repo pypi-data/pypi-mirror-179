178a44b8176db54221cd4288f7fcf3ae666911e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:49:42 +0000

    Add package sigmaepsilon/0.0.25rc1/sigmaepsilon-0.0.25rc1-py3-none-any.whl

---
+++ packages/sigmaepsilon/sigmaepsilon-0.0.25rc1-py3-none-any.whl/sigmaepsilon/topopt/fem/structure/domain.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .structure import Structure
+from ..mesh import FemMesh
+
+
+class Domain(Structure):
+    
+    def __init__(self, *args, mesh: FemMesh = None, **kwargs):
+        super().__init__(wrap=mesh)
+        
+        
+class VariableThicknessSheet(Domain):
+    ...
+        
