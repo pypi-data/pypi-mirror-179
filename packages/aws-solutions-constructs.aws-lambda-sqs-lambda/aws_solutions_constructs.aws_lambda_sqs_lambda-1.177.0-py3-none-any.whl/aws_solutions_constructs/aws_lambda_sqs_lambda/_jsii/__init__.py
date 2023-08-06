c3fadda56a1b62455d25dcb65699923a0ad6bd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:19 +0000

    Add package aws-solutions-constructs.aws-lambda-sqs-lambda/1.177.0/aws_solutions_constructs.aws_lambda_sqs_lambda-1.177.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-lambda-sqs-lambda/aws_solutions_constructs.aws_lambda_sqs_lambda-1.177.0-py3-none-any.whl/aws_solutions_constructs/aws_lambda_sqs_lambda/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_event_sources._jsii
+import aws_cdk.aws_sqs._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.aws_lambda_sqs._jsii
+import aws_solutions_constructs.aws_sqs_lambda._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-lambda-sqs-lambda",
+    "1.177.0",
+    __name__[0:-6],
+    "aws-lambda-sqs-lambda@1.177.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
