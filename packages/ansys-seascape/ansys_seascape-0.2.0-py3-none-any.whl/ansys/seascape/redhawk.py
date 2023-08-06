3d3e58d7473d44a52a44794ac6b2146c9da32bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:26 +0000

    Add package ansys-seascape/0.2.0/ansys_seascape-0.2.0-py3-none-any.whl

---
+++ packages/ansys-seascape/ansys_seascape-0.2.0-py3-none-any.whl/ansys/seascape/redhawk.py
@@ -0,0 +1,10 @@
+from .scapp import SCApp
+
+
+# RedHawkSC is same as SCApp for now but can be specialized later
+class RedHawkSC(SCApp):
+    """
+    A proxy class to handle the connection to RedHawk-SC installation.
+    """
+
+    pass
