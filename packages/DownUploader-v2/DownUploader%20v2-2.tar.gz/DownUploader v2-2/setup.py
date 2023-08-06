02395050075a15f9d401556eb3b6ef265e05fe42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:34 +0000

    Add package DownUploader-v2/2/DownUploader%20v2-2.tar.gz

---
+++ packages/DownUploader-v2/DownUploader%20v2-2.tar.gz/DownUploader v2-2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import *
+
+setup(
+    name='DownUploader v2',
+    version='2',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='benbrainard',
+    author_email='brainardbenjamin@gmail.com',
+    description='Video transfer automation'
+)
