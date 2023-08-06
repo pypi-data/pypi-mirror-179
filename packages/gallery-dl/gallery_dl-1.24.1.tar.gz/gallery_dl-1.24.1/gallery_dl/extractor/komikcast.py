c755d36666ea06e27ff707a238e3a6061916e85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:45 +0000

    Add package gallery-dl/1.24.1/gallery_dl-1.24.1.tar.gz

---
+++ packages/gallery-dl/gallery_dl-1.24.1.tar.gz/gallery_dl-1.24.1/gallery_dl/extractor/komikcast.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018-2022 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extractors for https://komikcast.me/"""
+
+from .common import ChapterExtractor, MangaExtractor
+from .. import text
+import re
+
+BASE_PATTERN = r"(?:https?://)?(?:www\.)?komikcast\.(?:me|com)"
+
+
+class KomikcastBase():
+    """Base class for komikcast extractors"""
+    category = "komikcast"
+    root = "https://komikcast.me"
+
+    @staticmethod
+    def parse_chapter_string(chapter_string, data=None):
+        """Parse 'chapter_string' value and add its info to 'data'"""
+        if not data:
+            data = {}
+
+        match = re.match(
+            r"(?:(.*) Chapter )?0*(\d+)([^ ]*)(?: (?:- )?(.+))?",
+            text.unescape(chapter_string),
+        )
+        manga, chapter, data["chapter_minor"], title = match.groups()
+
+        if manga:
+            data["manga"] = manga.partition(" Chapter ")[0]
+        if title and not title.lower().startswith("bahasa indonesia"):
+            data["title"] = title.strip()
+        else:
+            data["title"] = ""
+        data["chapter"] = text.parse_int(chapter)
+        data["lang"] = "id"
+        data["language"] = "Indonesian"
+
+        return data
+
+
+class KomikcastChapterExtractor(KomikcastBase, ChapterExtractor):
+    """Extractor for manga-chapters from komikcast.me"""
+    pattern = BASE_PATTERN + r"(/chapter/[^/?#]+/)"
+    test = (
+        (("https://komikcast.me/chapter"
+          "/apotheosis-chapter-02-2-bahasa-indonesia/"), {
+            "url": "74eca5c9b27b896816497f9b2d847f2a1fcfc209",
+            "keyword": "f3938e1aff9ad1f302f52447e9781b21f6da26d4",
+        }),
+        (("https://komikcast.me/chapter"
+          "/soul-land-ii-chapter-300-1-bahasa-indonesia/"), {
+            "url": "243a5250e210b40d17217e83b7547cefea5638bd",
+            "keyword": "cb646cfed3d45105bd645ab38b2e9f7d8c436436",
+        }),
+    )
+
+    def metadata(self, page):
+        info = text.extr(page, "<title>", " – Komikcast<")
+        return self.parse_chapter_string(info)
+
+    @staticmethod
+    def images(page):
+        readerarea = text.extr(
+            page, '<div class="main-reading-area', '</div')
+        return [
+            (text.unescape(url), None)
+            for url in re.findall(r"<img[^>]* src=[\"']([^\"']+)", readerarea)
+        ]
+
+
+class KomikcastMangaExtractor(KomikcastBase, MangaExtractor):
+    """Extractor for manga from komikcast.me"""
+    chapterclass = KomikcastChapterExtractor
+    pattern = BASE_PATTERN + r"(/(?:komik/)?[^/?#]+)/?$"
+    test = (
+        ("https://komikcast.me/komik/090-eko-to-issho/", {
+            "url": "08204f0a703ec5272121abcf0632ecacba1e588f",
+            "keyword": "837a7e96867344ff59d840771c04c20dc46c0ab1",
+        }),
+        ("https://komikcast.me/tonari-no-kashiwagi-san/"),
+    )
+
+    def chapters(self, page):
+        results = []
+        data = self.metadata(page)
+
+        for item in text.extract_iter(
+                page, '<a class="chapter-link-item" href="', '</a'):
+            url, _, chapter_string = item.rpartition('">Chapter ')
+            self.parse_chapter_string(chapter_string, data)
+            results.append((url, data.copy()))
+        return results
+
+    @staticmethod
+    def metadata(page):
+        """Return a dict with general metadata"""
+        manga , pos = text.extract(page, "<title>" , " – Komikcast<")
+        genres, pos = text.extract(
+            page, 'class="komik_info-content-genre">', "</span>", pos)
+        author, pos = text.extract(page, ">Author:", "</span>", pos)
+        mtype , pos = text.extract(page, ">Type:"  , "</span>", pos)
+
+        return {
+            "manga": text.unescape(manga),
+            "genres": text.split_html(genres),
+            "author": text.remove_html(author),
+            "type": text.remove_html(mtype),
+        }
---
+++ packages/gallery-dl/gallery_dl-1.24.1.tar.gz/gallery_dl-1.24.1/gallery_dl/extractor/zerochan.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2022 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extractors for https://www.zerochan.net/"""
+
+from .booru import BooruExtractor
+from ..cache import cache
+from .. import text, exception
+from xml.etree import ElementTree
+
+
+BASE_PATTERN = r"(?:https?://)?(?:www\.)?zerochan\.net"
+
+
+class ZerochanExtractor(BooruExtractor):
+    """Base class for zerochan extractors"""
+    category = "zerochan"
+    root = "https://www.zerochan.net"
+    filename_fmt = "{id}.{extension}"
+    archive_fmt = "{id}"
+    cookiedomain = ".zerochan.net"
+    cookienames = ("z_id", "z_hash")
+
+    def login(self):
+        if not self._check_cookies(self.cookienames):
+            username, password = self._get_auth_info()
+            if username:
+                self._update_cookies(self._login_impl(username, password))
+        # force legacy layout
+        self.session.cookies.set("v3", "0", domain=self.cookiedomain)
+
+    @cache(maxage=90*86400, keyarg=1)
+    def _login_impl(self, username, password):
+        self.log.info("Logging in as %s", username)
+
+        url = self.root + "/login"
+        headers = {
+            "Origin"  : self.root,
+            "Referer" : url,
+        }
+        data = {
+            "ref"     : "/",
+            "name"    : username,
+            "password": password,
+            "login"   : "Login",
+        }
+
+        response = self.request(url, method="POST", headers=headers, data=data)
+        if not response.history:
+            raise exception.AuthenticationError()
+
+        return response.cookies
+
+    def _parse_entry_html(self, entry_id):
+        url = "{}/{}".format(self.root, entry_id)
+        extr = text.extract_from(self.request(url).text)
+
+        return {
+            "id"    : entry_id,
+            "author": extr('"author": "', '"'),
+            "file_url": extr('"contentUrl": "', '"'),
+            "date"  : text.parse_datetime(extr(
+                '"datePublished": "', '"'), "%a %b %d %H:%M:%S %Y"),
+            "width" : extr('"width": "', ' '),
+            "height": extr('"height": "', ' '),
+            "size"  : text.parse_bytes(extr('"contentSize": "', 'B')),
+            "path"  : text.split_html(extr(
+                'class="breadcrumbs', '</p>'))[3::2],
+            "tags"  : extr('alt="Tags: Anime, ', '"').split(", ")
+        }
+
+    def _parse_entry_xml(self, entry_id):
+        url = "{}/{}?xml".format(self.root, entry_id)
+        item = ElementTree.fromstring(self.request(url).text)[0][-1]
+        #  content = item[4].attrib
+
+        return {
+            #  "id"    : entry_id,
+            #  "file_url": content["url"],
+            #  "width" : content["width"],
+            #  "height": content["height"],
+            #  "size"  : content["filesize"],
+            "name"  : item[2].text,
+            "tags"  : item[5].text.lstrip().split(", "),
+            "md5"   : item[6].text,
+        }
+
+
+class ZerochanTagExtractor(ZerochanExtractor):
+    subcategory = "tag"
+    directory_fmt = ("{category}", "{search_tags}")
+    pattern = BASE_PATTERN + r"/(?!\d+$)([^/?#]+)/?(?:\?([^#]+))?"
+    test = ("https://www.zerochan.net/Perth+%28Kantai+Collection%29", {
+        "pattern": r"https://static\.zerochan\.net/.+\.full\.\d+\.(jpg|png)",
+        "count": "> 24",
+        "keywords": {
+            "extension": r"re:jpg|png",
+            "file_url": "",
+            "filename": r"re:Perth.\(Kantai.Collection\).full.\d+",
+            "height": r"re:^\d+$",
+            "id": r"re:^\d+$",
+            "name": "Perth (Kantai Collection)",
+            "search_tags": "Perth (Kantai Collection)",
+            "size": r"re:^\d+k$",
+            "width": r"re:^\d+$",
+        },
+    })
+
+    def __init__(self, match):
+        ZerochanExtractor.__init__(self, match)
+        self.search_tag, self.query = match.groups()
+
+    def metadata(self):
+        return {"search_tags": text.unquote(
+            self.search_tag.replace("+", " "))}
+
+    def posts(self):
+        url = self.root + "/" + self.search_tag
+        params = text.parse_query(self.query)
+        params["p"] = text.parse_int(params.get("p"), 1)
+        metadata = self.config("metadata")
+
+        while True:
+            page = self.request(url, params=params).text
+            thumbs = text.extr(page, '<ul id="thumbs', '</ul>')
+            extr = text.extract_from(thumbs)
+
+            while True:
+                post = extr('<li class="', '>')
+                if not post:
+                    break
+
+                if metadata:
+                    entry_id = extr('href="/', '"')
+                    post = self._parse_entry_html(entry_id)
+                    post.update(self._parse_entry_xml(entry_id))
+                    yield post
+                else:
+                    yield {
+                        "id"    : extr('href="/', '"'),
+                        "name"  : extr('alt="', '"'),
+                        "width" : extr('title="', 'x'),
+                        "height": extr('', ' '),
+                        "size"  : extr('', 'B'),
+                        "file_url": "https://static." + extr(
+                            '<a href="https://static.', '"'),
+                    }
+
+            if 'rel="next"' not in page:
+                break
+            params["p"] += 1
+
+
+class ZerochanImageExtractor(ZerochanExtractor):
+    subcategory = "image"
+    pattern = BASE_PATTERN + r"/(\d+)"
+    test = ("https://www.zerochan.net/2920445", {
+        "pattern": r"https://static\.zerochan\.net/"
+                   r"Perth\.%28Kantai\.Collection%29\.full.2920445\.jpg",
+        "keyword": {
+            "author": "YukinoTokisaki",
+            "date": "dt:2020-04-24 21:33:44",
+            "file_url": str,
+            "filename": "Perth.(Kantai.Collection).full.2920445",
+            "height": "1366",
+            "id": "2920445",
+            "size": "1929k",
+            "width": "1920",
+        },
+    })
+
+    def __init__(self, match):
+        ZerochanExtractor.__init__(self, match)
+        self.image_id = match.group(1)
+
+    def posts(self):
+        post = self._parse_entry_html(self.image_id)
+        if self.config("metadata"):
+            post.update(self._parse_entry_xml(self.image_id))
+        return (post,)
