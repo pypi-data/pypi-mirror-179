0f1ffbf4816fd77f000a445531858b72c2eaedeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:31 +0000

    Add package ceODBC/3.1/ceODBC-3.1-cp38-cp38-win32.whl

---
+++ packages/ceODBC/ceODBC-3.1-cp38-cp38-win32.whl/ceODBC/__init__.py
@@ -0,0 +1,18 @@
+"""
+Initialization of package ceODBC.
+"""
+
+from .version import __version__
+from .constants import *
+from .constructors import *
+from .exceptions import *
+
+from .driver import Connection, data_sources, drivers
+
+def connect(dsn, autocommit=False):
+    """
+    Creates a connection to the database and returns a Connection object.
+    """
+    conn = Connection.__new__(Connection)
+    conn._connect(dsn, autocommit)
+    return conn
