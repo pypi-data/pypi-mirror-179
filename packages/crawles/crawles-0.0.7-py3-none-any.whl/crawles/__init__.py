bd5875dea1a51877fda524ae1c99d8d4cefe7510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:46 +0000

    Add package crawles/0.0.7/crawles-0.0.7-py3-none-any.whl

---
+++ packages/crawles/crawles-0.0.7-py3-none-any.whl/crawles/__init__.py
@@ -0,0 +1,14 @@
+from .CsvSave import csv_save
+from .api import get, post, session_get, session_post
+from .js_call import execjs
+
+
+__all__ = [
+    'execjs',
+    'csv_save',
+    'get',
+    'post',
+    'session_get',
+    'session_post']
+
+
---
+++ packages/crawles/crawles-0.0.7-py3-none-any.whl/crawles/response_optimized.py
@@ -0,0 +1,9 @@
+from .Response import Response
+
+
+def response_optimized(func):
+    def inner(*args, **kwargs):
+        reqs = Response(func(*args, **kwargs))  # Response对象
+        return reqs
+
+    return inner
