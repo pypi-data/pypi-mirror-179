ea163f4ce702e7278d1326f88900e058f6a9ea2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:22 +0000

    Add package music-assistant/1.8.8/music_assistant-1.8.8-py3-none-any.whl

---
+++ packages/music-assistant/music_assistant-1.8.8-py3-none-any.whl/music_assistant/music_providers/filesystem/__init__.py
@@ -0,0 +1,5 @@
+"""Package with FileSystem Music provider(s)."""
+
+from .base import FileSystemProviderBase  # noqa
+from .local import LocalFileSystemProvider  # noqa
+from .smb import SMBFileSystemProvider  # noqa
