fb7abd6031267892fdb0b136408db54aa5b55e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:34 +0000

    Add package aisearch/0.0.1/aisearch-0.0.1.tar.gz

---
+++ packages/aisearch/aisearch-0.0.1.tar.gz/aisearch-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="aisearch",
+    version="0.0.1",
+    author="LiuRuiYi",
+    author_email="2352129115@qq.com",
+    description="This project provides an easy way to search  using AI",
+    # 项目gitee主页
+    url="https://gitee.com/make-one/sddad",
+# 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+
+)
