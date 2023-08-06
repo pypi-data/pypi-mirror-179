60f83b4b928276266df061e3e81d3c1f92da013b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:26 +0000

    Add package zyc-love/0.0.6/zyc_love-0.0.6.tar.gz

---
+++ packages/zyc-love/zyc_love-0.0.6.tar.gz/zyc_love-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="zyc_love",
+    version="0.0.6",
+    author="Renaissance",
+    author_email="Renaissance3310@qq.com",
+    description=" Code of Love ",
+    url = "https://github.com/renaissancezyc/zyc-love.git",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+
+)
+
+
+
+
+
