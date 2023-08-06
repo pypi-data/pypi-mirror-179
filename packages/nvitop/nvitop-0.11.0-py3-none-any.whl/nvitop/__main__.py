14ce4722136fc6805a912f24b4f71c62c1697cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:02 +0000

    Add package nvitop/0.11.0/nvitop-0.11.0-py3-none-any.whl

---
+++ packages/nvitop/nvitop-0.11.0-py3-none-any.whl/nvitop/__main__.py
@@ -0,0 +1,12 @@
+# This file is part of nvitop, the interactive NVIDIA-GPU process viewer.
+# License: GNU GPL version 3.
+
+# pylint: disable=missing-module-docstring
+
+import sys
+
+from nvitop.cli import main
+
+
+if __name__ == '__main__':
+    sys.exit(main())
