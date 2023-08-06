7dcdc6e31122f88c782bdb45028e46eb32f0adc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:11 +0000

    Add package itk-montage/0.8.0/itk_montage-0.8.0-cp311-cp311-win_amd64.whl

---
+++ packages/itk-montage/itk_montage-0.8.0-cp311-cp311-win_amd64.whl/itk/Configuration/Montage_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('image_source', 'image_to_image_filter', 'phase_correlation_image_registration_method', 'phase_correlation_operator', 'phase_correlation_optimizer', 'tile_merge_image_filter', 'tile_montage', )
