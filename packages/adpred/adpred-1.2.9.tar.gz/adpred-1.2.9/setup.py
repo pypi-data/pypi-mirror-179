c3fbd97d6fd594fd3b463422f78717ec7269c3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:50 +0000

    Add package adpred/1.2.9/adpred-1.2.9.tar.gz

---
+++ packages/adpred/adpred-1.2.9.tar.gz/adpred-1.2.9/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = "adpred",
+    version="1.2.9",
+    author="Ariel Erijman",
+    author_email="aerijman@fredhutch.org, aerijman@neb.com",
+    description="Prediction of Transcription Activation Domains from protein sequences",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FredHutch/adpred-pkg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'keras==2.8.0',
+        'numpy==1.22.2',
+        'pandas==1.4.1',
+        'plotly==5.6.0',
+        'requests==2.27.1',
+        'scikit-learn==1.0.2',
+        'tensorflow==2.8.0',
+    ],
+    include_package_data=True,
+    scripts=[
+        'bin/run-adpred'
+    ]
+)
