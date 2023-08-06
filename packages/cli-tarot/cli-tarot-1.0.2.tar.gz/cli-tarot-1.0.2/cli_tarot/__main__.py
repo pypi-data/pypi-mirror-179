d1e9043e77778541123784d68e069d6cdde44aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:27 +0000

    Add package cli-tarot/1.0.2/cli-tarot-1.0.2.tar.gz

---
+++ packages/cli-tarot/cli-tarot-1.0.2.tar.gz/cli-tarot-1.0.2/cli_tarot/__main__.py
@@ -0,0 +1,304 @@
+import argparse
+import random
+import re
+import sys
+import time
+
+from . import ascii_art
+from . import constants
+from . import card_directory
+from . import card_meanings
+from . import output
+from . import readings
+
+from argparse import RawTextHelpFormatter
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="cli-tarot",
+        description="A command line tarot reader based on the Albano-Waite deck!",
+        formatter_class=RawTextHelpFormatter,
+        epilog="Happy tarot-ing! 😄"
+        )
+    group = parser.add_mutually_exclusive_group()
+
+    parser.add_argument("card",
+                        nargs="?",
+                        default=constants.default_count,
+                        choices=[None],
+                        help="Pass in no args for a single card reading"
+                        )
+
+    parser.add_argument("-i", "--interpretation",
+                        default=False,
+                        action="store_true",
+                        help="""
+        When invoked, this flag will display the meaning of
+        the cards.
+                        """,
+                        )
+
+    parser.add_argument("-a", "--art",
+                        default=False,
+                        action="store_true",
+                        help="""
+        Invoke this flag to show the card's art.
+                        """,
+                        )
+
+    parser.add_argument("-o", "--output",
+                        default=False,
+                        help="Directs the output to a text file stamped with current datetime.",
+                        action='store_true',
+                        )
+
+    group.add_argument("-f", "--free-draw",
+                       type=int,
+                       nargs="?",
+                       dest="free",
+                       const=constants.default_count,
+                       metavar="",
+                       help="""
+        'Free Draw'\n
+        Read a specific number of cards
+        (passed as an integer after the '-fd'
+        flag; defaults to a single-card pull)
+                       """,
+                       # required=False,
+                       )
+
+    group.add_argument("-s", "--seen_heard_held",
+                        help="""
+        'Seen Heard Held'\n
+        A 4-card spread which reveals:
+        Card 1 - What needs to be seen
+        Card 2 - What needs to be heard
+        Card 3 - What needs to be held
+        Card 4 - Action to be taken\n
+        Optional: Pull a single card afterwards as a clarifier
+                        """,
+                        dest="seen",
+                        action="store_true",
+                        required=False,
+                        )
+
+    group.add_argument("-cc", "--celtic_cross",
+                        help="""
+        'Celtic Cross'\n
+        A 10-card spread which reveals:
+        Card 1 - The present condition of the querent
+        Card 2 - Immediate influence / obstacles
+        Card 3 - Goal / destiny according to existing circumstances
+        Card 4 - Distant past foundation
+        Card 5 - Recent past events
+        Card 6 - Future influence
+        Card 7 - The questioner
+        Card 8 - Environmental factors
+        Card 9 - Inner emotions
+        Card 10 - Final results\n
+                        """,
+                        dest="celtic",
+                        action="store_true",
+                        required=False,
+                        )
+
+    group.add_argument("-ppf", "--past_present_future",
+                        help="""
+        'Past, Present, and Future'\n
+        A 3-card spread which reveals:
+        Card 1 - Past
+        Card 2 - Present
+        Card 3 - Future\n
+        Optional: Pull a single card afterwards as a clarifier
+                        """,
+                        dest="ppf",
+                        action="store_true",
+                        required=False,
+                        )
+
+    group.add_argument("-ch", "--choices",
+                        help="""
+        'Choices'\n
+        A 3-card spread which reveals:
+        Card 1 - Choice 1
+        Card 2 - Choice 2
+        Card 3 - Advice\n
+        Optional: Pull a single card afterwards as a clarifier
+                        """,
+                        dest="choices",
+                        action="store_true",
+                        required=False,
+                        )
+
+    group.add_argument("-cm", "--card_meaning",
+                        type=int,
+                        nargs="?",
+                        default=constants.default_meaning,
+                        help="""
+        Display the meaning of a specific card via
+        the assigned ID number (e.g., "-cm 0" will
+        look up the meaning for '(0) The Fool')
+        \n
+        Note: IDs range in number from 0 to 77
+                        """,
+                        choices=range(0,78),
+                        dest="meaning",
+                        metavar="",
+                        required=False,
+                        )
+
+    args = parser.parse_args()
+
+
+    reading = list(card_directory.card_dict.values())
+
+
+    # Great debugging print statement to see what args are invoked + what their values are!
+    # print(args)
+
+    list_index = list(card_meanings.meanings.keys())
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+
+    # For "Seen Heard Held" readings
+    if args.seen:
+        args.card = None
+        cards = random.sample(reading, k=constants.seen)
+
+        for i in range(len(cards)):
+            index = int(re.search(r'\((.*?)\)', cards[i]).group(1))
+        readings.seen_heard_held(cards)
+
+        for i in range(len(cards)):
+            index = int(re.search(r'\((.*?)\)', cards[i]).group(1))
+
+            if args.interpretation is True and args.art is False:
+                print("...interpreting...")
+                time.sleep(2)
+                print(f"\n{card_meanings.meanings[index]}\n")
+                if args.output is True:
+                    output.output_meaning("Seen_Heard_Held_Interpreted_", index)
+
+            if args.interpretation is False and args.art is True:
+                print("...generating card art...")
+                time.sleep(2)
+                print(f"\n{ascii_art.card_art[index]}\n")
+                if args.output is True:
+                    output.output_art("Seen_Heard_Held_Art_", index)
+
+            if args.interpretation is True and args.art is True:
+                print("...interpreting and generating card art...")
+                time.sleep(2)
+                print(f"\n{ascii_art.card_art[index]}\n")
+                print(f"\n{card_meanings.meanings[index]}\n")
+                if args.output is True:
+                    output.output_meaning_and_art("Seen_Heard_Held_Art_and_Meanings_", index)
+
+            if args.interpretation is False and args.art is False and args.output is True:
+                reading = card_directory.card_dict[index]
+                output.output_reading_only("Seen_Heard_Held_Reading_", reading)
+
+
+    # TODO: Celtic Cross Reading
+
+    # TODO: Past, Present, Future Spread
+
+    # TODO: Choices Spread
+
+    # When someone tries to look up a card but forgets to specify card index
+    elif args.card == 1 and args.meaning is None:
+        print("You need to specify a card ID! (choose between 0 and 77)")
+
+    # Looking up a card's meaning can only output to the terminal vs outputs to a file
+    elif args.card == 1 and args.meaning < 78:
+        meaning = list_index.index(args.meaning)
+        card = str(card_directory.card_dict[args.meaning])
+        if args.output is True:
+            print("\nSorry, output option is not available for this mode!")
+        if args.art is True:
+            time.sleep(1)
+            print(f"\nDisplaying card art for:\n{card}")
+            time.sleep(2)
+            print(f"\n{ascii_art.card_art[meaning]}\n")
+            time.sleep(2)
+        print(f"\n...looking up meaning for:\n{card}")
+        time.sleep(2)
+        print(f"{card_meanings.meanings[meaning]}\n")
+
+    # For single card readings
+    elif args.card == 1 and (args.free is None or args.free == 1):
+        cards = random.sample(reading, k=args.card)
+        index = int(re.search(r'\((.*?)\)', cards[0]).group(1))
+        args.free = None
+        readings.single_card_reading(cards)
+
+        if args.interpretation is False and args.art is True:
+            time.sleep(1.5)
+            print("...generating card art...")
+            time.sleep(2)
+            print(f"\n{ascii_art.card_art[index]}\n")
+            if args.output is True:
+                output.output_art("Single_Card_Art_", index)
+
+        if args.interpretation is True and args.art is False:
+            time.sleep(1.5)
+            print("...looking up interpretation...")
+            time.sleep(2)
+            print(f"\n{card_meanings.meanings[index]}\n")
+            if args.output is True:
+                output.output_meaning("Single_Card_Interpreted_", index)
+
+        if args.interpretation is True and args.art is True:
+            time.sleep(1.5)
+            print("...interpreting and generating card art...")
+            time.sleep(2)
+            print(f"\n{ascii_art.card_art[index]}\n")
+            print(f"\n{card_meanings.meanings[index]}\n")
+            if args.output is True:
+                output.output_meaning("Single_Card_Art_and_Interpretation_", index)
+
+        if args.interpretation is False and args.art is False:
+            if args.output is True:
+                output.output_reading_only("Single_Card_Reading_", card_directory.card_dict[index])
+
+    # For "Free Draw" readings, which involves any number of cards between 1 and 78
+    elif args.free in range(1, 79):
+        cards = random.sample(reading, k=args.free)
+        print(f"\n✨ You have pulled the following {args.free} cards: ✨\n")
+        time.sleep(1.5)
+        print(*cards, sep = "\n")
+        print("\r")
+        for i in range(len(cards)):
+            index = int(re.search(r'\((.*?)\)', cards[i]).group(1))
+
+            if args.interpretation is True and args.art is False:
+                print("...interpreting...")
+                time.sleep(2)
+                print(f"\n{card_meanings.meanings[index]}\n")
+                if args.output is True:
+                    output.output_meaning(f"{args.free}_Card_Draw_Interpreted_", index)
+
+            if args.interpretation is True and args.art is True:
+                print("...interpreting and generating card art...")
+                time.sleep(2)
+                print(f"\n{ascii_art.card_art[index]}\n")
+                print(f"\n{card_meanings.meanings[index]}\n")
+                if args.output is True:
+                    output.output_meaning_and_art(f"{args.free}_Card_Draw_Art_and_Interpretation_", index)
+
+            if args.interpretation is False and args.art is True:
+                print("...generating card art...")
+                time.sleep(2)
+                print(f"\n{ascii_art.card_art[index]}\n")
+                if args.output is True:
+                    output.output_art(f"{args.free}_Card_Draw_Art_Only_", index)
+
+            if args.interpretation is False and args.art is False and args.output is True:
+                reading = card_directory.card_dict[index]
+                output.output_reading_only(f"{args.free}_Card_Reading_", reading)
+
+    elif args.free not in range(1, 79):
+        print("""
+            You can't select a number of cards smaller than zero or
+            larger than the deck! (min: 1, max: 78)
+        """)
---
+++ packages/cli-tarot/cli-tarot-1.0.2.tar.gz/cli-tarot-1.0.2/cli_tarot/card_meanings.py
@@ -0,0 +1,405 @@
+meanings = {0: """
+    How to interpret The Fool card:
+
+    [ meaning ]
+    """,
+     1: """
+    The Magician represents strength of will, intuition, self-control, self-confidence,
+    autonomy, and diplomacy. The positive qualities of humankind: skill, initiative,
+    intelligence, discernment, and comprehension; freedom from control by others. All
+    of these can lead to success and the ability to control your environment.
+
+    Think about the times that you've wanted something so badly that you mustered every
+    ounce of your power to make it happen; the Magician represents *that* level of willpower.
+    This card also means skill, talent, and having all the tools you need to make incredible
+    things happen.
+
+    In effect, the Magician is one who has the potential to be like a god. If this card
+    represents the querent, then you are the one who has the power (or the potential for it)
+    to control your immediate world. Are you developing this potential or throwing it away on
+    demonstrations of mundanity? The rest of the cards in the reading can illuminate this.
+
+    The power is yours. It's up to you to stand in that power.
+
+    """,
+     2: """
+    How to interpret The High Priestess card:
+
+    [ meaning ]
+    """,
+     3: """
+    How to interpret The Empress card:
+
+    [ meaning ]
+     """,
+     4: """
+    How to interpret The Emperor card:
+
+    [ meaning ]
+    """,
+     5: """
+    How to interpret The Hierophant card:
+
+    [ meaning ]
+     """,
+     6: """
+    How to interpret The Lovers card:
+
+    [ meaning ]
+     """,
+     7: """
+    How to interpret The Chariot card:
+
+    [ meaning ]
+     """,
+     8: """
+    How to interpret Strength card:
+
+    [ meaning ]
+     """,
+     9: """
+    How to interpret The Hermit card:
+
+    [ meaning ]
+     """,
+     10: """
+    How to interpret Wheel of Fortune card:
+
+    [ meaning ]
+     """,
+     11: """
+    How to interpret Justice card:
+
+    [ meaning ]
+     """,
+     12: """
+    How to interpret The Hanged Man:
+
+    [ meaning ]
+     """,
+     13: """
+    How to interpret the Death card:
+
+    [ meaning ]
+     """,
+     14: """
+    How to interpret Temperance:
+
+    [ meaning ]
+    """,
+     15: """
+    How to interpret The Devil card:
+
+    [ meaning ]
+     """,
+     16: """
+    How to interpret The Tower card:
+
+    [ meaning ]
+     """,
+     17: """
+    How to interpret The Star card:
+
+    [ meaning ]
+     """,
+     18: """
+    How to interpret The Moon card:
+
+    [ meaning ]
+     """,
+     19: """
+    How to interpret The Sun card:
+
+    [ meaning ]
+     """,
+     20: """
+    How to interpret Judgment:
+
+    [ meaning ]
+     """,
+     21: """
+    How to interpret The World card:
+
+    [ meaning ]
+     """,
+     22: """
+    How to interpret the Ace of Wands:
+
+    [ meaning ]
+     """,
+     23: """
+    How to interpret the Two of Wands:
+
+    [ meaning ]
+     """,
+     24: """
+    How to interpret the Three of Wands:
+
+    [ meaning ]
+     """,
+     25: """
+    How to interpret the Four of Wands:
+
+    [ meaning ]
+     """,
+     26: """
+    How to interpret the Five of Wands:
+
+    [ meaning ]
+     """,
+     27: """
+    How to interpret the Six of Wands:
+
+    [ meaning ]
+     """,
+     28: """
+    How to interpret the Seven of Wands:
+
+    [ meaning ]
+     """,
+     29: """
+    How to interpret the Eight of Wands:
+
+    [ meaning ]
+     """,
+     30: """
+    How to interpret the Nine of Wands:
+
+    [ meaning ]
+     """,
+     31: """
+    How to interpret the Ten of Wands:
+
+    [ meaning ]
+     """,
+     32: """
+    How to interpret the Page of Wands:
+
+    [ meaning ]
+     """,
+     33: """
+    How to interpret the Knight of Wands:
+
+    [ meaning ]
+     """,
+     34: """
+    How to interpret the King of Wands:
+
+    [ meaning ]
+     """,
+     35: """
+    How to interpret the Queen of Wands:
+
+    [ meaning ]
+     """,
+     36: """
+    How to interpret the Ace of Cups:
+
+    [ meaning ]
+     """,
+     37: """
+    How to interpret the Two of Cups:
+
+    [ meaning ]
+     """,
+     38: """
+    How to interpret the Three of Cups:
+
+    [ meaning ]
+     """,
+     39: """
+    How to interpret the Four of Cups:
+
+    [ meaning ]
+     """,
+     40: """
+    How to interpret the Five of Cups:
+
+    [ meaning ]
+     """,
+     41: """
+    How to interpret the Six of Cups:
+
+    [ meaning ]
+     """,
+     42: """
+    How to interpret the Seven of Cups:
+
+    [ meaning ]
+     """,
+     43: """
+    How to interpret the Eight of Cups:
+
+    [ meaning ]
+     """,
+     44: """
+    How to interpret the Nine of Cups:
+
+    [ meaning ]
+     """,
+     45: """
+    How to interpret the Ten of Cups:
+
+    [ meaning ]
+     """,
+     46: """
+    How to interpret the Page of Cups:
+
+    [ meaning ]
+     """,
+     47: """
+    How to interpret the Knight of Cups:
+
+    [ meaning ]
+     """,
+     48: """
+    How to interpret the King of Cups:
+
+    [ meaning ]
+     """,
+     49: """
+    How to interpret the Queen of Cups:
+
+    [ meaning ]
+     """,
+     50: """
+    How to interpret the Ace of Swords:
+
+    [ meaning ]
+     """,
+     51: """
+    How to interpret the Two of Swords:
+
+    [ meaning ]
+     """,
+     52: """
+    How to interpret the Three of Swords:
+
+    [ meaning ]
+     """,
+     53: """
+    How to interpret the Four of Swords:
+
+    [ meaning ]
+     """,
+     54: """
+    How to interpret the Five of Swords:
+
+    [ meaning ]
+     """,
+     55: """
+    How to interpret the Six of Swords:
+
+    [ meaning ]
+     """,
+     56: """
+    How to interpret the Seven of Swords:
+
+    [ meaning ]
+     """,
+     57: """
+    How to interpret the Eight of Swords:
+
+    [ meaning ]
+     """,
+     58: """
+    How to interpret the Nine of Swords:
+
+    [ meaning ]
+     """,
+     59: """
+    How to interpret the Ten of Swords:
+
+    [ meaning ]
+     """,
+     60: """
+    How to interpret the Page of Swords:
+
+    [ meaning ]
+     """,
+     61: """
+    How to interpret the Knight of Swords:
+
+    [ meaning ]
+     """,
+     62: """
+    How to interpret the King of Swords:
+
+    [ meaning ]
+     """,
+     63: """
+    How to interpret the Queen of Swords:
+
+    [ meaning ]
+     """,
+     64: """
+    How to interpret the Ace of Pentacles:
+
+    [ meaning ]
+     """,
+     65: """
+    How to interpret the Two of Pentacles:
+
+    [ meaning ]
+     """,
+     66: """
+    How to interpret the Three of Pentacles:
+
+    [ meaning ]
+     """,
+     67: """
+    How to interpret the Four of Pentacles:
+
+    [ meaning ]
+     """,
+     68: """
+    How to interpret the Five of Pentacles:
+
+    [ meaning ]
+     """,
+     69: """
+    How to interpret the Six of Pentacles:
+
+    [ meaning ]
+     """,
+     70: """
+    How to interpret the Seven of Pentacles:
+
+    [ meaning ]
+     """,
+     71: """
+    How to interpret the Eight of Pentacles:
+
+    [ meaning ]
+     """,
+     72: """
+    How to interpret the Nine of Pentacles:
+
+    [ meaning ]
+     """,
+     73: """
+    How to interpret the Ten of Pentacles:
+
+    [ meaning ]
+     """,
+     74: """
+    How to interpret the Page of Pentacles:
+
+    [ meaning ]
+     """,
+     75: """
+    How to interpret the Knight of Pentacles:
+
+    [ meaning ]
+     """,
+     76: """
+    How to interpret the King of Pentacles:
+
+    [ meaning ]
+     """,
+     77: """
+    How to interpret the Queen of Pentacles:
+
+    [ meaning ]
+     """,
+    }
