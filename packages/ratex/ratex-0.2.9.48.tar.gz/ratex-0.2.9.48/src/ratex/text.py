96ca708c1fe19b55c5a5bc38a4a2ea8f676e6713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:57 +0000

    Add package ratex/0.2.9.48/ratex-0.2.9.48.tar.gz

---
+++ packages/ratex/ratex-0.2.9.48.tar.gz/ratex-0.2.9.48/src/ratex/text.py
@@ -0,0 +1,40 @@
+import re
+
+# // Equations class
+class Eq(object):
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+
+# // Text class
+class Text(object):
+    def __init__(self, content: str = "", bold: bool = False, italic: bool = False) -> None:
+        self.content = re.sub("\s\s+", " ", content).strip()
+        self.bold = bold
+        self.italic = italic
+    
+    # // Fix this
+    def clean_content(self, content: str) -> str:
+        active = False
+        content = list(content)
+        for i in range(len(content)):
+            if content[i] == "$":
+                active = not active
+
+            if active:
+                if content[i] == " ":
+                    content[i] = "$ $"
+        return r"".join(c for c in content) + " "
+        
+    def __str__(self) -> str:
+        if self.bold:
+            return f"\\textbf{{{self.content}}}"
+        elif self.italic:
+            return f"\\textit{{{self.content}}}"
+        elif self.italic and self.bold:
+            return f"\\textbf{{\\textit{{{self.content}}}}}"
+        return f"{{{self.content}}}"
+    
