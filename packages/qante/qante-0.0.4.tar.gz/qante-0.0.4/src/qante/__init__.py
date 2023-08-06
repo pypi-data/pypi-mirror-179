3212005964d73402dae9c3cf14c8716817cdd9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package qante/0.0.4/qante-0.0.4.tar.gz

---
+++ packages/qante/qante-0.0.4.tar.gz/qante-0.0.4/src/qante/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.0.4'
+
+__all__ = [
+    "extracterror",
+    "loctuplelist",
+    "loc",
+    "loctuple",
+    "loclist",
+    "tagger",
+    "taggerext",
+    "utilities",
+    "query",
+    "table",
+]
