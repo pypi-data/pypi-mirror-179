fe8d986ca744f9ff3ea53115ffcbd43336e575b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:59 +0000

    Add package manipulation/2022.12.4/manipulation-2022.12.4-py3-none-any.whl

---
+++ packages/manipulation/manipulation-2022.12.4-py3-none-any.whl/manipulation/meshcat_cpp_utils.py
@@ -0,0 +1,8 @@
+import warnings
+
+from manipulation.meshcat_utils import *
+
+warnings.warn(
+    "manipulation.meshcat_cpp_utils has been renamed to manipulation.meshcat_utils. This shim will be removed after 2022-12-31.",
+    DeprecationWarning,
+    stacklevel=2)
