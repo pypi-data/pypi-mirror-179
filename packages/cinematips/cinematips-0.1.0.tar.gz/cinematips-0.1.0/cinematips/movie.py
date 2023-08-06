c0f2749a91730170657a5f997cd551da03d9d4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:10 +0000

    Add package cinematips/0.1.0/cinematips-0.1.0.tar.gz

---
+++ packages/cinematips/cinematips-0.1.0.tar.gz/cinematips-0.1.0/cinematips/movie.py
@@ -0,0 +1,19 @@
+from cinematips.imdbrating import ImdbRating
+
+
+class Movie:
+    def __init__(self, id: str, image: str) -> None:
+        rating = ImdbRating.get_rating(id).content
+
+        self.id = id
+        self.image = image
+        self.title = rating['fullTitle']
+        intRating: int = 0
+        try:
+            intRating = int(rating['rottenTomatoes'])
+        except EnvironmentError:
+            intRating = 0
+
+        self.ratingRottenTomatoes = intRating
+        self.ratingMetacritic = rating['metacritic']
+        self.year = rating['year']
