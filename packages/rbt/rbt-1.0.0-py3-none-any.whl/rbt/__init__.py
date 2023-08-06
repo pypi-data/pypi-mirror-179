c090100667f77dd7c782254844a1eb8726ebf94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:14 +0000

    Add package rbt/1.0.0/rbt-1.0.0-py3-none-any.whl

---
+++ packages/rbt/rbt-1.0.0-py3-none-any.whl/rbt/__init__.py
@@ -0,0 +1,13 @@
+"""
+@author axiner
+@version v1.0.0
+@created 2022/12/5 12:12
+@abstract This is a rbt.
+@description
+@history
+"""
+from pathlib import Path
+
+here = Path(__file__).absolute().parent
+
+__version__ = "1.0.0"
