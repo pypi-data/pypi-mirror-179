41489f360001da7384c1b79f83042b5acba755bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:46 +0000

    Add package mysmallutils/1.1.9/mysmallutils-1.1.9.tar.gz

---
+++ packages/mysmallutils/mysmallutils-1.1.9.tar.gz/mysmallutils-1.1.9/setup.py
@@ -0,0 +1,62 @@
+import os
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+class CleanCommand(setuptools.Command):
+    """ Custom clean command to tidy up the project root. """
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+
+class PrepublishCommand(setuptools.Command):
+    """ Custom prepublish command. """
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('python setup.py clean')
+        os.system('python setup.py sdist bdist_wheel')
+
+
+setuptools.setup(
+    cmdclass={
+        'clean': CleanCommand,
+        'prepublish': PrepublishCommand,
+    },
+    name='mysmallutils',
+    version='1.1.9',
+    url='https://github.com/jmgomezsoriano/mysmallutils',
+    license='LGPL2',
+    author='José Manuel Gómez Soriano',
+    author_email='jmgomez.soriano@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='Small Python utils to do life easier.',
+    packages=setuptools.find_packages(exclude=["test"]),
+    package_dir={'mysutils': 'mysutils'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'deprecation==2.1.0'
+    ]
+)
