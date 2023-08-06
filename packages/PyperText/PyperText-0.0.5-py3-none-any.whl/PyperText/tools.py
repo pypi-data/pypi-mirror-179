24813eb3a4d1ce31a80d9a3aa24ac2b73b6edffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:39 +0000

    Add package PyperText/0.0.5/PyperText-0.0.5-py3-none-any.whl

---
+++ packages/PyperText/PyperText-0.0.5-py3-none-any.whl/PyperText/tools.py
@@ -0,0 +1,26 @@
+import os
+import cv2#type:ignore
+from validators import url as urls#type:ignore
+from .htmlWidget import htmlObject
+def exists(file:str)->bool:return os.path.exists(file)
+def isFile(file:str)->bool:return os.path.isfile(file)
+def isUrl(url:str)->bool:return urls(url)
+def getHeight(image:str)->int:return int(cv2.imread(image).shape[0])
+def getWidth(image:str)->int:return int(cv2.imread(image).shape[1])
+def stripLast(string:str)->str:return string.rstrip(string[-1])
+class SuspendedString(htmlObject):
+    '''
+    Class to hold suspended string to be properly handled if printed or if returned in str
+    '''
+    def __init__(self,header:str,val:str,footer:str) -> None:
+        self.header: str=header
+        self.footer: str=footer
+        self.value: str=val
+    def form(self) -> str:
+        '''
+        Passes Suspended Value        
+        '''
+        return self.header+self.value+self.footer
+    def __str__(self) -> str:return self.value
+    def __repr__(self)->str:return self.value
+    
