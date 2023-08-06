678cd29c0f7470eaaf8bf778fc2c3399438c7973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:31 +0000

    Add package secretsintheenv/1.0.3/secretsintheenv-1.0.3-py3-none-any.whl

---
+++ packages/secretsintheenv/secretsintheenv-1.0.3-py3-none-any.whl/secretsenv/secretsenv.py
@@ -0,0 +1,26 @@
+from .classes.config import config as CONFIG
+from .classes.vaults import vaults as VAULTS
+from .classes.dump import dump as DUMP
+from .classes.run import run as RUN
+
+secrets = {}
+
+def main():
+    config = CONFIG()
+    vaults = VAULTS(config)
+
+    for variable_name, settings in config.profile.items():
+        secrets[variable_name] = {
+            "type": settings['type'],
+            "content": vaults[settings['vault']].query(settings['query'])
+        }
+
+    if config.action == "dump":
+        dump = DUMP(config,secrets)
+    
+    if config.action == "run":
+        drun = RUN(config,secrets)
+    
+
+if __name__ == "__main__":
+    main()
