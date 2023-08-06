8a9a0686ec34da08c283047b44f86da127a45d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:52 +0000

    Add package hspylib-datasource/0.9.2/hspylib-datasource-0.9.2.tar.gz

---
+++ packages/hspylib-datasource/hspylib-datasource-0.9.2.tar.gz/hspylib-datasource-0.9.2/datasource/mysql/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-firebase v0.9.2
+#
+# Package: main.datasource.mysql
+"""Package initialization."""
+
+__all__ = [
+  'mysql_repository'
+]
+__version__ = '0.9.2'
