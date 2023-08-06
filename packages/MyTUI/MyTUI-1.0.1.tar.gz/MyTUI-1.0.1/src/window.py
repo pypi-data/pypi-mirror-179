10e9b0e22b7272c7b0351c090a725390a00c2bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:21 +0000

    Add package MyTUI/1.0.1/MyTUI-1.0.1.tar.gz

---
+++ packages/MyTUI/MyTUI-1.0.1.tar.gz/MyTUI-1.0.1/src/window.py
@@ -0,0 +1,21 @@
+from os import get_terminal_size
+
+def width() -> int:
+    return get_terminal_size().columns
+
+def height() -> int:
+    return get_terminal_size().lines
+
+def hide_cursor() -> None:
+    print('\033[?25l', end='')
+    
+def show_cursor() -> None:
+    print('\033[?25h', end='')
+
+def move(x: int, y: int) -> str:
+    return f'\033[{y};{x}H'
+
+
+if __name__ == '__main__':
+    
+    print(move(10, 2) + 'test')
