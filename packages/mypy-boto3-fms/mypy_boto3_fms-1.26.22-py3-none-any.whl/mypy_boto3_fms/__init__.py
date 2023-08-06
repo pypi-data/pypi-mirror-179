bdcc1a74ff00cb58ca625ab572903eecdfbc05cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:25 +0000

    Add package mypy-boto3-fms/1.26.22/mypy_boto3_fms-1.26.22-py3-none-any.whl

---
+++ packages/mypy-boto3-fms/mypy_boto3_fms-1.26.22-py3-none-any.whl/mypy_boto3_fms/__init__.py
@@ -0,0 +1,52 @@
+"""
+Main interface for fms service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_fms import (
+        Client,
+        FMSClient,
+        ListAppsListsPaginator,
+        ListComplianceStatusPaginator,
+        ListMemberAccountsPaginator,
+        ListPoliciesPaginator,
+        ListProtocolsListsPaginator,
+        ListThirdPartyFirewallFirewallPoliciesPaginator,
+    )
+
+    session = Session()
+    client: FMSClient = session.client("fms")
+
+    list_apps_lists_paginator: ListAppsListsPaginator = client.get_paginator("list_apps_lists")
+    list_compliance_status_paginator: ListComplianceStatusPaginator = client.get_paginator("list_compliance_status")
+    list_member_accounts_paginator: ListMemberAccountsPaginator = client.get_paginator("list_member_accounts")
+    list_policies_paginator: ListPoliciesPaginator = client.get_paginator("list_policies")
+    list_protocols_lists_paginator: ListProtocolsListsPaginator = client.get_paginator("list_protocols_lists")
+    list_third_party_firewall_firewall_policies_paginator: ListThirdPartyFirewallFirewallPoliciesPaginator = client.get_paginator("list_third_party_firewall_firewall_policies")
+    ```
+"""
+from .client import FMSClient
+from .paginator import (
+    ListAppsListsPaginator,
+    ListComplianceStatusPaginator,
+    ListMemberAccountsPaginator,
+    ListPoliciesPaginator,
+    ListProtocolsListsPaginator,
+    ListThirdPartyFirewallFirewallPoliciesPaginator,
+)
+
+Client = FMSClient
+
+
+__all__ = (
+    "Client",
+    "FMSClient",
+    "ListAppsListsPaginator",
+    "ListComplianceStatusPaginator",
+    "ListMemberAccountsPaginator",
+    "ListPoliciesPaginator",
+    "ListProtocolsListsPaginator",
+    "ListThirdPartyFirewallFirewallPoliciesPaginator",
+)
