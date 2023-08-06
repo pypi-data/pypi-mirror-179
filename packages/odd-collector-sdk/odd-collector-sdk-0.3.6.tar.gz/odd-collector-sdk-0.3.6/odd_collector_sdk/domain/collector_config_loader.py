59e990f3fa2f5cca6d5641ae7b585d5c7878754d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:09 +0000

    Add package odd-collector-sdk/0.3.6/odd-collector-sdk-0.3.6.tar.gz

---
+++ packages/odd-collector-sdk/odd-collector-sdk-0.3.6.tar.gz/odd-collector-sdk-0.3.6/odd_collector_sdk/domain/collector_config_loader.py
@@ -0,0 +1,23 @@
+from typing import Dict, Type
+from pyaml_env import parse_config
+
+from .plugin import Plugin
+from .collector_config import CollectorConfig
+
+
+class CollectorConfigLoader:
+    def __init__(
+        self, config_path: str, plugin_factory: Dict[str, Type[Plugin]]
+    ) -> None:
+        self.plugin_factory = plugin_factory
+        self.path = config_path
+
+    def load(self) -> CollectorConfig:
+        parsed = parse_config(self.path)
+
+        parsed["plugins"] = [
+            self.plugin_factory[plugin["type"]].parse_obj(plugin)
+            for plugin in parsed["plugins"]
+        ]
+
+        return CollectorConfig.parse_obj(parsed)
