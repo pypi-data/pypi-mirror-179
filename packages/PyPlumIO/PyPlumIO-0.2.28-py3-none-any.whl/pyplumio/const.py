e36ba744f0788106dcee9f03ea223345768be5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:08 +0000

    Add package PyPlumIO/0.2.28/PyPlumIO-0.2.28-py3-none-any.whl

---
+++ packages/PyPlumIO/PyPlumIO-0.2.28-py3-none-any.whl/pyplumio/const.py
@@ -0,0 +1,47 @@
+"""Contains constants."""
+from __future__ import annotations
+
+from typing import Final
+
+STATE_ON: Final = "on"
+STATE_OFF: Final = "off"
+
+ADDR_BROADCAST: Final = 0
+ADDR_ECONET: Final = 86
+
+ATTR_NAME: Final = "name"
+ATTR_VALUE: Final = "value"
+ATTR_EXTRA: Final = "extra"
+ATTR_ECOMAX_SENSORS: Final = "sensors"
+ATTR_ECOMAX_PARAMETERS: Final = "parameters"
+ATTR_PENDING_ALERTS: Final = "pending_alerts"
+ATTR_ALERTS: Final = "alerts"
+ATTR_FAN_POWER: Final = "fan_power"
+ATTR_FRAME_VERSIONS: Final = "frames"
+ATTR_FUEL_CONSUMPTION: Final = "fuel_consumption"
+ATTR_FUEL_LEVEL: Final = "fuel_level"
+ATTR_FUEL_BURNED: Final = "fuel_burned"
+ATTR_LOAD: Final = "load"
+ATTR_MIXERS: Final = "mixers"
+ATTR_MIXER_SENSORS: Final = "mixer_sensors"
+ATTR_MIXER_PARAMETERS: Final = "mixer_parameters"
+ATTR_MODE: Final = "mode"
+ATTR_MODULES: Final = "modules"
+ATTR_POWER: Final = "power"
+ATTR_THERMOSTAT: Final = "thermostat"
+ATTR_THERMOSTATS: Final = "thermostats"
+ATTR_TRANSMISSION: Final = "transmission"
+ATTR_PRODUCT: Final = "product"
+ATTR_NETWORK: Final = "network"
+ATTR_VERSION: Final = "version"
+ATTR_PASSWORD: Final = "password"
+ATTR_SCHEMA: Final = "schema"
+ATTR_REGDATA: Final = "regdata"
+ATTR_LAMBDA_SENSOR: Final = "lambda"
+ATTR_SCHEDULES: Final = "schedules"
+ATTR_TYPE: Final = "type"
+ATTR_SWITCH: Final = "switch"
+ATTR_PARAMETER: Final = "parameter"
+ATTR_SCHEDULE: Final = "schedule"
+
+BYTE_UNDEFINED: Final = 0xFF
---
+++ packages/PyPlumIO/PyPlumIO-0.2.28-py3-none-any.whl/pyplumio/devices/__init__.py
@@ -0,0 +1,200 @@
+"""Contains device representations."""
+from __future__ import annotations
+
+from abc import ABC
+import asyncio
+from enum import IntEnum, unique
+from typing import ClassVar, Dict, List, Optional
+
+from pyplumio import util
+from pyplumio.const import ATTR_FRAME_VERSIONS
+from pyplumio.exceptions import ParameterNotFoundError, UnknownDeviceError
+from pyplumio.frames import Frame
+from pyplumio.helpers.frame_versions import DEFAULT_FRAME_VERSION, FrameVersions
+from pyplumio.helpers.parameter import Parameter
+from pyplumio.helpers.task_manager import TaskManager
+from pyplumio.helpers.typing import (
+    DeviceDataType,
+    NumericType,
+    SensorCallbackType,
+    VersionsInfoType,
+)
+
+
+@unique
+class DeviceTypes(IntEnum):
+    """Contains device types."""
+
+    ECOMAX = 69
+    ECOSTER = 81
+
+
+def _handler_class_path(device_type_name: str) -> str:
+    """Return handler class path from device type name."""
+    device_type_name = util.to_camelcase(
+        device_type_name, overrides={"ecomax": "EcoMAX", "ecoster": "EcoSTER"}
+    )
+    return f"{device_type_name.lower()}.{device_type_name}"
+
+
+# Dictionary of device handler classes indexed by device types.
+# example: "69: ecomax.EcoMAX"
+DEVICE_TYPES: Dict[int, str] = {
+    device_type.value: _handler_class_path(device_type.name)
+    for device_type in DeviceTypes
+}
+
+
+def get_device_handler(device_type: int) -> str:
+    """Return module and class for device type."""
+    if device_type in DEVICE_TYPES:
+        return "devices." + DEVICE_TYPES[device_type]
+
+    raise UnknownDeviceError(f"Unknown device ({device_type})")
+
+
+class AsyncDevice(ABC, TaskManager):
+    """Represents a device with awaitable properties."""
+
+    data: DeviceDataType
+    _callbacks: Dict[str, List[SensorCallbackType]]
+
+    def __init__(self):
+        """Initializes Async Device object."""
+        super().__init__()
+        self.data = {}
+        self._callbacks = {}
+
+    async def get_value(self, name: str, timeout: Optional[float] = None):
+        """Return a value. When encountering a parameter, only it's
+        value will be returned. To return the Parameter object use
+        get_parameter method."""
+        if name not in self.data:
+            await asyncio.wait_for(self.create_event(name).wait(), timeout=timeout)
+
+        value = self.data[name]
+        return int(value) if isinstance(value, Parameter) else value
+
+    async def set_value(
+        self,
+        name: str,
+        value: NumericType,
+        timeout: Optional[float] = None,
+        await_confirmation: bool = True,
+    ) -> bool:
+        """Set parameter value. Name should point
+        to a valid parameter object."""
+        if name not in self.data:
+            await asyncio.wait_for(self.create_event(name).wait(), timeout=timeout)
+
+        parameter = self.data[name]
+        if not isinstance(parameter, Parameter):
+            raise ParameterNotFoundError(f"Parameter not found ({name})")
+
+        if await_confirmation:
+            return await parameter.set(value)
+
+        self.create_task(parameter.set(value))
+        return True
+
+    async def get_parameter(
+        self, name: str, timeout: Optional[float] = None
+    ) -> Parameter:
+        """Return a parameter."""
+        if name not in self.data:
+            await asyncio.wait_for(self.create_event(name).wait(), timeout=timeout)
+
+        parameter = self.data[name]
+        if isinstance(parameter, Parameter):
+            return parameter
+
+        raise ParameterNotFoundError(f"Parameter not found ({name})")
+
+    def set_device_data(self, *args, **kwargs) -> None:
+        """Call registered callbacks on value change."""
+        self.create_task(self.async_set_device_data(*args, **kwargs))
+
+    async def async_set_device_data(self, name: str, value) -> None:
+        """Asynchronously call registered callbacks on value change."""
+        if name in self._callbacks:
+            callbacks = self._callbacks[name].copy()
+            for callback in callbacks:
+                return_value = await callback(value)
+                value = return_value if return_value is not None else value
+
+        self.data[name] = value
+        self.set_event(name)
+
+    async def shutdown(self) -> None:
+        """Cancel scheduled tasks."""
+        self.cancel_tasks()
+        await self.wait_until_done()
+
+    def subscribe(self, name: str, callback: SensorCallbackType) -> None:
+        """Register a callback for a value change."""
+        if name not in self._callbacks:
+            self._callbacks[name] = []
+
+        self._callbacks[name].append(callback)
+
+    def subscribe_once(self, name: str, callback: SensorCallbackType) -> None:
+        """Register a callback for a single call."""
+
+        async def _callback(value):
+            """Unregister the callback once it was called."""
+            try:
+                return await callback(value)
+            finally:
+                self.unsubscribe(name, _callback)
+
+        self.subscribe(name, _callback)
+
+    def unsubscribe(self, name: str, callback: SensorCallbackType) -> None:
+        """Remove the callback."""
+        if name in self._callbacks and callback in self._callbacks[name]:
+            self._callbacks[name].remove(callback)
+
+
+class Device(AsyncDevice):
+    """Represents base device."""
+
+    address: ClassVar[int]
+    queue: asyncio.Queue
+    _required_frames: List[int] = []
+
+    def __init__(self, queue: asyncio.Queue):
+        """Initialize new Device object."""
+        super().__init__()
+        self.queue = queue
+        versions = FrameVersions(device=self)
+        self.subscribe_once(ATTR_FRAME_VERSIONS, self._merge_required_frames)
+        self.subscribe(ATTR_FRAME_VERSIONS, versions.async_update)
+
+    async def _merge_required_frames(
+        self, frame_versions: VersionsInfoType
+    ) -> VersionsInfoType:
+        """Merge required frames into version list."""
+        requirements = {int(x): DEFAULT_FRAME_VERSION for x in self.required_frames}
+        return {**requirements, **frame_versions}
+
+    def handle_frame(self, frame: Frame) -> None:
+        """Handle received frame."""
+        if frame.data is not None:
+            for name, value in frame.data.items():
+                self.set_device_data(name, value)
+
+    @property
+    def required_frames(self) -> List[int]:
+        """Return list of required frame types."""
+        return self._required_frames
+
+
+class Mixer(AsyncDevice):
+    """Represents mixer device."""
+
+    mixer_number: int
+
+    def __init__(self, mixer_number: int = 0):
+        """Initialize new Mixer object."""
+        super().__init__()
+        self.mixer_number = mixer_number
