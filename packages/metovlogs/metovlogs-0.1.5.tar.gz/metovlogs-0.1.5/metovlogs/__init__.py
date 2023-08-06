529931fd6f708e97ba4f092189f85215e05bfeb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:09 +0000

    Add package metovlogs/0.1.5/metovlogs-0.1.5.tar.gz

---
+++ packages/metovlogs/metovlogs-0.1.5.tar.gz/metovlogs-0.1.5/metovlogs/__init__.py
@@ -0,0 +1,28 @@
+import logging
+import coloredlogs
+
+from . import lib
+
+
+def get_log(name: str, default_level: str = None) -> logging.Logger:
+    log = logging.getLogger(name)
+    fields = [
+        "{name}:{lineno}",
+        "+{relativeCreated:,.0f} ms",
+        "{message}",
+    ]
+    coloredlogs.install(
+        fmt=" ".join(fields),
+        style="{",
+        level=default_level or lib.level_from_envar(),
+        logger=log,
+    )
+
+    return log
+
+
+_log = get_log(__name__)
+_log.info(
+    f"To change log levels, set the environment variable LOG_LEVEL to one of: "
+    + ", ".join(coloredlogs.find_defined_levels())
+)
