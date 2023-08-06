fd906f2cc64378d4aee05602f987ab0a2ea0919e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:45 +0000

    Add package csnlp/1.3.2/csnlp-1.3.2.tar.gz

---
+++ packages/csnlp/csnlp-1.3.2.tar.gz/csnlp-1.3.2/src/csnlp/multistart_nlp.py
@@ -0,0 +1,276 @@
+from contextlib import contextmanager
+from functools import lru_cache, partial
+from typing import (
+    Any,
+    Dict,
+    Generic,
+    Iterable,
+    List,
+    Literal,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+)
+
+import casadi as cs
+import numpy as np
+
+from csnlp.nlp import Nlp
+from csnlp.solutions import Solution, subsevalf
+from csnlp.util.data import DMStruct, dict2struct
+from csnlp.util.funcs import invalidate_cache
+
+T = TypeVar("T", cs.SX, cs.MX)
+
+
+def _n(sym_name: str, scenario: int) -> str:
+    """Internal utility for the naming convention of i-scenario's symbols."""
+    return f"{sym_name}__{scenario}"
+
+
+def _get_value(x, sol: Solution, old, new, eval: bool = True):
+    """Internal utility for substituting numerical values in solutions."""
+    return sol._get_value(cs.substitute(x, old, new), eval=eval)
+
+
+class MultistartNlp(Nlp[T], Generic[T]):
+    """A class to easily model and solve an NLP from multiple starting
+    initial guesses in parallel. This is especially useful in case of strong
+    nonlinearities, where the solver's initial conditions play a great role in
+    the optimality of the solution (rather, its sub-optimality)."""
+
+    is_multi: bool = True
+
+    def __init__(
+        self,
+        *args,
+        starts: int,
+        **kwargs,
+    ) -> None:
+        """Initializes a multistart NLP instance.
+
+        Parameters
+        ----------
+        args, kwargs
+            See inherited `csnlp.Nlp`.
+        starts : int
+            A positive integer for the number of multiple starting guesses to
+            optimize over.
+
+        Raises
+        ------
+        ValueError
+            Raises if the scenario number is invalid.
+        """
+        if starts <= 0:
+            raise ValueError("Number of scenarios must be positive and > 0.")
+        self._starts = starts
+        # this class essentially is a facade that hides an internal nlp in
+        # which the problem (variables, parameters, etc.) are duplicated by the
+        # requested number of multiple starts. For this reason, all methods are
+        # overridden to create multiples of these in the hidden nlp
+        super().__init__(*args, **kwargs)
+        self._multi_nlp = Nlp(*args, **kwargs)  # actual nlp
+        self._vars_per_start: Dict[int, Dict[str, T]] = {}
+
+    @contextmanager
+    def fullstate(self) -> None:
+        with super().fullstate(), self._multi_nlp.fullstate():
+            yield
+
+    @contextmanager
+    def pickleable(self) -> None:
+        with super().pickleable(), self._multi_nlp.pickleable():
+            yield
+
+    @property
+    def starts(self) -> int:
+        """Gets the number of starts."""
+        return self._starts
+
+    @lru_cache
+    def _symbols(
+        self,
+        i: Optional[int] = None,
+        vars: bool = False,
+        pars: bool = False,
+        dual: bool = False,
+    ) -> Dict[str, T]:
+        """Internal utility to retrieve the symbols of the i-th scenario."""
+        S: Dict[str, T] = {}
+        if vars:
+            S.update(
+                self._vars
+                if i is None
+                else {k: self._multi_nlp.unwrapped._vars[_n(k, i)] for k in self._vars}
+            )
+        if pars:
+            S.update(
+                self._pars
+                if i is None
+                else {k: self._multi_nlp.unwrapped._pars[_n(k, i)] for k in self._pars}
+            )
+        if dual:
+            S.update(
+                self._dual_vars
+                if i is None
+                else {
+                    k: self._multi_nlp.unwrapped._dual_vars[_n(k, i)]
+                    for k in self._dual_vars
+                }
+            )
+        return S
+
+    @invalidate_cache(_symbols)
+    def parameter(self, name: str, shape: Tuple[int, int] = (1, 1)) -> T:
+        out = super().parameter(name, shape)
+        for i in range(self._starts):
+            self._multi_nlp.parameter(_n(name, i), shape)
+        return out
+
+    @invalidate_cache(_symbols)
+    def variable(
+        self,
+        name: str,
+        shape: Tuple[int, int] = (1, 1),
+        lb: Union[np.ndarray, cs.DM] = -np.inf,
+        ub: Union[np.ndarray, cs.DM] = +np.inf,
+    ) -> Tuple[T, T, T]:
+        out = super().variable(name, shape, lb, ub)
+        for i in range(self._starts):
+            self._multi_nlp.variable(_n(name, i), shape, lb, ub)
+        return out
+
+    @invalidate_cache(_symbols)
+    def constraint(
+        self,
+        name: str,
+        lhs: Union[T, np.ndarray, cs.DM],
+        op: Literal["==", ">=", "<="],
+        rhs: Union[T, np.ndarray, cs.DM],
+        soft: bool = False,
+        simplify: bool = True,
+    ) -> Union[Tuple[T, T], Tuple[T, T, T]]:
+        expr = lhs - rhs
+        if simplify:
+            expr = cs.simplify(expr)
+        out = super().constraint(name, expr, op, 0, soft, simplify=False)
+
+        symbols = self._symbols(vars=True, pars=True)
+        for i in range(self._starts):
+            symbols_i = self._symbols(i, vars=True, pars=True)
+            expr_i = subsevalf(expr, symbols, symbols_i, eval=False)
+            self._multi_nlp.constraint(_n(name, i), expr_i, op, 0, soft, simplify=False)
+        return out
+
+    def minimize(self, objective: T) -> None:
+        out = super().minimize(objective)
+        symbols = self._symbols(vars=True, pars=True)
+        self._fs: List[T] = [
+            subsevalf(
+                objective, symbols, self._symbols(i, vars=True, pars=True), eval=False
+            )
+            for i in range(self._starts)
+        ]
+        self._multi_nlp.minimize(sum(self._fs))
+        return out
+
+    def init_solver(self, opts: Optional[Dict[str, Any]] = None) -> None:
+        out = super().init_solver(opts)
+        self._multi_nlp.init_solver(opts)
+        return out
+
+    def solve_multi(
+        self,
+        pars: Union[None, Iterable[DMStruct], Iterable[Dict[str, np.ndarray]]] = None,
+        vals0: Union[None, Iterable[DMStruct], Iterable[Dict[str, np.ndarray]]] = None,
+        return_all_sols: bool = False,
+        return_multi_sol: bool = False,
+    ) -> Union[Solution[T], List[Solution[T]]]:
+        """Solves the NLP with multiple initial conditions.
+
+        Parameters
+        ----------
+        pars : iterable of DMStruct, dict[str, array_like], optional
+            An iterable that, for each multistart, contains a dictionary or
+            structure containing, for each parameter in the NLP scheme, the
+            corresponding numerical value. Can be `None` if no parameters are
+            present.
+        vals0 : iterable of DMStruct, dict[str, array_like], optional
+            An iterable that, for each multistart, contains a dictionary or
+            structure containing, for each variable in the NLP scheme, the
+            corresponding initial guess. By default, initial guesses are not
+            passed to the solver.
+        return_all_sols : bool, optional
+            If `True`, returns the solution of each multistart of the NLP;
+            otherwise, only the best solution is returned. By default, `False`.
+        return_multi_sol : bool, optional
+            If `True`, returns the solution of the underlying multistart NLP.
+            Generally, only for debugging. By default, `False`.
+
+        Returns
+        -------
+        Solution or list of Solutions
+            Depending on the flags `return_all_sols` and `return_multi_sol`,
+            returns
+                - the best solution out of all multiple starts
+                - all the solutions (one per start)
+                - the solution to the underlying (hidden) multistart NLP.
+
+        Raises
+        ------
+        ValueError
+            Raises if `return_multi_sol` and `return_all_sols` are both true at
+            the same time.
+        """
+        if return_multi_sol and return_all_sols:
+            raise ValueError(
+                "`return_multi_sol` and `return_all_sols` can't be both true."
+            )
+
+        if pars is not None:
+            pars = {
+                _n(n, i): pars_i[n]
+                for i, pars_i in enumerate(pars)
+                for n in pars_i.keys()
+            }
+        if vals0 is not None:
+            vals0 = {
+                _n(n, i): vals0_i[n]
+                for i, vals0_i in enumerate(vals0)
+                for n in vals0_i.keys()
+            }
+        multi_sol = self._multi_nlp.solve(pars=pars, vals0=vals0)
+        if return_multi_sol:
+            return multi_sol
+
+        vars_ = self.variables
+        symbols = self._symbols(vars=True, pars=True, dual=True)
+        old = cs.vertcat(
+            self._p, self._x, self._lam_g, self._lam_h, self._lam_lbx, self._lam_ubx
+        )
+
+        sols: List[Solution[T]] = []
+        fs = [float(multi_sol.value(f)) for f in self._fs]
+        idx = range(self._starts) if return_all_sols else (np.argmin(fs),)
+        for i in idx:
+            vals = {n: multi_sol.vals[_n(n, i)] for n in vars_.keys()}
+
+            symbols_i = self._symbols(i, vars=True, pars=True, dual=True)
+            new = subsevalf(old, symbols, symbols_i, eval=False)
+            get_value = partial(_get_value, sol=multi_sol, old=old, new=new)
+
+            sols.append(
+                Solution[T](
+                    f=fs[i],
+                    vars=vars_,
+                    vals=dict2struct(vals),
+                    stats=multi_sol.stats,
+                    _get_value=get_value,
+                )
+            )
+        return sols if return_all_sols else sols[0]
+
+    def __call__(self, *args, **kwargs):
+        return self.solve_multi(*args, **kwargs)
---
+++ packages/csnlp/csnlp-1.3.2.tar.gz/csnlp-1.3.2/src/csnlp/util/array.py
@@ -0,0 +1,111 @@
+from typing import List, Optional, Tuple, Union
+
+import casadi as cs
+import numpy as np
+
+from csnlp.util.data import array2cs, cs2array
+
+
+def hojacobian(ex: Union[cs.MX, cs.SX], x: Union[cs.MX, cs.SX]) -> np.ndarray:
+    """Computes jacobian on higher-order matrices, i.e., with an output in 4D.
+
+    Parameters
+    ----------
+    ex : casadi.MX, SX
+        The expression to compute the jacobian for. Can be a matrix.
+    x : casadi.MX, SX
+        The variable to differentiate with respect to. Can be a matrix.
+
+    Returns
+    -------
+    np.ndarray
+        A 4D array of objects, where each entry `(i,j,k,m)` is the derivative
+        of `ex[i,j]` w.r.t. `x[k,m]`.
+    """
+    return cs2array(cs.jacobian(cs.vec(ex), cs.vec(x))).reshape(
+        ex.shape + x.shape, order="F"
+    )
+
+
+def hohessian(
+    ex: Union[cs.MX, cs.SX], x: Union[cs.MX, cs.SX], y: Union[cs.MX, cs.SX, None] = None
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Computes hessian on higher-order matrices, i.e., with an output in 6D.
+
+    Parameters
+    ----------
+    ex : casadi.MX, SX
+        The expression to compute the hessin for. Can be a matrix.
+    x : casadi.MX, SX
+        The variable to differentiate with respect to. Can be a matrix.
+    y : casadi.MX, SX, optional
+        Use this argument to specify the second partial derivative, i.e., if
+        the output requested is not the hessian of `ex` w.r.t. `x`, but rather
+        the second-order partial derivatives of `ex` w.r.t. `x` and `y`.
+
+    Returns
+    -------
+    np.ndarray, np.ndarray
+        The first element is a 6D array of objects, where each entry
+        `(i,j,k,m,l,p)` is the hessian of `ex[i,j]` w.r.t. `x[k,m], y[l,p]`,
+        while the second element contains the jacobian instead (see
+        `hojacobian` for details).
+    """
+    if y is None:
+        y = x
+    J = hojacobian(ex, x)
+    H = np.empty(ex.shape + x.shape + y.shape, dtype=object)
+    for i in np.ndindex(ex.shape):
+        H[i] = hojacobian(array2cs(J[i]), y)
+    return H, J
+
+
+def jaggedstack(
+    arrays,
+    axis: int = 0,
+    out: Optional[np.ndarray] = None,
+    constant_values: Union[float, np.ndarray] = np.nan,
+) -> np.ndarray:
+    """Joins a sequence of arrays with different shapes along a new axis. To do
+    so, each array is padded with `constant_values` (see `numpy.pad`) to the
+    right to even out the shapes. Then, the same-shape-arrays are stacked via
+    `numpy.stack`.
+
+    Parameters
+    ----------
+    arrays, axis, out
+        See `numpy.stack`.
+    constant_values
+        See `numpy.pad`.
+
+    Returns
+    -------
+    stacked : ndarray
+        The stacked array has one more dimension than the input arrays.
+
+    Raises
+    ------
+    ValueError
+        Raises if no array is passed as input.
+    """
+    arraylist: List[np.ndarray] = [np.asanyarray(a) for a in arrays]
+    if not arraylist:
+        raise ValueError("Need at least one array to stack.")
+    maxndim = max(map(lambda a: a.ndim, arraylist))
+    newarrays: List[np.ndarray] = []
+    maxshape = arraylist[0].shape
+    for a in arraylist:
+        if a.ndim < maxndim:
+            a = np.expand_dims(a, tuple(range(a.ndim, maxndim)))
+        maxshape = np.maximum(maxshape, a.shape)
+        newarrays.append(a)
+    newarrays = [
+        np.pad(
+            a,
+            [(0, d_max - d) for d, d_max in zip(a.shape, maxshape)],
+            mode="constant",
+            constant_values=constant_values,
+        )
+        for a in newarrays
+    ]
+    return np.stack(newarrays, axis, out)
