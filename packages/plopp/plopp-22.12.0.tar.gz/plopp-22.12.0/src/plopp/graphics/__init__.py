15efc21342bdd01c80249d28db64576673e8c8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:19 +0000

    Add package plopp/22.12.0/plopp-22.12.0.tar.gz

---
+++ packages/plopp/plopp-22.12.0.tar.gz/plopp-22.12.0/src/plopp/graphics/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+# flake8: noqa E402, F401
+
+from .canvas import Canvas
+from .canvas3d import Canvas3d
+from .interactive import InteractiveFig1d, InteractiveFig2d, InteractiveFig3d
+from .static import StaticFig1d, StaticFig2d
+from .colormapper import ColorMapper
