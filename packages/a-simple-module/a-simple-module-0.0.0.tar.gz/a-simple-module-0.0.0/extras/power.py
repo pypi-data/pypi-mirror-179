ca8c292c637a59c6bfb02ebc9bbbc88f304a67ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:18 +0000

    Add package a-simple-module/0.0.0/a-simple-module-0.0.0.tar.gz

---
+++ packages/a-simple-module/a-simple-module-0.0.0.tar.gz/a-simple-module-0.0.0/extras/power.py
@@ -0,0 +1,9 @@
+
+
+def power(a: int, b: int) -> int: 
+    """
+    does a to the power b
+
+    returns: a^b 
+    """
+    return a ** b
---
+++ packages/a-simple-module/a-simple-module-0.0.0.tar.gz/a-simple-module-0.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0' 
+DESCRIPTION = 'my first python package'
+LONG_DESCRIPTION = 'The first python package I ever created, it has functionality to do basic math operations.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="a-simple-module", 
+        version=VERSION,
+        author="Stijn Dom",
+        author_email="stijn.dom@hotmail.be",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'numpy'
+        
+        keywords=['python', 'first package', 'math'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
