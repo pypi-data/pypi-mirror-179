7d426dfe01a7a18a698595642e996e2add4d7273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:02 +0000

    Add package compas-rv3/0.2.1/compas_rv3-0.2.1.tar.gz

---
+++ packages/compas-rv3/compas_rv3-0.2.1.tar.gz/compas_rv3-0.2.1/src/compas_rv3/rhino/ui/RV3/dev/RV3_thrust_move_supports_cmd.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import compas_rhino
+from compas_ui.ui import UI
+
+
+__commandname__ = "RV3_thrust_move_supports"
+
+
+@UI.error()
+def RunCommand(is_interactive):
+
+    ui = UI()
+
+    form = ui.scene.active_object.get_child_by_name("FormDiagram")
+    if not form:
+        compas_rhino.display_message("No FormDiagram found in the active group.")
+        return
+
+    thrust = ui.scene.active_object.get_child_by_name("ThrustDiagram")
+    if not thrust:
+        compas_rhino.display_message("No ThrustDiagram found in the active group.")
+        return
+
+    # hide the form vertices
+    form_vertices = "{}::vertices".format(form.settings["layer"])
+    compas_rhino.rs.HideGroup(form_vertices)
+
+    # show the thrust vertices
+    thrust_vertices_free = "{}::vertices_free".format(thrust.settings["layer"])
+    thrust_vertices_anchor = "{}::vertices_anchor".format(thrust.settings["layer"])
+    compas_rhino.rs.HideGroup(thrust_vertices_free)
+    compas_rhino.rs.ShowGroup(thrust_vertices_anchor)
+
+    compas_rhino.rs.Redraw()
+
+    # select anchored vertices
+    vertices = thrust.select_vertices_anchor()
+
+    if vertices:
+        if thrust.move_vertices_vertical(vertices):
+            for vertex in vertices:
+                # update the corresponding form diagram vertices
+                z = thrust.diagram.vertex_attribute(vertex, "z")
+                form.diagram.vertex_attribute(vertex, "z", z)
+            thrust.is_valid = False
+
+    ui.scene.update()
+    ui.record()
+
+
+if __name__ == "__main__":
+    RunCommand(True)
