a25c92cc7f4d00f7207e648c25d4aeb3045ca8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:10 +0000

    Add package kfish/99.0.202212031533.202103111306/kfish-99.0.202212031533.202103111306-py3-none-any.whl

---
+++ packages/kfish/kfish-99.0.202212031533.202103111306-py3-none-any.whl/ailib/common/__init__.py
@@ -0,0 +1,141 @@
+from ast import literal_eval
+from base64 import b64decode
+from urllib.request import urlopen
+from urllib.parse import urlencode
+import json
+import os
+import sys
+import yaml
+from time import time
+from uuid import UUID
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# colors = {'blue': '36', 'red': '31', 'green': '32', 'yellow': '33', 'pink': '35', 'white': '37'}
+
+
+def error(text):
+    color = "31"
+    print(f'\033[0;{color}m{text}\033[0;0m')
+
+
+def warning(text):
+    color = "33"
+    print(f'\033[0;{color}m{text}\033[0;0m')
+
+
+def info(text):
+    color = "36"
+    print(f'\033[0;{color}m{text}\033[0;0m')
+
+
+def success(text):
+    color = "32"
+    print(f'\033[0;{color}m{text}\033[0;0m')
+
+
+def get_overrides(paramfile=None, param=[]):
+    """
+
+    :param paramfile:
+    :param param:
+    :return:
+    """
+    overrides = {}
+    if paramfile is not None:
+        if not os.path.exists(os.path.expanduser(paramfile)):
+            error(f"Parameter file {paramfile} not found. Leaving")
+            os._exit(1)
+        with open(os.path.expanduser(paramfile)) as f:
+            try:
+                overrides = yaml.safe_load(f)
+            except:
+                error(f"Couldn't parse your parameters file {paramfile}. Leaving")
+                os._exit(1)
+    if param is not None:
+        for x in param:
+            if len(x.split('=')) < 2:
+                continue
+            else:
+                if len(x.split('=')) == 2:
+                    key, value = x.split('=')
+                else:
+                    split = x.split('=')
+                    key = split[0]
+                    value = x.replace(f"{key}=", '')
+                if value.isdigit():
+                    value = int(value)
+                elif value.lower() == 'true':
+                    value = True
+                elif value.lower() == 'false':
+                    value = False
+                elif value == '[]':
+                    value = []
+                elif value.startswith('{') and value.endswith('}') and not value.startswith('{\"ignition'):
+                    value = literal_eval(value)
+                elif value.startswith('[') and value.endswith(']'):
+                    if '{' in value:
+                        value = literal_eval(value)
+                    else:
+                        value = value[1:-1].split(',')
+                        for index, v in enumerate(value):
+                            v = v.strip()
+                            value[index] = v
+                overrides[key] = value
+    return overrides
+
+
+def get_token(token, offlinetoken=None):
+    aihome = f"{os.environ['HOME']}/.aicli"
+    url = 'https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token'
+    if token is not None:
+        segment = token.split('.')[1]
+        padding = len(segment) % 4
+        segment += padding * '='
+        expires_on = json.loads(b64decode(segment))['exp']
+        remaining = expires_on - time()
+        if expires_on == 0 or remaining > 600:
+            return token
+    data = {"client_id": "cloud-services", "grant_type": "refresh_token", "refresh_token": offlinetoken}
+    data = urlencode(data).encode("ascii")
+    result = urlopen(url, data=data).read()
+    page = result.decode("utf8")
+    token = json.loads(page)['access_token']
+    with open(f"{aihome}/token.txt", 'w') as f:
+        f.write(token)
+    return token
+
+
+def confirm(message):
+    message = f"{message} [y/N]: "
+    try:
+        _input = input(message)
+        if _input.lower() not in ['y', 'yes']:
+            error("Leaving...")
+            sys.exit(1)
+    except:
+        sys.exit(1)
+    return
+
+
+def same_uuid(id1, id2):
+    return id1.split('-')[3:] == id2.split('-')[3:]
+
+
+def match_mac(host, mac):
+    if ':' not in mac or 'inventory' not in host:
+        return False
+    found = False
+    for interface in json.loads(host['inventory'])['interfaces']:
+        if interface.get('mac_address', '') == mac:
+            found = True
+            break
+    return found
+
+
+def valid_uuid(uuid):
+    try:
+        UUID(uuid)
+        return True
+    except:
+        return False
