0e301365c3aa7c4b56b410074473c5ffba731d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:16 +0000

    Add package ptac/0.1.2a0/ptac-0.1.2a0.tar.gz

---
+++ packages/ptac/ptac-0.1.2a0.tar.gz/ptac-0.1.2a0/setup.py
@@ -0,0 +1,35 @@
+
+"""
+PtAC setup script.
+
+See license in LICENSE.md
+"""
+
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="ptac",
+    version="0.1.2a",
+    author="Simon Nieland, Serra Yosmaoglu",
+    author_email="Simon.Nieland@dlr.de, Serra.Yosmaoglu@dlr.de",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DLR-VF/PtAC",
+    # platforms="any",
+    packages=["ptac", "ptac.urmoacjar"],
+    include_package_data=True,
+    # python_requires=">=3.8",
+    # install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
+    ],
+)
