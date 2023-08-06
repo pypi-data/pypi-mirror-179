9fe25e3312ac6d6b435bf1c0dcfec341fdd4613e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package hcai-nova-server-nightly/0.1.0.dev202212011528/hcai-nova-server-nightly-0.1.0.dev202212011528.tar.gz

---
+++ packages/hcai-nova-server-nightly/hcai-nova-server-nightly-0.1.0.dev202212011528.tar.gz/hcai-nova-server-nightly-0.1.0.dev202212011528/nova_server/utils/key_utils.py
@@ -0,0 +1,6 @@
+
+
+def get_key_from_request_form(request_form):
+    return request_form['username'] + '_' + request_form['database'] + '_' + request_form['scheme'] + '_' + \
+           request_form['streamName'] + '_' + request_form['annotator'] + '_' + \
+           request_form['sessions']
