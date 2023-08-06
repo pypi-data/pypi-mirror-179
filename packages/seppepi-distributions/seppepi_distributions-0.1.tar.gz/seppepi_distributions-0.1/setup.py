8295f16017efbead68dc1c49a57eec3cf83d47ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:18 +0000

    Add package seppepi-distributions/0.1/seppepi_distributions-0.1.tar.gz

---
+++ packages/seppepi-distributions/seppepi_distributions-0.1.tar.gz/seppepi_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='seppepi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['seppepi_distributions'],
+      author='Seppe Pi',
+      author_email='seppe.pintens@gmail.com',
+      zip_safe=False)
