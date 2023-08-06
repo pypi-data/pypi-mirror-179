8acc228d1af5a2be3083ce09c5366c4579a779fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:03 +0000

    Add package zypzctest/0.0.1/zypzctest-0.0.1.tar.gz

---
+++ packages/zypzctest/zypzctest-0.0.1.tar.gz/zypzctest-0.0.1/README.md
@@ -0,0 +1,6 @@
+#1.介绍
+This project is for testing. It doesn't have any function.
+#2.安装
+pip install zypzctest
+#3.准备工作
+什么都别准备了
---
+++ packages/zypzctest/zypzctest-0.0.1.tar.gz/zypzctest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zypzctest",
+    version="0.0.1",
+    author="PZC",
+    author_email="2556616974@qq.com",
+    description="This project is for testing. It doesn't have any function.",
+    url="https://gitee.com/always-on-the-road-pzc/220208",
+    packages=find_packages()
+)
