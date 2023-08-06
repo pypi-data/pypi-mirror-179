57bed896064f994adeb1e0e7a76495bfe45eb337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:13 +0000

    Add package dockyard-cli/0.1.1/dockyard-cli-0.1.1.tar.gz

---
+++ packages/dockyard-cli/dockyard-cli-0.1.1.tar.gz/dockyard-cli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dockyard_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0', 'requests>=2.28.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['dcli = dockyard_cli.main:main']}
+
+setup_kwargs = {
+    'name': 'dockyard-cli',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'rsingh',
+    'author_email': 'rsingh@altoslabs.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
