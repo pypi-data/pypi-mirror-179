81fd9b7c74362d06ba25590ef54fda2935868199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:19 +0000

    Add package re-common/0.2.51/re_common-0.2.51-py3-none-any.whl

---
+++ packages/re-common/re_common-0.2.51-py3-none-any.whl/re_common/baselibrary/utils/better_abc/__init__.py
@@ -0,0 +1,33 @@
+from abc import ABCMeta as NativeABCMeta
+# 该库来自于 better_abc 由于代码量太小 不想为此安装一个库，故将代码引入到基础库
+# 如果该库后面有更新，可以选择继续引入或者直接使用第三方库
+
+class DummyAttribute:
+    pass
+
+
+def abstract_attribute(obj=None):
+    if obj is None:
+        obj = DummyAttribute()
+    obj.__is_abstract_attribute__ = True
+    return obj
+
+
+class ABCMeta(NativeABCMeta):
+
+    def __call__(cls, *args, **kwargs):
+        instance = NativeABCMeta.__call__(cls, *args, **kwargs)
+        abstract_attributes = {
+            name
+            for name in dir(instance)
+            if getattr(getattr(instance, name), '__is_abstract_attribute__', False)
+        }
+        if abstract_attributes:
+            raise NotImplementedError(
+                "Can't instantiate abstract class {} with"
+                " abstract attributes: {}".format(
+                    cls.__name__,
+                    ', '.join(abstract_attributes)
+                )
+            )
+        return instance
