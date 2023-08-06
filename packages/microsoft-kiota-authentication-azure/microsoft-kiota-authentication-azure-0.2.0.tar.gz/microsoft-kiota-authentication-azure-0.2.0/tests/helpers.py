9dc2259b2fbfb2da4b21197c1c72c3459c42997c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:43 +0000

    Add package microsoft-kiota-authentication-azure/0.2.0/microsoft-kiota-authentication-azure-0.2.0.tar.gz

---
+++ packages/microsoft-kiota-authentication-azure/microsoft-kiota-authentication-azure-0.2.0.tar.gz/microsoft-kiota-authentication-azure-0.2.0/tests/helpers.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DummyToken:
+    token: str
+
+
+class DummySyncAzureTokenCredential():
+
+    def get_token(self, *args):
+        return DummyToken(token="This is a dummy token")
+
+
+class DummyAsyncAzureTokenCredential():
+
+    async def get_token(self, *args):
+        return DummyToken(token="This is a dummy token")
+
+    async def close(self, *args):
+        pass
---
+++ packages/microsoft-kiota-authentication-azure/microsoft-kiota-authentication-azure-0.2.0.tar.gz/microsoft-kiota-authentication-azure-0.2.0/tests/test_azure_identity_authentication_provider.py
@@ -0,0 +1,23 @@
+import pytest
+from kiota_abstractions.request_information import RequestInformation
+
+from kiota_authentication_azure.azure_identity_authentication_provider import (
+    AzureIdentityAuthenticationProvider,
+)
+
+from .helpers import DummyAsyncAzureTokenCredential, DummySyncAzureTokenCredential
+
+
+def test_invalid_instantiation_without_credentials():
+    with pytest.raises(Exception):
+        auth_provider = AzureIdentityAuthenticationProvider(None)
+
+
+@pytest.mark.asyncio
+async def test_valid_instantiation_without_options():
+    auth_provider = AzureIdentityAuthenticationProvider(DummyAsyncAzureTokenCredential())
+    request_info = RequestInformation()
+    request_info.url = "https://graph.microsoft.com"
+    await auth_provider.authenticate_request(request_info)
+    assert isinstance(auth_provider, AzureIdentityAuthenticationProvider)
+    assert 'authorization' in request_info.request_headers
