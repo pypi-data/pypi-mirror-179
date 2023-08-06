7ac26d277ee6011f89adc80108f8134e8d79d3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:41 +0000

    Add package quicklookts/0.0.9/quicklookts-0.0.9.tar.gz

---
+++ packages/quicklookts/quicklookts-0.0.9.tar.gz/quicklookts-0.0.9/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+# from distutils.core import setup
+import codecs
+import os
+
+
+VERSION = '0.0.9'
+DESCRIPTION = 'quicklookts'
+
+# Setting up
+setup(
+    name="quicklookts",
+    version=VERSION,
+    author="Hong Tang",
+    author_email="<stanghong@gmail.com>",
+    description=DESCRIPTION,
+    # url = 'https://github.com/stanghong/pypi_publication', 
+    # packages=find_packages('addup'),
+    packages=['libname'],
+    install_requires=['numpy','pandas',],
+    # download_url = 'https://github.com/stanghong/pypi_publication/archive/refs/tags/v0.0.9.tar.gz',
+    
+    keywords=['python', 'visualization','plot'],
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
