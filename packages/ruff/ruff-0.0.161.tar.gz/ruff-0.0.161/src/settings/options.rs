852ae3876ada50f379ed3231a623504a814eee50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:59 +0000

    Add package ruff/0.0.161/ruff-0.0.161.tar.gz

---
+++ packages/ruff/ruff-0.0.161.tar.gz/ruff-0.0.161/src/settings/options.rs
@@ -0,0 +1,45 @@
+//! Options that the user can provide via pyproject.toml.
+
+use rustc_hash::FxHashMap;
+use serde::{Deserialize, Serialize};
+
+use crate::checks_gen::CheckCodePrefix;
+use crate::settings::types::{PythonVersion, SerializationFormat};
+use crate::{
+    flake8_annotations, flake8_bugbear, flake8_quotes, flake8_tidy_imports, isort, mccabe,
+    pep8_naming, pyupgrade,
+};
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
+#[serde(deny_unknown_fields, rename_all = "kebab-case")]
+pub struct Options {
+    pub allowed_confusables: Option<Vec<char>>,
+    pub dummy_variable_rgx: Option<String>,
+    pub exclude: Option<Vec<String>>,
+    pub extend_exclude: Option<Vec<String>>,
+    pub extend_ignore: Option<Vec<CheckCodePrefix>>,
+    pub extend_select: Option<Vec<CheckCodePrefix>>,
+    pub external: Option<Vec<String>>,
+    pub fix: Option<bool>,
+    pub fixable: Option<Vec<CheckCodePrefix>>,
+    pub format: Option<SerializationFormat>,
+    pub ignore: Option<Vec<CheckCodePrefix>>,
+    pub ignore_init_module_imports: Option<bool>,
+    pub line_length: Option<usize>,
+    pub select: Option<Vec<CheckCodePrefix>>,
+    pub show_source: Option<bool>,
+    pub src: Option<Vec<String>>,
+    pub target_version: Option<PythonVersion>,
+    pub unfixable: Option<Vec<CheckCodePrefix>>,
+    // Plugins
+    pub flake8_annotations: Option<flake8_annotations::settings::Options>,
+    pub flake8_bugbear: Option<flake8_bugbear::settings::Options>,
+    pub flake8_quotes: Option<flake8_quotes::settings::Options>,
+    pub flake8_tidy_imports: Option<flake8_tidy_imports::settings::Options>,
+    pub isort: Option<isort::settings::Options>,
+    pub mccabe: Option<mccabe::settings::Options>,
+    pub pep8_naming: Option<pep8_naming::settings::Options>,
+    pub pyupgrade: Option<pyupgrade::settings::Options>,
+    // Tables are required to go last.
+    pub per_file_ignores: Option<FxHashMap<String, Vec<CheckCodePrefix>>>,
+}
