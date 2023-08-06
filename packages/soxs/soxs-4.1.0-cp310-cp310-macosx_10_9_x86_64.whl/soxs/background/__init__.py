a2949d007ffbc947cfac5b01e6da00ea55a5ae20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:49 +0000

    Add package soxs/4.1.0/soxs-4.1.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/soxs/soxs-4.1.0-cp310-cp310-macosx_10_9_x86_64.whl/soxs/background/__init__.py
@@ -0,0 +1,6 @@
+from .point_sources import make_ptsrc_background, \
+    make_point_sources_file, make_point_source_list
+from .diffuse import make_diffuse_background
+from .spectra import BackgroundSpectrum, \
+    ConvolvedBackgroundSpectrum
+from .events import add_background_from_file
