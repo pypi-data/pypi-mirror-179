aa0c745aa4341e433aef4c17054804b911a56a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:14 +0000

    Add package nocode-autonn/1.1.0/nocode-autonn-1.1.0.tar.gz

---
+++ packages/nocode-autonn/nocode-autonn-1.1.0.tar.gz/nocode-autonn-1.1.0/AutoNN/__init__.py
@@ -0,0 +1,14 @@
+
+
+__version__ ="1.0.0"
+__authors__ ='Anish Konar, Arjun Ghosh, Rajarshi Banerjee, Sagnik Nayak.' 
+
+print('''
+
+░█▀▀█ █░░█ ▀▀█▀▀ █▀▀█ ▒█▄░▒█ ▒█▄░▒█ 
+▒█▄▄█ █░░█ ░░█░░ █░░█ ▒█▒█▒█ ▒█▒█▒█ 
+▒█░▒█ ░▀▀▀ ░░▀░░ ▀▀▀▀ ▒█░░▀█ ▒█░░▀█
+
+An AutoML framework by
+Anish Konar, Arjun Ghosh, Rajarshi Banerjee, Sagnik Nayak.
+''')
