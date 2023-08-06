ff72ac1a19ff29c4a4cec309679403dce330aa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:47 +0000

    Add package biofilm/0.0.102/biofilm-0.0.102.tar.gz

---
+++ packages/biofilm/biofilm-0.0.102.tar.gz/biofilm-0.0.102/biofilm/optimize6.py
@@ -0,0 +1,98 @@
+import numpy as np
+import dirtyopts
+import structout as so
+from sklearn.metrics import  f1_score
+import pprint
+from biofilm import util
+from autosklearn.experimental.askl2 import AutoSklearn2Classifier as ASK2
+from autosklearn.classification import AutoSklearnClassifier as ASK1
+import autosklearn.metrics
+
+optidoc='''
+--methods str+ any  'extra_trees', 'passive_aggressive', 'random_forest', 'sgd', 'gradient_boosting', 'mlp'
+--out str jsongoeshere
+--n_jobs int 1
+--time int 3600
+--memoryMBthread int 8000
+--randinit int 1337  # should be the same as the one in data.py
+--preprocess bool False
+--tmp_folder str
+#--metric str f1 assert f1 auc   TODO
+'''
+
+'''
+this gives all the calssifiers:
+import autosklearn.pipeline.components.classification as cls
+cls._classifiers.keys()
+'adaboost', 'bernoulli_nb', 'decision_tree', 'extra_trees', 'gaussian_nb', 'gradient_boosting', 'k_nearest_neighbors', 'lda', 'liblinear_svc', 'libsvm_svc', 'mlp', 'multinomial_nb', 'passive_aggressive', 'qda', 'random_forest', 'sgd'
+'''
+
+def optimize(X,Y,x,y, args):
+
+    if args.methods[0] == 'any':
+        estis =  None #['extra_trees', 'passive_aggressive', 'random_forest', 'sgd', 'gradient_boosting', 'mlp']
+    else:
+        estis = args.methods
+
+
+    #include_estimators = estis,
+    #include_preprocessors = ["no_preprocessing"] if not args.preprocess else None,
+
+    include = {'classifier': estis} if estis else {}
+    print("ALLGOOD:", include)
+    if not args.preprocess:
+            include['feature_preprocessor'] =  ["no_preprocessing"]
+            #include['data_preprocessor']     =  ['NoPreprocessing']
+
+    estim = ASK1(
+            n_jobs = args.n_jobs,
+            ensemble_size = 1,
+            include = include,
+            memory_limit = args.memoryMBthread,
+            time_left_for_this_task = args.time,
+            metric = autosklearn.metrics.f1,
+            max_models_on_disc = 1,
+            tmp_folder = args.tmp_folder or None,
+            initial_configurations_via_metalearning=0 # autosklearn thros warnings otherwise
+            )
+
+    print('OPTIMIZATION DATATYPE:',type(X))
+    estim.fit(X,Y)
+    #IMPORT CODE
+    #code.interact(local=dict(globals(), **locals()))
+    # there is only 1 model in the end -> 0, we dont care about its weight -> 1 (this is the model)
+    #print(f" asdasdasd{estim.get_models_with_weights()}")
+    # models with weights is a list of model in the ensemble: [(weight_1, model_1), …, (weight_n, model_n)]
+    #pipeline = estim.get_models_with_weights()[0][1]
+    #print('estim',estim.get_params(deep=True))
+    #print('estimator:',estimator.get_params(deep=True))
+
+    return estim
+
+
+
+
+def main():
+    args = dirtyopts.parse(optidoc)
+    data, fea, ins = util.getfold()
+    model = optimize(*data,args)
+    scorehistory =  np.nan_to_num(\
+            model.performance_over_time_['single_best_optimization_score'].to_numpy(),nan=0.0)
+    util.report(model, args.out, additionaloutput=\
+            {'scorehistory': scorehistory , 'performancelog': model.performance_over_time_})
+    so.lprint(scorehistory)
+    if data[0].shape[1] < 100:
+        print("SELECTED FEATURES: ", end='')
+        pipeline = model.get_models_with_weights()[0][1]
+        try:
+            if type(pipeline.steps[2][1].choice.preprocessor) == str:
+                print("all")
+            else:
+                print('\n',pipeline.steps[2][1].choice.preprocessor.get_support())
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ packages/biofilm/biofilm-0.0.102.tar.gz/biofilm-0.0.102/biofilm/util/data.py
@@ -0,0 +1,96 @@
+import dirtyopts
+import numpy as np
+from sklearn.utils import resample
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from lmz import iterselect, Range
+import scipy.sparse as sparse
+from ubergauss import tools
+datadoc='''
+# theese are the options for reading data
+--infile str myNumpyDump
+--loader str  # optional path to a python file that introduces a load function
+
+
+--randinit int 1337
+--folds int 5
+--foldselect int 0
+
+--subsample int -1
+--Z bool False
+
+--featurefile str
+--featurecount int -1
+'''
+
+
+def getargs():
+    return dirtyopts.parse(datadoc)
+
+def getfold():
+    args= dirtyopts.parse(datadoc).__dict__
+    return loadfolds(**args)
+
+
+def loadfolds(infile=None,loader=None,randinit=None, folds=None,foldselect=None, subsample=None, Z=None, featurefile=None, featurecount=None):
+    if not loader:
+        # assume data as saved via ubergauss.tools.ndumpfile
+        raw = tools.nloadfile(infile)
+        X  = raw[0]
+        if len(raw) < 3:
+            raw.append( np.array(Range(X.shape[1])) )
+        if len(raw) < 4:
+            raw.append( np.array(Range(X.shape[0])) )
+        """
+        d = np.load(infile,allow_pickle=True)
+        X,y  = [d[f'arr_{x}'] for x in range(2)]
+        print(f" {X.shape=} {y.shape=}")
+        instances = np.array(Range(X.shape[0]))
+        features = np.array(Range(X.shape[1]))
+        """
+
+        X,y, features, instances = raw
+    else:
+        scope = {}
+        exec(open(loader,'r').read(), scope)
+        X,y, features, instances = scope['read'](infile)
+
+    assert X.shape[1] == len(features), f'{X.shape[1]=} {len(features)=}'
+
+    if featurefile != '':
+        featurefile += '.npz'
+        if featurecount > 0: # size constrained list
+            ft_quality = np.load(featurefile, allow_pickle=True)['arr_1']
+            want=np.argsort(ft_quality)[-featurecount:]
+            X=X[:,want]
+            features=features[:,want]
+
+        else: # default list
+            ft = np.load(featurefile)['arr_0']
+            X=X[:,ft%2==1] # works with 0/1 and False/True
+            features=[ f for f, ok in zip(features,ft%2==1) if ok]
+
+
+
+    if subsample > 1 and X.shape[0]>subsample:
+        X,y, instances = resample(X,y,instances, replace=False,
+                n_samples=subsample,
+                random_state=randinit,
+                stratify =y)
+    if Z:
+        X = StandardScaler(with_mean= type(X) != sparse.csr_matrix ).fit_transform(X )
+
+    if folds > 1:
+        return iterselect(
+                kfold(X,y,n_splits = folds,randseed=randinit,
+                    feature_names = features, instance_names = instances),
+                foldselect)
+    else:
+        return (X,y,np.array([]),np.array([])),features,instances
+
+def kfold(X, y, n_splits=5, randseed=None, shuffle=True, feature_names=None, instance_names=None):
+    kf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=randseed)
+    for train,test in  kf.split(X, y):
+        yield (X[train], y[train], X[test], y[test]),  feature_names, instance_names[test]
+
+
