76284c1e51550526dc067413cba09e7ace5a053c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:14 +0000

    Add package a-simple-module-kul/0.0.3/a_simple_module_kul-0.0.3.tar.gz

---
+++ packages/a-simple-module-kul/a_simple_module_kul-0.0.3.tar.gz/a_simple_module_kul-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'my first python package'
+LONG_DESCRIPTION = 'The first python package I ever created, it has functionality to do basic math operations.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="a_simple_module_kul", 
+        version=VERSION,
+        author="Stijn Dom",
+        author_email="stijn.dom@hotmail.be",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'numpy'
+        
+        keywords=['python', 'first package', 'math', 'university'],
+        classifiers= [
+            "Development Status :: 1 - Planning",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
