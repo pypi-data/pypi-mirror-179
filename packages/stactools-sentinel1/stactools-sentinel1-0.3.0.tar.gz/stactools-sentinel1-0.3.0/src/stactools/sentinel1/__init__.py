89a95ddfb947b638df897df381b63579b9d001b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:06 +0000

    Add package stactools-sentinel1/0.3.0/stactools-sentinel1-0.3.0.tar.gz

---
+++ packages/stactools-sentinel1/stactools-sentinel1-0.3.0.tar.gz/stactools-sentinel1-0.3.0/src/stactools/sentinel1/__init__.py
@@ -0,0 +1,15 @@
+import stactools.core
+from stactools.cli.registry import Registry
+
+from stactools.sentinel1.commands import create_sentinel1_command
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry: Registry):
+    # Register subcommands
+
+    registry.register_subcommand(create_sentinel1_command)
+
+
+__version__ = '0.3.0'
