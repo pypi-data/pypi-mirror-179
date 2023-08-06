8952c1777c396d667206490f3622c1aaecff7e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:24 +0000

    Add package timelinecluster/0.5/timelinecluster-0.5.tar.gz

---
+++ packages/timelinecluster/timelinecluster-0.5.tar.gz/timelinecluster-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(name='timelinecluster',
+      version='0.5',
+      description='This is timelinecluster.',
+      url='',
+      author='author test',
+      author_email='fayristh0268@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires= [],
+      python_requires='>=3.6',
+      zip_safe=False)
