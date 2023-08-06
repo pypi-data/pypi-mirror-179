bd1260b833d1882ffe12f41fb85bea0ebb2dac5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:08 +0000

    Add package tensortrax/0.0.5/tensortrax-0.0.5.tar.gz

---
+++ packages/tensortrax/tensortrax-0.0.5.tar.gz/tensortrax-0.0.5/tensortrax/__init__.py
@@ -0,0 +1,20 @@
+r"""
+ _                            
+| |                          ████████╗██████╗  █████╗ ██╗  ██╗
+| |_ ___ _ __  ___  ___  _ __╚══██╔══╝██╔══██╗██╔══██╗╚██╗██╔╝
+| __/ _ \ '_ \/ __|/ _ \| '__|  ██║   ██████╔╝███████║ ╚███╔╝ 
+| ||  __/ | | \__ \ (_) | |     ██║   ██╔══██╗██╔══██║ ██╔██╗ 
+ \__\___|_| |_|___/\___/|_|     ██║   ██║  ██║██║  ██║██╔╝ ██╗
+                                ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝  
+"""
+
+from ._tensor import Tensor
+from ._helpers import f, δ, Δ, Δδ
+from .math._tensor import (
+    function,
+    gradient,
+    hessian,
+    gradient_vector_product,
+    hessian_vector_product,
+)
+from . import math
