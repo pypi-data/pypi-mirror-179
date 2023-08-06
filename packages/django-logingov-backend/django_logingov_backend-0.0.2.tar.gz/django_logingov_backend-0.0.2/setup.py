faa460e699d8748a185309e7f4c7050118c0de49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:00 +0000

    Add package django-logingov-backend/0.0.2/django_logingov_backend-0.0.2.tar.gz

---
+++ packages/django-logingov-backend/django_logingov_backend-0.0.2.tar.gz/django_logingov_backend-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='tdb-django-logingov-oidc',
+    version='0.0.2',
+    description='Django Login.gov OpenID Connect authentication provider',
+    packages=find_packages(),
+)
