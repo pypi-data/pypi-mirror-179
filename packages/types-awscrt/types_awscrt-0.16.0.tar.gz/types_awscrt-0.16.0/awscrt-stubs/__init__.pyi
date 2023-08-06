a713f563aae3af3d1d2d8f4f3f53bd81155e1ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:23 +0000

    Add package types-awscrt/0.16.0/types_awscrt-0.16.0.tar.gz

---
+++ packages/types-awscrt/types_awscrt-0.16.0.tar.gz/types_awscrt-0.16.0/awscrt-stubs/__init__.pyi
@@ -0,0 +1,9 @@
+from awscrt import auth as auth
+from awscrt import crypto as crypto
+from awscrt import http as http
+from awscrt import io as io
+from awscrt import mqtt as mqtt
+from awscrt import s3 as s3
+
+class NativeResource:
+    def __init__(self) -> None: ...
