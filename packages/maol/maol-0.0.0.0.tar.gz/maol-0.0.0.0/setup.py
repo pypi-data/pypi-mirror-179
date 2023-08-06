8bfdeea45ffe6e601df36105f99f10fffb7a738a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:27 +0000

    Add package maol/0.0.0.0/maol-0.0.0.0.tar.gz

---
+++ packages/maol/maol-0.0.0.0.tar.gz/maol-0.0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+
+
+setup(
+    name="maol",
+    version="0.0.0.0",
+    description="Model Assisted Object Labeling Toolkit",
+    long_description_content_type="text/markdown",
+    author="Matthew Sessions",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=[],
+
+)
