b7259d20c4dfe0dfd4d94c29d36ee7ba4da42c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:15 +0000

    Add package protobuf-to-pydantic/0.1.5/protobuf_to_pydantic-0.1.5-py3-none-any.whl

---
+++ packages/protobuf-to-pydantic/protobuf_to_pydantic-0.1.5-py3-none-any.whl/protobuf_to_pydantic/grpc_types.py
@@ -0,0 +1,24 @@
+from google.protobuf.any_pb2 import Any as AnyMessage  # type: ignore
+from google.protobuf.descriptor import Descriptor, FieldDescriptor  # type: ignore
+from google.protobuf.duration_pb2 import Duration  # type: ignore
+from google.protobuf.json_format import MessageToDict  # type: ignore
+from google.protobuf.message import Message  # type: ignore
+
+try:
+    from google.protobuf.pyext._message import RepeatedCompositeContainer, RepeatedScalarContainer  # type: ignore
+except ModuleNotFoundError:
+    from google._upb._message import RepeatedCompositeContainer, RepeatedScalarContainer
+
+from google.protobuf.timestamp_pb2 import Timestamp  # type: ignore
+
+__all__ = [
+    "AnyMessage",
+    "Descriptor",
+    "Duration",
+    "FieldDescriptor",
+    "Message",
+    "Timestamp",
+    "MessageToDict",
+    "RepeatedCompositeContainer",
+    "RepeatedScalarContainer",
+]
