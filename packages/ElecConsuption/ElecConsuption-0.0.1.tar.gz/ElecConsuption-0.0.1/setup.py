a54c8cca59938d894c1d771bb519c8c1959e8e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:50 +0000

    Add package ElecConsuption/0.0.1/ElecConsuption-0.0.1.tar.gz

---
+++ packages/ElecConsuption/ElecConsuption-0.0.1.tar.gz/ElecConsuption-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ElecConsuption",
+    version='0.0.1',
+    description=" Prediction of electricity consumption in France for 8/12/2022",
+    url="https://github.com/Paul-Crp/Projet-Conso-Electrique-France.git",
+    author="Thamara Renoir,Oualid Lamrini,Emma Sinibaldi,Paul Crespin",
+			
+    author_email="paul.crespin@etu.umontpellier.fr",
+    license="MIT",
+
+    packages=[],
+    zip_safe=False,
+)
