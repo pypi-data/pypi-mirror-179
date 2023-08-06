edec422df1a1f75d504d58992efc72e96eaf5228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:38 +0000

    Add package randseal/1.4.2/randseal-1.4.2-py3-none-any.whl

---
+++ packages/randseal/randseal-1.4.2-py3-none-any.whl/randseal/get.py
@@ -0,0 +1,37 @@
+import discord, random
+from importlib import resources
+from .utils import blank
+
+def file():
+	"""
+	Returns a `discord.File()` of a seal for py-cord
+	"""
+	try:
+		try:
+			sealrand = f"{random.randrange(1, 82)}"
+		finally:
+			if len(sealrand) == 1:
+				sussy = sealrand
+				sealrand = "0" + f"{sussy}"
+	finally:
+		with resources.open_text('randseal', f'00{sealrand}.jpg') as f:
+			return discord.File(fp=f.name, filename=f"{sealrand}.png")
+
+def embed(title: str = random.choice(["Here is your seal!", "Arff Arff!", ":3", "Seap!"])):
+	"""
+	Returns a `discord.Embed()` of a seal
+	"""
+	try:
+		try:
+			sealrand = f"{random.randrange(1, 82)}"
+		finally:
+			if len(sealrand) == 1:
+					sussy = sealrand
+					sealrand = "0" + f"{sussy}"
+	finally:
+		try:
+			embeda = discord.Embed(colour=blank(), title=title).set_image(url=f"https://raw.githubusercontent.com/mariohero24/randseal/main/randseal/00{sealrand}.jpg")
+		finally:
+			return embeda
+
+# python3 -m twine upload --repository pypi dist/*
