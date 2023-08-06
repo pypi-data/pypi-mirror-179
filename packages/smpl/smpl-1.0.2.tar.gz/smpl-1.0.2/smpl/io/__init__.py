43823e7a081135f098621138626e8ed0d5a6ed83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:03 +0000

    Add package smpl/1.0.2/smpl-1.0.2.tar.gz

---
+++ packages/smpl/smpl-1.0.2.tar.gz/smpl-1.0.2/smpl/io/__init__.py
@@ -0,0 +1,5 @@
+"""Simplified input and output."""
+
+from .io import append, files, find_file, gf, mkdirs, pr, pwd, read, write
+
+__all__ = ["pwd", "mkdirs", "pr", "files", "find_file", "gf", "write", "read", "append"]
