72be33407f04b174ea83fa41262aeb6719e10452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:31 +0000

    Add package seismicio/0.0.6/seismicio-0.0.6-py3-none-any.whl

---
+++ packages/seismicio/seismicio-0.0.6-py3-none-any.whl/seismicio/__init__.py
@@ -0,0 +1,4 @@
+from .services.readsu import readsu
+from .services.readsuInMemory import readsuInMemory
+from .services.writesu import writesu
+from .services.writesuInMemory import writesuInMemory
