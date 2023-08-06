4e57f0e97045a14c244ab2a15dd1a0335feea1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:54 +0000

    Add package apricity-labs-colemen/0.0.5/apricity_labs_colemen-0.0.5-py3-none-any.whl

---
+++ packages/apricity-labs-colemen/apricity_labs_colemen-0.0.5-py3-none-any.whl/apricity/services/home/__init__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-import
+
+# import colemen_utils as c
+# from apricity_labs import main as _main
+# # from cerberus import Validator
+# import modules.Validator.cerberus as cerberus
+# from flask import Blueprint
+
+# home_blueprint = Blueprint('home_blueprint', __name__)
+
+from apricity.services.home.routes import home_blueprint
