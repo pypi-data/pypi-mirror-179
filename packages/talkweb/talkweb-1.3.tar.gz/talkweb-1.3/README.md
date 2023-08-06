645b67ba0bf82401c326a61a3e4fc0a2e63ed169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:43 +0000

    Add package talkweb/1.3/talkweb-1.3.tar.gz

---
+++ packages/talkweb/talkweb-1.3.tar.gz/talkweb-1.3/README.md
@@ -0,0 +1,23 @@
+## Talkweb
+
+Assemble pages.
+
+Talkweb makes a Object tree of html and allows addition, insertion and deletion to the tree like DOM.  
+
+```python
+from talkweb import *
+roots = h2o(htmlfile)
+#assuming htmlfile has &lt;!Doctype html&gt;&lt;html&gt;&lt;div id="x"&gt;&lt;/div&gt;&lt;/html&gt;
+
+#cell is a node
+roots[1].findcellbyid("x") 
+widget = h2o(widgethtmlfile)
+roots[1].addcell(widget)
+
+html=''
+for root in roots:
+    html += root.html()
+print(html)
+```
+
+Like it, then you will like to develop with talkweb. If not, leave you to templates and it's rules.
---
+++ packages/talkweb/talkweb-1.3.tar.gz/talkweb-1.3/setup.py
@@ -0,0 +1,36 @@
+"""
+    Copyright(c) 2022 Madhukumar Seshadri, All rights reserved.
+"""
+from setuptools import setup
+import os
+
+md_path = os.path.join(os.path.dirname(__file__),'README.md')
+long_description=open(md_path).read()
+
+setup(
+    name = 'talkweb',
+    version = '1.3',
+    license = 'MIT License',
+    url = 'https://www.github.com/madhukumarseshadri/talkweb3',
+    description = "A html to object framework",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    author = 'Madhukumar Seshadri',
+    author_email = 'madhukumarseshadri@gmail.com',
+    zip_safe = False,
+    platforms = 'any',
+    packages = [
+	    'talkweb',
+    ],
+    include_package_data=True,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ]
+)
