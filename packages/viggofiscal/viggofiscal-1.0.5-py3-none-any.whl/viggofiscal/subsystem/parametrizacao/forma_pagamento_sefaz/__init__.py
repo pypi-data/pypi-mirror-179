10d82f6d478f38308ecedc19e27014042d7596a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:19 +0000

    Add package viggofiscal/1.0.5/viggofiscal-1.0.5-py3-none-any.whl

---
+++ packages/viggofiscal/viggofiscal-1.0.5-py3-none-any.whl/viggofiscal/subsystem/parametrizacao/forma_pagamento_sefaz/__init__.py
@@ -0,0 +1,4 @@
+from viggocore.common import subsystem
+from viggofiscal.subsystem.parametrizacao.forma_pagamento_sefaz import resource
+
+subsystem = subsystem.Subsystem(resource=resource.FormaPagamentoSefaz)
