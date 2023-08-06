bb2490733078f056bd96d0ab1a6304148c95612a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:27 +0000

    Add package eyedlog/0.0.7/eyedlog-0.0.7-py3-none-any.whl

---
+++ packages/eyedlog/eyedlog-0.0.7-py3-none-any.whl/eyedlog/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from .logging import (
+    NOTSET, DEBUG, INFO, WARN, WARNING, ERROR, CRITICAL,
+    ZIP_STORED, ZIP_DEFLATED, ZIP_BZIP2, ZIP_LZMA,
+    ColoredLogger, get_logger
+)
