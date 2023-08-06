952ab733d4bd5407d556f7b19f4fc371a9246a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:37 +0000

    Add package snek5000-cbox/0.1.0/snek5000-cbox-0.1.0.tar.gz

---
+++ packages/snek5000-cbox/snek5000-cbox-0.1.0.tar.gz/snek5000-cbox-0.1.0/src/snek5000_cbox/output.py
@@ -0,0 +1,45 @@
+from snek5000 import mpi
+from snek5000.output.base import Output as OutputBase
+from snek5000_cbox.templates import box, size, makefile_usr
+
+
+class OutputCbox(OutputBase):
+    @property
+    def makefile_usr_sources(self):
+        """
+        Sources for inclusion to makefile_usr.inc
+        Dict[directory]  -> list of source files
+        """
+        return {
+            "toolbox": [
+                ("frame.f", "FRAMELP"),
+                ("mntrlog_block.f", "MNTRLOGD"),
+                ("mntrlog.f", "MNTRLOGD"),
+                ("mntrtmr_block.f", "MNTRLOGD", "MNTRTMRD"),
+                ("mntrtmr.f", "MNTRLOGD", "MNTRTMRD", "FRAMELP"),
+                ("rprm_block.f", "RPRMD"),
+                ("rprm.f", "RPRMD", "FRAMELP"),
+                ("io_tools_block.f", "IOTOOLD"),
+                ("io_tools.f", "IOTOOLD"),
+                ("chkpoint.f", "CHKPOINTD"),
+                ("chkpt_mstp.f", "CHKPTMSTPD", "CHKPOINTD"),
+                ("map2D.f", "MAP2D", "FRAMELP"),
+                ("stat.f", "STATD", "MAP2D", "FRAMELP"),
+                ("stat_IO.f", "STATD", "MAP2D", "FRAMELP"),
+                ("sfd.f", "SFDD", "FRAMELP"),
+                ("math_tools.f",),
+            ],
+        }
+
+    def post_init(self):
+        super().post_init()
+
+        # Write additional source files to compile the simulation
+        if mpi.rank == 0 and self._has_to_save and self.sim.params.NEW_DIR_RESULTS:
+            self.write_box(box)
+            self.write_size(size)
+            self.write_makefile_usr(makefile_usr)
+            print(f"{makefile_usr = }")
+
+
+Output = OutputCbox
