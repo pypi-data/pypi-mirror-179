4d3639104010f8e50be961e76fa1becef087af8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:31 +0000

    Add package json-linter/1.1.0/json_linter-1.1.0-py3-none-any.whl

---
+++ packages/json-linter/json_linter-1.1.0-py3-none-any.whl/json_linter/utils.py
@@ -0,0 +1,59 @@
+""" Utility functions """
+from importlib import import_module
+from pathlib import Path
+from types import ModuleType
+from typing import List, Dict
+
+COLOR_GREEN = "\033[92m"
+COLOR_RED = "\033[91m"
+COLOR_RESET = "\033[00m"
+
+
+def flatten(items: List) -> List:
+    """ Flatten list """
+    new_list: List = []
+
+    if not isinstance(items, list):
+        return new_list
+
+    for item in items:
+        if isinstance(item, list):
+            new_list.extend(flatten(item))
+            continue
+        new_list.append(item)
+    return new_list
+
+
+def get_modules_next_to_file(file: str, package: str) -> List[ModuleType]:
+    """ Get all modules next to file in package """
+    folder = Path(file).parent
+
+    modules = []
+
+    for python_file in folder.glob("*.py"):
+        if python_file.name.startswith("__"):
+            continue
+
+        module_name = python_file.name[:-3]
+        modules.append(import_module(f".{module_name}", package))
+
+    return modules
+
+
+def parse_var(string: str) -> (str, str):
+    """ Parses a key value pair """
+    items = string.split("=")
+    key = items[0].strip()
+    value = None
+    if len(items) > 1:
+        value = "=".join(items[1:])
+    return key, value
+
+
+def parse_vars(items: List[str]) -> Dict[str, str]:
+    """ Parse a list of key value pairs"""
+    result = {}
+    for item in items:
+        key, value = parse_var(item)
+        result[key] = value
+    return result
