54ee43a3b729ea7c3aeef338ae6480953fc3721b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:24 +0000

    Add package rics/2.0.0/rics-2.0.0.tar.gz

---
+++ packages/rics/rics-2.0.0.tar.gz/rics-2.0.0/src/rics/logs.py
@@ -0,0 +1,98 @@
+"""Utility methods for logging tasks."""
+import logging
+from contextlib import contextmanager as _contextmanager
+from typing import Any, Dict, Tuple, Union
+
+FORMAT: str = "%(asctime)s.%(msecs)03d [%(name)s:%(levelname)s] %(message)s"
+"""Default logging format; ``<date-format>.378 [rics:DEBUG] I'm a debug message!``"""
+
+DATE_FORMAT: str = "%Y-%m-%dT%H:%M:%S"
+"""Default logging date format; ``2022-02-05T11:17:05<logging-format>``"""
+
+
+def basic_config(
+    *,
+    format: str = FORMAT,  # noqa: A002
+    datefmt: str = DATE_FORMAT,
+    rics_level: Union[int, str] = None,
+    force: bool = True,
+    **kwargs: Any,
+) -> None:
+    """Do basic logging configuration with package defaults.
+
+    Simple wrapper for the standard :py:func:`logging.basicConfig`-method, using my personal preferences for defaults.
+
+    Args:
+        format: Format string for emitted messages; see :attr:`FORMAT`.
+        datefmt: Format string for date/time; see :attr:`DATE_FORMAT`.
+        rics_level: Log level for the `rics` package. Inherit if ``None``.
+        force: If ``True``, override existing configuration if it exists.
+        **kwargs: Keyword arguments for :py:func:`logging.basicConfig`.
+
+    Keyword Args:
+        <namespace>_level: Log level for the namespace denoted by `namespace` (without the `"_level"`-suffix).
+            Use underscores instead of dots for submodules, eg ``module.submodule`` => ``module_submodule``.
+
+    Examples:
+        Basic usage.
+
+        >>> from rics.logs import basic_config, logging
+        >>> root_logger = logging.getLogger()
+        >>> basic_config(level=logging.INFO, rics_level=logging.DEBUG)
+        >>> logging.getLogger("rics").debug("I'm a debug message!")
+        >>> root_logger.debug("I'm a debug message!")
+        >>> root_logger.critical("I'm a critical message!") # Doctest: +SKIP
+        2022-02-05T11:17:05.378 [rics:DEBUG] I'm a debug message!
+        2022-02-05T11:17:05.378 [root:CRITICAL] I'm a critical message!
+    """
+    wildcard_levels, kwargs = _extract_wildcards(rics_level=rics_level, force=force, **kwargs)
+    logging.basicConfig(format=format, datefmt=datefmt, **kwargs)
+
+    for name, level in wildcard_levels.items():
+        logging.getLogger(name).setLevel(level)
+
+
+@_contextmanager
+def disable_temporarily(logger: logging.Logger, *more_loggers: logging.Logger):  # type: ignore[no-untyped-def]  # noqa: ANN201
+    """Temporarily disable logging.
+
+    Args:
+        logger: A logger to disable.
+        *more_loggers: Additional loggers to disable.
+
+    Yields:
+        Nothing.
+
+    Examples:
+        Disable all logging temporarily.
+
+        >>> import logging
+        >>> logging.basicConfig()
+        >>> with disable_temporarily(logging.root):
+        ...     logging.info("This message is ignored.")
+    """
+    loggers = [logger] + list(more_loggers)
+    states = [lgr.disabled for lgr in loggers]
+
+    try:
+        for lgr in loggers:
+            lgr.disabled = True
+        yield
+    finally:
+        for lgr, old_state in zip(loggers, states):
+            lgr.disabled = old_state
+
+
+_LOG_LEVEL_SUFFIX = "_level"
+
+
+def _extract_wildcards(**kwargs: Any) -> Tuple[Dict[str, Union[int, str]], Dict[str, Any]]:
+    wildcard_levels: Dict[str, Union[int, str]] = {}
+    for key in list(kwargs.keys()):
+        if key and key.endswith(_LOG_LEVEL_SUFFIX):
+            wildcard_key = key[: -len(_LOG_LEVEL_SUFFIX)].replace("_", ".")
+            level = kwargs.pop(key)
+            if level is not None:  # pragma: no cover
+                wildcard_levels[wildcard_key] = level
+
+    return wildcard_levels, kwargs
---
+++ packages/rics/rics-2.0.0.tar.gz/rics-2.0.0/src/rics/performance/_util.py
@@ -0,0 +1,105 @@
+from typing import Any, Literal, Tuple, Union
+
+import pandas as pd
+
+from ._multi_case_timer import ResultsDict
+
+
+def to_dataframe(run_results: ResultsDict) -> pd.DataFrame:
+    """Create a DataFrame from performance run output.
+
+    Args:
+        run_results: Output from :meth:`rics.performance.MultiCaseTimer.run`.
+
+    Returns:
+        The `run_result` input wrapped in a DataFrame.
+    """
+    ans = []
+    for candidate_label, candidate_results in run_results.items():
+        for data_label, data_results in candidate_results.items():
+            ans.append(
+                pd.DataFrame(
+                    {
+                        "Candidate": candidate_label,
+                        "Test data": data_label,
+                        "Run no": range(len(data_results)),
+                        "Time [s]": data_results,
+                    }
+                )
+            )
+
+    df = pd.concat(ans, ignore_index=True)
+    df["Time [ms]"] = df["Time [s]"] * 1000
+    df["Time [μs]"] = df["Time [ms]"] * 1000
+    return df
+
+
+def get_best(run_results: Union[ResultsDict, pd.DataFrame]) -> pd.DataFrame:
+    """Get a summarized view of the best run results for each candidate/data pair.
+
+    Args:
+        run_results: Output of :meth:`rics.performance.MultiCaseTimer.run`.
+
+    Returns:
+        The best (lowest) times for each candidate/data pair.
+    """
+    best = (
+        (run_results if isinstance(run_results, pd.DataFrame) else to_dataframe(run_results))
+        .groupby(["Candidate", "Test data"])
+        .min()
+    )
+    return best[filter(lambda s: "time" in s.lower(), best.columns)]
+
+
+def plot_run(
+    run_results: Union[ResultsDict, pd.DataFrame],
+    x: Literal["candidate", "data"] = None,
+    unit: Literal["s", "ms", "μs", "us"] = "ms",
+    **figure_kwargs: Any,
+) -> None:
+    """Plot the results of a performance test.
+
+    .. figure:: ../_images/perf_plot.png
+
+       Comparison of ``time.sleep(t)`` and ``time.sleep(5*t)``.
+
+    Args:
+        run_results: Output of :meth:`rics.performance.MultiCaseTimer.run`.
+        x: The value to plot on the X-axis. Default=derive.
+        unit: Time unit to plot on the Y-axis.
+        **figure_kwargs: Keyword arguments for the barplot.
+
+    Raises:
+        ModuleNotFoundError: If Seaborn isn't installed.
+        ValueError: For unknown `unit` arguments.
+    """
+    import matplotlib.pyplot as plt
+    from seaborn import barplot
+
+    data = to_dataframe(run_results) if isinstance(run_results, dict) else run_results.copy()
+    data[["Test data", "Candidate"]] = data[["Test data", "Candidate"]].astype("category")
+
+    x_arg, hue = (
+        _smaller_as_hue(data)
+        if x is None
+        else (("Test data", "Candidate") if x == "data" else ("Candidate", "Test data"))
+    )
+    y = f"Time [{unit.replace('us', 'μs')}]"
+    if y not in data:  # pragma: no cover
+        raise ValueError(f"Bad {unit=}; column '{y}' not present in data.")
+
+    fig, (left, right) = plt.subplots(
+        ncols=2, tight_layout=True, figsize=(8 + 4 * data.Candidate.nunique(), 7), sharey=True
+    )
+    left.set_title("Average")
+    right.set_title("Best")
+    fig.suptitle("Performance", size=24)
+
+    barplot(ax=left, data=data, x=x_arg, y=y, hue=hue, errorbar="sd", **figure_kwargs)
+    best = data.groupby(["Test data", "Candidate"]).min().reset_index()
+    barplot(ax=right, data=best, x=x_arg, y=y, hue=hue, errorbar=None, **figure_kwargs)
+
+
+def _smaller_as_hue(data: pd.DataFrame) -> Tuple[str, str]:
+    unique = data.nunique()
+    return ("Test data", "Candidate") if unique["Test data"] < unique["Candidate"] else ("Candidate", "Test data")
