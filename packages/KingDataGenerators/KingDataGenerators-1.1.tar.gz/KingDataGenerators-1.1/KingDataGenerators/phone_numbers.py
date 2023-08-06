fbbd92b7503d1378fe6e7d249a520828dae900e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package KingDataGenerators/1.1/KingDataGenerators-1.1.tar.gz

---
+++ packages/KingDataGenerators/KingDataGenerators-1.1.tar.gz/KingDataGenerators-1.1/KingDataGenerators/phone_numbers.py
@@ -0,0 +1,24 @@
+from KingDataGenerators.variables import print_error_application, print_error_connection, print_time_load
+
+from fordev import generators
+
+def generator_cel(quant_cel, dd='(11)'):
+    print_time_load('NUMBERs')
+    confirm_int = int(quant_cel)
+    count_how_loops = 0
+    list_celulares = []
+    while count_how_loops < confirm_int:
+        count_how_loops += 1
+        
+        try:
+            peoples = generators.people()
+        except ConnectionError as error:
+            print_error_connection(error)
+        except Exception as error:
+            print_error_application(error)
+        
+        for people in peoples:
+            only_cel = people['celular'].replace('()', dd).replace(' ', '').replace('-', '').strip()
+            list_celulares.append(only_cel.strip())
+    
+    return list_celulares
---
+++ packages/KingDataGenerators/KingDataGenerators-1.1.tar.gz/KingDataGenerators-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+setup(
+    name='KingDataGenerators',
+    version=1.1,
+    description="This packet generates fictitious human data, uniquely (names,emails,password,personal-documents), for the use of the develpoer",
+    long_description=Path('README.md').read_text(),
+    author='Black King',
+    author_email='docomeco2k20@gmail.com',
+    keywords={'data human', 'generator', 'gerador', 'dados pessoais', 'fordev', '4dev'},
+    packages=find_packages()
+)
