9e9b0da9dbe914a30828053c5c659e6755e25e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:43 +0000

    Add package seq-pipeline/0.0.7/seq_pipeline-0.0.7.tar.gz

---
+++ packages/seq-pipeline/seq_pipeline-0.0.7.tar.gz/seq_pipeline-0.0.7/seq_pipeline/config.py
@@ -0,0 +1,5 @@
+import os
+
+
+PIPELINES_DIR = os.path.join(os.path.dirname(__file__), "pipelines")
+VERSION = "2022-12-01"
---
+++ packages/seq-pipeline/seq_pipeline-0.0.7.tar.gz/seq_pipeline-0.0.7/seq_pipeline/pipelines/bigwig_lab.sh
@@ -0,0 +1,22 @@
+# --- VARIABLES ---
+# > input_type = indexed_bam
+BASE_PATH="{ path base_exists }"
+CPU_CORES="{ integer > 0 }"
+BIGWIG_BIN_SIZE="{ integer > 0 }"
+
+
+# --- MODULES ---
+echo "# initializing environment and loading modules $(date '+%Y/%m/%d %H:%M:%S UTC%:::z')" >&2
+module reset
+export MUGQIC_INSTALL_HOME="/cvmfs/soft.mugqic/CentOS6"
+module use "$MUGQIC_INSTALL_HOME/modulefiles"
+module load mugqic/deepTools/3.5.0
+
+
+# --- 1 BIGWIG ---
+echo "# bigwig: start $(date '+%Y/%m/%d %H:%M:%S UTC%:::z')" >&2
+bamCoverage -b "$BASE_PATH.bam" -o "$BASE_PATH.fpkm.bigwig" -bs "$BIGWIG_BIN_SIZE" -e 150 --normalizeUsing RPKM -p "$CPU_CORES"
+
+
+# --- DONE ---
+echo "# done $(date '+%Y/%m/%d %H:%M:%S UTC%:::z')" >&2
