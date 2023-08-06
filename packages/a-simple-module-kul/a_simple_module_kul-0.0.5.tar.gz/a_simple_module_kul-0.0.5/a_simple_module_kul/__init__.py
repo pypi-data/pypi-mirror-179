064f95e722edbf037e7a5a7293200fc8a9bfd4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:09 +0000

    Add package a-simple-module-kul/0.0.5/a_simple_module_kul-0.0.5.tar.gz

---
+++ packages/a-simple-module-kul/a_simple_module_kul-0.0.5.tar.gz/a_simple_module_kul-0.0.5/a_simple_module_kul/__init__.py
@@ -0,0 +1,5 @@
+from .add import add
+from .subtract import subtract
+from .helloworld import hello
+
+from .extras import *
---
+++ packages/a-simple-module-kul/a_simple_module_kul-0.0.5.tar.gz/a_simple_module_kul-0.0.5/a_simple_module_kul/extras/__init__.py
@@ -0,0 +1,5 @@
+from .divide import divide
+from .multiply import multiply
+from .power import power
+
+__all__ = ["divide", "multiply", "power"]
