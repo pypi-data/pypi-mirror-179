377daf1711fc0341c33ebcaf1c3cce32c139d0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:30 +0000

    Add package MBCalculator/0.0.1/MBCalculator-0.0.1.tar.gz

---
+++ packages/MBCalculator/MBCalculator-0.0.1.tar.gz/MBCalculator-0.0.1/src/MBCalculator/Calc.py
@@ -0,0 +1,22 @@
+class Calc():
+
+    def __init__(self):
+        self.result = 0
+    
+    def add(self, a, b):
+        self.result = a + b
+        return self.result
+    
+    def sub(self, a, b):
+        self.result = a - b
+        return self.result
+    
+    def mul(self, a, b):
+        self.result = a * b
+        return self.result
+    
+    def div(self, a, b):
+        self.result = a / b
+        return self.result
+   
+        
