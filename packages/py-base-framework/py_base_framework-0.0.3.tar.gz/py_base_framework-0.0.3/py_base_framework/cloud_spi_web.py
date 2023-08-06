58367482aba7340053919f51e50a4d3ebc85a4ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:19 +0000

    Add package py-base-framework/0.0.3/py_base_framework-0.0.3.tar.gz

---
+++ packages/py-base-framework/py_base_framework-0.0.3.tar.gz/py_base_framework-0.0.3/py_base_framework/cloud_spi_web.py
@@ -0,0 +1,27 @@
+import json
+import threading
+
+from flask import Flask, request
+
+from py_base_framework.cloud_spi_core import app
+from py_base_framework.cloud_spi_log import log_key
+
+
+class JSONObject:
+    def __init__(self, d):
+        self.__dict__ = d
+
+
+@app.route('/base/cloud/spi', methods=['POST'])
+def cloud_spi():
+    app.logger.info(request.json)
+    threading.current_thread().__dict__[log_key] = request.headers.get(log_key)
+
+    method = request.json.get('method')
+    for rule in app.url_map.iter_rules():
+        if rule.rule == method:
+            request.json['data'] = json.loads(request.json.get('data'), object_hook=JSONObject)
+            func = app.view_functions[rule.endpoint]
+            return func()
+
+    return "null"
