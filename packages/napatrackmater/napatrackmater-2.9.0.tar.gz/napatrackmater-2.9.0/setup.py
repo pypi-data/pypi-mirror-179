bae12527c1f32f7c5b7002312a536c804e6a122b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:36 +0000

    Add package napatrackmater/2.9.0/napatrackmater-2.9.0.tar.gz

---
+++ packages/napatrackmater/napatrackmater-2.9.0.tar.gz/napatrackmater-2.9.0/setup.py
@@ -0,0 +1,50 @@
+import setuptools
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name="napatrackmater",
+
+    version='2.9.0',
+
+    author='Varun Kapoor,Claudia Carabana Garcia, Mari Tolonen',
+    author_email='randomaccessiblekapoor@gmail.com',
+    url='https://github.com/kapoorlab/NapaTrackMater/',
+    description='Import Trackmate XML files for Track Visualization and analysis in Napari.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "napari",
+        "pyqt5",
+        "natsort",
+        "scikit-image",
+        "scipy",
+        "tifffile",
+        "matplotlib",
+        "ffmpeg-python",
+        "imageio_ffmpeg",
+        "dask",
+        "lmfit",
+        "seaborn",
+        "perfmetrics",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'track = napatrackmater.__main__:main',
+        ]
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
