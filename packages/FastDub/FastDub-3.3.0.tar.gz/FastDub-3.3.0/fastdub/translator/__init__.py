60e44cb1a742cf58f0a105fab97a53bd89ccfd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:33 +0000

    Add package FastDub/3.3.0/FastDub-3.3.0.tar.gz

---
+++ packages/FastDub/FastDub-3.3.0.tar.gz/FastDub-3.3.0/fastdub/translator/__init__.py
@@ -0,0 +1,15 @@
+__all__ = 'SUPPORTED',
+try:
+    import translators
+    from fastdub.translator import subs_translate
+
+    SERVICES = [i[1:] for i in dir(translators.apis) if i.startswith('_') and not i.startswith('__')]
+
+
+    def get_service_by_name(service: str):
+        return getattr(translators, service)
+except ImportError:
+    SUPPORTED = False
+else:
+    SUPPORTED = True
+    __all__ += ('translators', 'SERVICES', 'get_service_by_name', 'subs_translate')
