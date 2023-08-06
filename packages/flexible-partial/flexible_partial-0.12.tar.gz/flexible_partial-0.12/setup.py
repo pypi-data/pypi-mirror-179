f47b46129e4bb5f3a312cb3f6155a06177fbf440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:41 +0000

    Add package flexible-partial/0.12/flexible_partial-0.12.tar.gz

---
+++ packages/flexible-partial/flexible_partial-0.12.tar.gz/flexible_partial-0.12/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.12'
+DESCRIPTION = "Like partial, but you can determine the order of *args"
+
+# Setting up
+setup(
+    name="flexible_partial",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/flexible_partial',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=[],
+    keywords=['partial', 'functools', 'args', 'kwargs'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=[],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
