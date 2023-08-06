7003be990ae98440f20b94da0e7b484249a19a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:22 +0000

    Add package voxpopuli/0.3.9/voxpopuli-0.3.9-py3-none-any.whl

---
+++ packages/voxpopuli/voxpopuli-0.3.9-py3-none-any.whl/voxpopuli/__init__.py
@@ -0,0 +1,5 @@
+from .main import Voice
+from .phonemes import (PhonemeList, Phoneme, FrenchPhonemes,
+                       BritishEnglishPhonemes, GreekPhonemes, ArabicPhonemes,
+                       SpanishPhonemes, GermanPhonemes, ItalianPhonemes,
+                       PortuguesePhonemes, AmericanEnglishPhonemes)
