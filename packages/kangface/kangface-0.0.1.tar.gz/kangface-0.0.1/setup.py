40eb761c0cf5bff82bc35c95a2b414f2a51413f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:17 +0000

    Add package kangface/0.0.1/kangface-0.0.1.tar.gz

---
+++ packages/kangface/kangface-0.0.1.tar.gz/kangface-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="kangface",
+    version="0.0.1",
+    author="HuDongkang",
+    author_email="2376699679@qq.com",
+    description="This project provides an easy way to recognize faces using AI",
+    # 项目主页
+    url="https://gitee.com/hudongkang/p9-project/kangface",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages()
+)
