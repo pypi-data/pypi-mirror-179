380ca22d824323a130ee93d075e58ece7e9cd6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:08 +0000

    Add package streamlit-img-label-ita/0.1.16/streamlit_img_label_ita-0.1.16-py3-none-any.whl

---
+++ packages/streamlit-img-label-ita/streamlit_img_label_ita-0.1.16-py3-none-any.whl/streamlit_img_label_ita/frontend/build/precache-manifest.dd1a8dc56f69f2842381c1d277a769ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "280c0e65f95ecb148933",
+    "url": "./static/js/main.280c0e65.chunk.js"
+  },
+  {
+    "revision": "13aec5dab3896b7e4f2c",
+    "url": "./static/js/1.13aec5da.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "416560b4836a9483579e6e8f349d9e89",
+    "url": "./index.html"
+  }
+];
