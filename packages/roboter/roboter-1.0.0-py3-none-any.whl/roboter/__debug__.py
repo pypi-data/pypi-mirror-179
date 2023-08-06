67e6dd66df3dcea0bfec7e5ec05c8d1421a130fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:14 +0000

    Add package roboter/1.0.0/roboter-1.0.0-py3-none-any.whl

---
+++ packages/roboter/roboter-1.0.0-py3-none-any.whl/roboter/__debug__.py
@@ -0,0 +1,17 @@
+"""
+@author axiner
+@version v1.0.0
+@created 2022/12/5 12:12
+@abstract
+@description
+@history
+"""
+import unittest
+
+from roboter import here
+
+
+class TestDebug(unittest.TestCase):
+
+    def test_001(self):
+        print(here)
