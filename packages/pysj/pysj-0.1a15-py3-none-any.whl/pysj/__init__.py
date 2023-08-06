b6d032981a1562459347e48d18a2de7b73a9abe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:48 +0000

    Add package pysj/0.1a15/pysj-0.1a15-py3-none-any.whl

---
+++ packages/pysj/pysj-0.1a15-py3-none-any.whl/pysj/__init__.py
@@ -0,0 +1,9 @@
+""" Pysj makes Python development more comfortable, with utils, classes and helper
+functions I find myself reimplementing in several projects."""
+__version__ = "0.1a15"
+
+from .crypto import md5, sha1, sha256, uuid
+from .main import (ExtendedJSONDecoder, ExtendedJSONEncoder, Timer, flatten,
+                   isotime, moving_average, n_wise, paginate, seconds,
+                   transpose, triplewise)
+from .nanoservice import NanoService, NanoServiceClient
