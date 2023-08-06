b2388a4a718a78dcf8385a4cf2debd9c83b87c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:42 +0000

    Add package fast-ple/0.0.3/fast_ple-0.0.3-py3-none-any.whl

---
+++ packages/fast-ple/fast_ple-0.0.3-py3-none-any.whl/ple/all.py
@@ -0,0 +1,8 @@
+from ple.lit_model import *
+try:
+    from ple.loss import *
+except Exception as e:
+    print(f'{e} while import loss, consider this command "pip install mmcv-full"')
+from ple.base_exp import *
+from ple.trainer import *
+from ple.datasets import *
