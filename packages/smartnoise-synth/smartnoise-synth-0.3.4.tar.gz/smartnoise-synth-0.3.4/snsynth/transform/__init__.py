c0bf948e90dc5b75799714ae817132b15a4cca17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:39 +0000

    Add package smartnoise-synth/0.3.4/smartnoise-synth-0.3.4.tar.gz

---
+++ packages/smartnoise-synth/smartnoise-synth-0.3.4.tar.gz/smartnoise-synth-0.3.4/snsynth/transform/__init__.py
@@ -0,0 +1,26 @@
+from .table import TableTransformer
+from .onehot import OneHotEncoder
+from .label import LabelTransformer
+from .minmax import MinMaxTransformer
+from .bin import BinTransformer
+from .chain import ChainTransformer
+from .log import LogTransformer
+from .standard import StandardScaler
+from .clamp import ClampTransformer
+from .log import LogTransformer
+from .table import NoTransformer
+from .anonymization import AnonymizationTransformer
+__all__ = [
+    "TableTransformer", 
+    "OneHotEncoder", 
+    "LabelTransformer", 
+    "MinMaxTransformer", 
+    "BinTransformer", 
+    "ChainTransformer", 
+    "LogTransformer",
+    "StandardScaler",
+    "LogTransformer",
+    "ClampTransformer",
+    "NoTransformer",
+    "AnonymizationTransformer"
+    ]
