b24c3137b3f02a9a0ff1cdace6e4b170e5320fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:55 +0000

    Add package py-algorand-sdk/1.20.2/py-algorand-sdk-1.20.2.tar.gz

---
+++ packages/py-algorand-sdk/py-algorand-sdk-1.20.2.tar.gz/py-algorand-sdk-1.20.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="py-algorand-sdk",
+    description="Algorand SDK in Python",
+    author="Algorand",
+    author_email="pypiservice@algorand.com",
+    version="v1.20.2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    project_urls={
+        "Source": "https://github.com/algorand/py-algorand-sdk",
+    },
+    install_requires=[
+        "pynacl>=1.4.0,<2",
+        "pycryptodomex>=3.6.0,<4",
+        "msgpack>=1.0.0,<2",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+    package_data={"": ["data/langspec.json"]},
+    include_package_data=True,
+)
