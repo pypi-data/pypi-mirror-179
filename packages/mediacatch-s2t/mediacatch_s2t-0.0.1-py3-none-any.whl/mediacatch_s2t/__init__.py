83e1f070529ce66361c4a4b6669266417c4a1b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:16 +0000

    Add package mediacatch-s2t/0.0.1/mediacatch_s2t-0.0.1-py3-none-any.whl

---
+++ packages/mediacatch-s2t/mediacatch_s2t-0.0.1-py3-none-any.whl/mediacatch_s2t/__init__.py
@@ -0,0 +1,22 @@
+"""MediaCatch speech-to-text file uploader.
+
+"""
+
+# Version of the mc-s2t-mediacatch_s2t
+__version__ = "0.0.1"
+
+import os
+
+PRESIGNED_URL = (
+    os.environ.get('MEDIACATCH_PRESIGN_URL') or
+    'https://s2t.mediacatch.io/presigned-post-url'
+)
+UPDATE_STATUS_URL = (
+    os.environ.get('MEDIACATCH_UPDATE_STATUS_URL') or
+    'https://s2t.mediacatch.io/upload-completed'
+)
+TRANSCRIPT_URL = (
+    os.environ.get('MEDIACATCH_TRANSCRIPT_URL') or
+    'https://s2t.mediacatch.io/result'
+)
+PROCESSING_TIME_RATIO = 0.75
