a9c95a028be190799017954c4836deb467dc87b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:53:59 +0000

    Add package pip-plus-cli/0.1.0b0/pip_plus_cli-0.1.0b0-py3-none-any.whl

---
+++ packages/pip-plus-cli/pip_plus_cli-0.1.0b0-py3-none-any.whl/pip_plus/__version__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Contains semantic version information for PIP+ """
+
+patch: int = 0
+minor: int = 1
+major: int = 0
+
+semantic_version: str = f"{major}.{minor}.{patch}beta"
